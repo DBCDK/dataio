b6d41b033b925136f0688f1fb1bc29457f675971
Jesper Damkjær <jda@dbc.dk>
2013-04-04 07:06:46 +0000

    Some refactoring of packages
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@57052 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/MainEntryPoint.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/MainEntryPoint.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.yournamehere.client;
+package dk.dbc.dataio.gui.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;

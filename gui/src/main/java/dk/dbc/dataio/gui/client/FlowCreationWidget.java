221c32848e6102e35ff5634f68b2786754becb5e
Jesper Damkjær <jda@dbc.dk>
2013-04-12 08:14:39 +0000

    Removed some code which was part of the prototype, but wasnt useful anymore.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@57593 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/FlowCreationWidget.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/FlowCreationWidget.java
@@ -1,52 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dk.dbc.dataio.gui.client;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- *
- * @author damkjaer
- */
 class FlowCreationWidget extends VerticalPanel {
    
-
-    // Constants
+    // Constants (These are not private since we use them in the selenium tests)
     final static String GUIID_FLOW_CREATION_WIDGET = "flowcreationwidget";
     final static String GUIID_FLOW_CREATION_NAME_TEXT_BOX = "flowcreationnametextbox";
     final static String GUIID_FLOW_CREATION_DESCRIPTION_TEXT_AREA = "flowcreationdescriptiontextarea";
     final static String GUIID_FLOW_CREATION_SAVE_BUTTON = "flowcreationsavebutton";
     final static String GUIID_FLOW_CREATION_SAVE_RESULT_LABEL = "flowcreationsaveresultlabel";
     final static String SAVE_RESULT_LABEL_SUCCES_MESSAGE = "Tillykke du har gemt";
-    private static final int CREATION_TEXT_MAX_LENGTH = 160;
+    private static final int FLOW_CREATION_DESCRIPTION_MAX_LENGTH = 160;
 
     // Local variables
     private final HorizontalPanel flowDescriptionPanel = new HorizontalPanel();
-    private final Label flowDescriptionLabel = new Label("Skriv noget tekst. Max " + CREATION_TEXT_MAX_LENGTH + " tegn");
+    private final Label flowDescriptionLabel = new Label("Skriv noget tekst. Max " + FLOW_CREATION_DESCRIPTION_MAX_LENGTH + " tegn");
     private final TextArea flowDescriptionTextArea = new TextArea();
-    private final Button saveButton = new Button("Gem");
 
    
-    public FlowCreationWidget(final DataContentObject dataContentObject, final ViewPage viewPage) {
+    public FlowCreationWidget() {
         getElement().setId(GUIID_FLOW_CREATION_WIDGET);
+
+        // Example of how a flow description text area could be set up
         setUpFlowDescriptionPanel();
+        // This class extends VerticalPanel and can therefore use 
+        // methods in a VerticalPanel directly:
         add(flowDescriptionPanel);
-        saveButton.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                dataContentObject.add(flowDescriptionTextArea.getText());
-                viewPage.onUpdate();
-            }
-        });
-        add(saveButton);
     }
     
     private void setUpFlowDescriptionPanel() {
@@ -58,7 +42,8 @@ class FlowCreationWidget extends VerticalPanel {
     private void setUpFlowDescriptionTextArea() {
         flowDescriptionTextArea.setCharacterWidth(40);
         flowDescriptionTextArea.setVisibleLines(4);
-        flowDescriptionTextArea.getElement().setAttribute("Maxlength", String.valueOf(CREATION_TEXT_MAX_LENGTH));
+        // MaxLength is an attribute on a textarea, and can be set with setAttribute():
+        flowDescriptionTextArea.getElement().setAttribute("Maxlength", String.valueOf(FLOW_CREATION_DESCRIPTION_MAX_LENGTH));
         flowDescriptionTextArea.getElement().setId(GUIID_FLOW_CREATION_DESCRIPTION_TEXT_AREA);
     }
 }
--- gui/src/main/java/dk/dbc/dataio/gui/client/MainEntryPoint.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/MainEntryPoint.java
@@ -15,19 +15,12 @@ import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- * Main entry point.
- *
- * @author damkjaer
- */
 public class MainEntryPoint implements EntryPoint {
 
     final static String GUIID_NAVIGATION_MENU_ITEM_FLOW_CREATION = "navigationbuttoncreation";
     final static String GUIID_NAVIGATION_MENU_BUTTON_VIEW_AND_DELETION = "navigationbuttonviewanddelete";
     private DeckLayoutPanel contentPanel = new DeckLayoutPanel();
 
-    private DataContentObject dataContentObject = new DataContentObject();
-    
     /**
      * The entry point method, called automatically by loading a module that
      * declares an implementing class as an entry-point
@@ -35,24 +28,18 @@ public class MainEntryPoint implements EntryPoint {
     public void onModuleLoad() {
         final DockLayoutPanel masterPanel = new DockLayoutPanel(Style.Unit.PX);
         final VerticalPanel navigationPanel = new VerticalPanel();
-        final Button button1 = new Button("Opret");
+
+        final Button button1 = new Button("Flow Creation");
         button1.getElement().setId(GUIID_NAVIGATION_MENU_ITEM_FLOW_CREATION);
         button1.addClickHandler(new ButtonHandler(0));
-        final Button button2 = new Button("Se");
-        button2.getElement().setId(GUIID_NAVIGATION_MENU_BUTTON_VIEW_AND_DELETION);
-        button2.addClickHandler(new ButtonHandler(1));
 
         final Label headerLabel = new Label("DataIO");
         masterPanel.addNorth(headerLabel, 20);
 
         navigationPanel.add(button1);
-        navigationPanel.add(button2);
         masterPanel.addWest(navigationPanel, 220);
-
-        ViewPage viewPage = new ViewPage(dataContentObject);
         
-        contentPanel.add(new FlowCreationWidget(dataContentObject, viewPage));
-        contentPanel.add(viewPage);
+        contentPanel.add(new FlowCreationWidget());
 
         masterPanel.add(contentPanel);
         RootLayoutPanel.get().add(masterPanel);

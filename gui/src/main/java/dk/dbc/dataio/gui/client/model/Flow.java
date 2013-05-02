26aa315f4b567d3bd3100d1434fb0d5b9df309e3
Jan Bauer Nielsen <jbn@dbc.dk>
2013-05-02 12:46:22 +0000

    Added model object.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@58281 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/main/java/dk/dbc/dataio/gui/client/model/Flow.java
@@ -0,0 +1,24 @@
+package dk.dbc.dataio.gui.client.model;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class Flow implements IsSerializable {
+    public String flowname;
+    public String description;
+
+    public void setFlowname(String flowname) {
+        this.flowname = flowname;
+    }
+
+    public String getFlowname() {
+        return flowname;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}

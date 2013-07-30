1a409c78db6aaf9047f834623ca9d68750a5ed15
Jan Bauer Nielsen <jbn@dbc.dk>
2013-07-30 10:44:16 +0000

    More general cleanup.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@60602 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- flow-store-service/src/main/resources/flow_store.sql
+++
@@ -1,9 +0,0 @@
-/*
-This is for documentation purposes only, should be auto created by JPA
-
-CREATE TABLE flows (
-    id SERIAL NOT NULL PRIMARY KEY,
-    data TEXT NOT NULL,
-    flowname_idx TEXT NOT NULL UNIQUE
-);
-*/

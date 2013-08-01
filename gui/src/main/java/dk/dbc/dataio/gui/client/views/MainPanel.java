fdd3dcc53f7db9339c8f4178928283f8488cf44f
Jan Bauer Nielsen <jbn@dbc.dk>
2013-08-01 07:18:09 +0000

    Removing findbugs warnings.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@60668 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/views/MainPanel.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/views/MainPanel.java
@@ -5,7 +5,6 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceController;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -15,19 +14,17 @@ import dk.dbc.dataio.gui.client.places.FlowCreatePlace;
 import dk.dbc.dataio.gui.client.places.SubmitterCreatePlace;
 import dk.dbc.dataio.gui.util.ClientFactory;
 
-/**
- *
- * @author slf
- */
 public class MainPanel extends DockLayoutPanel {
-    final static String CONTEXT_HEADER = "DBC DATAINDSYSTEM";
-    public final static String GUIID_NAVIGATION_MENU_ITEM_FLOW_CREATION = "navigationbuttonflowcreation";
-    public final static String GUIID_NAVIGATION_MENU_ITEM_SUBMITTER_CREATION = "navigationbuttonsubmittercreation";
-    final static String GUIID_MAIN_PANEL_LAYOUT = "main-panel-layout";
-    
-    private HeaderLabelPanel headerLabel = new HeaderLabelPanel(CONTEXT_HEADER, "header-panel-layout");
-    private NavigationPanel navigationPanel = new NavigationPanel("navigation-panel-layout");
-    public ContentPanel contentPanel = new ContentPanel("content-panel-layout");
+    public static final String GUIID_NAVIGATION_MENU_ITEM_FLOW_CREATION = "navigationbuttonflowcreation";
+    public static final String GUIID_NAVIGATION_MENU_ITEM_SUBMITTER_CREATION = "navigationbuttonsubmittercreation";
+
+    private static final String CONTEXT_HEADER = "DBC DATAINDSYSTEM";
+    private static final String GUIID_MAIN_PANEL_LAYOUT = "main-panel-layout";
+
+    public final ContentPanel contentPanel = new ContentPanel("content-panel-layout");
+    private final HeaderLabelPanel headerLabel = new HeaderLabelPanel(CONTEXT_HEADER, "header-panel-layout");
+    private final NavigationPanel navigationPanel = new NavigationPanel("navigation-panel-layout");
+
     private PlaceController placeController = null;
 
     public MainPanel(ClientFactory clientFactory) {
@@ -39,7 +36,16 @@ public class MainPanel extends DockLayoutPanel {
         placeController = clientFactory.getPlaceController();
     }
 
-    private class HeaderLabelPanel extends Label {
+    private Place newPlace(int index) {
+        switch (index) {
+            case 0: return new FlowCreatePlace();
+            case 1: return new SubmitterCreatePlace();
+            // ...
+            default: return null;
+        }
+    }
+
+    private static class HeaderLabelPanel extends Label {
         public HeaderLabelPanel(String label, String styleName) {
             super();
             setStylePrimaryName(styleName);
@@ -47,12 +53,9 @@ public class MainPanel extends DockLayoutPanel {
         }
     }
 
-    private Place newPlace(int index) {
-        switch (index) {
-            case 0: return new FlowCreatePlace();
-            case 1: return new SubmitterCreatePlace();
-            // ...
-            default: return null;
+    private static class ContentPanel extends SimpleLayoutPanel {
+        public ContentPanel(String styleName) {
+            setStylePrimaryName(styleName);
         }
     }
 
@@ -65,13 +68,7 @@ public class MainPanel extends DockLayoutPanel {
         }
     }
 
-    private class ContentPanel extends SimpleLayoutPanel {
-        public ContentPanel(String styleName) {
-            setStylePrimaryName(styleName);
-        }
-    }
-
-    private class NavigationButton extends Button {
+   private class NavigationButton extends Button {
         private String caption = null;
         
         public NavigationButton(int index, String caption, String id) {
--- gui/src/main/java/dk/dbc/dataio/gui/server/FlowStoreProxyServlet.java
+++ gui/src/main/java/dk/dbc/dataio/gui/server/FlowStoreProxyServlet.java
@@ -3,16 +3,18 @@ package dk.dbc.dataio.gui.server;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import dk.dbc.dataio.gui.client.model.FlowData;
 import dk.dbc.dataio.gui.client.proxies.FlowStoreProxy;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.servlet.ServletException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FlowStoreProxyServlet extends RemoteServiceServlet implements FlowStoreProxy {
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
 
+public class FlowStoreProxyServlet extends RemoteServiceServlet implements FlowStoreProxy {
     private static final Logger log = LoggerFactory.getLogger(FlowStoreProxyServlet.class);
-    private final static String FLOW_STORE_SERVICE_ENDPOINT_RESOURCE = "dataioGuiFlowStoreServiceEndpoint";
+    private static final long serialVersionUID = 358109395377092219L;
+    private static final String FLOW_STORE_SERVICE_ENDPOINT_RESOURCE = "dataioGuiFlowStoreServiceEndpoint";
+
     private transient FlowStoreProxy flowStoreProxy;
 
     @Override

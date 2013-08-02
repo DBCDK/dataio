ed14e1859255dd09d6292ea7dd32f19cf511e846
Jesper Damkjær <jda@dbc.dk>
2013-08-02 11:19:58 +0000

    Added a javascript wrapper for a single script without jscommon and friends.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@60727 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ engine-prototype/src/main/java/dk/dbc/dataio/engine/JSWrapperSingleScript.java
@@ -0,0 +1,25 @@
+package dk.dbc.dataio.engine;
+
+import dk.dbc.jslib.Environment;
+import dk.dbc.jslib.FileSchemeHandler;
+import dk.dbc.jslib.ModuleHandler;
+
+public class JSWrapperSingleScript {
+
+    private final Environment jsEnvironment;
+
+    public JSWrapperSingleScript(String jsScript) {
+
+        ModuleHandler mh = new ModuleHandler();
+        FileSchemeHandler fsh = new FileSchemeHandler(".");
+        mh.registerHandler("file", fsh);
+        jsEnvironment = new Environment();
+        jsEnvironment.registerUseFunction(mh);
+
+        jsEnvironment.eval(jsScript);
+    }
+
+    public Object callMethod(String methodName, final Object[] args) {
+        return jsEnvironment.callMethod(methodName, args);
+    }
+}
--- engine-prototype/src/main/java/dk/dbc/dataio/engine/Job.java
+++ engine-prototype/src/main/java/dk/dbc/dataio/engine/Job.java
@@ -2,4 +2,7 @@ package dk.dbc.dataio.engine;
 
 public class Job {
     
+    public FlowInfo getFlowInfo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }

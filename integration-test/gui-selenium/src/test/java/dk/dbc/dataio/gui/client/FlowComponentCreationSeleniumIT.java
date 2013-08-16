c951500b249e9cf5bb32df3e7aa1ad6370c32a9d
Jesper Damkjær <jda@dbc.dk>
2013-08-16 21:11:08 +0000

    Initial selenium testing of FlowComponentCreation
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@61468 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ integration-test/gui-selenium/src/test/java/dk/dbc/dataio/gui/client/FlowComponentCreationSeleniumIT.java
@@ -0,0 +1,84 @@
+package dk.dbc.dataio.gui.client;
+
+import dk.dbc.dataio.gui.client.views.FlowComponentCreateViewImpl;
+import dk.dbc.dataio.gui.client.views.FlowCreateViewImpl;
+import dk.dbc.dataio.gui.client.views.MainPanel;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FlowComponentCreationSeleniumIT {
+
+    private static WebDriver driver;
+    private static String APP_URL;
+    private static Connection conn;
+
+    @BeforeClass
+    public static void setUpClass() throws ClassNotFoundException, SQLException {
+        String glassfishPort = System.getProperty("glassfish.port");
+        APP_URL = "http://localhost:" + glassfishPort + "/gui/gui.html";
+
+        Class.forName("org.h2.Driver");
+        conn = DriverManager.getConnection("jdbc:h2:tcp://localhost:" + System.getProperty("h2.port") + "/mem:submitter_store", "root", "root");
+        conn.setAutoCommit(true);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws SQLException {
+        conn.close();
+    }
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.get(APP_URL);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws SQLException {
+        clearDbTables();
+        driver.quit();
+    }
+
+        @Test
+    public void testFlowCreationNavigationItemIsVisible() {
+        WebElement element = driver.findElement(By.id(MainPanel.GUIID_NAVIGATION_MENU_ITEM_FLOW_COMPONENT_CREATION));
+        assertEquals(true, element.isDisplayed());
+    }
+
+    @Test
+    public void testFlowCreationNavigationItemIsClickable() throws Exception {
+        navigateToFlowComponentCreationContext();
+        WebElement widget = driver.findElement(By.id(FlowComponentCreateViewImpl.GUIID_FLOW_COMPONENT_CREATION_WIDGET));
+        assertEquals(true, widget.isDisplayed());
+    }
+
+    
+    
+    private void clearDbTables() throws SQLException {
+//        PreparedStatement deleteStmt = conn.prepareStatement("DELETE FROM flows");
+//        try {
+//            deleteStmt.executeUpdate();
+//        } finally {
+//            deleteStmt.close();
+//        }
+    }
+
+    private void navigateToFlowComponentCreationContext() {
+        WebElement element = driver.findElement(By.id(MainPanel.GUIID_NAVIGATION_MENU_ITEM_FLOW_COMPONENT_CREATION));
+        element.click();
+    }
+}

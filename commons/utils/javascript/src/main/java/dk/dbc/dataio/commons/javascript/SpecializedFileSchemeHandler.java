8428ce85cf588c2a158d86e620e84e175c5ba38d
Jesper Damkjær <jda@dbc.dk>
2013-09-04 12:19:23 +0000

    Begun work on a specialization of a FileSchemeHandler which internally stores the javascripts as they are evaluated
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@62152 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ commons/utils/javascript/src/main/java/dk/dbc/dataio/commons/javascript/SpecializedFileSchemeHandler.java
@@ -0,0 +1,11 @@
+package dk.dbc.dataio.commons.javascript;
+
+import dk.dbc.jslib.FileSchemeHandler;
+
+public class SpecializedFileSchemeHandler extends FileSchemeHandler {
+
+    public SpecializedFileSchemeHandler(String root) {
+        super(root);
+    }
+
+}
---
+++ commons/utils/javascript/src/test/java/dk/dbc/dataio/commons/javascript/SpecializedFileSchemeHandlerTest.java
@@ -0,0 +1,39 @@
+
+package dk.dbc.dataio.commons.javascript;
+
+import dk.dbc.jslib.Environment;
+import dk.dbc.jslib.ModuleHandler;
+import dk.dbc.jslib.SchemeURI;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Test;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+
+public class SpecializedFileSchemeHandlerTest {
+
+    XLogger log = XLoggerFactory.getXLogger(SpecializedFileSchemeHandlerTest.class);
+
+    static {
+        org.apache.log4j.BasicConfigurator.configure();
+    }
+
+    @Test
+    public void test() throws IOException {
+        Path rootDir = (new File("/home/damkjaer/dbc/tmp/jscommon")).toPath();
+
+        DirectoriesContainingJavascriptFinder finder = new DirectoriesContainingJavascriptFinder();
+        Files.walkFileTree(rootDir, finder);
+        for(Path path : finder.getJavascriptDirectories()) {
+            System.out.println("Path: " + path.toString());
+        }
+
+//        ModuleHandler mh = new ModuleHandler();
+//        SpecializedFileSchemeHandler sfsh = new SpecializedFileSchemeHandler("");
+//        mh.registerHandler("file", sfsh);
+//        mh.addSearchPath(new SchemeURI("file", "."));
+//        Environment jsEnvironment = new Environment();
+    }
+}

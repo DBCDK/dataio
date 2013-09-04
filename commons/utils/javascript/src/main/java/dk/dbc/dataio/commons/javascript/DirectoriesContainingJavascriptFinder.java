d47ead1293385c7d9b8c207d95e5073c16f21046
Jesper Damkjær <jda@dbc.dk>
2013-09-04 12:11:24 +0000

    Added a class for finding all directories containing javascripts using Files.walkFileTree
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@62149 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ commons/utils/javascript/src/main/java/dk/dbc/dataio/commons/javascript/DirectoriesContainingJavascriptFinder.java
@@ -0,0 +1,44 @@
+package dk.dbc.dataio.commons.javascript;
+
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.FileSystems;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.FileVisitResult;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DirectoriesContainingJavascriptFinder extends SimpleFileVisitor<Path> {
+
+    private final PathMatcher matcher;
+    private final Set<Path> javascriptDirectories;
+
+
+    DirectoriesContainingJavascriptFinder() {
+        matcher = FileSystems.getDefault().getPathMatcher("glob:*.js");
+        javascriptDirectories = new HashSet<>();
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (isFileAJavascript(file)) {
+            javascriptDirectories.add(file.getParent());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    private boolean isFileAJavascript(Path file) {
+        Path name = file.getFileName();
+        if (name != null && matcher.matches(name)) {
+            return true;
+        }
+        return false;
+    }
+
+    public List<Path> getJavascriptDirectories() {
+        return new ArrayList<>(javascriptDirectories);
+    }
+}
---
+++ commons/utils/javascript/src/test/java/dk/dbc/dataio/commons/javascript/DirectoriesContainingJavascriptFinderTest.java
@@ -0,0 +1,84 @@
+package dk.dbc.dataio.commons.javascript;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+public class DirectoriesContainingJavascriptFinderTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void testUnusedFinder_emptyResultList() {
+        DirectoriesContainingJavascriptFinder finder = new DirectoriesContainingJavascriptFinder();
+        assertThat(finder.getJavascriptDirectories().isEmpty(), is(true));
+    }
+
+    @Test
+    public void testFinderWithDirectoriesContainingJavascriptFiles_findsAllDirectories() throws IOException {
+        createTestDirectoryStructure();
+        DirectoriesContainingJavascriptFinder finder = new DirectoriesContainingJavascriptFinder();
+        Files.walkFileTree(folder.getRoot().toPath(), finder);
+        List<Path> javascriptDirectories = finder.getJavascriptDirectories();
+
+        assertThat(javascriptDirectories.size(), is(4));
+
+        List<String> javascriptDirectoriesAsString = new ArrayList<>();
+        for(Path p : javascriptDirectories) {
+            javascriptDirectoriesAsString.add(p.toString());
+        }
+        assertThat("Could not find dir: /root/dir1", javascriptDirectoriesAsString.contains(folder.getRoot()+"/root/dir1"), is(true));
+        assertThat("Could not find dir: /root/dir2", javascriptDirectoriesAsString.contains(folder.getRoot()+"/root/dir2"), is(true));
+        assertThat("Could not find dir: /root/dir2/dir3", javascriptDirectoriesAsString.contains(folder.getRoot()+"/root/dir2/dir3"), is(true));
+        assertThat("Could not find dir: /root/dir5/dir6", javascriptDirectoriesAsString.contains(folder.getRoot()+"/root/dir5/dir6"), is(true));
+    }
+
+    /*
+     * The following file/directory structure is created:
+     *
+     * root
+     *  |-dir1
+     *  |   |-file1.js
+     *  |-dir2
+     *  |   |-file2.txt
+     *  |   |-file3.js
+     *  |   |-dir3
+     *  |   |   |-file4.js
+     *  |-dir4.js   // this is a trick
+     *  |   |-file5.txt
+     *  |-dir5
+     *      |-dir6
+     *          |-file6.js
+     *
+     * This should generate the following directories:
+     *  root/dir1
+     *  root/dir2
+     *  root/dir2/dir3
+     *  root/dir5/dir6
+     */
+    private void createTestDirectoryStructure() throws IOException {
+        folder.newFolder("root");
+        folder.newFolder("root/dir1");
+        folder.newFolder("root/dir2");
+        folder.newFolder("root/dir2/dir3");
+        folder.newFolder("root/dir4.js");
+        folder.newFolder("root/dir5");
+        folder.newFolder("root/dir5/dir6");
+
+        folder.newFile("root/dir1/file1.js");
+        folder.newFile("root/dir2/file2.txt");
+        folder.newFile("root/dir2/file3.js");
+        folder.newFile("root/dir2/dir3/file4.js");
+        folder.newFile("root/dir4.js/file5.txt");
+        folder.newFile("root/dir5/dir6/file6.js");
+    }
+}

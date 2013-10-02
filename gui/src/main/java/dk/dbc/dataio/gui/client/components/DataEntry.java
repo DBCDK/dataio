c9c39adeb7d0c8d8072626adc25b81909ef356cc
Steen Lillethorup Frederiksen <slf@dbc.dk>
2013-10-02 08:50:53 +0000

    [16355] Ups - jeg glemte DataEntry klassen - den er her...
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@63144 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/main/java/dk/dbc/dataio/gui/client/components/DataEntry.java
@@ -0,0 +1,23 @@
+package dk.dbc.dataio.gui.client.components;
+
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ *
+ */
+public class DataEntry extends HorizontalPanel {
+    public static final String DATA_ENTRY_COMPONENT_CLASS = "dio-DataEntry";
+    public static final String DATA_ENTRY_PROMPT_LABEL_CLASS = "dio-DataEntry-PromptLabelClass";
+
+    public DataEntry(String guiId, String prompt) {
+        super();
+        getElement().setId(guiId);
+        setStyleName(DATA_ENTRY_COMPONENT_CLASS);
+        Label promptLabel = new Label(prompt);
+        promptLabel.setStyleName(DATA_ENTRY_PROMPT_LABEL_CLASS);
+        promptLabel.setStyleName(DATA_ENTRY_PROMPT_LABEL_CLASS);
+        add(promptLabel);
+    }
+
+}

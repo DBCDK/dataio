e8104e39dbcebb4a12865fd313a3699c9200b2bd
Steen Lillethorup Frederiksen <slf@dbc.dk>
2013-11-04 13:26:36 +0000

    [16565] SaveButton skal også lige inkluderes
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@63986 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/main/java/dk/dbc/dataio/gui/client/components/SaveButton.java
@@ -0,0 +1,68 @@
+package dk.dbc.dataio.gui.client.components;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ *
+ * @author slf
+ */
+public class SaveButton extends HorizontalPanel {
+    public static final String SAVE_BUTTON_CLASS = "dio-SaveButton";
+    public static final String SAVE_BUTTON_RESULT_LABEL_CLASS = "dio-SaveButton-ResultLabel";
+    public static final String SAVE_BUTTON_BUTTON_CLASS = "dio-SaveButton-Button";
+
+    /**
+     * Event interface for signalling button pressed event
+     */
+    public interface ButtonEvent {
+        public void buttonPressed();
+    }
+    
+    /**
+     * Implementation of button handler for key press
+     */
+    private class SaveButtonHandler implements ClickHandler {
+        @Override
+        public void onClick(ClickEvent event) {
+            saveButtonEvent.buttonPressed();
+        }
+    }
+    
+    // Instance variables
+    private final Button saveButton = new Button("");
+    private final Label resultLabel = new Label("");
+    private ButtonEvent saveButtonEvent = null;
+
+    /**
+     * Constructor
+     * 
+     * @param buttonText The text to be displayed on the save button
+     * @param saveButtonEvent The callback event interface, that gets called upon press on the save button
+     */
+    public SaveButton(String guiId, String buttonText, ButtonEvent saveButtonEvent) {
+        this.saveButtonEvent = saveButtonEvent;
+        getElement().setId(guiId);
+        resultLabel.setStylePrimaryName(SAVE_BUTTON_RESULT_LABEL_CLASS);
+        add(resultLabel);
+        saveButton.setText(buttonText);
+        saveButton.addStyleName(SAVE_BUTTON_BUTTON_CLASS);
+        saveButton.addClickHandler(new SaveButtonHandler());
+        add(saveButton);
+    }
+
+    /**
+     * Sets the text, displaying the status info (busy, completed etc.)
+     * 
+     * @param statusText 
+     */
+    public void setStatusText(String statusText) {
+        resultLabel.setText(statusText);
+    }
+
+    
+}
+    

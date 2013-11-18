7017a092f2bfdb5bfa6ba85b9e72324414d2c13d
Jesper Damkjær <jda@dbc.dk>
2013-11-18 12:06:51 +0000

    [16743] Added unittests for ESTaskPackageInserter
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@64347 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- sink/es/ejb/src/main/java/dk/dbc/dataio/sink/es/ESTaskPackageInserter.java
+++ sink/es/ejb/src/main/java/dk/dbc/dataio/sink/es/ESTaskPackageInserter.java
@@ -4,6 +4,7 @@ import dk.dbc.commons.es.ESUtil;
 import dk.dbc.commons.addi.AddiReader;
 import dk.dbc.commons.addi.AddiRecord;
 import dk.dbc.dataio.commons.types.ChunkResult;
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -28,8 +29,12 @@ public class ESTaskPackageInserter {
      * @param chunkResult Object containing the addi-records to insert into the ES-base.
      * @throws java.sql.SQLException if a database error occurs.
      * @throws java.io.IOException if an error occurs during reading of the addi-data.
+     * @throws IllegalStateException if either the chunk is invalid or the number of records in the chunk and the taskpackage differ.
      */
     public ESTaskPackageInserter(Connection esConn, String dbname, ChunkResult chunkResult) throws IllegalStateException, IOException, SQLException{
+        InvariantUtil.checkNotNullOrThrow(esConn, "esConn");
+        InvariantUtil.checkNotNullNotEmptyOrThrow(dbname, "dbname");
+        InvariantUtil.checkNotNullOrThrow(chunkResult, "chunkResult");
         this.jobId = chunkResult.getJobId();
         this.chunkId = chunkResult.getChunkId();
         String creator = createCreatorString(jobId, chunkId);
@@ -59,12 +64,15 @@ public class ESTaskPackageInserter {
         return sb.toString();
     }
 
-    private List<AddiRecord> getAddiRecordsFromChunk(ChunkResult chunkResult) throws IOException {
+    private List<AddiRecord> getAddiRecordsFromChunk(ChunkResult chunkResult) throws IllegalStateException, IOException {
         List<AddiRecord> addiRecords = new ArrayList<>();
         for(String result : chunkResult.getResults()) {
             AddiReader addiReader = new AddiReader(new ByteArrayInputStream(result.getBytes(chunkResult.getEncoding())));
-            // todo: ensure that there is only one AddiRecord per result.
             addiRecords.add(addiReader.getNextRecord());
+            if(addiReader.getNextRecord() != null) {
+                String errMsg = String.format("More than one Addi in record in: [jobId, chunkId] [%d, %d]", jobId, chunkId);
+                throw new IllegalStateException(errMsg);
+            }
         }
         return addiRecords;
     }

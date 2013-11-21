dca0065ce0f73b5e17b569a74b56e630774acf62
Jesper Damkjær <jda@dbc.dk>
2013-11-21 09:46:21 +0000

    [16782] Now with Extra file! -- Changed functionality for finding a flow - new you need to find the flowbinder, and get the flowId from it. This flowId can then be used to get the actual flow.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@64476 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ commons/utils/test/src/main/java/dk/dbc/dataio/commons/utils/test/json/FlowBinderJsonBuilder.java
@@ -0,0 +1,32 @@
+package dk.dbc.dataio.commons.utils.test.json;
+
+public class FlowBinderJsonBuilder extends JsonBuilder {
+    private long id = 42L;
+    private long version = 1L;
+    private String content = new FlowBinderContentJsonBuilder().build();
+
+    public FlowBinderJsonBuilder setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public FlowBinderJsonBuilder setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public FlowBinderJsonBuilder setVersion(long version) {
+        this.version = version;
+        return this;
+    }
+
+    public String build() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(START_OBJECT);
+        stringBuilder.append(asLongMember("id", id)); stringBuilder.append(MEMBER_DELIMITER);
+        stringBuilder.append(asLongMember("version", version)); stringBuilder.append(MEMBER_DELIMITER);
+        stringBuilder.append(asObjectMember("content", content));
+        stringBuilder.append(END_OBJECT);
+        return stringBuilder.toString();
+    }
+}

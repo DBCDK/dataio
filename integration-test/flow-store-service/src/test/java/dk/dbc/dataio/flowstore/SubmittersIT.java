c3278b4b1f3b993f6974623c54232e0f30df72ea
Steen Lillethorup Frederiksen <slf@dbc.dk>
2013-12-11 20:27:44 +0000

    [16902] US #207: Visning af submitter ved oprettelse af flowbinder - minor correction after review
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@64998 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/SubmittersIT.java
+++ integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/SubmittersIT.java
@@ -7,24 +7,22 @@ import dk.dbc.dataio.commons.utils.httpclient.HttpClient;
 import dk.dbc.dataio.commons.utils.json.JsonUtil;
 import dk.dbc.dataio.commons.utils.test.json.SubmitterContentJsonBuilder;
 import dk.dbc.dataio.integrationtest.ITUtil;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.core.Response;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
 import static dk.dbc.dataio.integrationtest.ITUtil.clearAllDbTables;
 import static dk.dbc.dataio.integrationtest.ITUtil.createSubmitter;
 import static dk.dbc.dataio.integrationtest.ITUtil.getResourceIdFromLocationHeaderAndAssertHasValue;
 import static dk.dbc.dataio.integrationtest.ITUtil.newDbConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import org.junit.After;
+import org.junit.AfterClass;
 import static org.junit.Assert.assertThat;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Integration tests for the submitters collection part of the flow store service
@@ -163,7 +161,7 @@ public class SubmittersIT {
      * Given: a deployed flow-store service containing three submitters
      * When: GETing submitters collection
      * Then: request returns with a OK http status code
-     * And: request returns with list as JSON of submitters sorted alphabetically by number
+     * And: request returns with list as JSON of submitters sorted numerically by number
      */
     @Test
     public void findAllSubmitters_Ok() throws Exception {

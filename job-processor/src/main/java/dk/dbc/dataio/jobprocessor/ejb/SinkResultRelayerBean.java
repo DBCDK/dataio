854d93a5c7b361225f47c5880a6e4b11b158495e
Jan Bauer Nielsen <jbn@dbc.dk>
2014-01-13 07:17:24 +0000

    [17048] Added job-processor dispatch and relay beans.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@67863 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ job-processor/src/main/java/dk/dbc/dataio/jobprocessor/ejb/SinkResultRelayerBean.java
@@ -0,0 +1,38 @@
+package dk.dbc.dataio.jobprocessor.ejb;
+
+import dk.dbc.dataio.commons.types.SinkChunkResult;
+import dk.dbc.dataio.commons.utils.json.JsonException;
+import dk.dbc.dataio.commons.utils.json.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Resource;
+import javax.ejb.EJBException;
+import javax.ejb.LocalBean;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+
+@LocalBean
+public class SinkResultRelayerBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SinkResultRelayerBean.class);
+
+    @Resource
+    ConnectionFactory jobStoreQueueConnectionFactory;
+
+    @Resource(name="jobStoreJmsQueue") // this resource gets its jndi name mapping from xml-deploy-descriptors
+    Queue jobStoreQueue;
+
+    public void relay(SinkChunkResult sinkChunkResult) {
+        LOGGER.info("Relaying SinkChunkResult for chunk {} in job {}", sinkChunkResult.getChunkId(), sinkChunkResult.getJobId());
+        try (JMSContext context = jobStoreQueueConnectionFactory.createContext()) {
+            final TextMessage message = context.createTextMessage(JsonUtil.toJson(sinkChunkResult));
+            message.setStringProperty("chunkResultSource", "processor"); // todo: US#232 Get these values from configuration
+            context.createProducer().send(jobStoreQueue, message);
+        } catch (JsonException | JMSException e) {
+            throw new EJBException(e);
+        }
+    }
+}

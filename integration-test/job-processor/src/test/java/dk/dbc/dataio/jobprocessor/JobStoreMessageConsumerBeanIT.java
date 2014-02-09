63635e180a7110a872040cf6be974999abe891c5
Jan Bauer Nielsen <jbn@dbc.dk>
2014-02-09 11:22:38 +0000

    [17252] job-processor integration test now uses mocked job-store service.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@68733 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- integration-test/job-processor/src/test/java/dk/dbc/dataio/jobprocessor/JobStoreMessageConsumerBeanIT.java
+++ integration-test/job-processor/src/test/java/dk/dbc/dataio/jobprocessor/JobStoreMessageConsumerBeanIT.java
@@ -8,37 +8,21 @@ import dk.dbc.dataio.commons.types.json.mixins.MixIns;
 import dk.dbc.dataio.commons.utils.json.JsonException;
 import dk.dbc.dataio.commons.utils.json.JsonUtil;
 import dk.dbc.dataio.commons.utils.test.jms.MockedJmsTextMessage;
-import dk.dbc.dataio.commons.utils.test.json.ChunkJsonBuilder;
-import dk.dbc.dataio.commons.utils.test.json.FlowComponentContentJsonBuilder;
-import dk.dbc.dataio.commons.utils.test.json.FlowComponentJsonBuilder;
-import dk.dbc.dataio.commons.utils.test.json.FlowContentJsonBuilder;
-import dk.dbc.dataio.commons.utils.test.json.FlowJsonBuilder;
-import dk.dbc.dataio.commons.utils.test.json.JavaScriptJsonBuilder;
 import dk.dbc.dataio.commons.utils.test.model.NewJobBuilder;
 import dk.dbc.dataio.commons.utils.test.model.SinkBuilder;
 import dk.dbc.dataio.commons.utils.test.model.SinkContentBuilder;
-import dk.dbc.dataio.integrationtest.ITUtil;
 import dk.dbc.dataio.integrationtest.JmsQueueConnector;
 import dk.dbc.dataio.jobprocessor.ejb.SinkMessageProducerBean;
 import dk.dbc.dataio.jobstore.ejb.JobProcessorMessageProducerBean;
-import org.apache.commons.io.FileUtils;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.jms.JMSContext;
 import javax.jms.JMSException;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 import static dk.dbc.dataio.jobprocessor.util.Base64Util.base64decode;
-import static dk.dbc.dataio.jobprocessor.util.Base64Util.base64encode;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -49,17 +33,9 @@ import static org.powermock.api.mockito.PowerMockito.when;
 public class JobStoreMessageConsumerBeanIT {
     private static final long MAX_QUEUE_WAIT_IN_MS = 5000;
 
-    private final String javaScriptInvocationMethod = "toUpper";
-    private final String modulesInfoModuleResource = "/ModulesInfo.json";
-    private final String useModuleResource = "/Use.json";
     private final String sinkResourceName = "sinkResourceName";
 
     private JMSContext jmsContext;
-
-    @AfterClass
-    public static void clearJobStore() {
-        ITUtil.clearJobStore();
-    }
 
     @Before
     public void setupMocks() {
@@ -88,13 +64,6 @@ public class JobStoreMessageConsumerBeanIT {
     public void jobStoreMessageConsumerBean_validNewJobOnProcessorQueue_eventuallyRemovedForProcessingWithResultsOnProcessorAndSinkQueues() throws Exception {
         final long jobId = 42;
 
-        final String record1 = "one";
-        final String record2 = "two";
-        final String flow = getFlow();
-        final String chunk1 = getChunk(1, flow, base64encode(record1));
-        final String chunk2 = getChunk(2, flow, base64encode(record2));
-        setupJobStore(jobId, chunk1, chunk2);
-
         final NewJob newJob = new NewJobBuilder()
                 .setJobId(jobId)
                 .setChunkCount(2)
@@ -113,16 +82,14 @@ public class JobStoreMessageConsumerBeanIT {
         assertThat(processorResult.getJobId(), is(jobId));
         assertThat(processorResult.getChunkId(), is(1L));
         assertThat(processorResult.getResults().size(), is(1));
-        assertThat(base64decode(processorResult.getResults().get(0)), is(record1.toUpperCase()));
+        assertThat(base64decode(processorResult.getResults().get(0)), is("ONE"));
 
         processorResult = assertProcessorMessageForSink(sinksQueue.get(1));
         assertThat(processorResult.getJobId(), is(jobId));
         assertThat(processorResult.getChunkId(), is(2L));
         assertThat(processorResult.getResults().size(), is(1));
-        assertThat(base64decode(processorResult.getResults().get(0)), is(record2.toUpperCase()));
+        assertThat(base64decode(processorResult.getResults().get(0)), is("TWO"));
 
-        // When the job-store starts reacting to these messages,
-        // this assertion won't work any longer.
         JmsQueueConnector.awaitQueueSize(JmsQueueConnector.PROCESSOR_QUEUE_NAME, 2, MAX_QUEUE_WAIT_IN_MS);
     }
 
@@ -132,17 +99,6 @@ public class JobStoreMessageConsumerBeanIT {
         assertThat(message.getStringProperty(SinkMessageProducerBean.PAYLOAD_PROPERTY_NAME), is(SinkMessageProducerBean.PAYLOAD_PROPERTY_VALUE));
         assertThat(message.getStringProperty(SinkMessageProducerBean.RESOURCE_PROPERTY_NAME), is(sinkResourceName));
         return JsonUtil.fromJson(message.getText(), ChunkResult.class, MixIns.getMixIns());
-    }
-
-    private Path setupJobStore(long jobId, String... chunks) throws IOException {
-        // This method is tightly bound to job-store internal workings...
-        final Path jobPath = FileSystems.getDefault().getPath(System.getProperty("java.io.tmpdir"), "dataio-job-store", Long.toString(jobId));
-        FileUtils.deleteQuietly(jobPath.toFile());
-        Files.createDirectories(jobPath);
-        for (int i = 0; i < chunks.length; i++) {
-            FileUtils.writeStringToFile(Paths.get(jobPath.toString(), String.format("%d.json", i+1)).toFile(), chunks[i], "UTF-8");
-        }
-        return jobPath;
     }
 
     private MockedJmsTextMessage newJobStoreMessageForJobProcessor(NewJob newJob) throws JMSException, JsonException {
@@ -162,60 +118,5 @@ public class JobStoreMessageConsumerBeanIT {
         return new SinkContentBuilder()
                 .setResource(sinkResourceName)
                 .build();
-    }
-
-    private String getFlow() throws Exception {
-        return new FlowJsonBuilder()
-                .setContent(getFlowContent())
-                .build();
-    }
-
-    private String getFlowContent() throws Exception {
-        return new FlowContentJsonBuilder()
-                .setComponents(Arrays.asList(getFlowComponent()))
-                .build();
-    }
-
-    private String getFlowComponent() throws Exception {
-        return new FlowComponentJsonBuilder()
-                .setContent(getFlowComponentContent())
-                .build();
-    }
-
-    private String getFlowComponentContent() throws Exception {
-        return new FlowComponentContentJsonBuilder()
-                .setInvocationMethod(javaScriptInvocationMethod)
-                .setJavascripts(Arrays.asList(
-                        getJavaScript(),
-                        resourceToString(modulesInfoModuleResource),
-                        resourceToString(useModuleResource)))
-                .build();
-    }
-
-    private String getJavaScript() {
-        return new JavaScriptJsonBuilder()
-                .setJavascript(base64encode(getJavaScriptToUpperFunction()))
-                .build();
-    }
-
-    private String getJavaScriptToUpperFunction() {
-        return ""
-            + "function " + javaScriptInvocationMethod + "(str) {\n"
-            + "    return str.toUpperCase();\n"
-            + "}\n";
-    }
-
-    private String getChunk(long id, String flow, String... records) {
-        return new ChunkJsonBuilder()
-                .setId(id)
-                .setFlow(flow)
-                .setRecords(Arrays.asList(records))
-                .build();
-    }
-
-    private String resourceToString(String resourceName) throws Exception {
-        final java.net.URL url = JobStoreMessageConsumerBeanIT.class.getResource(resourceName);
-        final java.nio.file.Path resPath = java.nio.file.Paths.get(url.toURI());
-        return new String(java.nio.file.Files.readAllBytes(resPath), "UTF8");
     }
 }

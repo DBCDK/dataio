85867ae696cefeaeca42c944e6605ac2de6702b8
Steen Lillethorup Frederiksen <slf@dbc.dk>
2014-02-13 12:48:16 +0000

    [17318] US #237: Udvidet visning af flowkomponenter - Selenium Test
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@68953 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- integration-test/gui-selenium/src/test/java/dk/dbc/dataio/gui/client/FlowComponentsShowSeleniumIT.java
+++ integration-test/gui-selenium/src/test/java/dk/dbc/dataio/gui/client/FlowComponentsShowSeleniumIT.java
@@ -5,30 +5,16 @@ import dk.dbc.dataio.gui.util.ClientFactoryImpl;
 import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
-@Ignore
 public class FlowComponentsShowSeleniumIT extends AbstractGuiSeleniumTest {
     @Test
     public void testFlowComponentsShowEmptyList_NoContentIsShown() throws TimeoutException, Exception {
         navigateToFlowComponentsShowWidget(webDriver);
         SeleniumGWTTable table = new SeleniumGWTTable(webDriver, FlowComponentsShowViewImpl.GUIID_FLOW_COMPONENTS_SHOW_WIDGET);
         table.waitAssertNoRows();
-    }
-
-    @Test
-    public void testFlowComponentsInsertOneRow_OneElementShown() {
-        final String COMPONENT_NAME = "Flow-component-name";
-        FlowComponentCreationSeleniumIT.createTestFlowComponent(webDriver, COMPONENT_NAME);
-        navigateToFlowComponentsShowWidget(webDriver);
-        SeleniumGWTTable table = new SeleniumGWTTable(webDriver, FlowComponentsShowViewImpl.GUIID_FLOW_COMPONENTS_SHOW_WIDGET);
-        table.waitAssertRows();
-        List<String> rowData = table.getRow(0);
-        assertThat(rowData.get(0), is(COMPONENT_NAME));
-        assertThat(rowData.get(1), is("invocationMethod"));
     }
 
     @Test
@@ -42,9 +28,17 @@ public class FlowComponentsShowSeleniumIT extends AbstractGuiSeleniumTest {
         table.waitAssertRows(2);
         List<List<String>> rowData = table.get();
         assertThat(rowData.get(0).get(0), is(COMPONENT_NAME_1));
-        assertThat(rowData.get(0).get(1), is("invocationMethod"));
+        assertThat(rowData.get(0).get(1), is("javascriptname"));
+        assertThat(rowData.get(0).get(2), is("invocationMethod"));
+        assertThat(rowData.get(0).get(3), is("svnprojectforinvocationjavascript"));
+        assertThat(rowData.get(0).get(4), is("1"));
+        assertThat(rowData.get(0).get(5), is("moduleName"));
         assertThat(rowData.get(1).get(0), is(COMPONENT_NAME_2));
-        assertThat(rowData.get(1).get(1), is("invocationMethod"));
+        assertThat(rowData.get(1).get(1), is("javascriptname"));
+        assertThat(rowData.get(1).get(2), is("invocationMethod"));
+        assertThat(rowData.get(1).get(3), is("svnprojectforinvocationjavascript"));
+        assertThat(rowData.get(1).get(4), is("1"));
+        assertThat(rowData.get(1).get(5), is("moduleName"));
     }
 
     private static void navigateToFlowComponentsShowWidget(WebDriver webDriver) {

6056a3f6adb35987f0767d9ad529120642fe8631
Jan Bauer Nielsen <jbn@dbc.dk>
2014-02-27 12:34:08 +0000

    Added katalog format.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@69393 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/utils/transfile/src/main/java/dk/dbc/dataio/commons/utils/transfile/TransFileField.java
+++ commons/utils/transfile/src/main/java/dk/dbc/dataio/commons/utils/transfile/TransFileField.java
@@ -26,7 +26,7 @@ public class TransFileField {
     }
     private final static Set<String> VALID_TRANSFILE_b_CONTENT_VALUES = new HashSet<>(Arrays.asList("databroendpr2"));
     private final static Set<String> VALID_TRANSFILE_t_CONTENT_VALUES = new HashSet<>(Arrays.asList("xml"));
-    private final static Set<String> VALID_TRANSFILE_o_CONTENT_VALUES = new HashSet<>(Arrays.asList("nmalbum", "nmtrack"));
+    private final static Set<String> VALID_TRANSFILE_o_CONTENT_VALUES = new HashSet<>(Arrays.asList("nmalbum", "nmtrack", "katalog"));
     private final static Set<String> VALID_TRANSFILE_c_CONTENT_VALUES = new HashSet<>(Arrays.asList("utf8"));
 
     // RFC822 compliant regex

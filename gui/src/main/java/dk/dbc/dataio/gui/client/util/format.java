158586da2c369baf65d181d1a36fa1501279ce5c
Steen Lillethorup Frederiksen <slf@dbc.dk>
2014-02-28 08:06:10 +0000

    [17349] Fix compiler errors
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@69423 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/main/java/dk/dbc/dataio/gui/client/util/format.java
@@ -0,0 +1,41 @@
+/*
+ *
+ * Utility functions for formatting texts to be displayed
+ *
+ */
+
+package dk.dbc.dataio.gui.client.util;
+
+import java.util.List;
+
+/**
+ *
+ * format class
+ *
+ */
+public final class format {
+
+    // Private constructor to avoid instantiation
+    private format() {
+    }
+
+    /**
+     * comma separates a list of String
+     *
+     * @param parameters The list of strings
+     * @return The comma separated list
+     *
+     */
+    public static String commaSeparate(List<String> parameters) {
+        String result = "";
+        for (String parameter: parameters) {
+            if (result.isEmpty()) {
+                result = parameter;
+            } else {
+                result += ", " + parameter;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ gui/src/test/java/dk/dbc/dataio/gui/client/FormatTest.java
@@ -0,0 +1,40 @@
+
+package dk.dbc.dataio.gui.client;
+
+import dk.dbc.dataio.gui.client.util.format;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+/**
+ * Format unit tests
+ *
+ * The test methods of this class uses the following naming convention:
+ *
+ *  unitOfWork_stateUnderTest_expectedBehavior
+ */
+public class FormatTest {
+
+    @Test(expected = NullPointerException.class)
+    public void commaSeparate_nullInput_nullPointerException() {
+        format.commaSeparate(null);
+    }
+
+    @Test
+    public void commaSeparate_emptyList_emptyResultString() {
+        List<String> empty = new ArrayList<String>();
+        String result = format.commaSeparate(empty);
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void commaSeparate_validList_validResultString() {
+        List<String> empty = Arrays.asList("Monkey", "Elephant", "Bird");
+        String result = format.commaSeparate(empty);
+        assertThat(result, is("Monkey, Elephant, Bird"));
+    }
+
+}

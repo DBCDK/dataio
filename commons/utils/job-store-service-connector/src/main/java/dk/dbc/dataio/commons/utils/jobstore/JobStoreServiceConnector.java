e4e3c82e101079f9f7448d4216eede6fd9652dc3
Jan Bauer Nielsen <jbn@dbc.dk>
2014-03-04 12:16:06 +0000

    [17387] Finished JobStoreServiceConnector.createJob() implementation.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@69573 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/utils/job-store-service-connector/src/main/java/dk/dbc/dataio/commons/utils/jobstore/JobStoreServiceConnector.java
+++ commons/utils/job-store-service-connector/src/main/java/dk/dbc/dataio/commons/utils/jobstore/JobStoreServiceConnector.java
@@ -1,10 +1,14 @@
 package dk.dbc.dataio.commons.utils.jobstore;
 
+import dk.dbc.dataio.commons.types.JobErrorCode;
 import dk.dbc.dataio.commons.types.JobInfo;
 import dk.dbc.dataio.commons.types.JobSpecification;
+import dk.dbc.dataio.commons.types.rest.JobStoreServiceConstants;
+import dk.dbc.dataio.commons.utils.httpclient.HttpClient;
 import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 
 /**
  * JobStoreServiceConnector - dataIO job-store REST service client.
@@ -22,10 +26,8 @@ public class JobStoreServiceConnector {
 
     /**
      * Class constructor
-     *
      * @param httpClient web resources client
      * @param baseUrl base URL for job-store service endpoint
-     *
      * @throws NullPointerException if given null-valued argument
      * @throws IllegalArgumentException if given empty-valued {@code baseUrl} argument
      */
@@ -35,15 +37,44 @@ public class JobStoreServiceConnector {
     }
 
     /**
-     *
-     * @param jobSpecification
-     * @return
+     * Creates new job defined by given job specification in the job-store
+     * @param jobSpecification job specification
+     * @return job info
      * @throws NullPointerException if given null-valued argument
-     * @throws JobStoreServiceConnectorException
+     * @throws JobStoreServiceConnectorJobCreationFailedException if job creation failed due to invalid input data
+     * @throws JobStoreServiceConnectorException on general failure to create job
      */
     public JobInfo createJob(JobSpecification jobSpecification) throws NullPointerException, JobStoreServiceConnectorException {
         InvariantUtil.checkNotNullOrThrow(jobSpecification, "jobSpecification");
-        return null;
+        final Response response = HttpClient.doPostWithJson(httpClient, jobSpecification, baseUrl, JobStoreServiceConstants.JOB_COLLECTION);
+        try {
+            verifyResponseStatus(Response.Status.fromStatusCode(response.getStatus()), Response.Status.CREATED);
+            final JobInfo jobInfo = readResponseEntity(response, JobInfo.class);
+            if (jobInfo.getJobErrorCode() != JobErrorCode.NO_ERROR) {
+                throw new JobStoreServiceConnectorJobCreationFailedException(
+                        String.format("job-store service was unable to finish creation of job<%s> due to %s", jobInfo.getJobId(), jobInfo.getJobErrorCode()),
+                        jobInfo.getJobErrorCode());
+            }
+            return jobInfo;
+        } finally {
+            response.close();
+        }
+    }
+
+    private void verifyResponseStatus(Response.Status actualStatus, Response.Status expectedStatus) throws JobStoreServiceConnectorException {
+        if (actualStatus != expectedStatus) {
+            throw new JobStoreServiceConnectorException(
+                    String.format("job-store service returned with unexpected status code: %s", actualStatus));
+        }
+    }
+
+    private <T> T readResponseEntity(Response response, Class<T> tClass) throws JobStoreServiceConnectorException {
+        final T entity = response.readEntity(tClass);
+        if (entity == null) {
+            throw new JobStoreServiceConnectorException(
+                    String.format("job-store service returned with null-valued %s entity", tClass.getName()));
+        }
+        return entity;
     }
 
     public Client getHttpClient() {
--- commons/utils/job-store-service-connector/src/test/java/dk/dbc/dataio/commons/utils/jobstore/JobStoreServiceConnectorTest.java
+++ commons/utils/job-store-service-connector/src/test/java/dk/dbc/dataio/commons/utils/jobstore/JobStoreServiceConnectorTest.java
@@ -1,17 +1,42 @@
 package dk.dbc.dataio.commons.utils.jobstore;
 
+import dk.dbc.dataio.commons.types.JobErrorCode;
+import dk.dbc.dataio.commons.types.JobInfo;
+import dk.dbc.dataio.commons.types.JobSpecification;
+import dk.dbc.dataio.commons.types.rest.JobStoreServiceConstants;
+import dk.dbc.dataio.commons.utils.httpclient.HttpClient;
+import dk.dbc.dataio.commons.utils.json.JsonException;
+import dk.dbc.dataio.commons.utils.test.model.JobInfoBuilder;
+import dk.dbc.dataio.commons.utils.test.model.JobSpecificationBuilder;
+import dk.dbc.dataio.commons.utils.test.rest.MockedResponse;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+        HttpClient.class,
+})
 public class JobStoreServiceConnectorTest {
     private static final Client CLIENT = mock(Client.class);
     private static final String JOB_STORE_URL = "http://dataio/job-store";
+
+    @Before
+    public void setup() throws Exception {
+        mockStatic(HttpClient.class);
+    }
 
     @Test(expected = NullPointerException.class)
     public void constructor_httpClientArgIsNull_throws() {
@@ -40,6 +65,54 @@ public class JobStoreServiceConnectorTest {
     public void createJob_jobSpecificationArgIsNull_throws() throws JobStoreServiceConnectorException {
         final JobStoreServiceConnector instance = newJobStoreServiceConnector();
         instance.createJob(null);
+    }
+
+    @Test(expected = JobStoreServiceConnectorException.class)
+    public void createJob_responseWithUnexpectedStatusCode_throws() throws JobStoreServiceConnectorException {
+        final JobSpecification jobSpecification = new JobSpecificationBuilder().build();
+        when(HttpClient.doPostWithJson(CLIENT, jobSpecification, JOB_STORE_URL, JobStoreServiceConstants.JOB_COLLECTION))
+                .thenReturn(new MockedResponse<>(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), ""));
+
+        final JobStoreServiceConnector instance = newJobStoreServiceConnector();
+        instance.createJob(jobSpecification);
+    }
+
+    @Test(expected = JobStoreServiceConnectorException.class)
+    public void createJob_responseWithNullEntity_throws() throws JobStoreServiceConnectorException {
+        final JobSpecification jobSpecification = new JobSpecificationBuilder().build();
+        when(HttpClient.doPostWithJson(CLIENT, jobSpecification, JOB_STORE_URL, JobStoreServiceConstants.JOB_COLLECTION))
+                .thenReturn(new MockedResponse<>(Response.Status.CREATED.getStatusCode(), null));
+
+        final JobStoreServiceConnector instance = newJobStoreServiceConnector();
+        instance.createJob(jobSpecification);
+    }
+
+    @Test(expected = JobStoreServiceConnectorJobCreationFailedException.class)
+    public void createJob_responseWithJobInfoWithErrorCodeIndicatingJobCreationFailure_throws() throws JobStoreServiceConnectorException, JsonException {
+        final JobSpecification jobSpecification = new JobSpecificationBuilder().build();
+        final JobInfo jobInfo = new JobInfoBuilder()
+                .setJobErrorCode(JobErrorCode.DATA_FILE_INVALID)
+                .build();
+        when(HttpClient.doPostWithJson(CLIENT, jobSpecification, JOB_STORE_URL, JobStoreServiceConstants.JOB_COLLECTION))
+                .thenReturn(new MockedResponse<>(Response.Status.CREATED.getStatusCode(), jobInfo));
+
+        final JobStoreServiceConnector instance = newJobStoreServiceConnector();
+        instance.createJob(jobSpecification);
+    }
+
+    @Test
+    public void createJob_jobIsCreated_returnsJobInfo() throws JobStoreServiceConnectorException, JsonException {
+        final JobSpecification jobSpecification = new JobSpecificationBuilder().build();
+        final JobInfo expectedJobInfo = new JobInfoBuilder()
+                .setJobErrorCode(JobErrorCode.NO_ERROR)
+                .build();
+        when(HttpClient.doPostWithJson(CLIENT, jobSpecification, JOB_STORE_URL, JobStoreServiceConstants.JOB_COLLECTION))
+                .thenReturn(new MockedResponse<>(Response.Status.CREATED.getStatusCode(), expectedJobInfo));
+
+        final JobStoreServiceConnector instance = newJobStoreServiceConnector();
+        final JobInfo jobInfo = instance.createJob(jobSpecification);
+        assertThat(jobInfo, is(notNullValue()));
+        assertThat(jobInfo.getJobId(), is(expectedJobInfo.getJobId()));
     }
 
     private static JobStoreServiceConnector newJobStoreServiceConnector() {

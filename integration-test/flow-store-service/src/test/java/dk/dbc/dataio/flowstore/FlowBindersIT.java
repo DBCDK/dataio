7bdb2b6fa629997a49a14a7bfc2fe7edf9ab678c
Steen Lillethorup Frederiksen <slf@dbc.dk>
2014-03-05 14:49:14 +0000

    [17349] US #225: Overblik over flowbindere - Integrationstest REST metode
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@69631 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/FlowBindersIT.java
+++ integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/FlowBindersIT.java
@@ -1,25 +1,15 @@
 package dk.dbc.dataio.flowstore;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import dk.dbc.commons.jdbc.util.JDBCUtil;
 import dk.dbc.dataio.commons.types.rest.FlowStoreServiceConstants;
 import dk.dbc.dataio.commons.utils.httpclient.HttpClient;
+import dk.dbc.dataio.commons.utils.json.JsonUtil;
 import dk.dbc.dataio.commons.utils.test.json.FlowBinderContentJsonBuilder;
 import dk.dbc.dataio.commons.utils.test.json.FlowContentJsonBuilder;
 import dk.dbc.dataio.commons.utils.test.json.SinkContentJsonBuilder;
 import dk.dbc.dataio.commons.utils.test.json.SubmitterContentJsonBuilder;
 import dk.dbc.dataio.integrationtest.ITUtil;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.core.Response;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
-
 import static dk.dbc.dataio.integrationtest.ITUtil.clearAllDbTables;
 import static dk.dbc.dataio.integrationtest.ITUtil.createFlow;
 import static dk.dbc.dataio.integrationtest.ITUtil.createFlowBinder;
@@ -27,8 +17,19 @@ import static dk.dbc.dataio.integrationtest.ITUtil.createSink;
 import static dk.dbc.dataio.integrationtest.ITUtil.createSubmitter;
 import static dk.dbc.dataio.integrationtest.ITUtil.getResourceIdFromLocationHeaderAndAssertHasValue;
 import static dk.dbc.dataio.integrationtest.ITUtil.newDbConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import org.junit.After;
+import org.junit.AfterClass;
 import static org.junit.Assert.assertThat;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Integration tests for the flow binders collection part of the flow store service
@@ -253,4 +254,110 @@ public class FlowBindersIT {
         // Then...
         assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.NOT_ACCEPTABLE.getStatusCode()));
     }
+
+    /**
+     * Given: a deployed flow-store service containing no flow binders
+     * When: GETing flow binders collection
+     * Then: request returns with a OK http status code
+     * And: request returns with empty list as JSON
+     */
+    @Test
+    public void findAllFlowBinders_emptyResult() throws Exception {
+        // When...
+        final Response response = HttpClient.doGet(restClient, baseUrl, FlowStoreServiceConstants.FLOW_BINDERS);
+
+        // Then...
+        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.OK.getStatusCode()));
+
+        // And...
+        final String responseContent = response.readEntity(String.class);
+        assertThat(responseContent, is(notNullValue()));
+        final ArrayNode responseContentNode = (ArrayNode) JsonUtil.getJsonRoot(responseContent);
+        assertThat(responseContentNode.size(), is(0));
+    }
+
+    /**
+     * Given: a deployed flow-store service containing three flow binders
+     * When: GETing flow binders collection
+     * Then: request returns with a OK http status code
+     * And: request returns with list as JSON of flow binders sorted alphabetically by name
+     */
+    @Test
+    public void findAllFlowBinders_Ok() throws Exception {
+        // Given...
+        // Create a Flow with name flow1
+        String flowContent = new FlowContentJsonBuilder()
+            .setName("flow1")
+            .build();
+        final long flow1Id = createFlow(restClient, baseUrl, flowContent);
+
+        // Create a Sink with name sink1
+        String sink = new SinkContentJsonBuilder()
+            .setName("sink1")
+            .build();
+        final long sink1Id = createSink(restClient, baseUrl, sink);
+
+        // Create Submitter with name submitter1
+        String submitter = new SubmitterContentJsonBuilder()
+            .setName("submitter1")
+            .build();
+        final long submitter1Id = createSubmitter(restClient, baseUrl, submitter);
+
+        // Create Flowbinders with sortable names
+        String flowBinderContent = new FlowBinderContentJsonBuilder()
+            .setName("c-flowbinder")
+            .setCharset("charset1")
+            .setDescription("description1")
+            .setDestination("destination1")
+            .setFormat("format1")
+            .setPackaging("packaging1")
+            .setRecordSplitter("recordsplitter1")
+            .setFlowId(flow1Id)
+            .setSinkId(sink1Id)
+            .setSubmitterIds(Arrays.asList(submitter1Id))
+            .build();
+        final long sortsThird = createFlowBinder(restClient, baseUrl, flowBinderContent);
+        flowBinderContent = new FlowBinderContentJsonBuilder()
+            .setName("a-flowbinder")
+            .setCharset("charset2")
+            .setDescription("description2")
+            .setDestination("destination2")
+            .setFormat("format2")
+            .setPackaging("packaging2")
+            .setRecordSplitter("recordsplitter2")
+            .setFlowId(flow1Id)
+            .setSinkId(sink1Id)
+            .setSubmitterIds(Arrays.asList(submitter1Id))
+            .build();
+        final long sortsFirst = createFlowBinder(restClient, baseUrl, flowBinderContent);
+        flowBinderContent = new FlowBinderContentJsonBuilder()
+            .setName("b-flowbinder")
+            .setCharset("charset3")
+            .setDescription("description3")
+            .setDestination("destination3")
+            .setFormat("format3")
+            .setPackaging("packaging3")
+            .setRecordSplitter("recordsplitter3")
+            .setFlowId(flow1Id)
+            .setSinkId(sink1Id)
+            .setSubmitterIds(Arrays.asList(submitter1Id))
+            .build();
+        final long sortsSecond = createFlowBinder(restClient, baseUrl, flowBinderContent);
+
+        // When...
+        final Response response = HttpClient.doGet(restClient, baseUrl, FlowStoreServiceConstants.FLOW_BINDERS);
+
+        // Then...
+        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.OK.getStatusCode()));
+
+        // And...
+        final String responseContent = response.readEntity(String.class);
+        assertThat(responseContent, is(notNullValue()));
+        final ArrayNode responseContentNode = (ArrayNode) JsonUtil.getJsonRoot(responseContent);
+        assertThat(responseContentNode.size(), is(3));
+        assertThat(responseContentNode.get(0).get("id").longValue(), is(sortsFirst));
+        assertThat(responseContentNode.get(1).get("id").longValue(), is(sortsSecond));
+        assertThat(responseContentNode.get(2).get("id").longValue(), is(sortsThird));
+    }
+
 }

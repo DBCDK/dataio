5a439a958dc43c4b6053a652bb9277c4b6a75dbd
Jesper Damkjær <jda@dbc.dk>
2014-03-28 14:54:16 +0000

    Refactoring: Now both es and dummy depends on mocked classes from testutil.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@70526 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- sink/es/ejb/src/test/java/dk/dbc/dataio/sink/es/EsMessageProcessorBeanTest.java
+++ sink/es/ejb/src/test/java/dk/dbc/dataio/sink/es/EsMessageProcessorBeanTest.java
@@ -9,29 +9,20 @@ import dk.dbc.dataio.commons.utils.test.model.ChunkResultBuilder;
 import dk.dbc.dataio.sink.InvalidMessageSinkException;
 import dk.dbc.dataio.sink.SinkException;
 import dk.dbc.dataio.sink.es.entity.EsInFlight;
+import dk.dbc.dataio.sink.testutil.MockedTextMessage;
+import dk.dbc.dataio.sink.testutil.MockedMessageDrivenContext;
+import dk.dbc.dataio.sink.testutil.NotTextMessage;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 
-import javax.ejb.EJBHome;
-import javax.ejb.EJBLocalHome;
-import javax.ejb.MessageDrivenContext;
-import javax.ejb.TimerService;
-import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.TextMessage;
-import javax.transaction.UserTransaction;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.Principal;
 import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.Map;
-import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -244,138 +235,5 @@ public class EsMessageProcessorBeanTest {
 
     private String encodeBase64(String dataToEncode) {
         return Base64.encodeBase64String(dataToEncode.getBytes(StandardCharsets.UTF_8));
-    }
-
-    private static class MockedTextMessage implements TextMessage {
-        private String payload;
-        @Override
-        public void setText(String s) throws JMSException {
-            payload = s;
-        }
-
-        @Override
-        public String getText() throws JMSException {
-            return payload;
-        }
-
-        @Override public String getJMSMessageID() throws JMSException { return "mockedMsg"; }
-        @Override public void setJMSMessageID(String s) throws JMSException { }
-        @Override public long getJMSTimestamp() throws JMSException { return 0; }
-        @Override public void setJMSTimestamp(long l) throws JMSException { }
-        @Override public byte[] getJMSCorrelationIDAsBytes() throws JMSException { return new byte[0]; }
-        @Override public void setJMSCorrelationIDAsBytes(byte[] bytes) throws JMSException { }
-        @Override public void setJMSCorrelationID(String s) throws JMSException { }
-        @Override public String getJMSCorrelationID() throws JMSException { return null; }
-        @Override public Destination getJMSReplyTo() throws JMSException { return null; }
-        @Override public void setJMSReplyTo(Destination destination) throws JMSException { }
-        @Override public Destination getJMSDestination() throws JMSException { return null; }
-        @Override public void setJMSDestination(Destination destination) throws JMSException { }
-        @Override public int getJMSDeliveryMode() throws JMSException { return 0; }
-        @Override public void setJMSDeliveryMode(int i) throws JMSException { }
-        @Override public boolean getJMSRedelivered() throws JMSException { return false; }
-        @Override public void setJMSRedelivered(boolean b) throws JMSException { }
-        @Override public String getJMSType() throws JMSException { return null; }
-        @Override public void setJMSType(String s) throws JMSException { }
-        @Override public long getJMSExpiration() throws JMSException { return 0; }
-        @Override public void setJMSExpiration(long l) throws JMSException { }
-        @Override public long getJMSDeliveryTime() throws JMSException { return 0; }
-        @Override public void setJMSDeliveryTime(long l) throws JMSException { }
-        @Override public int getJMSPriority() throws JMSException { return 0; }
-        @Override public void setJMSPriority(int i) throws JMSException { }
-        @Override public void clearProperties() throws JMSException { }
-        @Override public boolean propertyExists(String s) throws JMSException { return false; }
-        @Override public boolean getBooleanProperty(String s) throws JMSException { return false; }
-        @Override public byte getByteProperty(String s) throws JMSException { return 0; }
-        @Override public short getShortProperty(String s) throws JMSException { return 0; }
-        @Override public int getIntProperty(String s) throws JMSException { return 0; }
-        @Override public long getLongProperty(String s) throws JMSException { return 0; }
-        @Override public float getFloatProperty(String s) throws JMSException { return 0; }
-        @Override public double getDoubleProperty(String s) throws JMSException { return 0; }
-        @Override public String getStringProperty(String s) throws JMSException { return null; }
-        @Override public Object getObjectProperty(String s) throws JMSException { return null; }
-        @Override public Enumeration getPropertyNames() throws JMSException { return null; }
-        @Override public void setBooleanProperty(String s, boolean b) throws JMSException { }
-        @Override public void setByteProperty(String s, byte b) throws JMSException { }
-        @Override public void setShortProperty(String s, short i) throws JMSException { }
-        @Override public void setIntProperty(String s, int i) throws JMSException { }
-        @Override public void setLongProperty(String s, long l) throws JMSException { }
-        @Override public void setFloatProperty(String s, float v) throws JMSException { }
-        @Override public void setDoubleProperty(String s, double v) throws JMSException { }
-        @Override public void setStringProperty(String s, String s2) throws JMSException { }
-        @Override public void setObjectProperty(String s, Object o) throws JMSException { }
-        @Override public void acknowledge() throws JMSException { }
-        @Override public void clearBody() throws JMSException { }
-        @Override public <T> T getBody(Class<T> tClass) throws JMSException { return null; }
-        @Override public boolean isBodyAssignableTo(Class aClass) throws JMSException { return false; }
-    }
-
-    private static class NotTextMessage implements Message {
-        @Override public String getJMSMessageID() throws JMSException { return null; }
-        @Override public void setJMSMessageID(String s) throws JMSException { }
-        @Override public long getJMSTimestamp() throws JMSException { return 0; }
-        @Override public void setJMSTimestamp(long l) throws JMSException { }
-        @Override public byte[] getJMSCorrelationIDAsBytes() throws JMSException { return new byte[0]; }
-        @Override public void setJMSCorrelationIDAsBytes(byte[] bytes) throws JMSException { }
-        @Override public void setJMSCorrelationID(String s) throws JMSException { }
-        @Override public String getJMSCorrelationID() throws JMSException { return null; }
-        @Override public Destination getJMSReplyTo() throws JMSException { return null; }
-        @Override public void setJMSReplyTo(Destination destination) throws JMSException { }
-        @Override public Destination getJMSDestination() throws JMSException { return null; }
-        @Override public void setJMSDestination(Destination destination) throws JMSException { }
-        @Override public int getJMSDeliveryMode() throws JMSException { return 0; }
-        @Override public void setJMSDeliveryMode(int i) throws JMSException { }
-        @Override public boolean getJMSRedelivered() throws JMSException { return false; }
-        @Override public void setJMSRedelivered(boolean b) throws JMSException { }
-        @Override public String getJMSType() throws JMSException { return null; }
-        @Override public void setJMSType(String s) throws JMSException { }
-        @Override public long getJMSExpiration() throws JMSException { return 0; }
-        @Override public void setJMSExpiration(long l) throws JMSException { }
-        @Override public long getJMSDeliveryTime() throws JMSException { return 0; }
-        @Override public void setJMSDeliveryTime(long l) throws JMSException { }
-        @Override public int getJMSPriority() throws JMSException { return 0; }
-        @Override public void setJMSPriority(int i) throws JMSException { }
-        @Override public void clearProperties() throws JMSException { }
-        @Override public boolean propertyExists(String s) throws JMSException { return false; }
-        @Override public boolean getBooleanProperty(String s) throws JMSException { return false; }
-        @Override public byte getByteProperty(String s) throws JMSException { return 0; }
-        @Override public short getShortProperty(String s) throws JMSException { return 0; }
-        @Override public int getIntProperty(String s) throws JMSException { return 0; }
-        @Override public long getLongProperty(String s) throws JMSException { return 0; }
-        @Override public float getFloatProperty(String s) throws JMSException { return 0; }
-        @Override public double getDoubleProperty(String s) throws JMSException { return 0; }
-        @Override public String getStringProperty(String s) throws JMSException { return null; }
-        @Override public Object getObjectProperty(String s) throws JMSException { return null; }
-        @Override public Enumeration getPropertyNames() throws JMSException { return null; }
-        @Override public void setBooleanProperty(String s, boolean b) throws JMSException { }
-        @Override public void setByteProperty(String s, byte b) throws JMSException { }
-        @Override public void setShortProperty(String s, short i) throws JMSException { }
-        @Override public void setIntProperty(String s, int i) throws JMSException { }
-        @Override public void setLongProperty(String s, long l) throws JMSException { }
-        @Override public void setFloatProperty(String s, float v) throws JMSException { }
-        @Override public void setDoubleProperty(String s, double v) throws JMSException { }
-        @Override public void setStringProperty(String s, String s2) throws JMSException { }
-        @Override public void setObjectProperty(String s, Object o) throws JMSException { }
-        @Override public void acknowledge() throws JMSException { }
-        @Override public void clearBody() throws JMSException { }
-        @Override public <T> T getBody(Class<T> tClass) throws JMSException { return null; }
-        @Override public boolean isBodyAssignableTo(Class aClass) throws JMSException { return false; }
-    }
-
-    @SuppressWarnings("deprecation")
-    private static class MockedMessageDrivenContext implements MessageDrivenContext {
-        private boolean rollbackOnly = false;
-        @Override public EJBHome getEJBHome() throws IllegalStateException { return null; }
-        @Override public EJBLocalHome getEJBLocalHome() throws IllegalStateException { return null; }
-        @Override public Properties getEnvironment() { return null; }
-        @Override public java.security.Identity getCallerIdentity() { return null; }
-        @Override public Principal getCallerPrincipal() throws IllegalStateException { return null; }
-        @Override public boolean isCallerInRole(java.security.Identity identity) { return false; }
-        @Override public boolean isCallerInRole(String s) throws IllegalStateException { return false; }
-        @Override public UserTransaction getUserTransaction() throws IllegalStateException { return null; }
-        @Override public void setRollbackOnly() throws IllegalStateException { rollbackOnly = true; }
-        @Override public boolean getRollbackOnly() throws IllegalStateException { return rollbackOnly; }
-        @Override public TimerService getTimerService() throws IllegalStateException { return null; }
-        @Override public Object lookup(String s) throws IllegalArgumentException { return null; }
-        @Override public Map<String, Object> getContextData() { return null; }
     }
 }

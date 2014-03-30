105f480f4b0af80bbc5eb0bea7ddf4302887de82
Jesper Damkjær <jda@dbc.dk>
2014-03-30 21:12:53 +0000

    Added more functionality to performance-test - but still not enough to be operational.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@70541 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- performance-test/src/test/java/dk/dbc/dataio/perftest/PerformanceIT.java
+++ performance-test/src/test/java/dk/dbc/dataio/perftest/PerformanceIT.java
@@ -3,6 +3,8 @@ package dk.dbc.dataio.perftest;
 import dk.dbc.dataio.commons.types.SubmitterContent;
 import dk.dbc.dataio.commons.types.rest.FlowStoreServiceConstants;
 import dk.dbc.dataio.commons.utils.httpclient.HttpClient;
+import dk.dbc.dataio.commons.utils.json.JsonException;
+import dk.dbc.dataio.commons.utils.json.JsonUtil;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
@@ -12,18 +14,27 @@ import org.junit.Test;
 public class PerformanceIT {
 
     @Test
-    public void performanceTest() {
+    public void performanceTest() throws JsonException {
         String baseUrl = String.format("http://localhost:%s/flow-store", System.getProperty("glassfish.port"));
         Client restClient = HttpClient.newClient();
 
         // insert submitter
-        // insert flowcomponent
+        SubmitterContent submitterContent = new SubmitterContent(424242L, "perftestbib", "Library for performancetest");
+        final String submitterJson = JsonUtil.toJson(submitterContent);
+        final Response response = HttpClient.doPostWithJson(restClient, submitterJson, baseUrl, FlowStoreServiceConstants.SUBMITTERS);
+        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.CREATED.getStatusCode()));
+
+        // insert flowcomponent with javascript with no functionality
         // insert flow
         // insert sink
         // insert flowbinder
 
-        SubmitterContent submitterContent = new SubmitterContent(424242L, "perftestbib", "Library for performancetest");
-        final Response response = HttpClient.doPostWithJson(restClient, submitterContent, baseUrl, FlowStoreServiceConstants.SUBMITTERS);
-        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.CREATED.getStatusCode()));
+
+        // Start timer
+        // Create Job
+        // Insert Job
+        // Wait for Job-completion
+        // End Timer
+        // Somehow write result of timer in useful format
     }
 }

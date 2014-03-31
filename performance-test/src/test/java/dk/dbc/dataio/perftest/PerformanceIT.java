ffa179cfe471249564cd2ea477049b0a4e489b2e
Jesper Damkjær <jda@dbc.dk>
2014-03-31 20:50:09 +0000

    Added complete functionality for performance testing - kind of broken since it depends on a file on my (jda's) laptop - still missing real job-creator.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@70606 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- performance-test/src/test/java/dk/dbc/dataio/perftest/PerformanceIT.java
+++ performance-test/src/test/java/dk/dbc/dataio/perftest/PerformanceIT.java
@@ -1,13 +1,31 @@
 package dk.dbc.dataio.perftest;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import dk.dbc.dataio.commons.types.FlowBinderContent;
+import dk.dbc.dataio.commons.types.FlowComponent;
+import dk.dbc.dataio.commons.types.FlowComponentContent;
+import dk.dbc.dataio.commons.types.FlowContent;
+import dk.dbc.dataio.commons.types.JavaScript;
+import dk.dbc.dataio.commons.types.JobInfo;
+import dk.dbc.dataio.commons.types.JobSpecification;
+import dk.dbc.dataio.commons.types.JobState;
+import dk.dbc.dataio.commons.types.SinkContent;
 import dk.dbc.dataio.commons.types.SubmitterContent;
 import dk.dbc.dataio.commons.types.rest.FlowStoreServiceConstants;
 import dk.dbc.dataio.commons.utils.httpclient.HttpClient;
 import dk.dbc.dataio.commons.utils.json.JsonException;
 import dk.dbc.dataio.commons.utils.json.JsonUtil;
+import dk.dbc.dataio.commons.utils.jobstore.JobStoreServiceConnector;
+import dk.dbc.dataio.commons.utils.jobstore.JobStoreServiceConnectorException;
+import dk.dbc.dataio.integrationtest.ITUtil;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
@@ -15,26 +33,87 @@ public class PerformanceIT {
 
     @Test
     public void performanceTest() throws JsonException {
-        String baseUrl = String.format("http://localhost:%s/flow-store", System.getProperty("glassfish.port"));
+        String flowStorebaseUrl = String.format("http://localhost:%s/flow-store", System.getProperty("glassfish.port"));
+        String jobStorebaseUrl = String.format("http://localhost:%s/job-store", System.getProperty("glassfish.port"));
         Client restClient = HttpClient.newClient();
 
-        // insert submitter
+        // insert submitter:
         SubmitterContent submitterContent = new SubmitterContent(424242L, "perftestbib", "Library for performancetest");
-        final String submitterJson = JsonUtil.toJson(submitterContent);
-        final Response response = HttpClient.doPostWithJson(restClient, submitterJson, baseUrl, FlowStoreServiceConstants.SUBMITTERS);
-        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.CREATED.getStatusCode()));
+        long submitterId = insertObjectInFlowStore(restClient, flowStorebaseUrl, submitterContent, FlowStoreServiceConstants.SUBMITTERS);
 
-        // insert flowcomponent with javascript with no functionality
-        // insert flow
-        // insert sink
+        // insert flowcomponent with javascript with no functionality:
+        JavaScript js = new JavaScript("function invocationFunction(record, supplementaryData) { return \"Hello from javascript!\" }", "NoModule");
+        FlowComponentContent flowComponentContent = new FlowComponentContent("perftest-flow-component", "No_SVN_project", 1L, "invocationJavaScript", Arrays.asList(js), "invocationFunction");
+        long flowComponentId = insertObjectInFlowStore(restClient, flowStorebaseUrl, flowComponentContent, FlowStoreServiceConstants.FLOW_COMPONENTS);
+
+        // get flowcomponent:
+        // The flow needs the complete flowcomponent to be created - not just the flowcomponentcontent, hence we need to retrieve the flowcomponent.
+        final Response response = HttpClient.doGet(restClient, flowStorebaseUrl, FlowStoreServiceConstants.FLOW_COMPONENTS);
+        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.OK.getStatusCode()));
+        final String responseContent = response.readEntity(String.class);
+        assertThat(responseContent, is(notNullValue()));
+        final ArrayNode responseContentNode = (ArrayNode) JsonUtil.getJsonRoot(responseContent);
+        assertThat(responseContentNode.get(0).get("id").longValue(), is(flowComponentId));
+        String flowComponentJson = responseContentNode.get(0).toString();
+        FlowComponent flowComponent = JsonUtil.fromJson(flowComponentJson, FlowComponent.class);
+
+        System.out.println("response: " + flowComponentJson);
+
+        // insert flow:
+        FlowContent flowContent = new FlowContent("perftest-flow", "Flow for perftest", Arrays.asList(flowComponent));
+        long flowId = insertObjectInFlowStore(restClient, flowStorebaseUrl, flowContent, FlowStoreServiceConstants.FLOWS);
+
+        // insert sink:
+        SinkContent sinkContent = new SinkContent("perftest-dummy-sink", "jdbc/dataio/dummy");
+        long sinkId = insertObjectInFlowStore(restClient, flowStorebaseUrl, sinkContent, FlowStoreServiceConstants.SINKS);
+
         // insert flowbinder
+        FlowBinderContent flowBinderContent = new FlowBinderContent("perftest-flowbinder", "flowbinder for perftest", "xml", "testdata", "utf8", "dummysink", "Default Record Splitter", flowId, Arrays.asList(new Long(submitterId)), sinkId);
+        insertObjectInFlowStore(restClient, flowStorebaseUrl, flowBinderContent, FlowStoreServiceConstants.FLOW_BINDERS);
 
-
-        // Start timer
         // Create Job
-        // Insert Job
-        // Wait for Job-completion
-        // End Timer
-        // Somehow write result of timer in useful format
+        JobStoreServiceConnector jobStoreServiceConnector = new JobStoreServiceConnector(restClient, jobStorebaseUrl);
+        JobSpecification jobSpec = new JobSpecification("xml", "testdata", "utf8", "dummysink", 424242L, "jda@dbc.dk", "jda@dbc.dk", "jda", "/home/damkjaer/svn.dbc.dk/repos/dataio/trunk/performance-test/data.xml");
+        try {
+            // Start timer
+            long timer = System.currentTimeMillis();
+            // Insert Job
+            JobInfo jobInfo = jobStoreServiceConnector.createJob(jobSpec);
+
+            boolean done = false;
+            // Wait for Job-completion
+            while(!done) {
+                JobState jobState = jobStoreServiceConnector.getState(jobInfo.getJobId());
+                if(jobState.getLifeCycleStateFor(JobState.OperationalState.DELIVERING) == JobState.LifeCycleState.DONE) {
+                    done = true;
+                } else {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(PerformanceIT.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            timer = System.currentTimeMillis() - timer;
+            // End Timer
+
+            // Somehow write result of timer in useful format
+            System.out.println("Time to handler job (ms): " + timer);
+   
+        } catch (NullPointerException ex) {
+            Logger.getLogger(PerformanceIT.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ProcessingException ex) {
+            Logger.getLogger(PerformanceIT.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JobStoreServiceConnectorException ex) {
+            Logger.getLogger(PerformanceIT.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private <T> long insertObjectInFlowStore(Client restClient, String baseUrl, T type, String restEndPoint) throws JsonException {
+        final String json = JsonUtil.toJson(type);
+        final Response response = HttpClient.doPostWithJson(restClient, json, baseUrl, restEndPoint);
+        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.CREATED.getStatusCode()));
+        long id = ITUtil.getResourceIdFromLocationHeaderAndAssertHasValue(response);
+        return id;
     }
 }

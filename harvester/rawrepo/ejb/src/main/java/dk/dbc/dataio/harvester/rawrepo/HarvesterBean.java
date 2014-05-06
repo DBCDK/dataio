7447227c4e2a81979b28df6abea55da862dd9884
Jan Bauer Nielsen <jbn@dbc.dk>
2014-05-06 12:55:03 +0000

    [17535] Added initial harvester beans.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@71651 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- harvester/rawrepo/ejb/src/main/java/dk/dbc/dataio/harvester/rawrepo/HarvesterBean.java
+++ harvester/rawrepo/ejb/src/main/java/dk/dbc/dataio/harvester/rawrepo/HarvesterBean.java
@@ -3,9 +3,14 @@ package dk.dbc.dataio.harvester.rawrepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ejb.Singleton;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 
+/**
+ * This singleton Enterprise Java Bean (EJB) handles an actual RawRepo harvest
+ */
+@Singleton
 public class HarvesterBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(HarvesterBean.class);
 

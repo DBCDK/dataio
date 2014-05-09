74e1c19f7c5b9b70ed880a08e14c17cec96bf069
Jan Bauer Nielsen <jbn@dbc.dk>
2014-05-09 06:47:19 +0000

    [17535] Added HarvesterDataFile unit tests.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@71796 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ harvester/rawrepo/ejb/src/test/dk/dbc/dataio/harvester/rawrepo/HarvesterDataFileTest.java
@@ -0,0 +1,151 @@
+package dk.dbc.dataio.harvester.rawrepo;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+public class HarvesterDataFileTest {
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    private final Charset charset = StandardCharsets.UTF_8;
+    private final OutputStream outputStream = mock(OutputStream.class);
+
+    @Test(expected = NullPointerException.class)
+    public void constructor_charsetArgIsNull_throws() throws HarvesterException {
+        new HarvesterDataFile(null, outputStream);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void constructor_outputStreamArgIsNull_throws() throws HarvesterException {
+        new HarvesterDataFile(charset, null);
+    }
+
+    @Test(expected = HarvesterException.class)
+    public void constructor_writingOfHeaderThrowsIOException_throws() throws HarvesterException, IOException {
+        doThrow(new IOException()).when(outputStream).write(any(byte[].class), anyInt(), anyInt());
+        new HarvesterDataFile(charset, outputStream);
+    }
+
+    @Test
+    public void constructor_allArgsAreValid_returnsNewInstance() throws HarvesterException {
+        final HarvesterDataFile harvesterDataFile = new HarvesterDataFile(charset, outputStream);
+        assertThat(harvesterDataFile, is(notNullValue()));
+    }
+
+    @Test
+    public void addRecord_recordArgIsNull_throws() throws HarvesterException {
+        final HarvesterDataFile harvesterDataFile = new HarvesterDataFile(charset, outputStream);
+        try {
+            harvesterDataFile.addRecord(null);
+            fail("No exception thrown");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @Test
+    public void addRecord_charsetMismatch_throws() throws HarvesterException {
+        final MockedHarvesterRecord harvesterRecord = new MockedHarvesterRecord();
+        harvesterRecord.setCharset(StandardCharsets.ISO_8859_1);
+        final HarvesterDataFile harvesterDataFile = new HarvesterDataFile(charset, outputStream);
+        try {
+            harvesterDataFile.addRecord(harvesterRecord);
+            fail("No exception thrown");
+        } catch (HarvesterInvalidRecordException e) {
+        }
+    }
+
+    @Test
+    public void addRecord_writingOfRecordDataThrowsIOException_throws() throws HarvesterException, IOException {
+        final MockedHarvesterRecord harvesterRecord = new MockedHarvesterRecord();
+        harvesterRecord.setCharset(StandardCharsets.UTF_8);
+        harvesterRecord.setData("data".getBytes(StandardCharsets.UTF_8));
+        final HarvesterDataFile harvesterDataFile = new HarvesterDataFile(charset, outputStream);
+        try {
+            doThrow(new IOException()).when(outputStream).write(any(byte[].class), anyInt(), anyInt());
+            harvesterDataFile.close();
+            fail("No exception thrown");
+        } catch (HarvesterException e) {
+        }
+    }
+
+    @Test
+    public void close_writingOfFooterThrowsIOException_throws() throws HarvesterException, IOException {
+        final HarvesterDataFile harvesterDataFile = new HarvesterDataFile(charset, outputStream);
+        try {
+            doThrow(new IOException()).when(outputStream).write(any(byte[].class), anyInt(), anyInt());
+            harvesterDataFile.close();
+            fail("No exception thrown");
+        } catch (HarvesterException e) {
+        }
+    }
+
+    @Test
+    public void isAutoClosable() throws HarvesterException {
+        try (final HarvesterDataFile harvesterDataFile = new HarvesterDataFile(charset, outputStream)) {
+            assertThat(harvesterDataFile, is(notNullValue()));
+        }
+    }
+
+    @Test
+    public void writesXml() throws HarvesterException, IOException, ParserConfigurationException, SAXException {
+        final File outputFile = testFolder.newFile();
+        final HarvesterDataFile harvesterDataFile =
+                new HarvesterDataFile(charset, getOutputStreamForFile(outputFile));
+        harvesterDataFile.close();
+        parseXmlFile(outputFile);
+    }
+
+    private OutputStream getOutputStreamForFile(File file) throws IOException {
+        return new FileOutputStream(file);
+    }
+
+    private void parseXmlFile(File xmlFile) throws ParserConfigurationException, IOException, SAXException {
+        final DocumentBuilder documentBuilder =
+                DocumentBuilderFactory.newInstance().newDocumentBuilder();
+	    documentBuilder.parse(xmlFile);
+    }
+
+    private static class MockedHarvesterRecord implements HarvesterRecord {
+        byte[] data;
+        Charset charset;
+
+        public void setData(byte[] data) {
+            this.data = data;
+        }
+
+        public void setCharset(Charset charset) {
+            this.charset = charset;
+        }
+
+        @Override
+        public byte[] getData() {
+            return data;
+        }
+
+        @Override
+        public Charset getCharset() {
+            return charset;
+        }
+    }
+}

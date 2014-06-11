80a9d14d6e34b43ca129a4b04b5eb391dd8de9b6
Jan Bauer Nielsen <jbn@dbc.dk>
2014-06-11 06:20:50 +0000

    [17627] Added integration-test/sinks module.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@72999 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ integration-test/sinks/fbs/src/test/java/dk/dbc/dataio/sink/fbs/SinkIT.java
@@ -0,0 +1,13 @@
+package dk.dbc.dataio.sink.fbs;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SinkIT {
+    @Test
+    public void test() {
+        assertThat(true, is(true));
+    }
+}

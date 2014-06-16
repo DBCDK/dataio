1aede5a78f3488b297abc8f0a9e8cac1f66f2d88
Jan Bauer Nielsen <jbn@dbc.dk>
2014-06-16 11:30:48 +0000

    [17627] Added additional AbstractMessageConsumerBean unit tests test.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@73147 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/utils/service/src/test/java/dk/dbc/dataio/commons/utils/service/AbstractMessageConsumerBeanTest.java
+++ commons/utils/service/src/test/java/dk/dbc/dataio/commons/utils/service/AbstractMessageConsumerBeanTest.java
@@ -16,55 +16,75 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AbstractMessageConsumerBeanTest {
+    protected enum HandleConsumedMessageReaction { ACCEPT, INVALID, THROW }
+
+    private static final String PAYLOAD = "{'key': 'value'}";
+    private static final String PAYLOAD_TYPE = HandleConsumedMessageReaction.ACCEPT.toString();
+
     @Test
     public void onMessage_messageArgIsNull_noTransactionRollback() {
         final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
         messageConsumerBean.onMessage(null);
-        assertThat(messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(false));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
     }
 
     @Test
     public void onMessage_messageArgIsNotOfTypeTextMessage_noTransactionRollback() {
         final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
         messageConsumerBean.onMessage(new NotJmsTextMessage());
-        assertThat(messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(false));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
     }
 
     @Test
     public void onMessage_messageArgPayloadIsNull_noTransactionRollback() throws JMSException {
         final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
-        final MockedJmsTextMessage textMessage = new MockedJmsTextMessage();
-        textMessage.setText(null);
-        messageConsumerBean.onMessage(textMessage);
-        assertThat(messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+        messageConsumerBean.onMessage(getMockedJmsTextMessage(PAYLOAD_TYPE, null));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(false));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
     }
 
     @Test
     public void onMessage_messageArgPayloadIsEmpty_noTransactionRollback() throws JMSException {
         final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
-        final MockedJmsTextMessage textMessage = new MockedJmsTextMessage();
-        textMessage.setText("");
-        messageConsumerBean.onMessage(textMessage);
-        assertThat(messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+        messageConsumerBean.onMessage(getMockedJmsTextMessage(PAYLOAD_TYPE, ""));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(false));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
     }
 
     @Test
     public void onMessage_messageArgHasNoPayloadTypeProperty_noTransactionRollback() throws JMSException {
         final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
         final MockedJmsTextMessage textMessage = new MockedJmsTextMessage();
-        textMessage.setText("{'key': 'value'}");
+        textMessage.setText(PAYLOAD);
         messageConsumerBean.onMessage(textMessage);
-        assertThat(messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(false));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
     }
 
     @Test
     public void onMessage_messageArgHasEmptyPayloadTypeProperty_noTransactionRollback() throws JMSException {
         final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
-        final MockedJmsTextMessage textMessage = new MockedJmsTextMessage();
-        textMessage.setStringProperty(JmsConstants.PAYLOAD_PROPERTY_NAME, "   ");
-        textMessage.setText("{'key': 'value'}");
-        messageConsumerBean.onMessage(textMessage);
-        assertThat(messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+        messageConsumerBean.onMessage(getMockedJmsTextMessage("", PAYLOAD));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(false));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+    }
+
+    @Test
+    public void onMessage_consumedMessageHandlingThrowsInvalidMessageException_noTransactionRollback() throws JMSException {
+        final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
+        messageConsumerBean.onMessage(getMockedJmsTextMessage(HandleConsumedMessageReaction.INVALID.toString(), PAYLOAD));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(true));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(false));
+    }
+
+    @Test
+    public void onMessage_consumedMessageHandlingThrowsServiceException_transactionRollback() throws JMSException {
+        final TestableMessageConsumerBean messageConsumerBean = getInitializedBean();
+        messageConsumerBean.onMessage(getMockedJmsTextMessage(HandleConsumedMessageReaction.THROW.toString(), PAYLOAD));
+        assertThat("handleConsumedMessage() called", messageConsumerBean.handleConsumedMessageCalled, is(true));
+        assertThat("rollback", messageConsumerBean.getMessageDrivenContext().getRollbackOnly(), is(true));
     }
 
     @Test(expected = InvalidMessageException.class)
@@ -123,7 +143,16 @@ public class AbstractMessageConsumerBeanTest {
         return messageConsumerBean;
     }
 
+    private MockedJmsTextMessage getMockedJmsTextMessage(String payloadType, String payload) throws JMSException {
+        final MockedJmsTextMessage textMessage = new MockedJmsTextMessage();
+        textMessage.setStringProperty(JmsConstants.PAYLOAD_PROPERTY_NAME, payloadType);
+        textMessage.setText(payload);
+        return textMessage;
+    }
+
     private static class TestableMessageConsumerBean extends AbstractMessageConsumerBean {
+        public boolean handleConsumedMessageCalled = false;
+
         public MessageDrivenContext getMessageDrivenContext() {
             return messageDrivenContext;
         }
@@ -133,6 +162,20 @@ public class AbstractMessageConsumerBeanTest {
 
         @Override
         public void handleConsumedMessage(ConsumedMessage consumedMessage) throws ServiceException, InvalidMessageException {
+            handleConsumedMessageCalled = true;
+            switch (HandleConsumedMessageReaction.valueOf(consumedMessage.getPayloadType())) {
+                case INVALID:
+                    throw new InvalidMessageException("INVALID");
+                case THROW:
+                    throw new TestableMessageConsumerBeanException("THROWS");
+            }
+        }
+
+        public static class TestableMessageConsumerBeanException extends ServiceException {
+            private static final long serialVersionUID = -1908673274650168855L;
+            public TestableMessageConsumerBeanException(String message) {
+                super(message);
+            }
         }
     }
 }

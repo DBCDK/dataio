18dc02ef1361908a1a1e69c661f512da6b4d3880
Steen Lillethorup Frederiksen <slf@dbc.dk>
2014-06-20 09:14:19 +0000

    [17658] US #311: Joboversigt - fejlindikator - Tilføj funktionalitet til lampen i Job Oversigten
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@73322 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/jobsshow/JobsShowViewImpl.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/jobsshow/JobsShowViewImpl.java
@@ -1,5 +1,6 @@
 package dk.dbc.dataio.gui.client.pages.jobsshow;
 
+import com.google.gwt.cell.client.Cell;
 import com.google.gwt.cell.client.ImageResourceCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -11,6 +12,7 @@ import com.google.gwt.user.cellview.client.ColumnSortList;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.view.client.ListDataProvider;
+import dk.dbc.dataio.commons.types.JobErrorCode;
 import dk.dbc.dataio.commons.types.JobInfo;
 import dk.dbc.dataio.gui.client.components.DioCellTable;
 import dk.dbc.dataio.gui.client.resource.Resources;
@@ -33,6 +35,12 @@ public class JobsShowViewImpl extends ContentPanel<JobsShowPresenter> implements
     // Constants (These are not all private since we use them in the selenium tests)
     public static final String GUIID_JOBS_SHOW_WIDGET = "jobsshowwidget";
     public static final String GUIID_JOBS_MORE_BUTTON = "jobsshowmorebutton";
+    public static final String GUICLASS_GRAY = "gray-lamp";
+    public static final String GUICLASS_GREEN = "green-lamp";
+    public static final String GUICLASS_RED = "red-lamp";
+
+    // Enums
+    private enum JobStatusEnum {NOT_DONE, DONE_WITH_ERROR, DONE_WITHOUT_ERROR}
 
     // Configuration constants
     private static final int PAGE_SIZE = 20;
@@ -111,8 +119,28 @@ public class JobsShowViewImpl extends ContentPanel<JobsShowPresenter> implements
         // Job State Column
         jobStateColumn = new Column<JobInfo, ImageResource>(new ImageResourceCell()) {
             @Override
+            public String getCellStyleNames(Cell.Context context, JobInfo content) {
+                switch (getJobStatus(content)) {
+                    case DONE_WITHOUT_ERROR:
+                        return GUICLASS_GREEN;
+                    case DONE_WITH_ERROR:
+                        return GUICLASS_RED;
+                    default:
+                    case NOT_DONE:
+                        return GUICLASS_GRAY;
+                }
+            }
+            @Override
             public ImageResource getValue(JobInfo content) {
-                return resources.gray();
+                switch (getJobStatus(content)) {
+                    case DONE_WITHOUT_ERROR:
+                        return resources.green();
+                    case DONE_WITH_ERROR:
+                        return resources.red();
+                    default:
+                    case NOT_DONE:
+                        return resources.gray();
+                }
             }
         };
         table.addColumn(jobStateColumn, constants.columnHeader_JobStatus());
@@ -131,6 +159,35 @@ public class JobsShowViewImpl extends ContentPanel<JobsShowPresenter> implements
             }
         });
         add(showMoreButton);
+    }
+
+    /**
+     * This method maps the logical jobstatus to a JobStatusEnum
+     * @param jobInfo The JobInfo for the job to test
+     * @return JobStatusEnum: NOT_DONE, DONE_WITHOUT_ERROR or DONE_WITH_ERROR
+     */
+    private JobStatusEnum getJobStatus(JobInfo jobInfo) {
+        if (jobInfo.getJobErrorCode() != JobErrorCode.NO_ERROR ) {
+            return JobStatusEnum.DONE_WITH_ERROR;
+        }
+        // Now we know, that JobErrorCode is JobErrorCode.NO_ERROR
+        if (jobInfo.getChunkifyingChunkCounter() == null ||
+                jobInfo.getProcessingChunkCounter() == null ||
+                jobInfo.getDeliveringChunkCounter() == null ) {
+            return JobStatusEnum.NOT_DONE;
+        }
+        // Now we know, that the three counter have values, and we can now check the counters
+        if (jobInfo.getChunkifyingChunkCounter().getItemResultCounter().getFailure() > 0L) {
+            return JobStatusEnum.DONE_WITH_ERROR;
+        }
+        if (jobInfo.getProcessingChunkCounter().getItemResultCounter().getFailure() > 0L) {
+            return JobStatusEnum.DONE_WITH_ERROR;
+        }
+        if (jobInfo.getDeliveringChunkCounter().getItemResultCounter().getFailure() > 0L) {
+            return JobStatusEnum.DONE_WITH_ERROR;
+        }
+        // We couldn't find any errors, so return with DONE_WITHOUT_ERROR
+        return JobStatusEnum.DONE_WITHOUT_ERROR;
     }
 
     /*

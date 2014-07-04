18c0c4959315c605d11dc547e6f953a949fc752c
Jesper Damkjær <jda@dbc.dk>
2014-07-04 09:00:09 +0000

    [17707] Refactoring of unittests into given-when-then. One test is still missing
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@74177 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- sequence-analyser/src/test/java/dk/dbc/dataio/sequenceanalyser/naive/NaiveSequenceAnalyserTest.java
+++ sequence-analyser/src/test/java/dk/dbc/dataio/sequenceanalyser/naive/NaiveSequenceAnalyserTest.java
@@ -27,18 +27,41 @@ public class NaiveSequenceAnalyserTest {
 
     @Before
     public void setup() {
-        LOGGER.info("Testing");
         sa = new NaiveSequenceAnalyser();
     }
 
+    /*
+     * Given: An empty Sequence Analyser
+     * When : A single chunk is inserted
+     * Then : The chunk must be independent when retrieved.
+     */
     @Test
-    public void testInsertionAndRetrivalOfSingleChunk() {
+    public void insertionAndRetrievalOfSingleChunk() {
+        // GIVEN:
+        // Sequence analyser must be empty
+        assertThat(sa.size(), is(0));
+        // WHEN:
         Chunk chunk = createChunk(1L, 2L);
         Sink sink = new SinkBuilder().build();
-        // add chunk
         sa.addChunk(chunk, sink);
+        // THEN:
         // verify that chunk is independent
         assertChunks(sa.getInactiveIndependentChunks(), chunk);
+    }
+
+    /*
+     * Given: A Sequence Analyser with a single chunk
+     * When : The chunk is released and deleted
+     * Then : No more independent chunks must exist in the sequence analyser,
+     *        and the sequence analyser must be empty.
+     */
+    @Test
+    public void deleteAndReleaseOfSingleChunk() {
+        // GIVEN:
+        Chunk chunk = createChunk(1L, 2L);
+        Sink sink = new SinkBuilder().build();
+        sa.addChunk(chunk, sink);
+        // WHEN:
         // remove chunk
         sa.deleteAndReleaseChunk(new ChunkIdentifier(1L, 2L));
         // verify that there are no independent chunks left
@@ -47,58 +70,96 @@ public class NaiveSequenceAnalyserTest {
         assertThat(sa.size(), is(0));
     }
 
+    /*
+     * Given: An empty sequence.
+     * Given: A sequence analyser with two independent chunks destined to the same sink.
+     * When : Two independent chunks with same sink are inserted
+     * Then : Both chunks must be retreived as inacative and independent.
+     */
     @Test
     public void testInsertionAndRetrivalOfTwoIndependentChunks() {
+        // GIVEN:
+        assertThat(sa.size(), is(0));
+        // WHEN:
         Chunk chunk1 = createChunk(1L, 2L, "horse");
         Chunk chunk2 = createChunk(3L, 4L, "goat");
         Sink sink = new SinkBuilder().build();
         // add chunk
         sa.addChunk(chunk1, sink);
         sa.addChunk(chunk2, sink);
+        // THEN:
         // verify that chunks are independent and inactive
         assertChunks(sa.getInactiveIndependentChunks(), chunk1, chunk2);
         // verify number of elements in Sequenceanalyser
         assertThat(sa.size(), is(2));
-        // remove chunk1
-        sa.deleteAndReleaseChunk(new ChunkIdentifier(1L, 2L));
-        // verify that a single chunk is still present in sequenceanalyser
-        assertThat(sa.size(), is(1));
-        // remove chunk2
-        sa.deleteAndReleaseChunk(new ChunkIdentifier(3L, 4L));
-        // verify that there are no independent inactive chunks left
-        assertChunks(sa.getInactiveIndependentChunks());
-        // verify that the sequenceanalyser is empty
-        assertThat(sa.size(), is(0));
     }
 
-    // Test: Two chunks where one depends on the other - when added only the first chunk can be independendent.
-    //       But when first chunk is released, the second chunk must be independent.
+    /*
+     * Given: A sequence analyser with a single chunk
+     * When : A new chunk for the same sink, which depends on the existing chunk, is inserted,
+     * Then : Only the first chunk can be retrived
+     */
     @Test
-    public void test1() {
+    public void insertDependentChunkWhichDependsOnExistingChunk() {
+        // GIVEN:
+        // add first chunk
+        Chunk chunk1 = createChunk(1L, 2L, "animal", "horse");
+        Sink sink = new SinkBuilder().build();
+        sa.addChunk(chunk1, sink);
+        // verify that there is one chunk in the sequence analyser
+        assertThat(sa.size(), is(1));
+        // WHEN
+        // add the second chunk
+        Chunk chunk2 = createChunk(3L, 4L, "animal", "goat");
+        sa.addChunk(chunk2, sink);
+        // verify that tehere are two chunks in the sequence analyser
+        assertThat(sa.size(), is(2));
+        // THEN:
+        // verify that only the first chunk is independent
+        assertChunks(sa.getInactiveIndependentChunks(), chunk1);
+    }
+
+    /*
+     * Given: A seqeuce analyser with two chunks where one depends on the other.
+     * When : The first chunk is released
+     * Then : The second chunk can be retrieved.
+     */
+    @Test
+    public void deletionAndReleaseOfChunkMakesDependendChunkIndependent() {
+        // GIVEN:
+        // add chunkS
         Chunk chunk1 = createChunk(1L, 2L, "animal", "horse");
         Chunk chunk2 = createChunk(3L, 4L, "animal", "goat");
         Sink sink = new SinkBuilder().build();
-        // add chunks
         sa.addChunk(chunk1, sink);
         sa.addChunk(chunk2, sink);
-        // verify that chunks are dependent
+        // verify that only the first chunk is independent
         assertChunks(sa.getInactiveIndependentChunks(), chunk1);
+        // verify that the seqence analyser contains two chunks
+        assertThat(sa.size(), is(2));
+        // wHEN
         // remove chunk1
         sa.deleteAndReleaseChunk(new ChunkIdentifier(1L, 2L));
-        // verify that chunk2 is now present
+        // verify that the seqence analyser contains one chunk
+        assertThat(sa.size(), is(1));
+        // THEN
+        // verify that chunk2 is now independent
         assertChunks(sa.getInactiveIndependentChunks(), chunk2);
-        // remove chunk2
-        sa.deleteAndReleaseChunk(new ChunkIdentifier(3L, 4L));
-        // verify that there are no independent chunks left
-        assertChunks(sa.getInactiveIndependentChunks());
-        // verify that the sequenceanalyser is empty
-        assertThat(sa.size(), is(0));
     }
 
     // Test: Two chunks with overlapping keys, but destined for different sinks.
     //       They should not depend on each other.
+    /*
+     * Given: An empty Seqeuence analyser
+     * When : Two chunk with overlapping keys, but destined for different sinks are inserted
+     * Then : Both chunks must be independent.
+     */
     @Test
-    public void test2() {
+    public void twoChunksWithOverlappingKeysDestinedDifferentSinksMustBeIndependent() {
+        // GIVEN
+        // verify that sequence analyser is empty
+        assertThat(sa.size(), is(0));
+        // WHEN:
         Chunk chunk1 = createChunk(1L, 2L, "animal", "horse");
         Chunk chunk2 = createChunk(3L, 4L, "animal", "goat");
         Sink sink1 = new SinkBuilder().setId(1L).build();
@@ -106,6 +167,7 @@ public class NaiveSequenceAnalyserTest {
         // add chunks
         sa.addChunk(chunk1, sink1);
         sa.addChunk(chunk2, sink2);
+        // THEN
         // verify that chunks are independent
         assertChunks(sa.getInactiveIndependentChunks(), chunk1, chunk2);
     }
@@ -139,12 +201,15 @@ public class NaiveSequenceAnalyserTest {
         assertChunks(sa.getInactiveIndependentChunks(), chunk3);
     }
 
-    // Test:
-    // When: a ChunkIdentifier is retrieved with getInactiveIndependentChunksAndActivate(),
-    // Then: subsequent calls to getInactiveIndependentChunksAndActivate() will not return the same ChunkIdentifer,
-    //       but the size of the internal dependecy graph remains the same.
+    /*
+     * Given: A sequence analyser with two dependent chunks
+     * When : a ChunkIdentifier is retrieved with getInactiveIndependentChunksAndActivate(),
+     * Then : subsequent calls to getInactiveIndependentChunksAndActivate() will not return the same ChunkIdentifer,
+     *        but the size of the internal dependecy graph remains the same.
+     */
     @Test
-    public void test4() {
+    public void alreadyRetreivedChunkMustNotReappearAsIndependent() {
+        // GIVEN
         Chunk chunk1 = createChunk(1L, 2L, "animal", "horse");
         Chunk chunk2 = createChunk(3L, 4L, "animal", "goat");
         Sink sink = new SinkBuilder().build();
@@ -153,18 +218,16 @@ public class NaiveSequenceAnalyserTest {
         sa.addChunk(chunk2, sink);
         // verify that the Sequenceanalyser contains two elements
         assertThat(sa.size(), is(2));
+        // WHEN
         // verify that chunks are dependent
         assertChunks(sa.getInactiveIndependentChunks(), chunk1);
         // verify that the Sequenceanalyser still contains two elements
         assertThat(sa.size(), is(2));
-        // remove chunk1
-        sa.deleteAndReleaseChunk(new ChunkIdentifier(1L, 2L));
-        // verify that the Sequenceanalyser now contains one element
-        assertThat(sa.size(), is(1));
-        // verify that chunk2 is now present
-        assertChunks(sa.getInactiveIndependentChunks(), chunk2);
+        // THEN
+        // verify that chunk2 has not become independent
+        assertChunks(sa.getInactiveIndependentChunks());
         // verify that the Sequenceanalyser still contains one element
-        assertThat(sa.size(), is(1));
+        assertThat(sa.size(), is(2));
     }
 
     private void assertChunks(List<ChunkIdentifier> inactiveIndependentChunks, Chunk... chunks) {

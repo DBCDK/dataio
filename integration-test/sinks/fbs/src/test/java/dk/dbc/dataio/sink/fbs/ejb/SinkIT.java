ea26299dc6ee55f9c23de7d175c1f353fec55552
Jan Bauer Nielsen <jbn@dbc.dk>
2014-07-04 09:11:03 +0000

    Re-enabled integration tests.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@74186 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- integration-test/sinks/fbs/src/test/java/dk/dbc/dataio/sink/fbs/ejb/SinkIT.java
+++ integration-test/sinks/fbs/src/test/java/dk/dbc/dataio/sink/fbs/ejb/SinkIT.java
@@ -10,7 +10,6 @@ import dk.dbc.dataio.commons.utils.test.model.ChunkItemBuilder;
 import dk.dbc.dataio.commons.utils.test.model.ChunkResultBuilder;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.naming.Context;
@@ -41,7 +40,6 @@ public class SinkIT {
      * And: the first item has status SUCCESS <br/>
      * And: the second item has status FAILURE <br/>
      */
-    @Ignore
     @Test
     public void fbsPusherBean_endpointResponds() throws NamingException {
         // When...

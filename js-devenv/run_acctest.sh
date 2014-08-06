82c47129b5e2a650e51a716cfb80d304b8a9baf0
Jan Bauer Nielsen <jbn@dbc.dk>
2014-08-06 11:23:04 +0000

    Added creation of js-revision.txt artifact.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@74750 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- js-devenv/run_acctest.sh
+++ js-devenv/run_acctest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+REPOS_DIR=svn.dbc.dk
 TEST_SUITE=`readlink -f $1`
 
 echo "Running test suite $TEST_SUITE"
@@ -16,5 +17,7 @@ suite_test $TEST_SUITE -c --verbose --pool-size 1
 deactivate
 
 cd ..
+
+svn info $REPOS_DIR/datawell-convert/ | grep 'Last Changed Rev' | awk '{ print $4; }' > js-revision.txt
 
 exit 0

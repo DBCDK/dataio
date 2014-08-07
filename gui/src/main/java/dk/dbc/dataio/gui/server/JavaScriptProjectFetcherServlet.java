86aed6f05fa1be7fbe172cb2f81463750ada94a1
Jesper Damkjær <jda@dbc.dk>
2014-08-07 10:15:19 +0000

    [17816] Added hack for setting a single revision number form svn in case a specific jndi-property is set. It is ment to be used temporarily in production.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@74772 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/server/JavaScriptProjectFetcherServlet.java
+++ gui/src/main/java/dk/dbc/dataio/gui/server/JavaScriptProjectFetcherServlet.java
@@ -9,6 +9,13 @@ import dk.dbc.dataio.gui.client.proxies.JavaScriptProjectFetcher;
 import javax.servlet.ServletException;
 import java.util.List;
 
+import dk.dbc.dataio.commons.utils.service.ServiceUtil; // To be removed when hack is removed
+import dk.dbc.dataio.gui.client.exceptions.JavaScriptProjectFetcherError; // To be removed when hack is removed
+import java.util.ArrayList; // To be removed when hack is removed
+import java.util.Collections; // To be removed when hack is removed
+import java.util.Date; // To be removed when hack is removed
+import javax.naming.NamingException; // To be removed when hack is removed
+
 public class JavaScriptProjectFetcherServlet extends RemoteServiceServlet implements JavaScriptProjectFetcher {
     private static final long serialVersionUID = 6080138003266070829L;
     private transient JavaScriptProjectFetcher javaScriptProjectFetcher;
@@ -22,6 +29,10 @@ public class JavaScriptProjectFetcherServlet extends RemoteServiceServlet implem
 
     @Override
     public List<RevisionInfo> fetchRevisions(String projectName) throws JavaScriptProjectFetcherException {
+        List<RevisionInfo> hackedRevisionList = deployHackForRetrievingASingleTestedRevision(); // To be removed when hack is removed
+        if (!hackedRevisionList.isEmpty()) { // To be removed when hack is removed
+            return hackedRevisionList; // To be removed when hack is removed
+        } // To be removed when hack is removed
         return javaScriptProjectFetcher.fetchRevisions(projectName);
     }
 
@@ -38,5 +49,20 @@ public class JavaScriptProjectFetcherServlet extends RemoteServiceServlet implem
     @Override
     public List<JavaScript> fetchRequiredJavaScript(String projectName, long revision, String javaScriptFileName, String javaScriptFunction) throws JavaScriptProjectFetcherException {
         return javaScriptProjectFetcher.fetchRequiredJavaScript(projectName, revision, javaScriptFileName, javaScriptFunction);
+    }
+
+    // Remove this method and the obsolete lines in fetchRevisions when hack is removed.
+    private List<RevisionInfo> deployHackForRetrievingASingleTestedRevision() throws JavaScriptProjectFetcherException {
+        List<RevisionInfo> hackedRevisionList = new ArrayList<RevisionInfo>();
+        try {
+            String revision = ServiceUtil.getStringValueFromResource("dataioGuiSubversionScmRevision");
+            hackedRevisionList.add(new RevisionInfo(Long.parseLong(revision), "", new Date(), "", Collections.EMPTY_LIST));
+        } catch(NumberFormatException e) {
+            // throw in case the retrivied revision can not be parsed as a long
+            throw new JavaScriptProjectFetcherException(JavaScriptProjectFetcherError.UNKNOWN, e);
+        } catch(NamingException e) {
+            // Ignoring this will let this method return an empty list.
+        }
+        return hackedRevisionList;
     }
 }

de5ab1a1bf687cd574923f5f24629c641ecc2616
Jesper Damkjær <jda@dbc.dk>
2014-08-19 20:23:58 +0000

    [17831] Added integration tests for updateSubmitter.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75116 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/SubmittersIT.java
+++ integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/SubmittersIT.java
@@ -8,6 +8,7 @@ import dk.dbc.dataio.commons.types.Submitter;
 import dk.dbc.dataio.commons.types.SubmitterContent;
 import dk.dbc.dataio.commons.types.rest.FlowStoreServiceConstants;
 import dk.dbc.dataio.commons.utils.httpclient.HttpClient;
+import dk.dbc.dataio.commons.utils.test.json.SubmitterContentJsonBuilder;
 import dk.dbc.dataio.commons.utils.test.model.SubmitterContentBuilder;
 import dk.dbc.dataio.integrationtest.ITUtil;
 import org.junit.After;
@@ -22,18 +23,26 @@ import java.sql.SQLException;
 import java.util.List;
 
 import static dk.dbc.dataio.integrationtest.ITUtil.clearAllDbTables;
+import static dk.dbc.dataio.integrationtest.ITUtil.createSubmitter;
 import static dk.dbc.dataio.integrationtest.ITUtil.newDbConnection;
+import java.util.HashMap;
+import java.util.Map;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Integration tests for the submitters collection part of the flow store service
  */
 public class SubmittersIT {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SubmittersIT.class);
+
     private static Client restClient;
     private static Connection dbConnection;
     private static String baseUrl;
@@ -55,7 +64,7 @@ public class SubmittersIT {
         clearAllDbTables(dbConnection);
     }
 
-    /**
+    /*
      * Given: a deployed flow-store service
      * When : valid JSON is POSTed to the submitters path without an identifier
      * Then : a submitter it created and returned
@@ -86,7 +95,7 @@ public class SubmittersIT {
     }
 
 
-    /**
+    /*
      * Given: a deployed flow-store service
      * When: JSON posted to the submitters path causes JsonException
      * Then: request returns with a BAD REQUEST http status code
@@ -100,7 +109,7 @@ public class SubmittersIT {
         assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.BAD_REQUEST.getStatusCode()));
     }
 
-    /**
+    /*
      * Given: a deployed flow-store service containing submitter resource
      * When : adding submitter with the same name
      * Then : assume that the exception thrown is of the type: FlowStoreServiceConnectorUnexpectedStatusCodeException
@@ -130,7 +139,7 @@ public class SubmittersIT {
         }
     }
 
-    /**
+   /*
     * Given: a deployed flow-store service containing submitter resource
     * When : adding submitter with the same number
     * Then : assume that the exception thrown is of the type: FlowStoreServiceConnectorUnexpectedStatusCodeException
@@ -160,7 +169,194 @@ public class SubmittersIT {
         }
     }
 
-    /**
+    /*
+     * Given: a deployed flow-store service
+     * And  : a valid submitter with given id is already stored
+     * When : valid JSON is POSTed to the submitters path with an identifier (update)
+     * Then : assert the correct fields have been set with the correct values
+     * And  : assert that the id of the submitter has not changed
+     * And  : assert that the version number has been updated
+     * And  : assert that updated data can be found in the underlying database and only one submitter exists
+     */
+    @Test
+    public void updateSubmitter_ok() throws Exception {
+
+        // Given...
+        final FlowStoreServiceConnector flowStoreServiceConnector = new FlowStoreServiceConnector(restClient, baseUrl);
+
+        // And...
+        final SubmitterContent submitterContent = new SubmitterContentBuilder().build();
+        Submitter submitter = flowStoreServiceConnector.createSubmitter(submitterContent);
+
+        // When...
+        final SubmitterContent newSubmitterContent = new SubmitterContentBuilder().setName("UpdatedSubmitterName").setNumber(43L).build();
+        Submitter updatedSubmitter = flowStoreServiceConnector.updateSubmitter(newSubmitterContent, submitter.getId(), submitter.getVersion());
+
+        // Then...
+        assertSubmitterNotNull(updatedSubmitter);
+        assertSubmitterContentEquals(updatedSubmitter.getContent(), newSubmitterContent);
+
+        // And...
+        assertThat(updatedSubmitter.getId(), is(submitter.getId()));
+
+        // And...
+        assertThat(updatedSubmitter.getVersion(), is(submitter.getVersion() + 1));
+
+        // And...
+        final List<Submitter> submitters = flowStoreServiceConnector.findAllSubmitters();
+        assertThat(submitters.size(), is(1));
+        assertSubmitterEquals(submitters.get(0), updatedSubmitter);
+    }
+
+    /*
+     * Given: a deployed flow-store service with a submitter
+     * When : JSON posted to the submitters path with update causes JsonException
+     * Then : request returns with a BAD REQUEST http status code
+     */
+    @Test
+    public void updateSubmitter_invalidJson_BadRequest() {
+        // Given ...
+        final long id = createSubmitter(restClient, baseUrl, new SubmitterContentJsonBuilder().build());
+
+        // Assume, that the very first created submitter has version number 1:
+        final Map<String, String> headers = new HashMap<>(1);
+        headers.put(FlowStoreServiceConstants.IF_MATCH_HEADER, "1");  // Set version=1
+        final Response response = HttpClient.doPostWithJson(restClient, headers, "<invalid json />", baseUrl,
+                FlowStoreServiceConstants.SUBMITTERS, Long.toString(id), "content");
+        // Then...
+        assertThat(response.getStatusInfo().getStatusCode(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+    }
+
+   /*
+    * Given: a deployed flow-store service
+    * When : valid JSON is POSTed to the submitters path with an identifier (update) and wrong id number
+    * Then : assume that the exception thrown is of the type: FlowStoreServiceConnectorUnexpectedStatusCodeException
+    * And  : request returns with a NOT_FOUND http status code
+    * And  : assert that no submitters exist in the underlying database
+    */
+    @Test
+    public void updateSubmitter_WrongIdNumber_NotFound() throws FlowStoreServiceConnectorException {
+        // Given...
+        final FlowStoreServiceConnector flowStoreServiceConnector = new FlowStoreServiceConnector(restClient, baseUrl);
+        try{
+            // When...
+            final SubmitterContent newSubmitterContent = new SubmitterContentBuilder().build();
+            flowStoreServiceConnector.updateSubmitter(newSubmitterContent, 1234, 1L);
+
+            fail("Wrong submitter Id was not detected as input to updateSubmitter().");
+            // Then...
+        }catch(FlowStoreServiceConnectorUnexpectedStatusCodeException e){
+
+            // And...
+            assertThat(e.getStatusCode(), is(Response.Status.NOT_FOUND.getStatusCode()));
+
+            // And...
+            final List<Submitter> submitters = flowStoreServiceConnector.findAllSubmitters();
+            assertNotNull(submitters);
+            assertThat(submitters.isEmpty(), is(true));
+        }
+    }
+
+    /*
+     * Given: a deployed flow-store service
+     * And  : Two valid submitters are already stored
+     * When : valid JSON is POSTed to the submitters path with an identifier (update) but with a submitter name that is already in use by another existing submitter
+     * Then : assume that the exception thrown is of the type: FlowStoreServiceConnectorUnexpectedStatusCodeException
+     * And  : request returns with a NOT_ACCEPTABLE http status code
+     * And  : assert that two submitters exists in the underlying database
+     * And  : updated data cannot be found in the underlying database
+     */
+    @Test
+    public void updateSubmitter_duplicateName_NotAcceptable() throws FlowStoreServiceConnectorException{
+        // Given...
+        final FlowStoreServiceConnector flowStoreServiceConnector = new FlowStoreServiceConnector(restClient, baseUrl);
+        final String FIRST_SUBMITTER_NAME = "FirstSubmitterName";
+        final String SECOND_SUBMITTER_NAME = "SecondSubmitterName";
+
+        try {
+            // And...
+            final SubmitterContent submitterContent1 = new SubmitterContentBuilder().setName(FIRST_SUBMITTER_NAME).setNumber(17L).build();
+            flowStoreServiceConnector.createSubmitter(submitterContent1);
+
+            final SubmitterContent submitterContent2 = new SubmitterContentBuilder().setName(SECOND_SUBMITTER_NAME).setNumber(31L).build();
+            Submitter submitter = flowStoreServiceConnector.createSubmitter(submitterContent2);
+
+            // When... (Attempting to save the second submitter created with the same name as the first submitter created)
+            flowStoreServiceConnector.updateSubmitter(submitterContent1, submitter.getId(), submitter.getVersion());
+
+            fail("Primary key violation was not detected as input to updateSubmitter().");
+        // Then...
+        }catch(FlowStoreServiceConnectorUnexpectedStatusCodeException e){
+
+            // And...
+            assertThat(e.getStatusCode(), is(Response.Status.NOT_ACCEPTABLE.getStatusCode()));
+
+            // And...
+            final List<Submitter> submitters = flowStoreServiceConnector.findAllSubmitters();
+            assertNotNull(submitters);
+            assertThat(submitters.size(), is(2));
+
+            // And...
+            assertThat(submitters.get(0).getContent().getName(), is (FIRST_SUBMITTER_NAME));
+            assertThat(submitters.get(1).getContent().getName(), is (SECOND_SUBMITTER_NAME));
+        }
+    }
+
+    /*
+     * Given: a deployed flow-store service
+     * And  : a valid submitter with given id is already stored and the submitter is opened for edit by two different users
+     * And  : the first user updates the submitter, valid JSON is POSTed to the submitters path with an identifier (update)
+     *        and correct version number
+     * When : the second user attempts to update the original version of the submitter, valid JSON is POSTed to the submitters
+     *        path with an identifier (update) and wrong version number
+
+     * Then : assume that the exception thrown is of the type: FlowStoreServiceConnectorUnexpectedStatusCodeException
+     * And  : request returns with a CONFLICT http status code
+     * And  : assert that only one submitter exists in the underlying database
+     * And  : assert that updated data from the first user can be found in the underlying database
+     * And  : assert that the version number has been updated only by the first user
+     */
+    @Test
+    public void updateSubmitter_WrongVersion_Conflict() throws FlowStoreServiceConnectorException {
+        // Given...
+        final FlowStoreServiceConnector flowStoreServiceConnector = new FlowStoreServiceConnector(restClient, baseUrl);
+        final String SUBMITTER_NAME_FROM_FIRST_USER = "UpdatedSubmitterNameFromFirstUser";
+        final String SUBMITTER_NAME_FROM_SECOND_USER = "UpdatedSubmitterNameFromSecondUser";
+        long version = -21;
+
+        try {
+            // And...
+            Submitter submitter = flowStoreServiceConnector.createSubmitter(new SubmitterContentBuilder().build());
+            version = submitter.getVersion(); // stored for use in catch-clause
+
+            // And... First user updates the submitter
+            SubmitterContent submitterContent1 = new SubmitterContentBuilder().setName(SUBMITTER_NAME_FROM_FIRST_USER).setNumber(98L).build();
+            flowStoreServiceConnector.updateSubmitter(submitterContent1, submitter.getId(), submitter.getVersion());
+
+            // When... Second user attempts to update the same submitter
+            SubmitterContent submitterContent2 = new SubmitterContentBuilder().setName(SUBMITTER_NAME_FROM_SECOND_USER).setNumber(99L).build();
+            flowStoreServiceConnector.updateSubmitter(submitterContent2, submitter.getId(), submitter.getVersion());
+
+            fail("Edit conflict, in the case of multiple updates, was not detected as input to updateSubmitter().");
+
+            // Then...
+        } catch (FlowStoreServiceConnectorUnexpectedStatusCodeException e) {
+
+            // And...
+            assertThat(e.getStatusCode(), is(Response.Status.CONFLICT.getStatusCode()));
+
+            // And...
+            final List<Submitter> submitters = flowStoreServiceConnector.findAllSubmitters();
+            assertNotNull(submitters);
+            assertThat(submitters.size(), is(1));
+            assertThat(submitters.get(0).getContent().getName(), is(SUBMITTER_NAME_FROM_FIRST_USER));
+
+            // And... Assert the version number has been updated after creation, but only by the first user.
+            assertThat(submitters.get(0).getVersion(), is(version + 1));
+        }
+    }
+
+    /*
      * Given: a deployed flow-store service containing no submitters
      * When: GETing submitters collection
      * Then: request returns with empty list
@@ -176,7 +372,7 @@ public class SubmittersIT {
         assertThat(submitters.size(), is(0));
     }
 
-    /**
+    /*
      * Given: a deployed flow-store service containing three submitters
      * When: GETing submitters collection
      * Then: request returns with 3 submitters
@@ -206,5 +402,25 @@ public class SubmittersIT {
         assertThat(listOfSubmitters.get(0).getContent().getNumber(), is (submitterSortsFirst.getContent().getNumber()));
         assertThat(listOfSubmitters.get(1).getContent().getNumber(), is (submitterSortsSecond.getContent().getNumber()));
         assertThat(listOfSubmitters.get(2).getContent().getNumber(), is (submitterSortsThird.getContent().getNumber()));
+    }
+
+    private void assertSubmitterNotNull(Submitter submitter) {
+        assertNotNull(submitter);
+        assertNotNull(submitter.getContent());
+        assertNotNull(submitter.getId());
+        assertNotNull(submitter.getVersion());
+
+    }
+
+    private void assertSubmitterContentEquals(SubmitterContent content1, SubmitterContent content2) {
+        assertThat(content1.getName(), is(content2.getName()));
+        assertThat(content1.getNumber(), is(content2.getNumber()));
+        assertThat(content1.getDescription(), is(content2.getDescription()));
+    }
+
+    private void assertSubmitterEquals(Submitter submitter1, Submitter submitter2) {
+        assertThat(submitter1.getId(), is(submitter2.getId()));
+        assertThat(submitter1.getVersion(), is(submitter2.getVersion()));
+        assertSubmitterContentEquals(submitter1.getContent(), submitter2.getContent());
     }
 }

6ccb468200304b3610ca1ab73580d609e788b9cf
Steen Lillethorup Frederiksen <slf@dbc.dk>
2014-08-20 13:48:29 +0000

    [17831] US #403: Redigering af submitter - Fixing PMD errors
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75164 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/submittermodify/Presenter.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/submittermodify/Presenter.java
@@ -3,9 +3,9 @@ package dk.dbc.dataio.gui.client.pages.submittermodify;
 import dk.dbc.dataio.gui.client.presenters.GenericPresenter;
 
 public interface Presenter extends GenericPresenter {
-    public void numberChanged(String number);
-    public void nameChanged(String name);
-    public void descriptionChanged(String description);
-    public void keyPressed();
-    public void saveButtonPressed();
+    void numberChanged(String number);
+    void nameChanged(String name);
+    void descriptionChanged(String description);
+    void keyPressed();
+    void saveButtonPressed();
 }

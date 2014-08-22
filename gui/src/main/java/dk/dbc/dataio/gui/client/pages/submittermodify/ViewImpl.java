05530b7db766ea8e987fc7d04a96ea0107b7a938
Steen Lillethorup Frederiksen <slf@dbc.dk>
2014-08-22 11:06:05 +0000

    [17831] US #403: Redigering af submitter - Refaktorering: Fjern brug af SaveButton komponenten i den nye Create Submitter View - som følge af reviewet med Jesper
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75242 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/submittermodify/ViewImpl.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/submittermodify/ViewImpl.java
@@ -2,9 +2,13 @@ package dk.dbc.dataio.gui.client.pages.submittermodify;
 
 import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
-import dk.dbc.dataio.gui.client.components.SaveButton;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
 import dk.dbc.dataio.gui.client.components.TextAreaEntry;
 import dk.dbc.dataio.gui.client.components.TextEntry;
 import dk.dbc.dataio.gui.client.views.ContentPanel;
@@ -21,12 +25,15 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
     public static final String GUIID_SUBMITTER_MODIFY_NAME_PANEL = "submittermodifynamepanel";
     public static final String GUIID_SUBMITTER_MODIFY_DESCRIPTION_PANEL = "submittermodifydescriptionpanel";
     public static final String GUIID_SUBMITTER_MODIFY_SAVE_BUTTON_PANEL = "submittermodifysavebuttonpanel";
+    public static final String CLASS_FLOW_ELEMENT = "dio-flow-element";
     private static final int SUBMITTER_MODIFY_DESCRIPTION_MAX_LENGTH = 160;
 
-    private final TextEntry submitterNumberPanel;
-    private final TextEntry submitterNamePanel;
-    private final TextAreaEntry submitterDescriptionPanel;
-    private final SaveButton saveButton;
+    private final TextEntry numberPanel;
+    private final TextEntry namePanel;
+    private final TextAreaEntry descriptionPanel;
+    private final FlowPanel buttonPanel;
+    private final Label statusText;
+    private final Button saveButton;
 
 
 
@@ -35,10 +42,12 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     public ViewImpl(String header, SubmitterModifyConstants constants) {
         super(header);
-        submitterNumberPanel = new TextEntry(GUIID_SUBMITTER_MODIFY_NUMBER_PANEL, constants.label_SubmitterNumber());
-        submitterNamePanel = new TextEntry(GUIID_SUBMITTER_MODIFY_NAME_PANEL, constants.label_SubmitterName());
-        submitterDescriptionPanel = new TextAreaEntry(GUIID_SUBMITTER_MODIFY_DESCRIPTION_PANEL, constants.label_Description(), SUBMITTER_MODIFY_DESCRIPTION_MAX_LENGTH);
-        saveButton = new SaveButton(GUIID_SUBMITTER_MODIFY_SAVE_BUTTON_PANEL, constants.button_Save(), new SaveButtonEvent());
+        numberPanel = new TextEntry(GUIID_SUBMITTER_MODIFY_NUMBER_PANEL, constants.label_SubmitterNumber());
+        namePanel = new TextEntry(GUIID_SUBMITTER_MODIFY_NAME_PANEL, constants.label_SubmitterName());
+        descriptionPanel = new TextAreaEntry(GUIID_SUBMITTER_MODIFY_DESCRIPTION_PANEL, constants.label_Description(), SUBMITTER_MODIFY_DESCRIPTION_MAX_LENGTH);
+        buttonPanel = new FlowPanel();
+        statusText = new Label("");
+        saveButton = new Button(constants.button_Save());
     }
 
     /**
@@ -46,20 +55,27 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     public void init() {
         getElement().setId(GUIID_SUBMITTER_MODIFY_WIDGET);
+        statusText.addStyleName(CLASS_FLOW_ELEMENT);
+        saveButton.getElement().setId(GUIID_SUBMITTER_MODIFY_SAVE_BUTTON_PANEL);
 
         InputFieldKeyDownHandler inputFieldKeyDownHandler = new InputFieldKeyDownHandler();
-        submitterNumberPanel.addKeyDownHandler(inputFieldKeyDownHandler);
-        submitterNamePanel.addKeyDownHandler(inputFieldKeyDownHandler);
-        submitterDescriptionPanel.addKeyDownHandler(inputFieldKeyDownHandler);
+        numberPanel.addKeyDownHandler(inputFieldKeyDownHandler);
+        namePanel.addKeyDownHandler(inputFieldKeyDownHandler);
+        descriptionPanel.addKeyDownHandler(inputFieldKeyDownHandler);
 
-        submitterNumberPanel.addBlurHandler(new SubmitterNumberBlurHandler());
-        submitterNamePanel.addBlurHandler(new SubmitterNameBlurHandler());
-        submitterDescriptionPanel.addBlurHandler(new SubmitterDescriptionBlurHandler());
+        numberPanel.addBlurHandler(new SubmitterNumberBlurHandler());
+        namePanel.addBlurHandler(new SubmitterNameBlurHandler());
+        descriptionPanel.addBlurHandler(new SubmitterDescriptionBlurHandler());
 
-        add(submitterNumberPanel);
-        add(submitterNamePanel);
-        add(submitterDescriptionPanel);
-        add(saveButton);
+        saveButton.addClickHandler(new SaveButtonHandler());
+
+        add(numberPanel);
+        add(namePanel);
+        add(descriptionPanel);
+        add(statusText);
+        buttonPanel.add(statusText);
+        buttonPanel.add(saveButton);
+        add(buttonPanel);
     }
 
 
@@ -75,7 +91,7 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     @Override
     public void setStatusText(String message) {
-        saveButton.setStatusText(message);
+        statusText.setText(message);
     }
 
     /**
@@ -84,7 +100,7 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     @Override
     public void setNumber(String number) {
-        submitterNumberPanel.setText(number);
+        numberPanel.setText(number);
     }
 
     /**
@@ -93,7 +109,7 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     @Override
     public String getNumber() {
-        return submitterNumberPanel.getText();
+        return numberPanel.getText();
     }
 
     /**
@@ -102,7 +118,7 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     @Override
     public void setName(String name) {
-        submitterNamePanel.setText(name);
+        namePanel.setText(name);
     }
 
     /**
@@ -111,7 +127,7 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     @Override
     public String getName() {
-        return submitterNamePanel.getText();
+        return namePanel.getText();
     }
 
     /**
@@ -120,7 +136,7 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     @Override
     public void setDescription(String description) {
-        submitterDescriptionPanel.setText(description);
+        descriptionPanel.setText(description);
     }
 
     /**
@@ -129,7 +145,7 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      */
     @Override
     public String getDescription() {
-        return submitterDescriptionPanel.getText();
+        return descriptionPanel.getText();
     }
 
 
@@ -139,9 +155,10 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
      * Direction: View -> Presenter
      */
 
-    private class SaveButtonEvent implements SaveButton.ButtonEvent {
+
+    private class SaveButtonHandler implements ClickHandler {
         @Override
-        public void buttonPressed() {
+        public void onClick(ClickEvent event) {
             presenter.saveButtonPressed();
         }
     }
@@ -156,21 +173,21 @@ public class ViewImpl extends ContentPanel<Presenter>  implements View {
     private class SubmitterNumberBlurHandler implements BlurHandler {
         @Override
         public void onBlur(BlurEvent blurEvent) {
-            presenter.numberChanged(submitterNumberPanel.getText());
+            presenter.numberChanged(numberPanel.getText());
         }
     }
 
     private class SubmitterNameBlurHandler implements BlurHandler {
         @Override
         public void onBlur(BlurEvent blurEvent) {
-            presenter.nameChanged(submitterNamePanel.getText());
+            presenter.nameChanged(namePanel.getText());
         }
     }
 
     private class SubmitterDescriptionBlurHandler implements BlurHandler {
         @Override
         public void onBlur(BlurEvent blurEvent) {
-            presenter.descriptionChanged(submitterDescriptionPanel.getText());
+            presenter.descriptionChanged(descriptionPanel.getText());
         }
     }
 

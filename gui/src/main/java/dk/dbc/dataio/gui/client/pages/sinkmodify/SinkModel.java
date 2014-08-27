9ec5089d45158d899a467086b54d5910da70d609
Stine Mia Madsen <sma@dbc.dk>
2014-08-27 23:09:46 +0000

    [17863] US #411 : Refaktorering af sinkCreateEdit - sinkModify: Tilføjelse af SinkModel og SinkModelMapper
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75505 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/sinkmodify/SinkModel.java
@@ -0,0 +1,59 @@
+package dk.dbc.dataio.gui.client.pages.sinkmodify;
+
+import dk.dbc.dataio.gui.client.model.GenericBackendModel;
+
+public class SinkModel extends GenericBackendModel {
+
+    private String sinkName;
+    private String resource;
+
+    public SinkModel(long id, long version, String name, String resource) {
+        super(id, version);
+        this.sinkName = name;
+        this.resource = resource;
+    }
+
+    public SinkModel() {
+        super(0L, 0L);
+        this.sinkName = "";
+        this.resource = "";
+    }
+
+    /**
+     * @return resourceName
+     */
+    public String getResourceName() {
+        return resource;
+    }
+
+    /**
+     * Set resource name
+     * @param resourceName
+     */
+    public void setResourceName(String resourceName) {
+        this.resource = resourceName;
+    }
+
+    /**
+     * @return sinkName;
+     */
+    public String getSinkName() {
+        return sinkName;
+    }
+
+    /**
+     * Set sink name
+     * @param sinkName
+     */
+    public void setSinkName(String sinkName) {
+        this.sinkName = sinkName;
+    }
+
+    /**
+     * Checks for empty String values
+     */
+    public boolean isInputFieldsEmpty() {
+        return sinkName.isEmpty() || resource.isEmpty();
+    }
+
+}
---
+++ gui/src/main/java/dk/dbc/dataio/gui/server/ModelMappers/SinkModelMapper.java
@@ -0,0 +1,43 @@
+package dk.dbc.dataio.gui.server.ModelMappers;
+
+
+import dk.dbc.dataio.commons.types.Sink;
+import dk.dbc.dataio.commons.types.SinkContent;
+import dk.dbc.dataio.gui.client.pages.sinkmodify.SinkModel;
+
+public class SinkModelMapper {
+    /**
+     * Private Constructor prevents instantiation of this static class
+     */
+    private SinkModelMapper (){}
+
+    /**
+     * Maps a Sink to a SinkModel
+     * @param sink
+     * @return model
+     */
+    public static SinkModel toModel(Sink sink){
+        return new SinkModel(
+                sink.getId(),
+                sink.getVersion(),
+                sink.getContent().getName(),
+                sink.getContent().getResource());
+    }
+
+    /**
+     * Maps a model to submitter content
+     * @param model
+     * @return submitterContent
+     * @throws IllegalArgumentException
+     */
+    public static SinkContent toSinkContent(SinkModel model) throws IllegalArgumentException {
+
+        if(model.isInputFieldsEmpty()) {
+            throw new IllegalArgumentException("model.name, model.resource cannot be empty");
+        }
+
+        return new SinkContent(
+            model.getSinkName(),
+            model.getResourceName());
+    }
+}

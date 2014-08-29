c7b9b16490ca64166923773fad88baa2114c9c5f
Stine Mia Madsen <sma@dbc.dk>
2014-08-29 09:15:51 +0000

    [17831] US #403: Redigering af submitter - Implementering af jan's review kommentarer
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75580 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/test/java/dk/dbc/dataio/gui/client/pages/submittermodify/PresenterEditImplTest.java
+++ gui/src/test/java/dk/dbc/dataio/gui/client/pages/submittermodify/PresenterEditImplTest.java
@@ -66,8 +66,6 @@ public class PresenterEditImplTest {
         verify(mockedEditPlace, times(1)).getSubmitterId();
         // The instantiation of presenterEditImpl instantiates the "Edit version" of the presenter - and the basic test has been done in the test of PresenterImpl
         // Therefore, we only intend to test the Edit specific stuff, which basically is to assert, that the view attribute has been initialized correctly
-        // Since we cannot access the protected view attribute in presenterEditImpl, we can only test, that no exceptions are thrown
-        // We must therefore rely on the following tests to assure, that view has been initialized correctly
     }
 
     @Test

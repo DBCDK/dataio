c216444fbd4dae1407ecfb24e544c39453b29068
Jan Bauer Nielsen <jbn@dbc.dk>
2014-08-29 06:18:37 +0000

    Replaced existing harvester IT with simple deploy test.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75563 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ integration-test/harvesters/src/test/java/dk/dbc/dataio/harvester/HarvestersDeployIT.java
@@ -0,0 +1,10 @@
+package dk.dbc.dataio.harvester;
+
+import org.junit.Test;
+
+public class HarvestersDeployIT {
+    @Test
+    public void testDeploy() {
+        // intentionally left empty - cargo plugin handles actual deploy
+    }
+}

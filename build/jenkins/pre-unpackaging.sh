643fc5918350fbf885553c4835ffde0b871dc429
Jesper Damkjær <jda@dbc.dk>
2014-09-06 14:37:22 +0000

    Added pre-unpackagaing script
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75891 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ build/jenkins/pre-unpackaging.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function die() {
+    echo "Error:" "$@"
+    exit 1
+}
+
+# Detect environment: jenkins/local
+
+# Set constants according to environment
+
+echo "Copy m2.tar.gz from last successful build:"
+time curl -sO http://is.dbc.dk/job/dataio/lastStableBuild/artifact/m2.tar.gz || die "Could not download m2.tar.gz from last successfull build"
+
+echo "Unpackage .m2 from tar-file:"
+time tar zxf m2.tar.gz || die "Could not un tar-package m2.tar.gz"
+
+echo "Delete dk-jars from local .m2"
+rm -rf .m2/repository/dk/ || die "Could not delete dk-jars from local .m2"

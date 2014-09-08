59c1d97f84f58c70b8b2405fe2a9c7c6f00f953a
Jesper Damkjær <jda@dbc.dk>
2014-09-08 13:48:45 +0000

    Created a script for handling all stages of building/testing on jenkins
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@75950 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ build/jenkins/build.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+export IS_THIS_JENKINS=1 # Assume we are on Jenkins as default
+
+# Function for exiting gracefully - kind of.
+function die() {
+    echo "Error:" "$@"
+    exit 1
+}
+
+# Function to detect wether we operate on jenkins or in a local environment
+function detectEnvironment() {
+   if [[ -z "$WORKSPACE" && -z "$BUILD_NUMBER" ]]; then
+       IS_THIS_JENKINS=0;
+   fi
+}
+
+
+function setUpLocalEnvironment() {
+    if [ $IS_THIS_JENKINS -eq 0 ]; then
+	CURRENT_DIR=$PWD
+    else
+	CURRENT_DIR=$WORKSPACE
+    fi
+}
+
+
+# Function for creating a String for building and installing maven packages 
+# to local .m2, without running the integration-tests.
+function createMavenPackageInstallString() {
+    # To be used when there is a separate performance-test:
+    # MVN_CMD="-Dskip.install=false -Dmaven.repo.local=$CURRENT_DIR/.m2/repository -Dwebdriver.firefox.bin=$CURRENT_DIR/integration-test/firefox/linux_x86_64/firefox -P -integration-test install"
+
+    # For use while there are no separate performance-test:
+    MVN_CMD="clean deploy  pmd:pmd findbugs:findbugs javadoc:aggregate -Dwebdriver.firefox.bin=$CURRENT_DIR/integration-test/firefox/linux_x86_64/firefox -Dskip.install=false -Dmaven.repo.local=$CURRENT_DIR/.m2/repository"
+    
+    echo $MVN_CMD
+}
+
+function getIntegrationTestsInCommaSeparatedString() {
+    INTEGRATION_TEST_POMS=`find integration-test/ |grep pom.xml | egrep -v "\.svn|glassfish" | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/'`
+}
+
+# Function for creating a String for running only integration-tests in offline mode
+# using packages in local .m2, without running the build/unittest/package steps of maven.
+function createMavenIntegrationTestString() {
+    getIntegrationTestsInCommaSeparatedString
+    # cannot run offline - missing jars for failsafe-plugin
+    MVN_CMD="-Dskip.install=false -Dmaven.repo.local=$CURRENT_DIR/.m2/repository -Dwebdriver.firefox.bin=$CURRENT_DIR/integration-test/firefox/linux_x86_64/firefox -P integration-test -pl $INTEGRATION_TEST_POMS  verify" 
+    
+    echo $MVN_CMD
+}
+
+# todo: Missing doc
+function createMavenPerformanceTestParameters() {
+    MVN_CMD="-Dmaven.repo.local=$CURRENT_DIR/.m2/repository -P perftest -pl performance-test -o verify" 
+    
+    echo $MVN_CMD
+}
+
+detectEnvironment
+setUpLocalEnvironment
+
+function getBuildNumberOfLastStableDataIOBuild() {
+    wget http://is.dbc.dk/job/dataio/lastStableBuild/buildNumber -O lastStableDataIOBuildNumber.txt
+    LAST_STABLE_DATAIO_BUILD_NUMBER=`cat lastStableDataIOBuildNumber.txt`
+    echo "Last stable: $LAST_STABLE_DATAIO_BUILD_NUMBER"
+}
+
+function copyM2TarFromLastStableBuild() {
+    echo "Copy m2.tar.gz from last successful build:"
+    time wget http://is.dbc.dk/job/dataio/$LAST_STABLE_DATAIO_BUILD_NUMBER/artifact/m2.tar.gz || die "Could not download m2.tar.gz from last successfull build"
+}
+
+function unpackageM2() {
+    echo "Unpackage .m2 from tar-file:"
+    time tar zxf m2.tar.gz || die "Could not un tar-package m2.tar.gz"
+}
+
+function deleteDBCJarsFromM2() {
+    echo "Delete dk-jars from local .m2"
+    rm -rf .m2/repository/dk/ || die "Could not delete dk-jars from local .m2"
+}
+
+function packageM2() {
+    touch .m2 # temporary in order to ensure that .m2 exists.
+    echo "Packaging .m2 into tar-file:"
+    time tar zcf m2.tar.gz .m2 || die "Could not tar-package .m2"
+}
+
+function packageCompiledTrunk() {
+    echo "Packaging all but .m2 into tar file:"
+    touch trunk.tar.gz # touch in order to aviod warning about changed directory from 'tar'
+    time tar zcf trunk.tar.gz . --exclude ".m2" --exclude "m2.tar.gz" --exclude "trunk.tar.gz" || die "Could not package trunk into tar-package"
+}
+
+function buildPreStep() {
+    getBuildNumberOfLastStableDataIOBuild
+    copyM2TarFromLastStableBuild
+    unpackageM2
+    deleteDBCJarsFromM2
+    rm lastStableDataIOBuildNumber.txt
+    rm m2.tar.gz
+}
+
+function buildPostStep() {
+    packageM2
+    packageCompiledTrunk
+}
+
+function testingPreStep() {
+    getBuildNumberOfLastStableDataIOBuild
+    copyM2TarFromLastStableBuild
+    unpackageM2
+}
+
+function validateAndGetParameterString() {
+    legal_parameters="build integrationtest performancetest"
+    [[ $legal_parameters =~ $1 ]] && echo "" || echo "$1 is an illegal parameter. Legal parameters are : $legal_parameters"
+
+    case $1 in
+	build)
+	    createMavenPackageInstallString
+	    ;;
+	integrationtest)
+	    createMavenIntegrationTestString
+	    ;;
+	performancetest)
+	    createMavenPerformanceTestParameters
+	    ;;
+    esac
+}
+
+function validateAndPerformState() {
+    legal_parameters="pre-build post-build pre-integrationtest pre-performancetest"
+    [[ $legal_parameters =~ $1 ]] && echo "" || echo "$1 is an illegal parameter. Legal parameters are : $legal_parameters"
+
+    case $1 in
+	pre-build)
+	    buildPreStep
+	    ;;
+	post-build)
+	    buildPostStep
+	    ;;
+    esac
+}
+
+while getopts ":p:s:" optname
+do
+    case "$optname" in
+	"p")
+	    # Retriving a parameter string for $OPTARG
+	    validateAndGetParameterString $OPTARG
+	    ;;
+	"s")
+	    validateAndPerformState $OPTARG
+	    ;;
+	":")
+	    echo "No argument value for $OPTARG"
+	    ;;
+	"?")
+	    echo "Unknown option $OPTARG"
+	    ;;
+	*)
+	    echo "This should not happen"
+	    ;;
+    esac
+done
+
+# --getMavenParams build / integrationtest / performancetest
+
+# get version of last stable dataio build
+# copy .m2 from last stable dataio (from version)
+# 

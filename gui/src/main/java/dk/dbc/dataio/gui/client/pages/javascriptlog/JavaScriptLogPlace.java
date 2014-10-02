6f18f2060b31e083c0c72e3f9b61e2bde0ee9b5e
Stine Mia Madsen <sma@dbc.dk>
2014-10-02 10:33:23 +0000

    [17942] US #287: Logning af javascriptbehandling - Implemented review comments slf
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@77172 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/javascriptlog/JavaScriptLogPlace.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/javascriptlog/JavaScriptLogPlace.java
@@ -6,25 +6,46 @@ import com.google.gwt.place.shared.Prefix;
 
 public class JavaScriptLogPlace extends Place {
 
-    private String combinedUrl;
+    private static String combinedUrl;
 
     public JavaScriptLogPlace(String url) {
         this.combinedUrl = url;
     }
-    public JavaScriptLogPlace(Long jobId, Long chunkId, Long failedItemId) {
+    public JavaScriptLogPlace(long jobId, long chunkId, long failedItemId) {
         this(combineToUrl(jobId, chunkId, failedItemId));
     }
 
+   /*
+    Getter methods:
+    A general exception is thrown if the input is not as expected.
+    The reason for this is, that a user can type directly in the url. As a result, we do not have complete
+    control of the input values given to the place.
+    If input is flawed and an exception is thrown, the default value returned is: 0
+    A zero value will result in the gui correctly displaying the error message: "Javascript log kunne ikke hentes."
+    */
+
     public Long getJobId() {
-        return Long.valueOf(combinedUrl.split(":")[0]);
+        try {
+            return Long.valueOf(combinedUrl.split(":")[0]);
+        } catch (Exception e) {
+            return 0L;
+        }
     }
 
     public Long getChunkId() {
-        return Long.valueOf(combinedUrl.split(":")[1]);
+        try {
+            return Long.valueOf(combinedUrl.split(":")[1]);
+        } catch (Exception e) {
+            return 0L;
+        }
     }
 
     public Long getFailedItemId() {
-        return Long.valueOf(combinedUrl.split(":")[2]);
+        try {
+            return Long.valueOf(combinedUrl.split(":")[2]);
+        } catch (Exception e) {
+            return 0L;
+        }
     }
 
     private static String combineToUrl(Long jobId, Long chunkId, Long failedItemId) {
@@ -35,7 +56,7 @@ public class JavaScriptLogPlace extends Place {
     public static class Tokenizer implements PlaceTokenizer<JavaScriptLogPlace> {
         @Override
         public String getToken(JavaScriptLogPlace place) {
-            return String.valueOf(place.getFailedItemId());
+            return String.valueOf(combinedUrl);
         }
         @Override
         public JavaScriptLogPlace getPlace(String token) {

2fbc22276b8c23e1b6e7318318748044b394d1a7
Jesper Damkjær <jda@dbc.dk>
2014-10-02 12:49:56 +0000

    [17942] Implemented functionality for getChunkCompletionState and getJobCompletionState - tests for getJobCompletionState is lacking.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@77196 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/fsjobstore/JobCompletionStateFinder.java
@@ -0,0 +1,157 @@
+package dk.dbc.dataio.jobstore.fsjobstore;
+
+import dk.dbc.dataio.commons.types.AbstractChunk;
+import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ChunkCompletionState;
+import dk.dbc.dataio.commons.types.ChunkItem;
+import dk.dbc.dataio.commons.types.ChunkResult;
+import dk.dbc.dataio.commons.types.Constants;
+import dk.dbc.dataio.commons.types.ItemCompletionState;
+import dk.dbc.dataio.commons.types.JobCompletionState;
+import dk.dbc.dataio.commons.types.SinkChunkResult;
+import dk.dbc.dataio.jobstore.JobStore;
+import dk.dbc.dataio.jobstore.types.JobStoreException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class JobCompletionStateFinder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JobCompletionStateFinder.class);
+
+    private final JobStore jobStore;
+
+    public JobCompletionStateFinder(JobStore jobStore) {
+        this.jobStore = jobStore;
+    }
+
+    public JobCompletionState getJobCompletionState(long jobId) throws JobStoreException {
+        List<ChunkCompletionState> chunkCompletionStates = getAllChunkCompletionStatesForJob(jobId);
+        JobCompletionState jobCompletionState = new JobCompletionState(jobId, chunkCompletionStates);
+        return jobCompletionState;
+    }
+
+    public ChunkCompletionState getChunkCompletionState(long jobId, long chunkId) throws JobStoreException {
+        Chunk chunkifiedChunk = jobStore.getChunk(jobId, chunkId);
+        ChunkResult processedChunk = jobStore.getProcessorResult(jobId, chunkId);
+        SinkChunkResult deliveredChunk = jobStore.getSinkResult(jobId, chunkId);
+
+        // There must be a chunkifiedChunk in order to detect the size of the chunks
+        // It is assumed that all chunks have equal size.
+        // If no chunkifiedChunk is found an exception is thrown.
+        // todo: Should it be a NoSuchChunkException?
+        if(chunkifiedChunk == null) {
+            String message = String.format("No chunk with id: [%d] found in job: [%d]", chunkId, jobId);
+            LOGGER.info(message);
+            throw new JobStoreException(message);
+        }
+
+        int chunkSize = chunkifiedChunk.getItems().size(); // this size must hold for all chunks!
+
+        // if processedChunk is null, throw an exception
+        // - this should not happen, since there is both a
+        // chunkifiedChunk and a deliveredChunk.
+        if(processedChunk == null && deliveredChunk != null) {
+            // this is wrong
+            String message = String.format("No process chunk found for [%d, %d], even though both a chunkified chunk and a sink chunk exists.",
+                    jobId, chunkId);
+            LOGGER.info(message);
+            throw new IllegalStateException(message);
+        }
+        
+        // Check if the processedChunk chunk size differes from the chunkified chunk:
+        if(processedChunk != null && processedChunk.getItems().size() != chunkSize) {
+            String message = String.format("The chunkified chunk and the delivered chunk differ in size: [%d / %d] for chunkId [%d] in jobId [%d]",
+                    chunkifiedChunk.getItems().size(), deliveredChunk.getItems().size(), chunkId, jobId);
+            LOGGER.info(message);
+            throw new IllegalStateException(message);
+        }
+        // Check if the delivered chunk size differes from the chunkified chunk:
+        if(deliveredChunk != null && deliveredChunk.getItems().size() != chunkSize) {
+            String message = String.format("The chunkified chunk and the delivered chunk differ in size: [%d / %d] for chunkId [%d] in jobId [%d]",
+                    chunkifiedChunk.getItems().size(), deliveredChunk.getItems().size(), chunkId, jobId);
+            LOGGER.info(message);
+            throw new IllegalStateException(message);
+        }
+
+        long[] itemIds = getItemIdsFromChunk(chunkifiedChunk);
+        if(!validateItemIds(itemIds)) {
+            throw new IllegalStateException("The ItemIds in chunkifiedChunks are not valid");
+        }
+
+        List<ItemCompletionState> itemCompletionStates = new ArrayList<>();
+        for(long id : itemIds) {
+            ItemCompletionState.State chunkifiedStatus = convertChunkItemStatusToItemCompletionStatus(getItem(chunkifiedChunk, id).getStatus());
+            ItemCompletionState.State processStatus = processedChunk != null ? convertChunkItemStatusToItemCompletionStatus(getItem(processedChunk, id).getStatus()) : ItemCompletionState.State.INCOMPLETE;
+            ItemCompletionState.State deliveryStatus = deliveredChunk != null ? convertChunkItemStatusToItemCompletionStatus(getItem(deliveredChunk, id).getStatus()) : ItemCompletionState.State.INCOMPLETE;
+            
+            itemCompletionStates.add(new ItemCompletionState(id, chunkifiedStatus, processStatus, deliveryStatus));
+        }
+        return new ChunkCompletionState(chunkId, itemCompletionStates);
+    }
+
+    private ChunkItem getItem(AbstractChunk chunk, long id) {
+        for (ChunkItem item : chunk.getItems()) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+    
+    private long[] getItemIdsFromChunk(AbstractChunk chunk) {
+        long[] ids = new long[chunk.getItems().size()];
+        int idx = 0;
+        for(ChunkItem item : chunk.getItems()) {
+            ids[idx++] = item.getId();
+        }
+        Arrays.sort(ids);
+        return ids;
+    }
+
+    private boolean validateItemIds(long[] ids) {
+        long id = ids[0];
+        for(int i=1;i<ids.length;i++) {
+            if(ids[i] != id+1)
+                return false;
+            id++;
+        }
+        return true;
+    }
+
+    private ItemCompletionState.State convertChunkItemStatusToItemCompletionStatus(ChunkItem.Status status) throws JobStoreException {
+        final ItemCompletionState.State completionState;
+        switch(status) {
+            case FAILURE:
+                completionState = ItemCompletionState.State.FAILURE;
+                break;
+            case IGNORE:
+                completionState = ItemCompletionState.State.IGNORED;
+                break;
+            case SUCCESS:
+                completionState = ItemCompletionState.State.SUCCESS;
+                break;
+            default:
+                throw new JobStoreException("Unhandled state: " + status);
+        }
+        return completionState;
+    }
+
+    private List<ChunkCompletionState> getAllChunkCompletionStatesForJob(long jobId) throws JobStoreException {
+        long numberOfChunksInJob = jobStore.getNumberOfChunksInJob(jobId);
+        List<ChunkCompletionState> chunkCompletionStates = new ArrayList<>((int) numberOfChunksInJob);
+        for(long i=0L, chunkId = Constants.CHUNK_ID_LOWER_BOUND+1; i<numberOfChunksInJob; i++, chunkId++) {
+            ChunkCompletionState chunkCompletionState;
+            try {
+                chunkCompletionState = getChunkCompletionState(jobId, chunkId);
+                chunkCompletionStates.add(chunkCompletionState);
+            } catch(JobStoreException ex) {
+                LOGGER.debug("Could not find chunk: {} for job: {}", chunkId, jobId);
+            }
+        }
+        return chunkCompletionStates;
+    }
+
+
+}
---
+++ job-store-service/war/src/test/java/dk/dbc/dataio/jobstore/fsjobstore/JobCompletionStateFinderTest.java
@@ -0,0 +1,336 @@
+package dk.dbc.dataio.jobstore.fsjobstore;
+
+import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ChunkCompletionState;
+import dk.dbc.dataio.commons.types.ChunkItem;
+import dk.dbc.dataio.commons.types.ChunkResult;
+import dk.dbc.dataio.commons.types.ItemCompletionState;
+import dk.dbc.dataio.commons.types.SinkChunkResult;
+import dk.dbc.dataio.commons.utils.test.model.ChunkBuilder;
+import dk.dbc.dataio.commons.utils.test.model.ChunkResultBuilder;
+import dk.dbc.dataio.commons.utils.test.model.SinkChunkResultBuilder;
+import dk.dbc.dataio.jobstore.JobStore;
+import dk.dbc.dataio.jobstore.types.JobStoreException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+
+public class JobCompletionStateFinderTest {
+
+    private JobStore jobStore;
+    
+    @Before
+    public void setup() {
+        jobStore = mock(FileSystemJobStore.class);
+    }
+
+    @Test(expected = JobStoreException.class)
+    public void chunkCompletionState_noAvailableChunksForChunkId_throws() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        Mockito.when(jobStore.getChunk(jobId, chunkId)).thenReturn(null);
+        Mockito.when(jobStore.getProcessorResult(jobId, chunkId)).thenReturn(null);
+        Mockito.when(jobStore.getSinkResult(jobId, chunkId)).thenReturn(null);
+        jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void chunkCompletionState_sinkItemsAndChunkItemsDifferInSize_throws() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        List<ChunkItem> chunkifiedChunkItems = new ChunkItemStatusListBuilder().setItems(ChunkItem.Status.SUCCESS, 1).build();
+        List<ChunkItem> deliveredChunkItems = new ChunkItemStatusListBuilder().setItems(ChunkItem.Status.SUCCESS, 2).build();
+        Chunk chunkifiedChunk = new ChunkBuilder().setJobId(jobId).setChunkId(chunkId).setItems(chunkifiedChunkItems).build();
+        SinkChunkResult deliveredChunk = new SinkChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(deliveredChunkItems).build();
+
+        Mockito.when(jobStore.getChunk(jobId, chunkId)).thenReturn(chunkifiedChunk);
+        Mockito.when(jobStore.getSinkResult(jobId, chunkId)).thenReturn(deliveredChunk);
+
+        jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void chunkCompletionState_processorItemsAndChunkItemsDifferInSize_throws() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        List<ChunkItem> chunkifiedChunkItems = new ChunkItemStatusListBuilder().setItems(ChunkItem.Status.SUCCESS, 1).build();
+        List<ChunkItem> processedChunkItems = new ChunkItemStatusListBuilder().setItems(ChunkItem.Status.SUCCESS, 2).build();
+        List<ChunkItem> deliveredChunkItems = new ChunkItemStatusListBuilder().setItems(ChunkItem.Status.SUCCESS, 1).build();
+        Chunk chunkifiedChunk = new ChunkBuilder().setJobId(jobId).setChunkId(chunkId).setItems(chunkifiedChunkItems).build();
+        ChunkResult processedChunk = new ChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(processedChunkItems).build();
+        SinkChunkResult deliveredChunk = new SinkChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(deliveredChunkItems).build();
+
+        Mockito.when(jobStore.getChunk(jobId, chunkId)).thenReturn(chunkifiedChunk);
+        Mockito.when(jobStore.getProcessorResult(jobId, chunkId)).thenReturn(processedChunk);
+        Mockito.when(jobStore.getSinkResult(jobId, chunkId)).thenReturn(deliveredChunk);
+
+        jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void chunkCompletionState_chunkifyChunkAndSinkChunkExistsButProcessChunkIsMissing_throws() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        List<ChunkItem> chunkifiedChunkItems = new ChunkItemStatusListBuilder().setItems(ChunkItem.Status.SUCCESS, 1).build();
+        List<ChunkItem> deliveredChunkItems = new ChunkItemStatusListBuilder().setItems(ChunkItem.Status.IGNORE, 1).build();
+        Chunk chunkifiedChunk = new ChunkBuilder().setJobId(jobId).setChunkId(chunkId).setItems(chunkifiedChunkItems).build();
+        SinkChunkResult deliveredChunk = new SinkChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(deliveredChunkItems).build();
+
+        Mockito.when(jobStore.getChunk(jobId, chunkId)).thenReturn(chunkifiedChunk);
+        Mockito.when(jobStore.getProcessorResult(jobId, chunkId)).thenReturn(null);
+        Mockito.when(jobStore.getSinkResult(jobId, chunkId)).thenReturn(deliveredChunk);
+
+        jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+    }
+
+    @Test
+    public void chunkCompletionState_SuccessUnavailableUnavailable_SuccessIncompleteIncomplete() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        setupMockito(jobId, chunkId, ChunkItem.Status.SUCCESS, null, null);
+
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+
+        assertSingleItemCompletionStateWithStates(chunkCompletionState,
+                ItemCompletionState.State.SUCCESS,
+                ItemCompletionState.State.INCOMPLETE,
+                ItemCompletionState.State.INCOMPLETE);
+    }
+
+    @Test
+    public void chunkCompletionState_FailureUnavailableUnavailable_FailureIncompleteIncomplete() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        setupMockito(jobId, chunkId, ChunkItem.Status.FAILURE, null, null);
+
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+
+        assertSingleItemCompletionStateWithStates(chunkCompletionState,
+                ItemCompletionState.State.FAILURE,
+                ItemCompletionState.State.INCOMPLETE,
+                ItemCompletionState.State.INCOMPLETE);
+    }
+
+    @Test
+    public void chunkCompletionState_IgnoreUnavailableUnavailable_IgnoredIncompleteIncomplete() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        setupMockito(jobId, chunkId, ChunkItem.Status.IGNORE, null, null);
+
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+
+        assertSingleItemCompletionStateWithStates(chunkCompletionState,
+                ItemCompletionState.State.IGNORED,
+                ItemCompletionState.State.INCOMPLETE,
+                ItemCompletionState.State.INCOMPLETE);
+    }
+
+    @Test
+    public void chunkCompletionState_SuccessSuccessUnavailable_SuccessSuccessIncomplete() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        setupMockito(jobId, chunkId, ChunkItem.Status.SUCCESS, ChunkItem.Status.SUCCESS, null);
+
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+
+        assertSingleItemCompletionStateWithStates(chunkCompletionState,
+                ItemCompletionState.State.SUCCESS,
+                ItemCompletionState.State.SUCCESS,
+                ItemCompletionState.State.INCOMPLETE);
+    }
+
+    @Test
+    public void chunkCompletionState_SuccessFailureUnavailable_SuccessFailureIncomplete() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        setupMockito(jobId, chunkId, ChunkItem.Status.SUCCESS, ChunkItem.Status.FAILURE, null);
+
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+
+        assertSingleItemCompletionStateWithStates(chunkCompletionState,
+                ItemCompletionState.State.SUCCESS,
+                ItemCompletionState.State.FAILURE,
+                ItemCompletionState.State.INCOMPLETE);
+    }
+
+    @Test
+    public void chunkCompletionState_SuccessIgnoreUnavailable_SuccessIgnoredIncomplete() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        setupMockito(jobId, chunkId, ChunkItem.Status.SUCCESS, ChunkItem.Status.IGNORE, null);
+
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+
+        assertSingleItemCompletionStateWithStates(chunkCompletionState,
+                ItemCompletionState.State.SUCCESS,
+                ItemCompletionState.State.IGNORED,
+                ItemCompletionState.State.INCOMPLETE);
+    }
+
+    @Test
+    public void chunkCompletionState_SuccessFailureIgnore_SuccessFailureIgnored() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        setupMockito(jobId, chunkId, ChunkItem.Status.SUCCESS, ChunkItem.Status.FAILURE, ChunkItem.Status.IGNORE);
+
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+
+        assertSingleItemCompletionStateWithStates(chunkCompletionState,
+                ItemCompletionState.State.SUCCESS,
+                ItemCompletionState.State.FAILURE,
+                ItemCompletionState.State.IGNORED);
+    }
+
+    @Test
+    public void complexExample() throws JobStoreException {
+        final long jobId = 1L;
+        final long chunkId = 1L;
+        final int chunkSize = 7;
+        JobCompletionStateFinder jobCompletionStateFinder = new JobCompletionStateFinder(jobStore);
+        List<ChunkItem> chunkifiedItems = new ChunkItemStatusListBuilder()
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.FAILURE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .build();
+        List<ChunkItem> processedItems = new ChunkItemStatusListBuilder()
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.FAILURE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .build();
+        List<ChunkItem> deliveredItems = new ChunkItemStatusListBuilder()
+                .setItem(ChunkItem.Status.SUCCESS)
+                .setItem(ChunkItem.Status.FAILURE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .setItem(ChunkItem.Status.IGNORE)
+                .build();
+        Chunk chunkifiedChunk = new ChunkBuilder().setJobId(jobId).setChunkId(chunkId).setItems(chunkifiedItems).build();
+        ChunkResult processedChunk = new ChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(processedItems).build();
+        SinkChunkResult deliveredChunk = new SinkChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(deliveredItems).build();
+
+        Mockito.when(jobStore.getChunk(jobId, chunkId)).thenReturn(chunkifiedChunk);
+        Mockito.when(jobStore.getProcessorResult(jobId, chunkId)).thenReturn(processedChunk);
+        Mockito.when(jobStore.getSinkResult(jobId, chunkId)).thenReturn(deliveredChunk);
+
+        ChunkCompletionState chunkCompletionState = jobCompletionStateFinder.getChunkCompletionState(jobId, chunkId);
+        assertThat(chunkCompletionState.getItems().size(), is(chunkSize));
+        assertItemCompletionStateWithStates(chunkCompletionState, 0, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.SUCCESS);
+        assertItemCompletionStateWithStates(chunkCompletionState, 1, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.FAILURE);
+        assertItemCompletionStateWithStates(chunkCompletionState, 2, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.IGNORED);
+        assertItemCompletionStateWithStates(chunkCompletionState, 3, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.FAILURE, ItemCompletionState.State.IGNORED);
+        assertItemCompletionStateWithStates(chunkCompletionState, 4, ItemCompletionState.State.SUCCESS, ItemCompletionState.State.IGNORED, ItemCompletionState.State.IGNORED);
+        assertItemCompletionStateWithStates(chunkCompletionState, 5, ItemCompletionState.State.FAILURE, ItemCompletionState.State.IGNORED, ItemCompletionState.State.IGNORED);
+        assertItemCompletionStateWithStates(chunkCompletionState, 6, ItemCompletionState.State.IGNORED, ItemCompletionState.State.IGNORED, ItemCompletionState.State.IGNORED);
+    }
+
+    private void setupMockito(long jobId, long chunkId, ChunkItem.Status chunkifyStatus, ChunkItem.Status processingStatus, ChunkItem.Status deliveringStatus) throws JobStoreException {
+        // Setup chunkifyChunk:
+        if (chunkifyStatus != null) {
+            List<ChunkItem> chunkItems = new ChunkItemStatusListBuilder()
+                    .setItem(chunkifyStatus)
+                    .build();
+            Chunk chunkifiedChunk = new ChunkBuilder().setJobId(jobId).setChunkId(chunkId).setItems(chunkItems).build();
+            Mockito.when(jobStore.getChunk(jobId, chunkId)).thenReturn(chunkifiedChunk);
+        } else {
+            Mockito.when(jobStore.getChunk(jobId, chunkId)).thenReturn(null);
+        }
+
+        // Setup processingChunk:
+        if (processingStatus != null) {
+            List<ChunkItem> chunkItems = new ChunkItemStatusListBuilder()
+                    .setItem(processingStatus)
+                    .build();
+            ChunkResult processedChunk = new ChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(chunkItems).build();
+            Mockito.when(jobStore.getProcessorResult(jobId, chunkId)).thenReturn(processedChunk);
+        } else {
+            Mockito.when(jobStore.getProcessorResult(jobId, chunkId)).thenReturn(null);
+        }
+
+        // Setup deliveringChunk:
+        if (deliveringStatus != null) {
+            List<ChunkItem> chunkItems = new ChunkItemStatusListBuilder()
+                    .setItem(deliveringStatus)
+                    .build();
+            SinkChunkResult deliveredChunk = new SinkChunkResultBuilder().setJobId(jobId).setChunkId(chunkId).setItems(chunkItems).build();
+            Mockito.when(jobStore.getSinkResult(jobId, chunkId)).thenReturn(deliveredChunk);
+        } else {
+            Mockito.when(jobStore.getSinkResult(jobId, chunkId)).thenReturn(null);
+        }
+    }
+
+    private void assertSingleItemCompletionStateWithStates(ChunkCompletionState chunkCompletionState,
+            ItemCompletionState.State chunkifyState,
+            ItemCompletionState.State processorState,
+            ItemCompletionState.State deliveryState) {
+        assertThat(chunkCompletionState.getItems().size(), is(1));
+        assertItemCompletionStateWithStates(chunkCompletionState, 0, chunkifyState, processorState, deliveryState);
+    }
+
+    private void assertItemCompletionStateWithStates(ChunkCompletionState chunkCompletionState,
+            int index,
+            ItemCompletionState.State chunkifyState,
+            ItemCompletionState.State processorState,
+            ItemCompletionState.State deliveryState) {
+        ItemCompletionState itemCompletionState = chunkCompletionState.getItems().get(index);
+        assertThat(itemCompletionState.getChunkifyState(), is(chunkifyState));
+        assertThat(itemCompletionState.getProcessingState(), is(processorState));
+        assertThat(itemCompletionState.getDeliveryState(), is(deliveryState));
+    }
+
+    private class ChunkItemStatusListBuilder {
+
+        private List<ChunkItem> chunkItems = new ArrayList<>();
+
+        public ChunkItemStatusListBuilder() {
+        }
+
+        public ChunkItemStatusListBuilder setItems(ChunkItem.Status status, int numberOfItemsToSet) {
+            for (int i = 0; i < numberOfItemsToSet; i++) {
+                chunkItems.add(new ChunkItem(chunkItems.size() + 1, "", status));
+            }
+            return this;
+        }
+
+        public ChunkItemStatusListBuilder setItem(ChunkItem.Status status) {
+            chunkItems.add(new ChunkItem(chunkItems.size() + 1, "", status));
+            return this;
+        }
+
+        public List<ChunkItem> build() {
+            return chunkItems;
+        }
+    }
+
+//    private Chunk defaultChunk(long jobId, long chunkId) {
+//        Flow flow = new FlowBuilder().build();
+//        SupplementaryProcessData supplementaryProcessData = new SupplementaryProcessDataBuilder().build();
+//        return new Chunk(jobId, chunkId, flow, supplementaryProcessData, new ArrayList<ChunkItem>());
+//    }
+}

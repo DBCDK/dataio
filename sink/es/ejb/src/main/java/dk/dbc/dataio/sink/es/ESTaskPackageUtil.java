a4971266a5ce346d30cdf444691547ed4370d9c3
Jesper Damkjær <jda@dbc.dk>
2014-10-09 08:14:23 +0000

    Added extra logging when calling ESUtil.insertTaskPackage
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@77539 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- sink/es/ejb/src/main/java/dk/dbc/dataio/sink/es/ESTaskPackageUtil.java
+++ sink/es/ejb/src/main/java/dk/dbc/dataio/sink/es/ESTaskPackageUtil.java
@@ -164,6 +164,8 @@ public class ESTaskPackageUtil {
         InvariantUtil.checkNotNullNotEmptyOrThrow(dbname, "dbname");
         InvariantUtil.checkNotNullOrThrow(esWorkload, "esInFlight");
         final String creator = createCreatorString(esWorkload.getSinkChunkResult().getJobId(), esWorkload.getSinkChunkResult().getChunkId());
+        LOGGER.info("Calling ESUtil.insertAddiList with: numberOfRecords: [{}], dbname: [{}], encoding: [{}], creator: [{}], esConn-Object: [{}]",
+                esWorkload.getAddiRecords().size(), dbname, esWorkload.getSinkChunkResult().getEncoding(), creator, esConn);
         final ESUtil.AddiListInsertionResult insertionResult = ESUtil.insertAddiList(esConn, esWorkload.getAddiRecords(), dbname, esWorkload.getSinkChunkResult().getEncoding(), creator);
         validateTaskPackageState(insertionResult, esWorkload);
         return insertionResult.getTargetReference();

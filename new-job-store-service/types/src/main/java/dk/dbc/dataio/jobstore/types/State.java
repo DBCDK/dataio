e953ca064bba2b9c943a50718478d161a5e4f32b
Jan Bauer Nielsen <jbn@dbc.dk>
2014-12-01 10:02:43 +0000

    Added State class.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@79557 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ new-job-store-service/types/src/main/java/dk/dbc/dataio/jobstore/types/State.java
@@ -0,0 +1,9 @@
+package dk.dbc.dataio.jobstore.types;
+
+public class State {
+    private final String internals = "to-be-implemented";
+
+    public String getInternals() {
+        return internals;
+    }
+}

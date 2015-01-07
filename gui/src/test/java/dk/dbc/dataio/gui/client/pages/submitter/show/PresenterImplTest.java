a999ad8f624d60bae494a80e9c1f712c1441fcc5
Steen Lillethorup Frederiksen <slf@dbc.dk>
2015-01-07 10:35:42 +0000

    [18257] IS'en fejler periodisk i SubmittersShowSeleniumIT testen - Unit test af Submitter Show Presenter
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@80235 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/test/java/dk/dbc/dataio/gui/client/pages/submitter/show/PresenterImplTest.java
@@ -0,0 +1,137 @@
+
+package dk.dbc.dataio.gui.client.pages.submitter.show;
+
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwtmockito.GwtMockitoTestRunner;
+import dk.dbc.dataio.gui.client.model.SubmitterModel;
+import dk.dbc.dataio.gui.client.pages.submitter.modify.EditPlace;
+import dk.dbc.dataio.gui.client.proxies.FlowStoreProxyAsync;
+import dk.dbc.dataio.gui.util.ClientFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * PresenterImpl unit tests
+ *
+ * The test methods of this class uses the following naming convention:
+ *
+ *  unitOfWork_stateUnderTest_expectedBehavior
+ */
+@RunWith(GwtMockitoTestRunner.class)
+public class PresenterImplTest {
+    @Mock ClientFactory mockedClientFactory;
+    @Mock FlowStoreProxyAsync mockedFlowStore;
+    @Mock PlaceController mockedPlaceController;
+    @Mock AcceptsOneWidget mockedContainerWidget;
+    @Mock EventBus mockedEventBus;
+    @Mock View mockedView;
+    @Mock Widget mockedViewWidget;
+    @Mock Throwable mockedException;
+
+    // Setup mocked data
+    @Before
+    public void setupMockedData() {
+        when(mockedClientFactory.getFlowStoreProxyAsync()).thenReturn(mockedFlowStore);
+        when(mockedClientFactory.getPlaceController()).thenReturn(mockedPlaceController);
+        when(mockedClientFactory.getSubmittersShowView()).thenReturn(mockedView);
+        when(mockedView.asWidget()).thenReturn(mockedViewWidget);
+    }
+
+
+    // Subject Under Test
+    private PresenterImpl presenterImpl;
+
+
+    // Test specialization of Presenter to enable test of callback's
+    class PresenterImplConcrete extends PresenterImpl {
+        public PresenterImplConcrete(ClientFactory clientFactory) {
+            super(clientFactory);
+        }
+        public FetchSubmittersCallback fetchSubmittersCallback = new FetchSubmittersCallback();
+    }
+
+    // Test Data
+    private SubmitterModel testModel1 = new SubmitterModel(12345L, 1111L, "564738", "Submitter Name 1", "Submitter Description 1");
+    private SubmitterModel testModel2 = new SubmitterModel(12346L, 2222L, "564739", "Submitter Name 2", "Submitter Description 2");
+    private List<SubmitterModel> testModels = new ArrayList<SubmitterModel>(Arrays.asList(testModel1, testModel2));
+
+
+
+    @Test
+    public void constructor_instantiate_objectCorrectInitialized() {
+
+        // Test Subject Under Test
+        presenterImpl = new PresenterImpl(mockedClientFactory);
+
+        // Verify Test
+        verify(mockedClientFactory).getFlowStoreProxyAsync();
+        verify(mockedClientFactory).getPlaceController();
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void start_callStart_ok() {
+        presenterImpl = new PresenterImpl(mockedClientFactory);
+
+        // Test Subject Under Test
+        presenterImpl.start(mockedContainerWidget, mockedEventBus);
+
+        // Verify Test
+        verify(mockedClientFactory).getSubmittersShowView();
+        verify(mockedView).setPresenter(presenterImpl);
+        verify(mockedContainerWidget).setWidget(mockedViewWidget);
+        verify(mockedFlowStore).findAllSubmitters(any(AsyncCallback.class));
+    }
+
+    @Test
+    public void editSubmitter_call_gotoEditPlace() {
+        presenterImpl = new PresenterImpl(mockedClientFactory);
+        presenterImpl.start(mockedContainerWidget, mockedEventBus);
+
+        // Test Subject Under Test
+        presenterImpl.editSubmitter(testModel1);
+
+        // Verify Test
+        verify(mockedPlaceController).goTo(any(EditPlace.class));
+    }
+
+    @Test
+    public void fetchSubmitters_callbackWithError_errorMessageInView() {
+        PresenterImplConcrete presenterImpl = new PresenterImplConcrete(mockedClientFactory);
+        presenterImpl.start(mockedContainerWidget, mockedEventBus);
+
+        // Test Subject Under Test
+        presenterImpl.fetchSubmittersCallback.onFilteredFailure(mockedException);
+
+        // Verify Test
+        verify(mockedView).setErrorText(any(String.class));
+    }
+
+    @Test
+    public void fetchSubmitters_callbackWithSuccess_submittersAreFetched() {
+        PresenterImplConcrete presenterImpl = new PresenterImplConcrete(mockedClientFactory);
+        presenterImpl.start(mockedContainerWidget, mockedEventBus);
+
+        // Test Subject Under Test
+        presenterImpl.fetchSubmittersCallback.onSuccess(testModels);
+
+        // Verify Test
+        verify(mockedView).setSubmitters(testModels);
+    }
+
+}

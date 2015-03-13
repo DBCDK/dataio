3ae34e8b19fc04aa47e92cc933ac635bdcb76dc4
Stine Mia Madsen <sma@dbc.dk>
2015-03-13 14:06:29 +0000

    [18447] Fejl i læsning fra FIleStore - Implementering af ByteCountingInputStream
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@82773 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ new-job-store-service/ejb/src/main/java/dk/dbc/dataio/jobstore/service/partitioner/ByteCountingInputStream.java
@@ -0,0 +1,82 @@
+package dk.dbc.dataio.jobstore.service.partitioner;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * count the number of bytes read through the stream
+ */
+public class ByteCountingInputStream extends InputStream {
+    long count = 0;
+
+    long marked = -1;
+
+    InputStream is;
+
+    @Override
+    public int available() throws IOException {
+        return is.available();
+    }
+
+    @Override
+    public boolean markSupported() {
+        return is.markSupported();
+    }
+
+    @Override
+    public int read() throws IOException {
+        int r = is.read();
+        if (r > 0) {
+            count++;
+        }
+        return r;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        int r = is.read(b, off, len);
+        if (r > 0) {
+            count += r;
+        }
+        return r;
+    }
+
+    @Override
+    public long skip(long skipped) throws IOException {
+        long l = is.skip(skipped);
+        if (l > 0) {
+            count += l;
+        }
+        return l;
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+        is.mark(readlimit);
+        marked = count;
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+        is.reset();
+        count = marked;
+    }
+
+    @Override
+    public void close() throws IOException {
+        is.close();
+    }
+
+    /**
+     * get the actual number of bytes read
+     *
+     * @return a long, the number of bytes read
+     */
+    public long getBytesRead() {
+        return count;
+    }
+
+    public ByteCountingInputStream(InputStream is) {
+        this.is = is;
+    }
+}
---
+++ new-job-store-service/ejb/src/test/java/dk/dbc/dataio/jobstore/service/partitioner/ByteCountingInputStreamTest.java
@@ -0,0 +1,54 @@
+package dk.dbc.dataio.jobstore.service.partitioner;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ByteCountingInputStreamTest {
+
+    private final String byteString = "0123456789";
+
+    @Test
+    public void read_readWithBufferAndStartOffsetAndMaximumBytesToRead() throws IOException {
+        byte[] bytes = byteString.getBytes(StandardCharsets.UTF_8);
+        ByteCountingInputStream byteCountingInputStream = new ByteCountingInputStream(asInputStream(byteString));
+        byteCountingInputStream.read(new byte[bytes.length], 0, bytes.length);
+        assertThat(byteCountingInputStream.getBytesRead(), is(Long.valueOf(bytes.length)));
+    }
+
+    @Test
+    public void read_readWithBuffer() throws IOException {
+        byte[] bytes = byteString.getBytes(StandardCharsets.UTF_8);
+        ByteCountingInputStream byteCountingInputStream = new ByteCountingInputStream(asInputStream(byteString));
+        byteCountingInputStream.read(new byte[bytes.length]);
+        assertThat(byteCountingInputStream.getBytesRead(), is(Long.valueOf(bytes.length)));
+    }
+
+    @Test
+    public void read_readWithoutInput() throws IOException {
+        ByteCountingInputStream byteCountingInputStream = new ByteCountingInputStream(asInputStream(byteString));
+        long counter = 0;
+        while (byteCountingInputStream.read() != -1) {
+            assertThat(byteCountingInputStream.getBytesRead(), is(++counter));
+        }
+    }
+
+    /*
+     * Private methods
+     */
+    private InputStream asInputStream(String s) {
+        return asInputStream(s, StandardCharsets.UTF_8);
+    }
+
+    private InputStream asInputStream(String s, Charset encoding) {
+        return new ByteArrayInputStream(s.getBytes(encoding));
+    }
+
+}

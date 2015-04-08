0bdfe2a55699f1414d6ca8ff14365b49f7bf50b8
Jacob Christian Poulsen <ja7@dbc.dk>
2015-04-08 11:45:07 +0000

    Work on Bug 18527 - US #496: dataIO skal kunne håndtere JavaScript som benytter require moduler Initial Setup for flywaydb
    Finten er at den opretter bruger flowstore_schema_version som versions tabel. 
    V1_ _initial_schema.sql er taget fra stagingn databasen.. med pg_dump også tilrettet så constrains ligger sammen med tabellerne. 
    
    
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@83513 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ flow-store-service/src/main/java/dk/dbc/dataio/flowstore/ejb/StartupDBMigrator.java
@@ -0,0 +1,52 @@
+package dk.dbc.dataio.flowstore.ejb;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.EJBException;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+/**
+ * Created by ja7 on 04-04-15.
+ *
+ * Startup Single ton for creating the database.
+ */
+
+@Singleton
+@Startup
+public class StartupDBMigrator {
+    private static final Logger log = LoggerFactory.getLogger(StartupDBMigrator.class);
+
+    @Resource(lookup="jdbc/flowStoreDb")
+   	private DataSource dataSource;
+
+   	@PostConstruct
+   	private void onStartup() {
+		log.error("ja7 in onStartup");
+    	if (dataSource == null) {
+   			log.error("no datasource found to execute the db migrations!");
+            throw new EJBException(
+                    "no datasource found to execute the db migrations!");
+        }
+
+   		Flyway flyway = new Flyway();
+
+        flyway.setTable("flowstore_schema_version");
+        flyway.setBaselineOnMigrate(true);
+   		flyway.setDataSource(dataSource);
+   		for (MigrationInfo i : flyway.info().all()) {
+   			log.info("migrate task: " + i.getVersion() + " : " + i.getDescription() + " from file: " + i.getScript());
+			log.error("migrate task: " + i.getVersion() + " : " + i.getDescription() + " from file: " + i.getScript());
+   		}
+   		flyway.migrate();
+   	}
+
+}

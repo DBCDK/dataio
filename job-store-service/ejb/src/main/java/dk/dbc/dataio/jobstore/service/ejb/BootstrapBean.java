c314fdcbf24f4870c2d3defe5012a747ebdc62a5
Jan Bauer Nielsen <jbn@dbc.dk>
2015-05-07 06:34:21 +0000

    [18609] Added stubbed BootstrapBean class.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@84853 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ job-store-service/ejb/src/main/java/dk/dbc/dataio/jobstore/service/ejb/BootstrapBean.java
@@ -0,0 +1,43 @@
+package dk.dbc.dataio.jobstore.service.ejb;
+
+import dk.dbc.dataio.commons.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Singleton
+@Startup
+public class BootstrapBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BootstrapBean.class);
+
+    @EJB
+    PgJobStore jobStore;
+
+    @EJB
+    JobSchedulerBean jobSchedulerBean;
+
+    @PostConstruct
+    public void initialize() {
+        final StopWatch stopWatch = new StopWatch();
+        try {
+            restoreSystemState();
+        } finally {
+            LOGGER.debug("job-store initialization took {} milliseconds", stopWatch.getElapsedTime());
+        }
+    }
+
+    @Lock(LockType.READ)
+    public void waitForSystemInitialization() {
+        LOGGER.debug("job-store initialized");
+    }
+
+    private void restoreSystemState() {
+        LOGGER.info("Restoring job-store state");
+    }
+}

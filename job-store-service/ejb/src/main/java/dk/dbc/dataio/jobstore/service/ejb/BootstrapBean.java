b483db01b589547c875f209d1302e9ea5bb5d09f
Jan Bauer Nielsen <jbn@dbc.dk>
2015-05-07 07:27:52 +0000

    [18609] Added @DependsOn annotation to enforce db migration completion before bootstrap process.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@84871 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-store-service/ejb/src/main/java/dk/dbc/dataio/jobstore/service/ejb/BootstrapBean.java
+++ job-store-service/ejb/src/main/java/dk/dbc/dataio/jobstore/service/ejb/BootstrapBean.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.DependsOn;
 import javax.ejb.EJB;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
@@ -13,6 +14,7 @@ import javax.ejb.Startup;
 
 @Singleton
 @Startup
+@DependsOn("StartupDBMigrator")
 public class BootstrapBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(BootstrapBean.class);
 

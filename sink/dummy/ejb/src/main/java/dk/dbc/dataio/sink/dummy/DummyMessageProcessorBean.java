781d2d08d0a331610d7fd1278a0ee06d9c599d61
Jan Bauer Nielsen <jbn@dbc.dk>
2015-05-19 05:58:03 +0000

    [18622] Bugfix for missing dependency and wrong chunk ID.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@85335 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- sink/dummy/ejb/src/main/java/dk/dbc/dataio/sink/dummy/DummyMessageProcessorBean.java
+++ sink/dummy/ejb/src/main/java/dk/dbc/dataio/sink/dummy/DummyMessageProcessorBean.java
@@ -6,8 +6,12 @@ import dk.dbc.dataio.commons.types.ExternalChunk;
 import dk.dbc.dataio.commons.types.exceptions.InvalidMessageException;
 import dk.dbc.dataio.commons.types.exceptions.ServiceException;
 import dk.dbc.dataio.commons.utils.jobstore.JobStoreServiceConnectorException;
+import dk.dbc.dataio.commons.utils.jobstore.JobStoreServiceConnectorUnexpectedStatusCodeException;
 import dk.dbc.dataio.commons.utils.jobstore.ejb.JobStoreServiceConnectorBean;
 import dk.dbc.dataio.commons.utils.service.AbstractSinkMessageConsumerBean;
+import dk.dbc.dataio.jobstore.types.JobError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
@@ -15,23 +19,30 @@ import javax.ejb.MessageDriven;
 
 @MessageDriven
 public class DummyMessageProcessorBean extends AbstractSinkMessageConsumerBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DummyMessageProcessorBean.class);
 
     @EJB
     JobStoreServiceConnectorBean jobStoreServiceConnectorBean;
 
     @Override
     public void handleConsumedMessage(ConsumedMessage consumedMessage) throws ServiceException, InvalidMessageException {
-        ExternalChunk processedChunk = unmarshallPayload(consumedMessage);
+        final ExternalChunk processedChunk = unmarshallPayload(consumedMessage);
         final ExternalChunk deliveredChunk = processPayload(processedChunk);
         try {
-            jobStoreServiceConnectorBean.getConnector().addChunkIgnoreDuplicates(deliveredChunk, deliveredChunk.getJobId(), deliveredChunk.getJobId());
+            jobStoreServiceConnectorBean.getConnector().addChunkIgnoreDuplicates(deliveredChunk, deliveredChunk.getJobId(), deliveredChunk.getChunkId());
         } catch (JobStoreServiceConnectorException e) {
+            if (e instanceof JobStoreServiceConnectorUnexpectedStatusCodeException) {
+                final JobError jobError = ((JobStoreServiceConnectorUnexpectedStatusCodeException) e).getJobError();
+                if (jobError != null) {
+                    LOGGER.error("job-store returned error: {}", jobError.getDescription());
+                }
+            }
             throw new EJBException(e);
         }
     }
 
     ExternalChunk processPayload(ExternalChunk processedChunk) {
-        ExternalChunk deliveredChunk = new ExternalChunk(processedChunk.getJobId(), processedChunk.getChunkId(), ExternalChunk.Type.DELIVERED);
+        final ExternalChunk deliveredChunk = new ExternalChunk(processedChunk.getJobId(), processedChunk.getChunkId(), ExternalChunk.Type.DELIVERED);
         for (final ChunkItem item : processedChunk) {
             // Set new-item-status to success if chunkResult-item was success - else set new-item-status to ignore:
             ChunkItem.Status status = item.getStatus() == ChunkItem.Status.SUCCESS ? ChunkItem.Status.SUCCESS : ChunkItem.Status.IGNORE;

ff837f0a5022520050a65d165245ed50d9879f58
Stine Mia Madsen <sma@dbc.dk>
2015-05-22 13:23:35 +0000

    [18638] US #551: Lettere adgang til paginering og "tilbage-til-joboversigt.knap" i postvisningen - Ændring af JobInfo siden, såldes at knappen: Tilbage til joboversigten er i toppen af siden og ikke skifter plads afhængig af hvilken tab man har valgt.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@85603 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/item/show/JobInfoTabContent.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/item/show/JobInfoTabContent.java
@@ -1,8 +1,11 @@
 package dk.dbc.dataio.gui.client.pages.item.show;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import dk.dbc.dataio.gui.client.components.PromptedLabel;
@@ -24,5 +27,14 @@ public class JobInfoTabContent extends Composite {
     @UiField PromptedLabel mailForNotificationAboutVerification;
     @UiField PromptedLabel mailForNotificationAboutProcessing;
     @UiField PromptedLabel resultMailInitials;
+
+    /**
+     * Ui Handler to catch click events on the Back button
+     * @param event Clicked event
+     */
+    @UiHandler("backButton")
+    void backButtonPressed(ClickEvent event) {
+        History.back();
+    }
 
 }

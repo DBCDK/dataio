02d459eacc3b1422592e17e9c4b5c66e71957340
Steen Lillethorup Frederiksen <slf@dbc.dk>
2015-06-03 14:46:36 +0000

    [18670] US #457: Progress på kørende jobs. - Ny ProgressBar komponent
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@86071 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/main/java/dk/dbc/dataio/gui/client/components/MultiProgressBar.java
@@ -0,0 +1,81 @@
+package dk.dbc.dataio.gui.client.components;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiConstructor;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ * This class implements a Multi Progress Bar component
+ * The component includes two progress values in the same bar.
+ * Like this:
+ *
+ *   +-----------+----------+-------------------------+
+ *   | -first-   | -second- | -remainder-             |
+ *   +-----------+----------+-------------------------+
+ *
+ *   It is possible to set a value for the complete bar - the Max value
+ *   Then, by setting firstValue and secondValue, the length of these bars
+ *   are calculated correspondingly.
+ *   The color of the first bar is Green, the second one is Blue, and the
+ *   remainder is gray.
+ *
+ *   A text is overlayed on the bars, if set.
+ */
+public class MultiProgressBar extends Composite {
+    interface MultiProgressBarUiBinder extends UiBinder<HTMLPanel, MultiProgressBar> {
+    }
+
+    private static MultiProgressBarUiBinder ourUiBinder = GWT.create(MultiProgressBarUiBinder.class);
+
+    @UiField Label textProgress;
+    @UiField Element firstProgress;
+    @UiField Element secondProgress;
+
+    @UiConstructor
+    public MultiProgressBar(String text, String firstValue, String secondValue, String max) {
+        initWidget(ourUiBinder.createAndBindUi(this));
+        setText(text);
+        setFirstValue(firstValue);
+        setSecondValue(secondValue);
+        setMaxValue(max);
+    }
+
+    /**
+     * Sets the text to be displayed on top of, and in the center of the Progress Bar
+     * @param text Text to be displayen on the Progress Bar
+     */
+    public void setText(String text) {
+        textProgress.setText(text);
+    }
+
+    /**
+     * Sets the first value for the Progress Bar
+     * @param value The first value of the Progress Bar
+     */
+    public void setFirstValue(String value) {
+        firstProgress.setAttribute("value", value);
+    }
+
+    /**
+     * Sets the second value for the Progress Bar
+     * @param value The second value of the Progress Bar
+     */
+    public void setSecondValue(String value) {
+        secondProgress.setAttribute("value", value);
+    }
+
+    /**
+     * Sets the max value for the Progress Bar.
+     * @param value The max value for the Progress Bar
+     */
+    public void setMaxValue(String value) {
+        firstProgress.setAttribute("max", value);
+        secondProgress.setAttribute("max", value);
+    }
+
+}

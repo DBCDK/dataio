1c62e30fa8cd501279d58af378df4b3c69ffe85c
Stine Mia Madsen <sma@dbc.dk>
2015-06-15 14:28:35 +0000

    [18715] US #593: Høstede job med manglende flowbinder - Implementering af ny klasse: Diagnostic
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@86517 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ job-store-service/types/src/main/java/dk/dbc/dataio/jobstore/types/Diagnostic.java
@@ -0,0 +1,52 @@
+package dk.dbc.dataio.jobstore.types;
+
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
+
+public class Diagnostic {
+
+    public enum Level { FATAL, WARNING }
+
+    private final Level level;
+    private final String message;
+    private final String stacktrace;
+
+    public Diagnostic(Level level, String message, String stacktrace) throws NullPointerException, IllegalArgumentException {
+
+        this.level = InvariantUtil.checkNotNullOrThrow(level, "level");
+        this.message = InvariantUtil.checkNotNullNotEmptyOrThrow(message, "message");
+        this.stacktrace = InvariantUtil.checkNotNullNotEmptyOrThrow(stacktrace, "stacktrace");
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getStacktrace() {
+        return stacktrace;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Diagnostic)) return false;
+
+        Diagnostic that = (Diagnostic) o;
+
+        if (level != that.level) return false;
+        if (!message.equals(that.message)) return false;
+        return stacktrace.equals(that.stacktrace);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = level.hashCode();
+        result = 31 * result + message.hashCode();
+        result = 31 * result + stacktrace.hashCode();
+        return result;
+    }
+}

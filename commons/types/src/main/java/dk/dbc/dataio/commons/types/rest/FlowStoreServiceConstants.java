65a8fddee8256aaa9f77c67611585d9cf2792c9c
lbtdb <lbtdb@dbc.dk>
2015-06-30 13:47:01 +0000

    [18750] US #227: Sletning af submitter - Delete submitters backend finished - IT implemented
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@87221 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/types/src/main/java/dk/dbc/dataio/commons/types/rest/FlowStoreServiceConstants.java
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/rest/FlowStoreServiceConstants.java
@@ -21,6 +21,7 @@ public class FlowStoreServiceConstants {
 
     public static final String SINK_CONTENT = "sinks/{id}/content";
     public static final String SUBMITTER_CONTENT = "submitters/{id}/content";
+    public static final String SUBMITTER_DELETE = "submitters/{id}/delete";
     public static final String FLOW_COMPONENT_CONTENT = "components/{id}/content";
     public static final String FLOW_CONTENT = "flows/{id}/content";
     public static final String FLOW_BINDER_CONTENT = "binders/{id}/content";

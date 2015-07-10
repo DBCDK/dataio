b58790449f20a372307a5e7f370e9de8a012f8a2
lbtdb <lbtdb@dbc.dk>
2015-07-10 10:40:27 +0000

    [18750] US #227: Sletning af submitter - Delete implemented in GUI - unused imports removed
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@87542 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/submitter/modify/View.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/submitter/modify/View.java
@@ -6,7 +6,6 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.IsWidget;

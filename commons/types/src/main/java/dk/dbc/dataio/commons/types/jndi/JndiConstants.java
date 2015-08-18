8801a9e7a142b7f6181c65750ca2f25c78171ec1
Jan Bauer Nielsen <jbn@dbc.dk>
2015-08-18 11:12:30 +0000

    [18909] Added flyway database migration to ES inflight database.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@88366 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/types/src/main/java/dk/dbc/dataio/commons/types/jndi/JndiConstants.java
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/jndi/JndiConstants.java
@@ -3,6 +3,7 @@ package dk.dbc.dataio.commons.types.jndi;
 public class JndiConstants {
     public static final String JDBC_RESOURCE_JOBSTORE = "jdbc/dataio/jobstore";
     public static final String JDBC_RESOURCE_LOGSTORE = "jdbc/dataio/logstore";
+    public static final String JDBC_RESOURCE_ES_INFLIGHT = "jdbc/dataio/sinks/esInFlight";
     public static final String JDBC_RESOURCE_SINK_DIFF = "jdbc/dataio/diff";
 
     public static final String URL_RESOURCE_FBS_WS = "url/dataio/fbs/ws";

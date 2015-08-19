52f1ef2b1b607998ae732087a68732f6ccda10cf
Jacob Christian Poulsen <ja7@dbc.dk>
2015-08-19 07:07:04 +0000

    Extended list_job.py with options for limit and offset to the query
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@88399 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- developer-tools/list_jobs.py
+++ developer-tools/list_jobs.py
@@ -16,6 +16,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser("")
     parser.add_argument("--host", help="host til dataio systemet dataio-be-s01:8080 for staging", required=True)
     parser.add_argument("--job", help="job to show", type=int)
+    parser.add_argument("--limit", help="limit number of jobs to list", type=int, default=0)
+    parser.add_argument("--offset", help="start from offset ", type=int, default=0)
 
     args = parser.parse_args()
 
@@ -25,7 +27,14 @@ parse_arguments()
 print()
 url="http://"+args.host+"/dataio/job-store-service/jobs/searches"
 
-search_arguments={}
+search_arguments={ "limit": args.limit, "offset": args.offset, "ordering": [
+{
+   "field": "JOB_ID",
+   "sort": "DESC"
+   }
+   ]
+   }
+
 search_on_id={ "filtering": [ { "members": [
             {
                 "filter": {

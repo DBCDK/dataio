4e26f4d94e4bb16327db8789e4db7c9114eb15db
Steen Lillethorup Frederiksen <slf@dbc.dk>
2015-08-25 13:49:49 +0000

    [18877] US #636: Filtrering på sink i joboversigt - optimering - Opdateret Unit Test af SubmitterJobFilter
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@88674 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/components/jobfilter/SubmitterJobFilter.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/components/jobfilter/SubmitterJobFilter.java
@@ -2,14 +2,15 @@ package dk.dbc.dataio.gui.client.components.jobfilter;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiConstructor;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.inject.Inject;
 import dk.dbc.dataio.gui.client.components.PromptedTextBox;
-import dk.dbc.dataio.gui.client.model.JobListCriteriaModel;
 import dk.dbc.dataio.gui.client.resources.Resources;
 
 /**
@@ -33,22 +34,19 @@ public class SubmitterJobFilter extends BaseJobFilter {
         initWidget(ourUiBinder.createAndBindUi(this));
     }
 
-    @UiField
-    PromptedTextBox submitter;
+    @UiField PromptedTextBox submitter;
 
 
     /**
-     * Gets the value of the current Job List Criteria Model
-     * @return The current value of the Job List Criteria Model
+     * Event handler for handling changes in the selected submitter
+     * @param event The ValueChangeEvent
      */
-    public JobListCriteriaModel getValue() {
-        JobListCriteriaModel jobListCriteriaModel=new JobListCriteriaModel();
+    @UiHandler("submitter")
+    void filterSelectionChanged(ValueChangeEvent<String> event) {
         if( submitter.getValue().length() > 0 ) {
             jobListCriteriaModel.setSubmitter(submitter.getValue());
         }
-        return jobListCriteriaModel;
     }
-
 
     @Override
     public String getName() {
--- gui/src/test/java/dk/dbc/dataio/gui/client/components/jobfilter/SubmitterJobFilterTest.java
+++ gui/src/test/java/dk/dbc/dataio/gui/client/components/jobfilter/SubmitterJobFilterTest.java
@@ -1,35 +1,56 @@
 package dk.dbc.dataio.gui.client.components.jobfilter;
 
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwtmockito.GwtMockitoTestRunner;
-import dk.dbc.dataio.gui.client.model.JobListCriteriaModel;
 import dk.dbc.dataio.gui.client.resources.Resources;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
  * Created by ja7 on 19-08-15.
  * Test for SubmitterJobFilter
  */
-@Ignore
+
 @RunWith(GwtMockitoTestRunner.class)
 public class SubmitterJobFilterTest {
     @Mock Texts mockedTexts;
     @Mock Resources mockedResources;
     @Mock ValueChangeEvent<String> mockedValueChangeEvent;
-    @Mock ValueChangeHandler<JobListCriteriaModel> mockedValueChangeHandler;
+    @Mock ChangeHandler mockedChangeHandler;
     @Mock ValueChangeHandler<String> mockedSinkJobFilterValueChangeHandler;
     @Mock HandlerRegistration mockedSinkListHandlerRegistration;
 
+    final String DEFAULT_SUBMITTER_NAME = "default submitter name";
+
+    @Test
+    public void uiHandlerSelectionChanged_callFilterSelectionChanged_setSubmitterInJobListCriteriaModel() {
+        // Test Preparation
+        SubmitterJobFilter jobFilter = new SubmitterJobFilter(mockedTexts, mockedResources);
+        when(jobFilter.submitter.getValue()).thenReturn(DEFAULT_SUBMITTER_NAME);
+
+        // Activate Subject Under Test
+        class TestValueChangeEvent extends ValueChangeEvent<String> {
+            protected TestValueChangeEvent(String value) {
+                super(value);
+            }
+        }
+        jobFilter.filterSelectionChanged(new TestValueChangeEvent("Test Event"));
+
+        // Verify Test
+        assertThat(jobFilter.getValue().getSubmitter(), is(DEFAULT_SUBMITTER_NAME));
+    }
 
     @Test
     public void getName_callGetName_fetchesStoredName() {
@@ -47,23 +68,19 @@ public class SubmitterJobFilterTest {
         assertThat(jobFilterName, is(MOCKED_NAME));
     }
 
-
     @Test
-      public void addValueChangeHandler_callAddValueChangeHandler_valueChangeHandlerAdded() {
-          // Test Preparation
-          SubmitterJobFilter jobFilter = new SubmitterJobFilter(mockedTexts, mockedResources );
-          when(jobFilter.submitter.addValueChangeHandler(any(ValueChangeHandler.class))).thenReturn(mockedSinkListHandlerRegistration);
+    public void addValueChangeHandler_callAddValueChangeHandler_valueChangeHandlerAdded() {
+        // Test Preparation
+        SubmitterJobFilter jobFilter = new SubmitterJobFilter(mockedTexts, mockedResources);
+        when(jobFilter.submitter.addValueChangeHandler(any(ValueChangeHandler.class))).thenReturn(mockedSinkListHandlerRegistration);
 
-          // Activate Subject Under Test
-//          HandlerRegistration handlerRegistration = jobFilter.addValueChangeHandler(mockedValueChangeHandler);
-// Tjek lige den her......
+        // Activate Subject Under Test
+        HandlerRegistration handlerRegistration = jobFilter.addChangeHandler(mockedChangeHandler);
 
-
-          // Verify test
-//          assertThat(jobFilter.submitterJobValueChangeHandler, is(mockedValueChangeHandler));
-//          assertThat(jobFilter.submitterHandlerRegistration, is(mockedSinkListHandlerRegistration));
-//          assertThat(handlerRegistration, not(nullValue()));
-      }
+        // Verify test
+        verify(jobFilter.submitter).addChangeHandler(mockedChangeHandler);
+        assertThat(handlerRegistration, not(nullValue()));
+    }
 
 
 }

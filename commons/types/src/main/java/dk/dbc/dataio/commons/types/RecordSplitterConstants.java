bee005dee8828af46b9237d37482cf6b97a581c7
Stine Mia Madsen <sma@dbc.dk>
2015-09-04 12:26:12 +0000

    [19024] US #665: job oprettelse fra 2709 fil. - Oprettelse af ny enum for RecordSplitters
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@89260 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/RecordSplitterConstants.java
@@ -0,0 +1,17 @@
+package dk.dbc.dataio.commons.types;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class RecordSplitterConstants {
+
+    public enum RecordSplitter { XML, ISO2709 }
+
+    /**
+     * @return the list of recordSplitters, containing all available recordSplitters
+     */
+    public static List<RecordSplitter> getRecordSplitters() {
+        return Arrays.asList(RecordSplitter.values());
+    }
+}

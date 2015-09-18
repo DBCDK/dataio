79dca49e1422be27fad3e39353da7bac9d09d0d7
Jan Bauer Nielsen <jbn@dbc.dk>
2015-09-18 06:31:39 +0000

    [19052] Added json value template engine.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@89625 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/util/JsonValueTemplateEngine.java
@@ -0,0 +1,141 @@
+package dk.dbc.dataio.jobstore.service.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import dk.dbc.dataio.commons.types.Constants;
+import dk.dbc.dataio.jsonb.JSONBContext;
+import dk.dbc.dataio.jsonb.JSONBException;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * <p>
+ * Simple template engine taking a string template containing properties on
+ * the form ${json_path} or using the date macro form __DATE__{json_path}
+ * where json_path is a used to select property values from a given json document,
+ * where each path element is separated by a dot '.'.
+ * </p>
+ * <p>
+ * The json path given to the __DATE__ macro assumes that the selected property
+ * value is a numeric value representing the number of milliseconds since
+ * January 1, 1970, 00:00:00 GMT.
+ * </p>
+ */
+public class JsonValueTemplateEngine {
+    private static final String EMPTY_VALUE = "";
+
+    private final JSONBContext jsonbContext;
+    private final Pattern pattern = Pattern.compile("(\\$|__DATE__)\\{(.+?)\\}");
+
+    public JsonValueTemplateEngine() {
+        jsonbContext = new JSONBContext();
+    }
+
+    /**
+     * Applies property substitution to given template
+     * @param template template with ${json_path} or __DATE__{json_path} properties
+     * @param json json value used to lookup property values
+     * @return result of substituting property values in the template
+     * @throws NullPointerException if given null-valued argument
+     * @throws IllegalArgumentException if given invalid json string
+     */
+    public String apply(String template, String json) throws NullPointerException, IllegalArgumentException {
+        final JsonNode jsonTree;
+        try {
+            jsonTree = jsonbContext.getJsonTree(json);
+        } catch (JSONBException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        final Matcher matcher = pattern.matcher(template);
+        // StringBuilder cannot be used here because Matcher expects StringBuffer
+        final StringBuffer buffer = new StringBuffer();
+        while (matcher.find()) {
+            final String[] path = matcher.group(2).split("\\.");
+            String replacement = getReplacementValue(path, jsonTree);
+            if ("__DATE__".equals(matcher.group(1))) {
+                replacement = asDateString(replacement);
+            }
+            matcher.appendReplacement(buffer, Matcher.quoteReplacement(replacement));
+        }
+        matcher.appendTail(buffer);
+
+        return buffer.toString();
+    }
+
+    private String getReplacementValue(String[] pathElements, JsonNode jsonTree) {
+        final LinkedList<String> path = new LinkedList<>(Arrays.asList(pathElements));
+        if (path.isEmpty()) {
+            return EMPTY_VALUE;
+        }
+        final LinkedList<String> accumulator = new LinkedList<>();
+        getReplacementValue(path, jsonTree, accumulator);
+        return join(accumulator, "\n");
+    }
+
+    private void getReplacementValue(LinkedList<String> path, JsonNode jsonTree, LinkedList<String> accumulator) {
+        final JsonNode node;
+        if (jsonTree.isArray() || path.isEmpty()) {
+            node = jsonTree;
+        } else {
+            node = jsonTree.path(path.remove());
+        }
+
+        switch (node.getNodeType()) {
+            case ARRAY:
+                for (final JsonNode arrayElement : jsonTree) {
+                    // handle array value through recursive call
+                    getReplacementValue(new LinkedList<>(path), arrayElement, accumulator);
+                }
+                break;
+
+            case OBJECT:
+                // handle complex object through recursive call
+                getReplacementValue(path, node, accumulator);
+                break;
+
+            case STRING:
+            case NUMBER:
+                // Only simple value left ...
+                if (path.isEmpty()) {
+                    // extract value
+                    final String textValue = node.asText();
+                    accumulator.add(textValue.equals(Constants.MISSING_FIELD_VALUE) ? EMPTY_VALUE : textValue);
+                } else {
+                    // but path indicate still further steps
+                    accumulator.add(EMPTY_VALUE);
+                }
+                break;
+
+            case MISSING:
+            default:
+                break;
+        }
+    }
+
+    private String asDateString(String longValue) {
+        String dateString;
+        try {
+            final Long time = Long.valueOf(longValue);
+            dateString = new Date(time).toString();
+        } catch (Exception e) {
+            dateString = EMPTY_VALUE;
+        }
+        return dateString;
+    }
+
+    // sure wish we had java 8 by now
+    private String join(List<String> strings, String separator) {
+        final StringBuilder buffer = new StringBuilder();
+        String actualSeparator = "";
+        for (String s: strings) {
+            buffer.append(actualSeparator).append(s);
+            actualSeparator = separator;
+        }
+        return buffer.toString();
+    }
+}
---
+++ job-store-service/war/src/test/java/dk/dbc/dataio/jobstore/service/util/JsonValueTemplateEngineTest.java
@@ -0,0 +1,181 @@
+package dk.dbc.dataio.jobstore.service.util;
+
+import dk.dbc.dataio.commons.types.Constants;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class JsonValueTemplateEngineTest {
+    final JsonValueTemplateEngine templateEngine = new JsonValueTemplateEngine();
+    final String template = "key=${%s}";
+    final String expectedEmptyScalarOutput = "key=";
+    final String expectedScalarOutput = "key=text";
+    final String expectedArrayOutput = "key=text1\ntext2";
+
+    @Test
+    public void apply_templateArgIsNull_throws() {
+        try {
+            templateEngine.apply(null, "{}");
+            fail("No NullPointerException thrown");
+        } catch (NullPointerException e) {
+
+        }
+    }
+
+    @Test
+    public void apply_templateArgIsEmpty() {
+        assertThat(templateEngine.apply("", "{}"), is(""));
+    }
+
+    @Test
+    public void apply_jsonArgIsNull_throws() {
+        try {
+            templateEngine.apply(template, null);
+            fail("No NullPointerException thrown");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @Test
+    public void apply_jsonArgIsEmpty_throws() {
+        try {
+            templateEngine.apply(template, " ");
+            fail("No IllegalArgumentException thrown");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void apply_jsonArgIsInvalidJson_throws() {
+        try {
+            templateEngine.apply(template, "<>");
+            fail("No IllegalArgumentException thrown");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void apply_templateWithoutProperties() {
+        final String json = "{\"field\": \"text\"}";
+        final String expectedOutput = "unchanged";
+        final String output = templateEngine.apply(expectedOutput, json);
+        assertThat(output, is(expectedOutput));
+    }
+
+    @Test
+    public void apply_templateWithEmptyProperty() {
+        final String path = " ";
+        final String json = "{\"field\": \"text\"}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedEmptyScalarOutput));
+    }
+
+    @Test
+    public void apply_templateWithEmptyPathProperty() {
+        final String path = "...";
+        final String json = "{\"field\": \"text\"}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedEmptyScalarOutput));
+    }
+
+    @Test
+    public void apply_templateWithPropertySelectingValueAtZeroDepth() {
+        final String path = "field";
+        final String json = "{\"field\": \"text\"}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedScalarOutput));
+    }
+
+    @Test
+    public void apply_templateWithPropertySelectingValueAtDepth() {
+        final String path = "super.sub.field";
+        final String json = "{\"super\": {\"sub\": {\"field\": \"text\"}}}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedScalarOutput));
+    }
+
+    @Test
+    public void apply_templateWithNonMatchingProperty() {
+        final String path = "super.sub.field";
+        final String json = "{\"field\": \"text\"}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedEmptyScalarOutput));
+    }
+
+    @Test
+    public void apply_templateWithPropertySelectingArrayOfSimpleValues() {
+        final String path = "field";
+        final String json = "{\"field\": [\"text1\", \"text2\"]}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedArrayOutput));
+    }
+
+    @Test
+    public void apply_templateWithPropertySelectingArrayOfComplexValues() {
+        final String path = "field.value";
+        final String json = "{\"field\": [{\"value\": \"text1\"}, {\"value\": \"text2\"}]}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedArrayOutput));
+    }
+
+    @Test
+    public void apply_templateWithPropertySelectingValueWithMissingFieldValueConstant() {
+        final String path = "field";
+        final String json = "{\"field\": \"" + Constants.MISSING_FIELD_VALUE +"\"}";
+        final String output = templateEngine.apply(String.format(template, path), json);
+        assertThat(output, is(expectedEmptyScalarOutput));
+    }
+    
+    @Test
+    public void apply_templateWithMultipleProperties() {
+        final String template = "scalar=${fieldScalar}\narray=${fieldArray.value}";
+        final String json = "{\"fieldScalar\": \"text\", \"fieldArray\": [{\"value\": \"text1\"}, {\"value\": \"" + Constants.MISSING_FIELD_VALUE + "\"}, {\"value\": \"text2\"}]}";
+        final String output = templateEngine.apply(template, json);
+        assertThat(output, is("scalar=text\narray=text1\n\ntext2"));
+    }
+
+    @Test
+    public void apply_jsonValueContainsTemplateSpecialCharacters() {
+        final String template = "scalar=${field}";
+        final String json = "{\"field\": \"${text}\"}";
+        final String output = templateEngine.apply(template, json);
+        assertThat(output, is("scalar=${text}"));
+    }
+
+    @Test
+    public void apply_templateContainsDateMacro() {
+        final long dateTime = 1442554927498L;
+        final String dateString = "Fri Sep 18 07:42:07 CEST 2015";
+
+        final String template = "dateMacro=__DATE__{field}";
+        final String json = "{\"field\": " + dateTime +"}";
+        final String output = templateEngine.apply(template, json);
+        assertThat(output, is("dateMacro=" + dateString));
+    }
+
+    @Test
+    public void apply_templateContainsDateMacroSelectingNonNumberField() {
+        final String template = "dateMacro=__DATE__{field}";
+        final String json = "{\"field\": \"NaN\"}";
+        final String output = templateEngine.apply(template, json);
+        assertThat(output, is("dateMacro="));
+    }
+
+    @Test
+    public void apply_templateWithPropertySelectingInteger() {
+        final String template = "integer=${field}";
+        final String json = "{\"field\": 42}";
+        final String output = templateEngine.apply(template, json);
+        assertThat(output, is("integer=42"));
+    }
+
+    @Test
+    public void apply_templateWithPropertySelectingDouble() {
+        final String template = "double=${field}";
+        final String json = "{\"field\": 3.14}";
+        final String output = templateEngine.apply(template, json);
+        assertThat(output, is("double=3.14"));
+    }
+}

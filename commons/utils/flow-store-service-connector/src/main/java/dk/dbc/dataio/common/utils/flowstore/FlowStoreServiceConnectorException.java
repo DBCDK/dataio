9aec3006dd8e53bc09a751f4d5b723e1989c499e
Stine Mia Madsen <sma@dbc.dk>
2015-10-27 11:00:26 +0000

    [19401] US #694: FFU2RR: Respons på fejl i transfilindhold - Inkludering af FlowStoreError i FlowStoreServiceConnectorUnexpectedStatusCodeException
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@93648 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/utils/flow-store-service-connector/src/main/java/dk/dbc/dataio/common/utils/flowstore/FlowStoreServiceConnectorException.java
+++ commons/utils/flow-store-service-connector/src/main/java/dk/dbc/dataio/common/utils/flowstore/FlowStoreServiceConnectorException.java
@@ -25,7 +25,20 @@ public class FlowStoreServiceConnectorException extends Exception {
 
     private static final long serialVersionUID = -5384112996352499012L;
 
+    /**
+     * Constructs a new exception with the specified detail message
+     *
+     * The cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message detail message saved for later retrieval by the
+     *                {@link #getMessage()} method. May be null.
+     */
     public FlowStoreServiceConnectorException(String message) {
         super(message);
+    }
+
+    public FlowStoreServiceConnectorException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
--- commons/utils/flow-store-service-connector/src/main/java/dk/dbc/dataio/common/utils/flowstore/FlowStoreServiceConnectorUnexpectedStatusCodeException.java
+++ commons/utils/flow-store-service-connector/src/main/java/dk/dbc/dataio/common/utils/flowstore/FlowStoreServiceConnectorUnexpectedStatusCodeException.java
@@ -21,10 +21,13 @@
 
 package dk.dbc.dataio.common.utils.flowstore;
 
+import dk.dbc.dataio.commons.types.FlowStoreError;
+
 public class FlowStoreServiceConnectorUnexpectedStatusCodeException extends FlowStoreServiceConnectorException{
 
     private static final long serialVersionUID = -6137268696536418984L;
-    private int status;
+    private FlowStoreError flowStoreError;
+    private int statusCode;
 
     /**
      * Constructs a new exception with the specified detail message
@@ -35,17 +38,32 @@ public class FlowStoreServiceConnectorUnexpectedStatusCodeException extends Flow
      * @param message detail message saved for later retrieval by the
      *                {@link #getMessage()} method. May be null.
      *
-     * @param statusCode the http status code returned by the REST service
+     * @param statusCode the http statusCode code returned by the REST service
      */
     public FlowStoreServiceConnectorUnexpectedStatusCodeException(String message, int statusCode) {
         super(message);
-        status = statusCode;
+        this.statusCode = statusCode;
     }
 
     /**
-     * @return the status code
+     * @return the statusCode code
      */
     public int getStatusCode(){
-        return status;
+        return statusCode;
+    }
+
+    /**
+     * @return the flow store error
+     */
+    public FlowStoreError getFlowStoreError() {
+        return flowStoreError;
+    }
+
+    /**
+     * Sets the job error
+     * @param flowStoreError the flow store error to set
+     */
+    public void setFlowStoreError(FlowStoreError flowStoreError) {
+        this.flowStoreError = flowStoreError;
     }
 }

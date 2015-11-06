3b52d139033e3c0e178a7c267eb820835476845e
Steen Lillethorup Frederiksen <slf@dbc.dk>
2015-11-06 14:09:10 +0000

    Ifølge aftale med Jacob ignoreres denne test - Jacob kigger selv på den efter weekenden...
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@94253 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-store-service/war/src/test/java/dk/dbc/dataio/jobstore/service/entity/ItemEntityIT.java
+++ job-store-service/war/src/test/java/dk/dbc/dataio/jobstore/service/entity/ItemEntityIT.java
@@ -2,6 +2,7 @@ package dk.dbc.dataio.jobstore.service.entity;
 
 import dk.dbc.dataio.commons.utils.test.jpa.JPATestUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
@@ -14,6 +15,7 @@ import static org.junit.Assert.assertThat;
  * Created by ja7 on 02-11-15.
  * Test load af
  */
+@Ignore
 public class ItemEntityIT {
 
     private EntityManager em;

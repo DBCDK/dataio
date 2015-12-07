401d7b1b926ebf013c79748ff3756d2dc1546a19
Jan Bauer Nielsen <jbn@dbc.dk>
2015-12-07 13:15:08 +0000

    Jacob gi'r kage
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@95214 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-store-service/war/src/main/resources/db/migration/V18__foreige_key_refernces_for_chunk_and_item.sql
+++
@@ -1,7 +0,0 @@
-
-ALTER TABLE chunk DROP CONSTRAINT IF EXISTS jobid_fk;
-ALTER TABLE item DROP CONSTRAINT IF EXISTS jobid_chunk_fk;
-
-ALTER TABLE chunk ADD CONSTRAINT jobid_fk FOREIGN KEY (jobid) REFERENCES job (id) ON DELETE cascade;
-ALTER TABLE item ADD CONSTRAINT jobid_chunk_fk FOREIGN KEY (chunkid,jobid) REFERENCES chunk(id,jobid) on delete cascade;
-

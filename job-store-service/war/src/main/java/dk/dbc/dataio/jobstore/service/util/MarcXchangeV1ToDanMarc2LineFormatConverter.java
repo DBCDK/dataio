c393410f3eb3a329fa174ea164ec7da15d6800cd
Stine Mia Madsen <sma@dbc.dk>
2015-12-14 14:21:18 +0000

    [19650] US #706 FFU2RR: Tilbagemeldinger ved indholdsvalideringsfejl - nu med de fejlede poster og fejlbeskeder - Implementering af funktionalitet der gør det muligt at tilføje diagnostics til en marcRecord som subfields. Implementering af unit-test for MarcXchangeV1ToDanMarc2LineFormatConverter
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@95507 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/util/MarcXchangeV1ToDanMarc2LineFormatConverter.java
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/util/MarcXchangeV1ToDanMarc2LineFormatConverter.java
@@ -1,8 +1,32 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package dk.dbc.dataio.jobstore.service.util;
 
 import dk.dbc.dataio.commons.types.ChunkItem;
+import dk.dbc.dataio.commons.types.Diagnostic;
 import dk.dbc.dataio.jobstore.types.JobStoreException;
+import dk.dbc.dataio.marc.binding.DataField;
 import dk.dbc.dataio.marc.binding.MarcRecord;
+import dk.dbc.dataio.marc.binding.SubField;
 import dk.dbc.dataio.marc.reader.MarcReaderException;
 import dk.dbc.dataio.marc.reader.MarcXchangeV1Reader;
 import dk.dbc.dataio.marc.writer.DanMarc2LineFormatWriter;
@@ -11,6 +35,7 @@ import dk.dbc.dataio.marc.writer.MarcWriterException;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.Charset;
+import java.util.List;
 
 public class MarcXchangeV1ToDanMarc2LineFormatConverter implements ChunkItemConverter {
     final DanMarc2LineFormatWriter writer = new DanMarc2LineFormatWriter();
@@ -23,6 +48,9 @@ public class MarcXchangeV1ToDanMarc2LineFormatConverter implements ChunkItemConv
         } catch (MarcReaderException e) {
             throw new JobStoreException("Error reading chunk item data as MarcXchange", e);
         }
+
+        addDiagnosticsToMarcRecord(chunkItem.getDiagnostics(), record);
+
         try {
             return writer.write(record, encodedAs);
         } catch (MarcWriterException e) {
@@ -39,6 +67,16 @@ public class MarcXchangeV1ToDanMarc2LineFormatConverter implements ChunkItemConv
             return Charset.forName(chunkItem.getEncoding());
         } catch (Exception e) {
             throw new JobStoreException("Illegal encoding", e);
+        }
+    }
+
+    private void addDiagnosticsToMarcRecord(List<Diagnostic> diagnostics, MarcRecord record) {
+        if(diagnostics != null) {
+            for (Diagnostic diagnostic : diagnostics) {
+                SubField subField = new SubField().setCode('a').setData(diagnostic.getMessage());
+                DataField dataField = new DataField().setTag("e01").setInd1('0').setInd2('0').addSubfield(subField);
+                record.addField(dataField);
+            }
         }
     }
 }
---
+++ job-store-service/war/src/test/java/dk/dbc/dataio/jobstore/service/util/MarcXchangeV1ToDanMarc2LineFormatConverterTest.java
@@ -0,0 +1,179 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.jobstore.service.util;
+
+import dk.dbc.dataio.commons.types.ChunkItem;
+import dk.dbc.dataio.commons.types.Diagnostic;
+import dk.dbc.dataio.jobstore.types.JobStoreException;
+import dk.dbc.dataio.marc.binding.ControlField;
+import dk.dbc.dataio.marc.binding.DataField;
+import dk.dbc.dataio.marc.binding.Leader;
+import dk.dbc.dataio.marc.binding.MarcRecord;
+import dk.dbc.dataio.marc.binding.SubField;
+import dk.dbc.dataio.marc.reader.DanMarc2LineFormatReader;
+import dk.dbc.dataio.marc.reader.MarcReaderException;
+import dk.dbc.dataio.marc.writer.MarcWriterException;
+import dk.dbc.dataio.marc.writer.MarcXchangeV1Writer;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
+
+public class MarcXchangeV1ToDanMarc2LineFormatConverterTest {
+
+    private final String endTag = "$\n";
+    private final String expectedRecordAsLineFormat =
+            "245 12 *aA @*programmer is born*beveryday@@dbc\n" +
+            "530 00 *ithis is to be used in test\n";
+
+    private final String expectedDiagnosticSubfieldsAsLineFormat =
+            "e01 00 *aThis is the first diagnostic FATAL message\n" +
+            "e01 00 *aThis is the second diagnostic FATAL message\n" +
+            "e01 00 *aThis is the third diagnostic FATAL message\n";
+
+    @Test
+    public void MarcXchangeV1ToDanMarc2LineFormatConverter_convertInvalidMarc_throws() throws MarcReaderException, JobStoreException {
+        final ChunkItem chunkItem = buildChunkItem("invalid", ChunkItem.Status.FAILURE);
+        final MarcXchangeV1ToDanMarc2LineFormatConverter converter = new MarcXchangeV1ToDanMarc2LineFormatConverter();
+        try {
+            converter.convert(chunkItem, StandardCharsets.UTF_8);
+            fail("No JobStoreException thrown");
+        } catch (JobStoreException e) {
+            assertThat(e.getCause() instanceof MarcReaderException, is(true));
+        }
+    }
+
+    @Test
+    public void MarcXchangeV1ToDanMarc2LineFormatConverter_convertIllegalEncoding_throws() {
+        final ChunkItem chunkItem = buildChunkItem(asMarcXchange(getMarcRecord()), ChunkItem.Status.FAILURE, "unsupported");
+        final MarcXchangeV1ToDanMarc2LineFormatConverter converter = new MarcXchangeV1ToDanMarc2LineFormatConverter();
+        try {
+            converter.convert(chunkItem, StandardCharsets.UTF_8);
+            fail("No JobStoreException thrown");
+        } catch (JobStoreException e) {
+            assertThat(e.getCause() instanceof UnsupportedCharsetException, is(true));
+        }
+    }
+
+    @Test
+    public void MarcXchangeV1ToDanMarc2LineFormatConverter_convertMarcRecordContainingControlField_throws() {
+        final ChunkItem chunkItem = buildChunkItem(asMarcXchange(getMarcRecordWithControlFields()), ChunkItem.Status.FAILURE);
+        final MarcXchangeV1ToDanMarc2LineFormatConverter converter = new MarcXchangeV1ToDanMarc2LineFormatConverter();
+        try {
+            converter.convert(chunkItem, StandardCharsets.UTF_8);
+            fail("No JobStoreException thrown");
+        } catch (JobStoreException e) {
+            assertThat(e.getCause() instanceof MarcWriterException, is(true));
+        }
+    }
+
+    @Test
+    public void MarcXchangeV1ToDanMarc2LineFormatConverter_convertChunkItemWithDiagnostics_returnsDanmarc2LineFormatWithDiagnosticMessagesInSubfields() throws MarcReaderException, JobStoreException {
+        ChunkItem chunkItem = new ChunkItem(0, asMarcXchange(getMarcRecord()).getBytes(), ChunkItem.Status.FAILURE);
+        chunkItem.appendDiagnostics(new Diagnostic(Diagnostic.Level.FATAL, "This is the first diagnostic FATAL message"));
+        chunkItem.appendDiagnostics(new Diagnostic(Diagnostic.Level.FATAL, "This is the second diagnostic FATAL message"));
+        chunkItem.appendDiagnostics(new Diagnostic(Diagnostic.Level.FATAL, "This is the third diagnostic FATAL message"));
+
+        MarcXchangeV1ToDanMarc2LineFormatConverter converter = new MarcXchangeV1ToDanMarc2LineFormatConverter();
+
+        byte[] danmarc2LineFormat = converter.convert(chunkItem, StandardCharsets.UTF_8);
+        assertThat(asString(danmarc2LineFormat), is(expectedRecordAsLineFormat + expectedDiagnosticSubfieldsAsLineFormat + endTag));
+    }
+
+    @Test
+    public void MarcXchangeV1ToDanMarc2LineFormatConverter_convertChunkItemWithoutDiagnostics_returnsDanmarc2LineFormatWithoutDiagnosticMessagesInSubfields() throws MarcReaderException, JobStoreException {
+        ChunkItem chunkItem = new ChunkItem(0, asMarcXchange(getMarcRecord()).getBytes(), ChunkItem.Status.SUCCESS);
+
+        MarcXchangeV1ToDanMarc2LineFormatConverter converter = new MarcXchangeV1ToDanMarc2LineFormatConverter();
+
+        byte[] danmarc2LineFormat = converter.convert(chunkItem, StandardCharsets.UTF_8);
+        assertThat(asString(danmarc2LineFormat), is(expectedRecordAsLineFormat + endTag));
+    }
+
+
+    /*
+     * Private methods
+     */
+
+    private ChunkItem buildChunkItem(String data, ChunkItem.Status status) {
+        return new ChunkItem(0, data.getBytes(), status);
+    }
+
+    private ChunkItem buildChunkItem(String data, ChunkItem.Status status, String encoding) {
+        return new ChunkItem(0, data.getBytes(), status, new ArrayList<>(Collections.singletonList(ChunkItem.Type.UNKNOWN)), encoding);
+    }
+
+    private String asString(byte[] bytes) {
+        return asString(bytes, StandardCharsets.UTF_8);
+    }
+
+    private String asString(byte[] bytes, Charset encoding) {
+        return new String(bytes, encoding);
+    }
+
+    private MarcRecord getMarcRecord() {
+        final DataField dataField245 = new DataField()
+                .setTag("245")
+                .setInd1('1')
+                .setInd2('2')
+                .addSubfield(new SubField()
+                        .setCode('a')
+                        .setData("A *programmer is born"))
+                .addSubfield(new SubField()
+                        .setCode('b')
+                        .setData("everyday@dbc"));
+        final DataField dataField530 = new DataField()
+                .setTag("530")
+                .addSubfield(new SubField()
+                        .setCode('i')
+                        .setData("this is to be used in test"));
+        return new MarcRecord()
+                .setLeader(new Leader().setData(DanMarc2LineFormatReader.DEFAULT_LEADER))
+                .addAllFields(Arrays.asList(dataField245, dataField530));
+    }
+
+    private MarcRecord getMarcRecordWithControlFields() {
+        final ControlField f001 = new ControlField()
+                .setTag("001")
+                .setData("5 047 389 9");
+        final Leader leader = new Leader()
+                .setData("01471cjm a2200349 a 4500");
+        final MarcRecord marcRecord = new MarcRecord()
+                .setLeader(leader);
+        marcRecord.getFields().addAll(Arrays.asList(f001));
+        return marcRecord;
+    }
+
+    private String asMarcXchange(MarcRecord record) {
+        final MarcXchangeV1Writer writer = new MarcXchangeV1Writer()
+                .setProperty(MarcXchangeV1Writer.Property.ADD_XML_DECLARATION, Boolean.FALSE);
+        return new String(writer.write(record, StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+    }
+}

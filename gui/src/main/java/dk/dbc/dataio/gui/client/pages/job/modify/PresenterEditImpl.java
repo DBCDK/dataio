02a88d4b88cb0219c179529eadde066875f1eb7c
Stine Mia Madsen <sma@dbc.dk>
2015-12-17 15:17:58 +0000

    [19708] US #727: Sagsbehandlingsmarkeringer på postniveau - Fjernelse af CellPreviewEvent og tilføjelse af OnBrowserEvent for setWorkflowNote
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@95637 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/job/modify/PresenterEditImpl.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/job/modify/PresenterEditImpl.java
@@ -22,6 +22,7 @@
 package dk.dbc.dataio.gui.client.pages.job.modify;
 
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Window;
 import dk.dbc.dataio.gui.client.exceptions.FilteredAsyncCallback;
 import dk.dbc.dataio.gui.client.exceptions.ProxyErrorTranslator;
 import dk.dbc.dataio.gui.client.model.JobModel;
@@ -101,9 +102,6 @@ public class PresenterEditImpl <Place extends EditPlace> extends PresenterImpl {
                 setJobModel(jobModels.get(0));
                 updateAllFieldsAccordingToCurrentState();
             }
-//            else {
-                //prepareViewForJobNotFoundDisplay();
-//            }
         }
     }
 
@@ -123,6 +121,7 @@ public class PresenterEditImpl <Place extends EditPlace> extends PresenterImpl {
             getView().status.setText(getTexts().status_JobSuccesfullyRerun());
             setJobModel(jobModel);
             updateAllFieldsAccordingToCurrentState();
+            Window.alert("New JobId: " + jobModel.getJobId());
             History.back();
         }
     }

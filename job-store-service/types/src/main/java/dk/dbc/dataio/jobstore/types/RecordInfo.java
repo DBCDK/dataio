a5c474e19841f6cdd870703f7199c300c257dc30
Jan Bauer Nielsen <jbn@dbc.dk>
2016-01-13 11:23:04 +0000

    [19836] Added RecordInfo class.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@96081 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ job-store-service/types/src/main/java/dk/dbc/dataio/jobstore/types/RecordInfo.java
@@ -0,0 +1,63 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.jobstore.types;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
+
+/**
+ * This class contains information about a bibliographic record.
+ * Some time in the future this will also encompass keys for sequence analysis.
+ */
+public class RecordInfo {
+    protected final String id;
+
+    @JsonCreator
+    public RecordInfo(@JsonProperty("id") String id) throws NullPointerException, IllegalArgumentException {
+        this.id = InvariantUtil.checkNotNullNotEmptyOrThrow(id, "id");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        RecordInfo that = (RecordInfo) o;
+
+        return id.equals(that.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}

98b6ede9ae3cc66789c6cb773b5d58d9eff273bc
Jan Bauer Nielsen <jbn@dbc.dk>
2016-01-13 09:01:33 +0000

    MARC classes relocated to dbc-commons project.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@96064 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/utils/marc/src/main/java/dk/dbc/dataio/marc/writer/MarcWriterException.java
+++
@@ -1,9 +0,0 @@
-package dk.dbc.dataio.marc.writer;
-
-public class MarcWriterException extends Exception {
-    private static final long serialVersionUID = -2953151817802594255L;
-
-    public MarcWriterException(String message, Exception cause) {
-        super(message, cause);
-    }
-}

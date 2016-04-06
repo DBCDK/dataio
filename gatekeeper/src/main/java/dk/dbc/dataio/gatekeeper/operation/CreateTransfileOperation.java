f8b8628bb9f1fa2f415ec8a5d8ced6c1e826b77e
Jan Bauer Nielsen <jbn@dbc.dk>
2016-04-06 07:20:50 +0000

    [20192] Introduced one second wait period between CREATE_TRANSFILE operations to overcome posthus timestamp sorting limitation.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@98760 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gatekeeper/src/main/java/dk/dbc/dataio/gatekeeper/operation/CreateTransfileOperation.java
+++ gatekeeper/src/main/java/dk/dbc/dataio/gatekeeper/operation/CreateTransfileOperation.java
@@ -23,7 +23,6 @@ package dk.dbc.dataio.gatekeeper.operation;
 
 import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -31,6 +30,7 @@ import java.nio.file.StandardOpenOption;
 
 public class CreateTransfileOperation implements Operation {
     private static final Opcode OPCODE = Opcode.CREATE_TRANSFILE;
+    private static final long ONE_SECOND = 1000;
 
     final Path destination;
     final String content;
@@ -49,8 +49,11 @@ public class CreateTransfileOperation implements Operation {
     @Override
     public void execute() throws OperationExecutionException {
         try {
+            // Wait to overcome old POSTHUS limitation regarding transfile timestamp sorting
+            Thread.sleep(ONE_SECOND);
+
             Files.write(destination, content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE_NEW);
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new OperationExecutionException(e);
         }
     }

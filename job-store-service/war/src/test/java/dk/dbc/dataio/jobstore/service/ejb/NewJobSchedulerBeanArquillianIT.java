66157efa9b64dcf7b41ad793923bd1d6137b8ff3
Jacob Christian Poulsen <ja7@dbc.dk>
2016-04-28 12:11:06 +0000

    Small change test
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@99679 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-store-service/war/src/test/java/dk/dbc/dataio/jobstore/service/ejb/NewJobSchedulerBeanArquillianIT.java
+++ job-store-service/war/src/test/java/dk/dbc/dataio/jobstore/service/ejb/NewJobSchedulerBeanArquillianIT.java
@@ -121,6 +121,7 @@ public class NewJobSchedulerBeanArquillianIT {
             war.addAsLibraries(files);
 
 
+
             // Add DB Migrations
             for (File file : new File("src/main/resources/db/migration").listFiles()) {
                 if( file.getName().equals(".svn") ) { continue; };

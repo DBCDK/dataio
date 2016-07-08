c09623422adba2ddcbaf9edd5a32a4b0806d9566
Jan Bauer Nielsen <jbn@dbc.dk>
2016-07-08 12:25:54 +0000

    [20442] Fixed wrong Timestamp import in HarvestTask entity.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@102340 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- harvester/rr/src/main/java/dk/dbc/dataio/harvester/rr/entity/HarvestTask.java
+++ harvester/rr/src/main/java/dk/dbc/dataio/harvester/rr/entity/HarvestTask.java
@@ -33,7 +33,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import java.security.Timestamp;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Entity

da847e0a28333d452de824c7b3a7dc83ed07b86c
Jan Bauer Nielsen <jbn@dbc.dk>
2016-07-08 07:40:35 +0000

    [20442] Added configId attibute.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@102300 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- harvester/rr/src/main/resources/db/migration/V1__initial_schema.sql
+++ harvester/rr/src/main/resources/db/migration/V1__initial_schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE task (
     id                      SERIAL,
     timeOfCreation          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     timeOfCompletion        TIMESTAMP,
+    configId                BIGINT NOT NULL,
     submitterNumber         BIGINT NOT NULL,
     tag                     TEXT,
     status                  TEXT,
@@ -11,5 +12,6 @@ CREATE TABLE task (
     PRIMARY KEY (id)
 );
 CREATE INDEX task_timeOfCreation_index ON task(timeOfCreation);
+CREATE INDEX task_configId_index ON task(configId);
 CREATE INDEX task_tag_index ON task(tag);
 CREATE INDEX task_status_index ON task(status);

989c6b3492c27038e8933e05b4b6088f38badd25
Stine Mia Madsen <sma@dbc.dk>
2016-07-13 12:17:54 +0000

    Merge fra branch us798 into main
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@102543 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/EsSinkConfig.java
@@ -0,0 +1,86 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.commons.types;
+
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
+
+public class EsSinkConfig implements SinkConfig {
+
+    private int userId;
+    private String databaseName;
+    private String esAction;
+
+    public EsSinkConfig() {
+        this.esAction = "INSERT";
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public EsSinkConfig withUserId(int userId) throws IllegalArgumentException {
+        this.userId = userId;
+        return this;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public EsSinkConfig withDatabaseName(String databaseName) throws IllegalArgumentException {
+        this.databaseName = InvariantUtil.checkNotNullNotEmptyOrThrow(databaseName, "databaseName");
+        return this;
+    }
+
+    public String getEsAction() {
+        return esAction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EsSinkConfig)) return false;
+
+        EsSinkConfig that = (EsSinkConfig) o;
+
+        return userId == that.userId
+                && (databaseName != null ? databaseName.equals(that.databaseName) : that.databaseName == null
+                && (esAction != null ? esAction.equals(that.esAction) : that.esAction == null));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId;
+        result = 31 * result + (databaseName != null ? databaseName.hashCode() : 0);
+        result = 31 * result + (esAction != null ? esAction.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "EsSinkConfig{" +
+                "userId=" + userId +
+                ", databaseName='" + databaseName + '\'' +
+                ", esAction='" + esAction + '\'' +
+                '}';
+    }
+}
---
+++ commons/types/src/test/java/dk/dbc/dataio/commons/types/EsSinkConfigTest.java
@@ -0,0 +1,58 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.commons.types;
+
+import dk.dbc.dataio.jsonb.JSONBContext;
+import dk.dbc.dataio.jsonb.JSONBException;
+import org.junit.Test;
+
+import static dk.dbc.commons.testutil.Assert.assertThat;
+import static dk.dbc.commons.testutil.Assert.isThrowing;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class EsSinkConfigTest {
+
+    private static final int USER_ID = 0;
+    private static final String DATABASE_NAME = "databaseName";
+
+    @Test
+    public void constructor_databaseArgIsNull_throws() {
+        assertThat(() -> new EsSinkConfig().withDatabaseName(null), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void constructor_allArgsAreValid_returnsNewInstanceWithDefaultValuesSet() {
+        final EsSinkConfig esSinkConfig = new EsSinkConfig().withUserId(USER_ID).withDatabaseName(DATABASE_NAME);
+        assertThat(esSinkConfig.getUserId(), is(USER_ID));
+        assertThat(esSinkConfig.getDatabaseName(), is(DATABASE_NAME));
+        assertThat(esSinkConfig.getEsAction(), is("INSERT"));
+    }
+
+    @Test
+    public void marshalling() throws JSONBException {
+        final JSONBContext jsonbContext = new JSONBContext();
+        final EsSinkConfig esSinkConfig = new EsSinkConfig();
+        final EsSinkConfig unmarshalled = jsonbContext.unmarshall(jsonbContext.marshall(esSinkConfig), EsSinkConfig.class);
+        assertThat(unmarshalled, is(esSinkConfig));
+    }
+}

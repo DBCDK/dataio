6300e3a781e83407c2138351b288aaaf448a0ccf
Steen Lillethorup Frederiksen <slf@dbc.dk>
2016-08-08 14:56:58 +0000

    [20493] US #951: Slettefunktionalitet i GUI'en - Refaktorering af PopupBox - opdeling i ren dialog box og HasValue box
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@103186 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- gui/src/main/java/dk/dbc/dataio/gui/client/components/PopupValueBox.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/components/PopupValueBox.java
@@ -21,64 +21,42 @@
 
 package dk.dbc.dataio.gui.client.components;
 
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Focusable;
 import com.google.gwt.user.client.ui.HasValue;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
-import dk.dbc.dataio.gui.client.events.DialogEvent;
-import dk.dbc.dataio.gui.client.events.DialogHandler;
-import dk.dbc.dataio.gui.client.events.HasDialogHandlers;
 
 /**
- * <p>Popup box for entering a widget in a popup window </p>
- * <p>The popup box consist of a header, the widget itself, and up to three buttons:</p>
- *  <ul>
- *   <li>OK button</li>
- *   <li>Cancel button</li>
- *   <li>Extra button</li>
- *  </ul>
- * <p> All buttons have user definable labels</p>
+ * <p>Popup value box for displaying a modifiable widget in a popup window - in this context,
+ * a modifiable widget is a widget, that embeds a value, that can be changed (ie. implements HasValue)</p>
+ * <p>The popup value box extends PopupBox, and therefore is displayed and handled as such.</p>
  *
  * @param <W> The class defining the widget to be embedded (eg. TextBox)
- * @param <T> The class defining the type of the entered value
+ * @param <T> The class defining the type of the entered value (eg. String)
  */
-public class PopupValueBox<W extends HasValue<T> & Focusable, T> extends Composite implements HasValue<T>, HasDialogHandlers {
-    public static final String POPUP_BOX_GUID = "dio-PopupValueBox";
-
+public class PopupValueBox<W extends HasValue<T> & IsWidget & Focusable, T> extends PopupBox<W> implements HasValue<T> {
     ValueChangeHandler<T> valueChangeHandler = null;  // This is package private because of test - should be private
-    DialogHandler dialogHandler = null;  // This is package private because of test - should be private
-
-    FlowPanel basePanel;
-    DialogBox dialogBox;
-    VerticalPanel containerPanel;
-    FlowPanel buttonPanel;
-    Button okButton;
-    Button cancelButton;
-    Button extraButton;
-    protected W widget;
 
 
     /**
-     * Constructor
+     * Default constructor
      *
      * @param widget The widget to be embedded in a Popup Box
      * @param dialogTitle  The title text to display on the Dialog Box (mandatory)
      * @param okButtonText The text to be displayed in the OK Button (mandatory)
      */
     public PopupValueBox(W widget, String dialogTitle, String okButtonText) {
-        this(widget, dialogTitle, okButtonText, new FlowPanel(), new DialogBox(), new VerticalPanel(), new FlowPanel(), new Button(), new Button(), new Button());
+        super(widget, dialogTitle, okButtonText);
     }
 
     /**
-     * Constructor (with componente injections - to be used for testing)
+     * Constructor (with component injections - to be used for testing)
      * The Constructor is package scoped - not public
      *
      * @param widget The widget to be embedded in a Popup Box
@@ -93,140 +71,17 @@ public class PopupValueBox<W extends HasValue<T> & Focusable, T> extends Composi
      * @param extraButton The Extra Button
      */
     PopupValueBox(W widget,
-                  String dialogTitle,
-                  String okButtonText,
-                  FlowPanel basePanel,
-                  DialogBox dialogBox,
-                  VerticalPanel containerPanel,
-                  FlowPanel buttonPanel,
-                  Button okButton,
-                  Button cancelButton,
-                  Button extraButton) {
-        // Data injection
-        this.widget = widget;
-        this.basePanel = basePanel;
-        this.dialogBox = dialogBox;
-        this.containerPanel = containerPanel;
-        this.buttonPanel = buttonPanel;
-        this.okButton = okButton;
-        this.cancelButton = cancelButton;
-        this.extraButton = extraButton;
-
-        // Setup texts
-        dialogBox.setText(dialogTitle);
-        setButton(okButton, okButtonText);
-        setButton(cancelButton, "");  // The Cancel Button is optional, so no text is added yet - meaning until that is done, it is not set
-        setButton(extraButton, "");  // The Extra Button is optional, so no text is added yet - meaning until that is done, it is not set
-
-        // Setup click event handlers for the buttons
-        okButton.addClickHandler(this::okClickHandler);
-        cancelButton.addClickHandler(this::cancelClickHandler);
-        extraButton.addClickHandler(this::extraClickHandler);
-
-        // Build the widget tree
-        buttonPanel.add(okButton);
-        buttonPanel.add(cancelButton);
-        buttonPanel.add(extraButton);
-        containerPanel.add((Widget) widget);
-        containerPanel.add(buttonPanel);
-        dialogBox.add(containerPanel);
-        basePanel.add(dialogBox);
-        initWidget(basePanel);
-
-        // Set CSS Stuff
-        dialogBox.addStyleName(POPUP_BOX_GUID);
-
-        // Assure, that the dialogbox is included in the DOM
-        dialogBox.setAutoHideEnabled(true);
-        dialogBox.setModal(true);
-        boolean animationEnabled = dialogBox.isAnimationEnabled();
-        dialogBox.setAnimationEnabled(false);  // Assure, that the dialogbox will not blink upon startup when executing the next two statements...
-        show();  // First show the DialogBox in order to add it to the DOM
-        hide();  // ... but we don't want it shown upon startup - so hide it again
-        dialogBox.setAnimationEnabled(animationEnabled);
+             String dialogTitle,
+             String okButtonText,
+             FlowPanel basePanel,
+             DialogBox dialogBox,
+             VerticalPanel containerPanel,
+             FlowPanel buttonPanel,
+             Button okButton,
+             Button cancelButton,
+             Button extraButton) {
+        super(widget, dialogTitle, okButtonText, basePanel, dialogBox, containerPanel, buttonPanel, okButton, cancelButton, extraButton);
     }
-
-    /**
-     * Sets the Title of the Dialog
-     * @param title The Dialog Title
-     */
-    public void setDialogTitle(String title) {
-        dialogBox.setText(title);
-    }
-
-    /**
-     * Sets the OK Button Text
-     * @param value The text to display on the button
-     */
-    public void setOkButtonText(String value) {
-        setButton(okButton, value);
-    }
-
-    /**
-     * Sets the Cancel Button Text
-     * Optional setting in the UI Binder activation
-     * @param value The text to display on the button
-     */
-    public void setCancelButtonText(String value) {
-        setButton(cancelButton, value);
-    }
-
-    /**
-     * Sets the Extra Button Text
-     * Optional setting in the UI Binder activation
-     * @param value The text to display on the button
-     */
-    public void setExtraButtonText(String value) {
-        setButton(extraButton, value);
-    }
-
-    /**
-     * Sets the Guid for this element
-     * Optional setting in the UI Binder activation
-     * @param guid The Guid for this element
-     */
-    public void setGuid(String guid) {
-        if (!guid.isEmpty()) {
-            dialogBox.getElement().setId(guid);
-        }
-    }
-
-
-    /**
-     * Click Handlers
-     */
-
-    /**
-     * Ok button Click Handler
-     *
-     * @param clickEvent The click event for the OK Button
-     */
-    public void okClickHandler(ClickEvent clickEvent) {
-        triggerDialogEvent(DialogEvent.DialogButton.OK_BUTTON);
-        hide();
-        triggerValueChangeEvent();
-    }
-
-    /**
-     * Cancel button Click Handler
-     *
-     * @param clickEvent The click event for the Cancel Button
-     */
-    public void cancelClickHandler(ClickEvent clickEvent) {
-        triggerDialogEvent(DialogEvent.DialogButton.CANCEL_BUTTON);
-        hide();
-    }
-
-    /**
-     * Extra button Click Handler
-     *
-     * @param clickEvent The click event for the Cancel Button
-     */
-    public void extraClickHandler(ClickEvent clickEvent) {
-        triggerDialogEvent(DialogEvent.DialogButton.EXTRA_BUTTON);
-        hide();
-    }
-
 
     /*
      * Public methods
@@ -237,16 +92,8 @@ public class PopupValueBox<W extends HasValue<T> & Focusable, T> extends Composi
      */
     public void show() {
         widget.setValue(null);
-        dialogBox.center();
-        dialogBox.show();
+        super.show();
         widget.setFocus(true);
-    }
-
-    /**
-     * Hides the popup and detaches it from the page. This has no effect if it is not currently showing.
-     */
-    public void hide() {
-        dialogBox.hide();
     }
 
 
@@ -301,41 +148,9 @@ public class PopupValueBox<W extends HasValue<T> & Focusable, T> extends Composi
     }
 
 
-     /*
-     * HasDialogHandlers overrides
-     */
-
-    /**
-     * Adds a Dialog handler to be fired upon click on one of the three buttons
-     *
-     * @param handler The new DialogHandler to service
-     * @return A Handler Registration object
-     */
-    @Override
-    public HandlerRegistration addDialogHandler(DialogHandler handler) {
-        dialogHandler = handler;
-        return () -> dialogHandler = null;
-    }
-
     /*
      * Private methods
      */
-
-    /**
-     * Prepares a button on the dialog box
-     * @param button The button to prepare
-     * @param buttonText The text to display on the button
-     */
-    private void setButton(Button button, String buttonText) {
-        if (buttonText.isEmpty()) {
-            button.setEnabled(false);
-            button.setVisible(false);
-        } else {
-            button.setText(buttonText);
-            button.setEnabled(true);
-            button.setVisible(true);
-        }
-    }
 
     /**
      * Triggers a Value Change Event - but only if there is an eventhandler registered
@@ -343,20 +158,6 @@ public class PopupValueBox<W extends HasValue<T> & Focusable, T> extends Composi
     private void triggerValueChangeEvent() {
         if (valueChangeHandler != null) {
             valueChangeHandler.onValueChange(new ValueChangeEvent<T>(getValue()) {});
-        }
-    }
-
-    /**
-     * Triggers a ClickEvent
-     */
-    private void triggerDialogEvent(DialogEvent.DialogButton button) {
-        if (dialogHandler != null) {
-            dialogHandler.onDialogButtonClick(new DialogEvent() {
-                @Override
-                public DialogButton getDialogButton() {
-                    return button;
-                }
-            });
         }
     }
 
--- gui/src/test/java/dk/dbc/dataio/gui/client/components/PopupValueBoxTest.java
+++ gui/src/test/java/dk/dbc/dataio/gui/client/components/PopupValueBoxTest.java
@@ -21,9 +21,6 @@
 
 package dk.dbc.dataio.gui.client.components;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
@@ -56,7 +53,6 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(GwtMockitoTestRunner.class)
 public class PopupValueBoxTest {
-    @Mock KeyPressEvent mockedKeyPressEvent;
     @Mock TextBox mockedWidget;
     @Mock FlowPanel mockedBasePanel;
     @Mock DialogBox mockedDialogBox;
@@ -65,15 +61,12 @@ public class PopupValueBoxTest {
     @Mock Button mockedOkButton;
     @Mock Button mockedCancelButton;
     @Mock Button mockedExtraButton;
-    @Mock ClickEvent mockedClickEvent;
     @Mock ValueChangeHandler mockedValueChangeHandler;
-    @SuppressWarnings("deprecation")
-    @Mock com.google.gwt.user.client.Element mockedElement;
 
     /**
      * Subject Under Test
      */
-    PopupValueBox popupValueBox;
+    PopupValueBox<TextBox, String> popupValueBox;
 
 
     /**
@@ -86,307 +79,30 @@ public class PopupValueBoxTest {
         setupTest();
 
         // Test verification
-        constructorVerification();
-        noMoreMockInvocations();
+        verify(mockedWidget).setValue(null);
+        verify(mockedWidget).setFocus(true);
+        verifyNoMoreInteractions(mockedWidget);
     }
 
-    @Test
-    public void setDialogTitle_nullValue_exception() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setDialogTitle(null);
-
-        // Test Verification
-        verify(popupValueBox.dialogBox).setText(null);
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void setDialogTitle_emptyValue_buttonIsDisabled() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setDialogTitle("");
-
-        // Test Verification
-        verify(popupValueBox.dialogBox).setText("");
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void setDialogTitle_validValue_textIsSetCorrectly() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setDialogTitle("-Dialog-Title-");
-
-        // Test Verification
-        verify(popupValueBox.dialogBox).setText("-Dialog-Title-");
-        noMoreMockInvocations();
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void setOkButtonText_nullValue_exception() {
-        // Test Preparation
-        setupTest();
-
-        // Activate Subject Under Test
-        popupValueBox.setOkButtonText(null);
-    }
-
-    @Test
-    public void setOkButtonText_emptyValue_buttonIsDisabled() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setOkButtonText("");
-
-        // Test Verification
-        verify(mockedOkButton).setEnabled(false);
-        verify(mockedOkButton).setVisible(false);
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void setOkButtonText_validValue_textIsSetCorrectly() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setOkButtonText("-Ok-Button-Text-");
-
-        // Test Verification
-        verify(mockedOkButton).setText("-Ok-Button-Text-");
-        verify(mockedOkButton, times(2)).setEnabled(true);
-        verify(mockedOkButton, times(2)).setVisible(true);
-        noMoreMockInvocations();
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void setCancelButtonText_nullValue_exception() {
-        // Test Preparation
-        setupTest();
-
-        // Activate Subject Under Test
-        popupValueBox.setCancelButtonText(null);
-    }
-
-    @Test
-    public void setCancelButtonText_emptyValue_buttonIsDisabled() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setCancelButtonText("");
-
-        // Test Verification
-        verify(mockedCancelButton, times(2)).setEnabled(false);
-        verify(mockedCancelButton, times(2)).setVisible(false);
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void setCancelButtonText_validValue_textIsSetCorrectly() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setCancelButtonText("-Cancel-Button-Text-");
-
-        // Test Verification
-        verify(mockedCancelButton).setText("-Cancel-Button-Text-");
-        verify(mockedCancelButton).setEnabled(true);
-        verify(mockedCancelButton).setVisible(true);
-        noMoreMockInvocations();
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void setExtraButtonText_nullValue_exception() {
-        // Test Preparation
-        setupTest();
-
-        // Activate Subject Under Test
-        popupValueBox.setExtraButtonText(null);
-    }
-
-    @Test
-    public void setExtraButtonText_emptyValue_buttonIsDisabled() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setExtraButtonText("");
-
-        // Test Verification
-        verify(mockedExtraButton, times(2)).setEnabled(false);
-        verify(mockedExtraButton, times(2)).setVisible(false);
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void setExtraButtonText_validValue_textIsSetCorrectly() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setExtraButtonText("-Extra-Button-Text-");
-
-        // Test Verification
-        verify(mockedExtraButton).setText("-Extra-Button-Text-");
-        verify(mockedExtraButton).setEnabled(true);
-        verify(mockedExtraButton).setVisible(true);
-        noMoreMockInvocations();
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void setGuid_nullValue_exception() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.setGuid(null);
-    }
-
-    @Test
-    public void setGuid_emptyValue_noAction() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-        when(mockedDialogBox.getElement()).thenReturn(mockedElement);
-
-        // Activate Subject Under Test
-        popupValueBox.setGuid("");
-
-        // Test Verification
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void setGuid_validValue_setGuid() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-        when(mockedDialogBox.getElement()).thenReturn(mockedElement);
-
-        // Activate Subject Under Test
-        popupValueBox.setGuid("gui ID");
-
-        // Test Verification
-        verify(mockedDialogBox).getElement();
-        verify(mockedElement).setId("gui ID");
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void okClickHandler_noValueChangeHandler_onlyHide() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.okClickHandler(mockedClickEvent);
-
-        // Test Verification
-        verify(mockedDialogBox, times(2)).hide();
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void okClickHandler_validValueChangeHandler_bothHideAndTriggerEvent() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-        popupValueBox.addValueChangeHandler(mockedValueChangeHandler);
-
-        // Activate Subject Under Test
-        popupValueBox.okClickHandler(mockedClickEvent);
-
-        // Test Verification
-        verify(mockedDialogBox, times(2)).hide();
-        verify(mockedWidget).getValue();
-        verify(mockedValueChangeHandler).onValueChange(any(ValueChangeEvent.class));
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void cancelClickHandler_call_hide() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.cancelClickHandler(mockedClickEvent);
-
-        // Test Verification
-        verify(mockedDialogBox, times(2)).hide();
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void extraClickHandler_call_hide() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.extraClickHandler(mockedClickEvent);
-
-        // Test Verification
-        verify(mockedDialogBox, times(2)).hide();
-        noMoreMockInvocations();
-    }
 
     @Test
     public void show_call_show() {
         // Test Preparation
         setupTest();
-        constructorVerification();
 
         // Activate Subject Under Test
         popupValueBox.show();
 
         // Test Verification
         verify(mockedWidget, times(2)).setValue(null);
-        verify(mockedDialogBox, times(2)).center();
-        verify(mockedDialogBox, times(2)).show();
         verify(mockedWidget, times(2)).setFocus(true);
-        noMoreMockInvocations();
-    }
-
-    @Test
-    public void hide_call_hide() {
-        // Test Preparation
-        setupTest();
-        constructorVerification();
-
-        // Activate Subject Under Test
-        popupValueBox.hide();
-
-        // Test Verification
-        verify(mockedDialogBox, times(2)).hide();
-        noMoreMockInvocations();
+        verifyNoMoreInteractions(mockedWidget);
     }
 
     @Test
     public void getValue_call_getTheValue() {
         // Test Preparation
         setupTest();
-        constructorVerification();
         when(mockedWidget.getValue()).thenReturn("correct value");
 
         // Activate Subject Under Test
@@ -395,28 +111,30 @@ public class PopupValueBoxTest {
         // Test Verification
         assertThat(value, is("correct value"));
         verify(mockedWidget).getValue();
-        noMoreMockInvocations();
+        verify(mockedWidget).setValue(null);
+        verify(mockedWidget).setFocus(true);
+        verifyNoMoreInteractions(mockedWidget);
     }
 
     @Test
     public void setValue_call_newValueSet() {
         // Test Preparation
         setupTest();
-        constructorVerification();
 
         // Activate Subject Under Test
         popupValueBox.setValue("new value");
 
         // Test Verification
         verify(mockedWidget).setValue("new value");
-        noMoreMockInvocations();
+        verify(mockedWidget).setValue(null);
+        verify(mockedWidget).setFocus(true);
+        verifyNoMoreInteractions(mockedWidget);
     }
 
     @Test
     public void setValue_callWithFireEvent_newValueSetAndFireEvent() {
         // Test Preparation
         setupTest();
-        constructorVerification();
         popupValueBox.addValueChangeHandler(mockedValueChangeHandler);
 
         // Activate Subject Under Test
@@ -424,17 +142,17 @@ public class PopupValueBoxTest {
 
         // Test Verification
         verify(mockedWidget).setValue("new value", true);
-        verify(mockedDialogBox).hide();
+        verify(mockedWidget).setValue(null);
+        verify(mockedWidget).setFocus(true);
         verify(mockedWidget).getValue();
         verify(mockedValueChangeHandler).onValueChange(any(ValueChangeEvent.class));
-        noMoreMockInvocations();
+        verifyNoMoreInteractions(mockedWidget);
     }
 
     @Test
     public void addValueChangeHandler_removeHandler_handlerRemoved() {
         // Test Preparation
         setupTest();
-        constructorVerification();
         HandlerRegistration registration = popupValueBox.addValueChangeHandler(mockedValueChangeHandler);
         assertThat(popupValueBox.valueChangeHandler, is(notNullValue()));
 
@@ -451,7 +169,7 @@ public class PopupValueBoxTest {
      */
 
     private void setupTest() {
-        popupValueBox = new PopupValueBox(
+        popupValueBox = new PopupValueBox<TextBox, String>(
                 mockedWidget,
                 "Dialog Title",
                 "Ok Button Text",
@@ -462,50 +180,6 @@ public class PopupValueBoxTest {
                 mockedOkButton,
                 mockedCancelButton,
                 mockedExtraButton);
-    }
-
-    private void constructorVerification() {
-        verify(mockedDialogBox).setText("Dialog Title");
-        verify(mockedOkButton).setText("Ok Button Text");
-        verify(mockedOkButton).setEnabled(true);
-        verify(mockedOkButton).setVisible(true);
-        verify(mockedCancelButton).setEnabled(false);
-        verify(mockedCancelButton).setVisible(false);
-        verify(mockedExtraButton).setEnabled(false);
-        verify(mockedExtraButton).setVisible(false);
-        verify(mockedOkButton).addClickHandler(any(ClickHandler.class));
-        verify(mockedCancelButton).addClickHandler(any(ClickHandler.class));
-        verify(mockedExtraButton).addClickHandler(any(ClickHandler.class));
-        verify(mockedButtonPanel).add(mockedOkButton);
-        verify(mockedButtonPanel).add(mockedCancelButton);
-        verify(mockedButtonPanel).add(mockedExtraButton);
-        verify(mockedContainerPanel).add(mockedWidget);
-        verify(mockedContainerPanel).add(mockedButtonPanel);
-        verify(mockedDialogBox).add(mockedContainerPanel);
-        verify(mockedBasePanel).add(mockedDialogBox);
-        verify(mockedDialogBox).addStyleName("dio-PopupValueBox");
-        verify(mockedDialogBox).setAutoHideEnabled(true);
-        verify(mockedDialogBox).setModal(true);
-        verify(mockedDialogBox).isAnimationEnabled();
-        verify(mockedDialogBox, times(2)).setAnimationEnabled(false);
-        verify(mockedWidget).setValue(null);
-        verify(mockedDialogBox).center();
-        verify(mockedDialogBox).show();
-        verify(mockedWidget).setFocus(true);
-        verify(mockedDialogBox).hide();
-    }
-
-    private void noMoreMockInvocations() {
-        verifyNoMoreInteractions(mockedDialogBox);
-        verifyNoMoreInteractions(mockedOkButton);
-        verifyNoMoreInteractions(mockedCancelButton);
-        verifyNoMoreInteractions(mockedExtraButton);
-        verifyNoMoreInteractions(mockedButtonPanel);
-        verifyNoMoreInteractions(mockedContainerPanel);
-        verifyNoMoreInteractions(mockedBasePanel);
-        verifyNoMoreInteractions(mockedWidget);
-        verifyNoMoreInteractions(mockedKeyPressEvent);
-        verifyNoMoreInteractions(mockedElement);
     }
 
 }

cd9a15bf8c81ed1391829c8f7cf508c180f67892
Jan Bauer Nielsen <jbn@dbc.dk>
2016-08-17 06:58:02 +0000

    [20509] Added holdings-items-connector module.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@103388 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ harvester/utils/holdings-items-connector/src/main/java/dk/dbc/dataio/harvester/utils/holdingsitems/HoldingsItemsConnector.java
@@ -0,0 +1,105 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.harvester.utils.holdingsitems;
+
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.params.GroupParams;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * This class provides an API to an holdings-items Solr server.
+ * This class is thread safe.
+ */
+public class HoldingsItemsConnector {
+    public final static String AGENCY_ID_FIELD = "holdingsitem.agencyId";
+    public final static String BIBLIOGRAPHIC_RECORD_ID_FIELD = "holdingsitem.bibliographicRecordId";
+
+    private final SolrServer server;
+
+    public HoldingsItemsConnector(String solrServerEndpoint) throws NullPointerException, IllegalArgumentException {
+        server = new HttpSolrServer(InvariantUtil.checkNotNullNotEmptyOrThrow(solrServerEndpoint, "solrServerEndpoint"));
+    }
+
+    public void close() {
+        server.shutdown();
+    }
+
+    /**
+     * Tests for holdings for a given bibliographic record ID
+     * @param bibliographicRecordId ID of record for which to query
+     * @param agencyIds Set of agency IDs for which to limit the query, if empty all agencies are taken into account
+     * @return Set of IDs for agencies with holdings
+     * @throws NullPointerException if given null valued bibliographicRecordId or agencyIds arguments
+     * @throws IllegalArgumentException if given empty valued bibliographicRecordId argument
+     * @throws HoldingsItemsConnectorException on failure to communicate with holdings items server
+     */
+    public Set<Integer> hasHoldings(String bibliographicRecordId, Set<Integer> agencyIds)
+            throws NullPointerException, IllegalArgumentException, HoldingsItemsConnectorException {
+        InvariantUtil.checkNotNullNotEmptyOrThrow(bibliographicRecordId, "bibliographicRecordId");
+        InvariantUtil.checkNotNullOrThrow(agencyIds, "agencyIds");
+
+        final SolrQuery query = getHasHoldingsQuery(bibliographicRecordId, agencyIds);
+        return executeQuery(query).getResults().stream()
+                .map(doc -> (Integer) doc.getFieldValue(AGENCY_ID_FIELD))
+                .filter(agencyId -> agencyId != null)
+                .filter(agencyId -> agencyId > 0)
+                .collect(Collectors.toSet());
+    }
+
+    private SolrQuery getHasHoldingsQuery(String bibliographicRecordId, Set<Integer> agencyIds) {
+        final SolrQuery query = new SolrQuery();
+        query.setQuery(getHasHoldingsQueryString(bibliographicRecordId, agencyIds));
+        query.set(GroupParams.GROUP, true);
+        query.set(GroupParams.GROUP_MAIN, true);
+        query.set(GroupParams.GROUP_FIELD, AGENCY_ID_FIELD);
+        query.setFields(AGENCY_ID_FIELD);
+        query.setRows(agencyIds.isEmpty() ? 9999 : agencyIds.size());
+        return query;
+    }
+
+    private String getHasHoldingsQueryString(String bibliographicRecordId, Set<Integer> agencyIds) {
+        return String.format("%s:%s%s", BIBLIOGRAPHIC_RECORD_ID_FIELD, bibliographicRecordId, getAgencyIdQueryStringFilter(agencyIds));
+    }
+
+    private String getAgencyIdQueryStringFilter(Set<Integer> agencyIds) {
+        if (!agencyIds.isEmpty()) {
+            return String.format(" AND %s:(%s)", AGENCY_ID_FIELD, agencyIds.stream()
+                    .map(Object::toString).collect(Collectors.joining(" OR ")));
+        }
+        return "";
+    }
+
+    private QueryResponse executeQuery(SolrQuery query) throws HoldingsItemsConnectorException {
+        try {
+            return server.query(query);
+        } catch (SolrServerException e) {
+            throw new HoldingsItemsConnectorException(e);
+        }
+    }
+}
---
+++ harvester/utils/holdings-items-connector/src/test/java/dk/dbc/dataio/harvester/utils/holdingsitems/HoldingsItemsConnectorTest.java
@@ -0,0 +1,116 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.harvester.utils.holdingsitems;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static dk.dbc.commons.testutil.Assert.assertThat;
+import static dk.dbc.commons.testutil.Assert.isThrowing;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class HoldingsItemsConnectorTest {
+    /*
+        Steps to reproduce wiremock recording:
+
+        * Start standalone runner
+            java -jar wiremock-1.57-standalone.jar --proxy-all="http://cisterne-solr.dbc.dk:8014/solr/beholdplus" --record-mappings --verbose
+
+        * Record resultset for hasHoldings query without hits
+            curl "http://localhost:8080/select?q=holdingsitem.bibliographicRecordId%3Ano-such-record-id&group=true&group.main=true&group.field=holdingsitem.agencyId&fl=holdingsitem.agencyId&rows=9999&wt=javabin&version=2"
+
+        * Record resultset for hasHoldings query without agencyIds filter
+            curl "http://localhost:8080/select?q=holdingsitem.bibliographicRecordId%3A23181444&group=true&group.main=true&group.field=holdingsitem.agencyId&fl=holdingsitem.agencyId&rows=9999&wt=javabin&version=2"
+
+        * Record resultset for hasHoldings query with agencyIds filter
+            curl "http://localhost:8998/select?q=holdingsitem.bibliographicRecordId%3A23181444+AND+holdingsitem.agencyId%3A%28771000+OR+111111+OR+767100%29&group=true&group.main=true&group.field=holdingsitem.agencyId&fl=holdingsitem.agencyId&rows=3&wt=javabin&version=2"
+     */
+
+    private static final String WIREMOCK_PORT = System.getProperty("wiremock.port", "8998");
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(Integer.valueOf(WIREMOCK_PORT));
+
+    private final String solrServerEndpoint = String.format("http://localhost:%s/", WIREMOCK_PORT);
+    private final String bibliographicRecordId = "23181444";
+
+    @Test(expected = NullPointerException.class)
+    public void constructor_solrServerEndpointArgIsNull_throws() {
+        new HoldingsItemsConnector(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructor_solrServerEndpointArgIsEmpty_throws() {
+        new HoldingsItemsConnector(" ");
+    }
+
+    @Test
+    public void hasHoldings_bibliographicRecordIdArgIsNull_throws() {
+        final HoldingsItemsConnector connector = createConnector();
+        assertThat(() -> connector.hasHoldings(null, Collections.emptySet()), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void hasHoldings_bibliographicRecordIdArgIsEmpty_throws() {
+        final HoldingsItemsConnector connector = createConnector();
+        assertThat(() -> connector.hasHoldings(" ", Collections.emptySet()), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void hasHoldings_agencyIdsArgIsNull_throws() {
+        final HoldingsItemsConnector connector = createConnector();
+        assertThat(() -> connector.hasHoldings(bibliographicRecordId, null), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void hasHoldings_noHoldingsFound_returnsEmptySet() {
+        final HoldingsItemsConnector connector = createConnector();
+        assertThat(connector.hasHoldings("no-such-record-id", Collections.emptySet()), is(Collections.emptySet()));
+    }
+
+    @Test
+    public void hasHoldings_agencyIdsArgIsEmpty_returnsSetOfAllAgencyIdsWithHoldings() {
+        final HoldingsItemsConnector connector = createConnector();
+        assertThat(connector.hasHoldings(bibliographicRecordId, Collections.emptySet()), is(toSet(300320, 718700, 737600, 767100, 770700, 771000)));
+    }
+
+    @Test
+    public void hasHoldings_agencyIdsArgIsNonEmpty_returnsSetOfAgencyIdsWithHoldings() {
+        final HoldingsItemsConnector connector = createConnector();
+        assertThat(connector.hasHoldings(bibliographicRecordId, toSet(111111, 767100, 771000)), is(toSet(767100, 771000)));
+    }
+
+    private HoldingsItemsConnector createConnector() {
+        return new HoldingsItemsConnector(solrServerEndpoint);
+    }
+
+    private Set<Integer> toSet(Integer... ints) {
+        return Stream.of(ints).collect(Collectors.toSet());
+    }
+}

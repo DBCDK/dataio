83d2db2bda5417fa303d0c98fac835ce1a2abbb8
Stine Mia Madsen <sma@dbc.dk>
2016-08-22 09:25:44 +0000

    [20524] Implementering af ImsSinkConfig
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@103497 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/ImsSinkConfig.java
@@ -0,0 +1,66 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.commons.types;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
+
+import java.io.Serializable;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+public class ImsSinkConfig implements SinkConfig, Serializable {
+    private static final long serialVersionUID = 1257505129736059671L;
+
+    private String endpoint;
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public ImsSinkConfig withEndpoint(String endpoint) {
+        this.endpoint = InvariantUtil.checkNotNullNotEmptyOrThrow(endpoint, "endpoint");
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ImsSinkConfig)) return false;
+
+        ImsSinkConfig that = (ImsSinkConfig) o;
+
+        return endpoint != null ? endpoint.equals(that.endpoint) : that.endpoint == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return endpoint != null ? endpoint.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "ImsSinkConfig{" +
+                "endpoint='" + endpoint + '\'' +
+                '}';
+    }
+}
---
+++ commons/types/src/test/java/dk/dbc/dataio/commons/types/ImsSinkConfigTest.java
@@ -0,0 +1,55 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.commons.types;
+
+import dk.dbc.dataio.jsonb.JSONBContext;
+import dk.dbc.dataio.jsonb.JSONBException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static dk.dbc.commons.testutil.Assert.assertThat;
+import static dk.dbc.commons.testutil.Assert.isThrowing;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ImsSinkConfigTest {
+
+    private static final String ENDPOINT = "endpoint";
+
+    @Test
+    public void constructor_endpointArgIsNull_throws() {
+        assertThat(() -> new ImsSinkConfig().withEndpoint(null), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void constructor_allArgsAreValid_returnsNewInstanceWithDefaultValuesSet() {
+        final ImsSinkConfig sinkConfig = new ImsSinkConfig().withEndpoint(ENDPOINT);
+        Assert.assertThat(sinkConfig.getEndpoint(), is(ENDPOINT));
+    }
+
+    @Test
+    public void marshalling() throws JSONBException {
+        final JSONBContext jsonbContext = new JSONBContext();
+        final ImsSinkConfig sinkConfig = new ImsSinkConfig();
+        final ImsSinkConfig unmarshalled = jsonbContext.unmarshall(jsonbContext.marshall(sinkConfig), ImsSinkConfig.class);
+        Assert.assertThat(unmarshalled, is(sinkConfig));
+    }
+}

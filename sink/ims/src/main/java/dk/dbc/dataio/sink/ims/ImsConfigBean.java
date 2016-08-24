4f2d5175b2671656df151bbf48352225a3a15a0e
Stine Mia Madsen <sma@dbc.dk>
2016-08-24 09:00:21 +0000

    [20514] US #947: IMS: IMS sink - Implementering af ImsConfigBean
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@103564 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ sink/ims/src/main/java/dk/dbc/dataio/sink/ims/ImsConfigBean.java
@@ -0,0 +1,96 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.sink.ims;
+
+import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnectorException;
+import dk.dbc.dataio.common.utils.flowstore.ejb.FlowStoreServiceConnectorBean;
+import dk.dbc.dataio.commons.types.ConsumedMessage;
+import dk.dbc.dataio.commons.types.ImsSinkConfig;
+import dk.dbc.dataio.commons.types.Sink;
+import dk.dbc.dataio.commons.types.jms.JmsConstants;
+import dk.dbc.dataio.sink.ims.connector.ImsServiceConnector;
+import dk.dbc.dataio.sink.types.SinkException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ws.rs.ProcessingException;
+
+/**
+ * This Enterprise Java Bean (EJB) singleton is used as a config container for the the ims web-service sink
+ * to the ims web-service.
+ */
+@Singleton
+public class ImsConfigBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImsConfigBean.class);
+
+    @EJB
+    FlowStoreServiceConnectorBean flowStoreServiceConnectorBean;
+
+    private long highestVersionSeen = 0;
+    private ImsServiceConnector imsServiceConnector;
+
+    /**
+     *
+     * @param consumedMessage the consumed message
+     * @return configured imsServiceConnector
+     * @throws SinkException on error to retrieve property or sink
+     */
+    public ImsServiceConnector getConnector(ConsumedMessage consumedMessage) throws SinkException {
+        configureConnector(consumedMessage);
+        return imsServiceConnector;
+    }
+
+    /*
+     * Private methods
+     */
+
+    /**
+     * This method determines if the current instance of the ims update service connector is configured
+     * with the latest version config values.
+     *
+     * If the current config is outdated:
+     * The latest version of the config is retrieved through the referenced sink.
+     * A new ims update service connector is created with the new config values.
+
+     * @param consumedMessage consumed message containing the version and the id of the sink containing the version
+     * @throws SinkException on error to retrieve property for id or version or on error on fetching sink
+     */
+    private void configureConnector(ConsumedMessage consumedMessage) throws SinkException, NullPointerException, IllegalArgumentException, ProcessingException {
+        try {
+            final long sinkId = consumedMessage.getHeaderValue(JmsConstants.SINK_ID_PROPERTY_NAME, Long.class);
+            final long sinkVersion = consumedMessage.getHeaderValue(JmsConstants.SINK_VERSION_PROPERTY_NAME, Long.class);
+
+            LOGGER.trace("Sink version of message {} vs highest version seen {}", sinkVersion, highestVersionSeen);
+            if (sinkVersion > highestVersionSeen) {
+                final Sink sink = flowStoreServiceConnectorBean.getConnector().getSink(sinkId);
+                final ImsSinkConfig config = (ImsSinkConfig) sink.getContent().getSinkConfig();
+                LOGGER.info("Current sink config: {}", config);
+                highestVersionSeen = sink.getVersion();
+                imsServiceConnector = new ImsServiceConnector(config.getEndpoint());
+            }
+        } catch (FlowStoreServiceConnectorException e) {
+            throw new SinkException(e.getMessage(), e);
+        }
+    }
+}
---
+++ sink/ims/src/test/java/dk/dbc/dataio/sink/ims/ImsConfigBeanTest.java
@@ -0,0 +1,147 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.sink.ims;
+
+import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnector;
+import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnectorException;
+import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnectorUnexpectedStatusCodeException;
+import dk.dbc.dataio.common.utils.flowstore.ejb.FlowStoreServiceConnectorBean;
+import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ConsumedMessage;
+import dk.dbc.dataio.commons.types.ImsSinkConfig;
+import dk.dbc.dataio.commons.types.Sink;
+import dk.dbc.dataio.commons.types.SinkContent;
+import dk.dbc.dataio.commons.types.jms.JmsConstants;
+import dk.dbc.dataio.commons.utils.test.model.ChunkBuilder;
+import dk.dbc.dataio.commons.utils.test.model.SinkBuilder;
+import dk.dbc.dataio.commons.utils.test.model.SinkContentBuilder;
+import dk.dbc.dataio.jsonb.JSONBContext;
+import dk.dbc.dataio.jsonb.JSONBException;
+import dk.dbc.dataio.sink.ims.connector.ImsServiceConnector;
+import dk.dbc.dataio.sink.types.SinkException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ImsConfigBeanTest {
+    private String PAYLOAD;
+    private final FlowStoreServiceConnector flowStoreServiceConnector = mock(FlowStoreServiceConnector.class);
+    private Sink sink;
+
+    @Before
+    public void setup() throws JSONBException {
+
+        final SinkContent sinkContent = new SinkContentBuilder()
+                .setSinkType(SinkContent.SinkType.IMS)
+                .setSinkConfig(new ImsSinkConfig().withEndpoint("endpoint"))
+                .build();
+
+        sink    = new SinkBuilder().setContent(sinkContent).build();
+        PAYLOAD = new JSONBContext().marshall(new ChunkBuilder(Chunk.Type.PROCESSED).build());
+    }
+
+    @Test
+    public void getConnector_sinkNotFound_throwsSinkException() throws FlowStoreServiceConnectorException {
+        final ImsConfigBean configBean = getInitializedBean();
+        final ConsumedMessage consumedMessage = getConsumedMessage(42, 1);
+        final String message = "Error message from flowStore";
+
+        when(flowStoreServiceConnector.getSink(anyLong())).thenThrow(new FlowStoreServiceConnectorUnexpectedStatusCodeException(message, 404));
+
+        try {
+            // Subject under test
+            configBean.getConnector(consumedMessage);
+
+            // Verification
+            fail();
+        } catch (SinkException e) {
+            assertThat(e.getMessage(), is(message));
+        }
+    }
+
+    @Test
+    public void getConnector_connectorConfigured_ok() throws FlowStoreServiceConnectorException, SinkException {
+        final ImsConfigBean configBean = getInitializedBean();
+        final ConsumedMessage consumedMessage = getConsumedMessage(42, 1);
+
+        when(flowStoreServiceConnector.getSink(anyLong())).thenReturn(sink);
+
+        // Subject under test
+        final ImsServiceConnector connector = configBean.getConnector(consumedMessage);
+
+        // Verification
+        assertThat(connector, not(nullValue()));
+        verify(flowStoreServiceConnector, times(1)).getSink(sink.getId());
+    }
+
+    @Test
+    public void getConnector_connectorReConfiguredOnlyWhenVersionChanges_ok() throws SinkException, FlowStoreServiceConnectorException {
+        final ImsConfigBean configBean = getInitializedBean();
+
+        when(flowStoreServiceConnector.getSink(eq(10L))).thenReturn(sink);
+        when(flowStoreServiceConnector.getSink(eq(12L))).thenReturn(sink);
+
+        final ImsServiceConnector connector = configBean.getConnector(getConsumedMessage(10, 1));
+        assertThat(connector, not(nullValue()));
+        verify(flowStoreServiceConnector, times(1)).getSink(10L);
+
+        final ImsServiceConnector connectorUnchanged = configBean.getConnector(getConsumedMessage(11, 1));
+        assertThat(connector, is(connectorUnchanged));
+        verify(flowStoreServiceConnector, times(0)).getSink(11L);
+
+        final ImsServiceConnector connectorModified = configBean.getConnector(getConsumedMessage(12, 2));
+        assertThat(connectorUnchanged, not(connectorModified));
+        verify(flowStoreServiceConnector, times(1)).getSink(12L);
+    }
+
+     /*
+     * Private methods
+     */
+
+    private ImsConfigBean getInitializedBean() {
+        ImsConfigBean configBean = new ImsConfigBean();
+        configBean.flowStoreServiceConnectorBean = mock(FlowStoreServiceConnectorBean.class);
+        when(configBean.flowStoreServiceConnectorBean.getConnector()).thenReturn(flowStoreServiceConnector);
+        return configBean;
+    }
+
+    private ConsumedMessage getConsumedMessage(long sinkId, long sinkVersion) {
+        final Map<String, Object> headers = new HashMap<>();
+        headers.put(JmsConstants.SINK_ID_PROPERTY_NAME, sinkId);
+        headers.put(JmsConstants.SINK_VERSION_PROPERTY_NAME, sinkVersion);
+        return new ConsumedMessage("messageId", headers, PAYLOAD);
+    }
+}

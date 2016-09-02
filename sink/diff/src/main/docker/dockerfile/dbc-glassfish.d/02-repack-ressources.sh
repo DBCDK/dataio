2637567eab048ca28070f2bbbe71764353030777
Stine Mia Madsen <sma@dbc.dk>
2016-09-02 08:55:55 +0000

    docker for diff sink
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@103813 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ sink/diff/src/main/docker/dockerfile/dbc-glassfish.d/02-repack-ressources.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Stop on error
+set -e
+
+# when the shell expands, dont include the pattern in the list of results (e_g_ exclude *_xml)
+shopt -s nullglob
+
+WAR=dataio-sink-diff-1.0-SNAPSHOT
+
+
+mkdir ${GLASSFISH_USER_HOME}/${WAR}
+
+mv ${GLASSFISH_USER_HOME}/${WAR}.war ${GLASSFISH_USER_HOME}/${WAR}
+
+cd ${GLASSFISH_USER_HOME}/${WAR}
+
+unzip ${WAR}.war
+rm ${WAR}.war
+
+MODEL_FILE=${GLASSFISH_USER_HOME}/${WAR}/WEB-INF/ejb-jar.xml
+cp ${GLASSFISH_USER_HOME}/dbc-glassfish.d/02-ejb-jar.xml $MODEL_FILE
+rm ${GLASSFISH_USER_HOME}/dbc-glassfish.d/02-ejb-jar.xml
+
+cd ${GLASSFISH_USER_HOME}/${WAR}
+
+jar cmf META-INF/MANIFEST.MF ${WAR}.war *
+mv ${WAR}.war ..
+cd ..
+
+rm -rf ${WAR}
+

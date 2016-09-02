6e8be01286b591b4d57340dbf634969ac42ab292
Stine Mia Madsen <sma@dbc.dk>
2016-09-02 09:00:34 +0000

    docker for dummy sink
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@103814 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ sink/dummy/src/main/docker/dockerfile/dbc-glassfish.d/99-copy-ear-to-autodeploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "==> Preparing for deploy of dataio-sink-openupdate-1.0-SNAPSHOT.war"
+cp $GLASSFISH_USER_HOME/*.war $GLASSFISH_HOME/glassfish/domains/domain1/autodeploy/
+

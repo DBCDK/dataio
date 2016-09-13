d53ace8fc0bdf24cb9914eb7ab21b347e8691176
Stine Mia Madsen <sma@dbc.dk>
2016-09-13 09:20:59 +0000

    Docker for flowstore
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@104094 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- harvester/rr/src/main/docker/dockerfile/dbc-glassfish.d/01-can-create-cmdb-dynamic-resources.sh
+++ harvester/rr/src/main/docker/dockerfile/dbc-glassfish.d/01-can-create-cmdb-dynamic-resources.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-
-#shopt -s nullglob
 echo env
 echo $RAWREPOS | python $GLASSFISH_USER_HOME/dbc-glassfish.d/generateExtraRRPools.py $GLASSFISH_USER_HOME/dbc-glassfish.d/65-extra-rr.xml

20b93a2e56cb602af0965d07495e0aec39e641d5
Jan Bauer Nielsen <jbn@dbc.dk>
2016-09-26 10:13:00 +0000

    Added ReservedCodeCacheSize JVM option.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@104448 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-processor/src/main/docker/dockerfile/dbc-glassfish.d/memOpts.cmd
+++ job-processor/src/main/docker/dockerfile/dbc-glassfish.d/memOpts.cmd
@@ -1,2 +1,3 @@
 delete-jvm-options '-Xmx4000m' --passwordfile=./passfile.txt
 create-jvm-options '-Xmx8G' --passwordfile=./passfile.txt
+create-jvm-options '-XX\:ReservedCodeCacheSize=1G' --passwordfile=./passfile.txt

6c90b7d51a3864e7489e40cdd45ed245162b6e52
Jan Bauer Nielsen <jbn@dbc.dk>
2016-09-26 06:58:11 +0000

    Added EJB container settings.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@104427 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ job-processor/src/main/docker/dockerfile/dbc-glassfish.d/ejbContainer.cmd
@@ -0,0 +1,4 @@
+set server.mdb-container.max-pool-size=6 --passwordfile=./passfile.txt
+set server.mdb-container.pool-resize-quantity=2 --passwordfile=./passfile.txt
+set server.ejb-container.max-pool-size=6 --passwordfile=./passfile.txt
+set server.ejb-container.pool-resize-quantity=2 --passwordfile=./passfile.txt

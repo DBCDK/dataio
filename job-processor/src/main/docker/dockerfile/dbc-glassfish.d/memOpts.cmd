324a0d793ba23a6f775cc3f598cbb585a286abbf
Jan Bauer Nielsen <jbn@dbc.dk>
2016-09-26 06:54:42 +0000

    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@104425 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-processor/src/main/docker/dockerfile/dbc-glassfish.d/memOpts.cmd
+++ job-processor/src/main/docker/dockerfile/dbc-glassfish.d/memOpts.cmd
@@ -1,3 +1,2 @@
 delete-jvm-options '-Xmx4000m' --passwordfile=./passfile.txt
 create-jvm-options '-Xmx8G' --passwordfile=./passfile.txt
-create-jvm-options '-XX:ReservedCodeCacheSize=1G' --passwordfile=./passfile.txt

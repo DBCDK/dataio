924887c6b15d46fed98c31f3ccaf6e1725c40915
Jan Bauer Nielsen <jbn@dbc.dk>
2016-10-25 11:00:12 +0000

    Modified setup_devenv.sh script to call configure script.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@105262 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- js-devenv/setup_devenv.sh
+++ js-devenv/setup_devenv.sh
@@ -23,27 +23,6 @@
 REPOS_URL=https://svn.dbc.dk/repos
 REPOS_DIR=svn.dbc.dk
 
-isNotSet() {
-    name=$1
-    eval value=\$$name
-    if [ -z "$value" ]
-        then
-            return 0
-        else
-            return 1
-    fi
-}
-
-
-if isNotSet JOB_NAME ; then
-     JOB_NAME=dataio-js-acceptance-test-VERIFIED
-fi
-echo $JOB_NAME
-if isNotSet BUILD_NUMBER ; then
-     BUILD_NUMBER=`curl --silent http://is.dbc.dk/job/$JOB_NAME/lastSuccessfulBuild/buildNumber`
-fi
-echo $BUILD_NUMBER
-
 if [ ! -d "$REPOS_DIR" ]; then
     svn co --depth empty $REPOS_URL $REPOS_DIR
     cd $REPOS_DIR
@@ -52,26 +31,4 @@ if [ ! -d "$REPOS_DIR" ]; then
     cd ..
 fi
 
-JAIL_DIR=testarea
-rm -rf $JAIL_DIR
-mkdir $JAIL_DIR
-cd $JAIL_DIR
-BUILDSCRIPT_DIR=/usr/local/lib/is.dbc.dk
-
-# Start port allocator
-BUILD_ID=dontKillMe $BUILDSCRIPT_DIR/port_allocator_service_daemon.py
-
-VENV=`pwd`/ENV
-EGGS=`pwd`/dependencies
-rm -f dependencies.txt
-rm -f $EGGS/*
-
-DEPENDENCY_MANAGER=$BUILDSCRIPT_DIR/dependency_manager/bin/dependency-manager
-$DEPENDENCY_MANAGER $JOB_NAME $BUILD_NUMBER
-$DEPENDENCY_MANAGER $JOB_NAME $BUILD_NUMBER -a testrunner-jsshell-convert --verbose
-$DEPENDENCY_MANAGER --download $EGGS --pattern ".*\.(egg)$" --verbose
-
-$BUILDSCRIPT_DIR/envbuilder.sh -v -s -f -e $VENV $EGGS
-cd ..
-
-exit 0
+./configure

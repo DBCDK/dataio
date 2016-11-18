de67b8d248e55d63543bdb0072da75e26025bf41
Stine Mia Madsen <sma@dbc.dk>
2016-11-18 11:54:59 +0000

    [20642] US #1028: Opdateringstidspunkt for flows - Rettelse af javadoc
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@106261 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- commons/types/src/main/java/dk/dbc/dataio/commons/types/FlowContent.java
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/FlowContent.java
@@ -49,7 +49,7 @@ public class FlowContent implements Serializable {
      * @param name flow name
      * @param description flow description
      * @param components flow components attached to this flow (can be empty)
-     *
+     * @param timeOfFlowComponentUpdate time of last time the flow components nested within the flow were updated
      * @throws NullPointerException if given null-valued name, description or components argument
      * @throws IllegalArgumentException if given empty-valued name
      */

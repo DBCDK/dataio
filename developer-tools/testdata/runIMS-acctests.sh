000a930fd5f82846c54d78ef6bbbcadced18618f
Kirstine Wilfred Christensen <kwc@dbc.dk>
2016-11-30 09:44:43 +0000

    removed ims testdata - it can now be found in testdata-conversion-flows instead - removed run scripts for acctests for ffu and ims
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@106943 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- developer-tools/testdata/runIMS-acctests.sh
+++
@@ -1,5 +0,0 @@
-#start an acctest on dataio prod for IMS flow
-
-../create_job.py --jobstorehost jobstore.dataio.prod.mcp1.dbc.dk --filestorehost dataio-be-p01:8080 870970.records-with775100-holdingIMS.xml 775100.IMS.spec
-
-

813f11d287c9febdb1d321b74ae07b8f406ca276
Stine Mia Madsen <sma@dbc.dk>
2016-12-20 09:07:21 +0000

    [20751] US#1081: Kilderepohøster - HarvesterJobBuilder for ticklerepo
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@107798 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ harvester/tickle-repo/src/main/java/dk/dbc/dataio/harvester/ticklerepo/HarvesterJobBuilder.java
@@ -0,0 +1,69 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.harvester.ticklerepo;
+
+import dk.dbc.dataio.bfs.api.BinaryFileStore;
+import dk.dbc.dataio.commons.types.FileStoreUrn;
+import dk.dbc.dataio.commons.types.JobSpecification;
+import dk.dbc.dataio.commons.utils.jobstore.JobStoreServiceConnector;
+import dk.dbc.dataio.filestore.service.connector.FileStoreServiceConnector;
+import dk.dbc.dataio.harvester.types.HarvesterException;
+import dk.dbc.dataio.harvester.utils.harvesterjobbuilder.AbstractHarvesterJobBuilder;
+
+/**
+ * Class used to build dataIO job from harvested records
+ */
+public class HarvesterJobBuilder extends AbstractHarvesterJobBuilder {
+
+    /**
+     * Class constructor
+     * @param binaryFileStore binaryFileStore implementation for tmp file writing
+     * @param fileStoreServiceConnector file-store service connector for datafile uploads
+     * @param jobStoreServiceConnector job-store service connector for job creation
+     * @param jobSpecificationTemplate job specification template
+     * @throws NullPointerException if given null-valued argument
+     * @throws HarvesterException on failure to create harvester data file
+     * backed by temporary binary file
+     */
+    public HarvesterJobBuilder(BinaryFileStore binaryFileStore, FileStoreServiceConnector fileStoreServiceConnector,
+                               JobStoreServiceConnector jobStoreServiceConnector, JobSpecification jobSpecificationTemplate)
+            throws NullPointerException, HarvesterException {
+        super(binaryFileStore, fileStoreServiceConnector, jobStoreServiceConnector, jobSpecificationTemplate);
+    }
+
+    /* Returns job specification for given file ID */
+    @Override
+    protected JobSpecification createJobSpecification(String fileId) {
+        final FileStoreUrn fileStoreUrn = FileStoreUrn.create(fileId);
+        return new JobSpecification(
+                jobSpecificationTemplate.getPackaging(),
+                jobSpecificationTemplate.getFormat(),
+                jobSpecificationTemplate.getCharset(),
+                jobSpecificationTemplate.getDestination(),
+                jobSpecificationTemplate.getSubmitterId(),
+                JobSpecification.EMPTY_MAIL_FOR_NOTIFICATION_ABOUT_VERIFICATION,
+                JobSpecification.EMPTY_MAIL_FOR_NOTIFICATION_ABOUT_PROCESSING,
+                JobSpecification.EMPTY_RESULT_MAIL_INITIALS,
+                fileStoreUrn.toString(),
+                jobSpecificationTemplate.getType());
+    }
+}

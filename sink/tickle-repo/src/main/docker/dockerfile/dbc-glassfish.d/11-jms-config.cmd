ed1cbc05f14ee824028b15cfd7dbcbe464eb8ab2
Jan Bauer Nielsen <jbn@dbc.dk>
2016-12-20 09:40:10 +0000

    [20752] Added building of docker image to tickle-repo sink module.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@107801 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ sink/tickle-repo/src/main/docker/dockerfile/dbc-glassfish.d/11-jms-config.cmd
@@ -0,0 +1,3 @@
+set server-config.jms-service.type=REMOTE --passwordfile=./passfile.txt
+        set server-config.jms-service.jms-host.default_JMS_host.host=dbc-openmq --passwordfile=./passfile.txt
+        set server-config.jms-service.jms-host.default_JMS_host.port=7676 --passwordfile=./passfile.txt

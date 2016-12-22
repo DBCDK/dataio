8ae78b1646ade950d2b96514e1943f2a4509ebe7
Stine Mia Madsen <sma@dbc.dk>
2016-12-22 08:40:38 +0000

    [20751] US#1081: Kilderepohøster - Unit test for HarvesterBean samt ScheduledHarvestBean
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@108012 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ harvester/tickle-repo/src/test/java/dk/dbc/dataio/harvester/ticklerepo/HarvesterBeanTest.java
@@ -0,0 +1,71 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.harvester.ticklerepo;
+
+import dk.dbc.dataio.harvester.types.HarvesterException;
+import dk.dbc.dataio.harvester.types.TickleRepoHarvesterConfig;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.ejb.SessionContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class HarvesterBeanTest {
+    private SessionContext sessionContext = mock(SessionContext.class);
+    private HarvestOperation harvestOperation = mock(HarvestOperation.class);
+    private EntityManagerFactory entityManagerFactory = mock(EntityManagerFactory.class);
+    private EntityManager entityManager = mock(EntityManager.class);
+
+    @Test
+    public void harvest_harvestOperationCompletes_returnsNumberOfItemsHarvested() throws HarvesterException, ExecutionException, InterruptedException {
+        final HarvesterBean harvesterBean = getHarvesterBean();
+        final TickleRepoHarvesterConfig config = createConfig();
+        final int expectedNumberOfItemsHarvested = 42;
+        doReturn(harvestOperation).when(harvesterBean).getHarvestOperation(config);
+        when(harvestOperation.execute()).thenReturn(expectedNumberOfItemsHarvested);
+
+        final Future<Integer> harvestResult = harvesterBean.harvest(config);
+        assertThat("Items harvested", harvestResult.get(), is(expectedNumberOfItemsHarvested));
+    }
+
+    private HarvesterBean getHarvesterBean() {
+        final HarvesterBean harvesterBean = Mockito.spy(new HarvesterBean());
+        harvesterBean.sessionContext = sessionContext;
+        when(sessionContext.getBusinessObject(HarvesterBean.class)).thenReturn(harvesterBean);
+        when(entityManagerFactory.createEntityManager()).thenReturn(entityManager);
+        return harvesterBean;
+    }
+
+    private TickleRepoHarvesterConfig createConfig() {
+        return new TickleRepoHarvesterConfig(1, 1, new TickleRepoHarvesterConfig.Content());
+    }
+
+}

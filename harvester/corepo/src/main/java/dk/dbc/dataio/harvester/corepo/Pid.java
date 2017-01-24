3501ea1d022401be6b016d003fa051c04517d80f
Jan Bauer Nielsen <jbn@dbc.dk>
2017-01-24 11:33:14 +0000

    [20828] Added Pid abtraction.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@109301 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ harvester/corepo/src/main/java/dk/dbc/dataio/harvester/corepo/Pid.java
@@ -0,0 +1,136 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.harvester.corepo;
+
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
+
+public class Pid {
+    public static Pid of(String value) throws NullPointerException, IllegalArgumentException {
+        return new Pid(value);
+    }
+
+    public enum Type { BIBLIOGRAPHIC_OBJECT, UNIT, WORK }
+
+    private final String value;
+    private Type type;
+    private Integer agencyId;
+    private String bibliographicRecordId;
+    private String format;
+
+    private Pid(String value) throws NullPointerException, IllegalArgumentException {
+        this.value = InvariantUtil.checkNotNullNotEmptyOrThrow(value, "value");
+        parse(value);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Integer getAgencyId() {
+        return agencyId;
+    }
+
+    public String getBibliographicRecordId() {
+        return bibliographicRecordId;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Pid pid = (Pid) o;
+
+        return value.equals(pid.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    private void parse(String value) throws IllegalArgumentException {
+        final String[] parts = value.split(":");
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("invalid pid");
+        }
+        parseType(parts[0]);
+        parseBibliographicRecordId(parts[1]);
+    }
+
+    private void parseType(String value) {
+        try {
+            final String[] parts = notEmpty(value, "type").split("-");
+            switch (parts[0].toLowerCase()) {
+                case "unit":
+                    type = Type.UNIT;
+                    break;
+                case "work":
+                    type = Type.WORK;
+                    break;
+                default:
+                    type = Type.BIBLIOGRAPHIC_OBJECT;
+            }
+            if (type == Type.BIBLIOGRAPHIC_OBJECT) {
+                if (parts.length != 2) {
+                    throw new IllegalArgumentException("illegal number of type parts");
+                }
+                agencyId = parseAgencyId(parts[0]);
+                format = notEmpty(parts[1], "format");
+            }
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("unable to parse type", e);
+        }
+    }
+
+    private Integer parseAgencyId(String value) throws IllegalArgumentException {
+        agencyId = Integer.parseInt(value);
+        if (agencyId < 100000 || agencyId > 999999) {
+            throw new IllegalArgumentException("invalid agencyId: " + agencyId);
+        }
+        return agencyId;
+    }
+
+    private void parseBibliographicRecordId(String value) {
+        bibliographicRecordId = notEmpty(value.split("_")[0], "bibliographicRecordId");
+    }
+
+    private String notEmpty(String value, String field) {
+        if (value.trim().isEmpty()) {
+            throw new IllegalArgumentException("can not be empty: " + field);
+        }
+        return value;
+    }
+}
---
+++ harvester/corepo/src/test/java/dk/dbc/dataio/harvester/corepo/PidTest.java
@@ -0,0 +1,111 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.harvester.corepo;
+
+import org.junit.Test;
+
+import static dk.dbc.commons.testutil.Assert.assertThat;
+import static dk.dbc.commons.testutil.Assert.isThrowing;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class PidTest {
+    @Test
+    public void pidHasNoColon_throws() {
+        assertThat(() -> Pid.of("nocolon"), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void pidMissingType_throws() {
+        assertThat(() -> Pid.of(":id"), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void pidMissingBibliographicId_throws() {
+        assertThat(() -> Pid.of("unit:"), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void pidMissingFormat_throws() {
+        assertThat(() -> Pid.of("870970:23142546"), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void pidFormatIsEmpty_throws() {
+        assertThat(() -> Pid.of("870970-:23142546"), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void pidAgencyIdHasLessThatSixDigits_throws() {
+        assertThat(() -> Pid.of("99999-basis:23142546"), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void pidAgencyIdHasMoreThatSixDigits_throws() {
+        assertThat(() -> Pid.of("1000000-basis:23142546"), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void pidOfTypeUnit() {
+        final String value = "unit:1354373";
+        final Pid pid = Pid.of(value);
+        assertThat("type", pid.getType(), is(Pid.Type.UNIT));
+        assertThat("value", pid.toString(), is(value));
+        assertThat("agencyId", pid.getAgencyId(), is(nullValue()));
+        assertThat("bibliographicRecordId", pid.getBibliographicRecordId(), is("1354373"));
+        assertThat("format", pid.getFormat(), is(nullValue()));
+    }
+
+    @Test
+    public void pidOfTypeWork() {
+        final String value = "work:995029";
+        final Pid pid = Pid.of(value);
+        assertThat("type", pid.getType(), is(Pid.Type.WORK));
+        assertThat("value", pid.toString(), is(value));
+        assertThat("agencyId", pid.getAgencyId(), is(nullValue()));
+        assertThat("bibliographicRecordId", pid.getBibliographicRecordId(), is("995029"));
+        assertThat("format", pid.getFormat(), is(nullValue()));
+    }
+
+    @Test
+    public void pidOfTypeBibliographicObject() {
+        final String value = "870970-basis:23142546";
+        final Pid pid = Pid.of(value);
+        assertThat("type", pid.getType(), is(Pid.Type.BIBLIOGRAPHIC_OBJECT));
+        assertThat("value", pid.toString(), is(value));
+        assertThat("agencyId", pid.getAgencyId(), is(870970));
+        assertThat("bibliographicRecordId", pid.getBibliographicRecordId(), is("23142546"));
+        assertThat("format", pid.getFormat(), is("basis"));
+    }
+
+    @Test
+    public void pidIsMultipart() {
+        final String value = "870970-basis:23142546_1";
+        final Pid pid = Pid.of(value);
+        assertThat("type", pid.getType(), is(Pid.Type.BIBLIOGRAPHIC_OBJECT));
+        assertThat("value", pid.toString(), is(value));
+        assertThat("agencyId", pid.getAgencyId(), is(870970));
+        assertThat("bibliographicRecordId", pid.getBibliographicRecordId(), is("23142546"));
+        assertThat("format", pid.getFormat(), is("basis"));
+    }
+}

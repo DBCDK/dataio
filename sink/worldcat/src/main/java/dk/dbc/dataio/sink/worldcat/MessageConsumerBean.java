3afde786b028cfe3429a58a5b8e38e7d19a055d4
Stine Mia Madsen <sma@dbc.dk>
2017-02-08 13:15:02 +0000

    [20900] US #1104: OCLC-sink - Tilføjelse af tom MessageConsumerBean for workdCat sinken
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@109856 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ sink/worldcat/src/main/java/dk/dbc/dataio/sink/worldcat/MessageConsumerBean.java
@@ -0,0 +1,51 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.sink.worldcat;
+
+import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ConsumedMessage;
+import dk.dbc.dataio.commons.types.exceptions.InvalidMessageException;
+import dk.dbc.dataio.commons.types.interceptor.Stopwatch;
+import dk.dbc.dataio.commons.utils.jobstore.ejb.JobStoreServiceConnectorBean;
+import dk.dbc.dataio.commons.utils.service.AbstractSinkMessageConsumerBean;
+import dk.dbc.dataio.sink.types.SinkException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import javax.xml.ws.WebServiceException;
+
+@MessageDriven
+public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBean.class);
+
+    @EJB JobStoreServiceConnectorBean jobStoreServiceConnectorBean;
+
+    @Stopwatch
+    @Override
+    public void handleConsumedMessage(ConsumedMessage consumedMessage) throws SinkException, InvalidMessageException, NullPointerException, WebServiceException {
+        final Chunk chunk = unmarshallPayload(consumedMessage);
+        LOGGER.info("Chunk {} in job {} received successfully", chunk.getChunkId(), chunk.getJobId());
+    }
+}

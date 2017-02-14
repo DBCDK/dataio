f66ff741a0385193638b422229fb67f0db29c157
Stine Mia Madsen <sma@dbc.dk>
2017-02-14 09:42:39 +0000

    [20900] US #1104: OCLC-sink - Implementering af WordCatSinkConfig klasse
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@110073 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/WorldCatSinkConfig.java
@@ -0,0 +1,86 @@
+package dk.dbc.dataio.commons.types;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import dk.dbc.dataio.commons.utils.invariant.InvariantUtil;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+public class WorldCatSinkConfig implements SinkConfig, Serializable {
+
+    private static final long serialVersionUID = 1257505129736059983L;
+    private String userId;
+    private String password;
+    private String projectId;
+    private List<String> retryDiagnostics;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public WorldCatSinkConfig withUserId(String userId) {
+        this.userId = InvariantUtil.checkNotNullNotEmptyOrThrow(userId, "userId");
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public WorldCatSinkConfig withPassword(String password) {
+        this.password = InvariantUtil.checkNotNullNotEmptyOrThrow(password, "password");
+        return this;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public WorldCatSinkConfig withProjectId(String projectId) {
+        this.projectId = InvariantUtil.checkNotNullNotEmptyOrThrow(projectId, "projectId");
+        return this;
+    }
+
+    public List<String> getRetryDiagnostics() {
+        return retryDiagnostics == null ? null : new ArrayList<>(retryDiagnostics);
+    }
+
+    public WorldCatSinkConfig withRetryDiagnostics(List<String> retryDiagnostics) {
+        this.retryDiagnostics = InvariantUtil.checkNotNullOrThrow(retryDiagnostics, "retryDiagnostics");
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof WorldCatSinkConfig)) return false;
+
+        WorldCatSinkConfig that = (WorldCatSinkConfig) o;
+
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (projectId != null ? !projectId.equals(that.projectId) : that.projectId != null) return false;
+        return retryDiagnostics != null ? retryDiagnostics.equals(that.retryDiagnostics) : that.retryDiagnostics == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (projectId != null ? projectId.hashCode() : 0);
+        result = 31 * result + (retryDiagnostics != null ? retryDiagnostics.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "WorldCatSinkConfig{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", projectId='" + projectId + '\'' +
+                ", retryDiagnostics=" + retryDiagnostics +
+                '}';
+    }
+}
---
+++ commons/types/src/test/java/dk/dbc/dataio/commons/types/WorldCatSinkConfigTest.java
@@ -0,0 +1,104 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.dataio.commons.types;
+
+import dk.dbc.dataio.jsonb.JSONBContext;
+import dk.dbc.dataio.jsonb.JSONBException;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static dk.dbc.commons.testutil.Assert.assertThat;
+import static dk.dbc.commons.testutil.Assert.isThrowing;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * WorldCatSinkConfig unit tests
+ */
+public class WorldCatSinkConfigTest {
+    private static final String USER_ID = "userId";
+    private static final String PASSWORD = "password";
+    private static final String PROJECT_ID = "projectId";
+    private static final List<String> RETRY_DIAGNOSTICS = Arrays.asList("rt1", "rt2");
+
+    @Test
+    public void withUserId_userIdArgIsNull_throws() {
+        assertThat(() -> new WorldCatSinkConfig().withUserId(null), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void withUserId_userIdArgIsEmpty_throws() {
+        assertThat(() -> new WorldCatSinkConfig().withUserId(""), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void withPassword_passwordArgIsNull_throws() {
+        assertThat(() -> new WorldCatSinkConfig().withPassword(null), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void withPassword_passwordArgIsEmpty_throws() {
+        assertThat(() -> new WorldCatSinkConfig().withPassword(""), isThrowing(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void withProjectId_projectIdArgIsNull_throws() {
+        assertThat(() -> new WorldCatSinkConfig().withProjectId(null), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void withProjectId_projectIdArgIsEmpty_throws() {
+        assertThat(() -> new WorldCatSinkConfig().withProjectId(""), isThrowing(IllegalArgumentException.class));;
+    }
+
+    @Test
+    public void withRetryDiagnostics_retryDiagnosticsArgIsNull_throws() {
+        assertThat(() -> new WorldCatSinkConfig().withRetryDiagnostics(null), isThrowing(NullPointerException.class));
+    }
+
+    @Test
+    public void withRetryDiagnostics_retryDiagnosticsArgIsEmpty_returnsNewInstance() {
+        WorldCatSinkConfig worldCatSinkConfig = new WorldCatSinkConfig().withRetryDiagnostics(Collections.emptyList());
+        assertThat(worldCatSinkConfig.getRetryDiagnostics(), is(Collections.emptyList()));
+    }
+
+    @Test
+    public void constructor_allArgsAreValid_returnsNewInstanceWithValuesSet() {
+        final WorldCatSinkConfig worldCatSinkConfig = new WorldCatSinkConfig().withUserId(USER_ID)
+                .withPassword(PASSWORD).withProjectId(PROJECT_ID).withRetryDiagnostics(RETRY_DIAGNOSTICS);
+
+        assertThat(worldCatSinkConfig.getUserId(), is(USER_ID));
+        assertThat(worldCatSinkConfig.getPassword(), is(PASSWORD));
+        assertThat(worldCatSinkConfig.getProjectId(), is(PROJECT_ID));
+        assertThat(worldCatSinkConfig.getRetryDiagnostics(), is(RETRY_DIAGNOSTICS));
+    }
+
+    @Test
+    public void marshalling() throws JSONBException {
+        final JSONBContext jsonbContext = new JSONBContext();
+        final WorldCatSinkConfig worldCatSinkConfig = new WorldCatSinkConfig();
+        final WorldCatSinkConfig unmarshalled = jsonbContext.unmarshall(jsonbContext.marshall(worldCatSinkConfig), WorldCatSinkConfig.class);
+        assertThat(unmarshalled, is(worldCatSinkConfig));
+    }
+}

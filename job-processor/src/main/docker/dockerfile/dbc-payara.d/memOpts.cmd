05bcb82d18f354a73e13ccf2b44fc60b4cfa2e4c
Jan Bauer Nielsen <jbn@dbc.dk>
2017-03-01 12:34:34 +0000

    [20926] Setting JVM -Xms = -Xmx.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@110666 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-processor/src/main/docker/dockerfile/dbc-payara.d/memOpts.cmd
+++ job-processor/src/main/docker/dockerfile/dbc-payara.d/memOpts.cmd
@@ -1,4 +1,5 @@
 delete-jvm-options '-Xmx512m' --passwordfile=./passfile.txt
 delete-jvm-options '-Xmx4G' --passwordfile=./passfile.txt
 create-jvm-options '-Xmx4G' --passwordfile=./passfile.txt
+create-jvm-options '-Xms4G' --passwordfile=./passfile.txt
 create-jvm-options '-XX\:ReservedCodeCacheSize=2G' --passwordfile=./passfile.txt

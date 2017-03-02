8be439acf9aec4f34ed824536bffd509ce2d6902
Jan Bauer Nielsen <jbn@dbc.dk>
2017-03-02 11:38:26 +0000

    [20926] New set of settings.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@110693 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-processor/src/main/docker/dockerfile/dbc-payara.d/40-codecachesize.cmd
+++ job-processor/src/main/docker/dockerfile/dbc-payara.d/40-codecachesize.cmd
@@ -1,2 +1,3 @@
 create-jvm-options '-XX\:ReservedCodeCacheSize=2G' --passwordfile=./passfile.txt
 create-jvm-options '-XX\:InitialCodeCacheSize=2G' --passwordfile=./passfile.txt
+create-jvm-options '-XX\:MinMetaspaceExpansion=1M' --passwordfile=./passfile.txt

110c24c9c41546960d53d8476ae400f5ef7947c9
Jesper Bruun <jbr@dbc.dk>
2017-03-29 14:43:26 +0000

    Added logback jars to endorsed dir.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@111870 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-processor/src/main/docker/dockerfile/dbc-payara.d/02-repack-ressources.sh
+++ job-processor/src/main/docker/dockerfile/dbc-payara.d/02-repack-ressources.sh
@@ -20,6 +20,12 @@ rm ${WAR}.war
 
 MODEL_FILE=${PAYARA_USER_HOME}/${WAR}/WEB-INF/ejb-jar.xml
 cp ${PAYARA_USER_HOME}/dbc-payara.d/02-ejb-jar.xml ${MODEL_FILE}
+
+# Copy postgres logstore specific logback dependencies to top payara endorsed dir
+cp ${PAYARA_USER_HOME}/${WAR}/WEB-INF/lib/dataio-log-store-service-logback-appender-1.0-SNAPSHOT.jar ${PAYARA_HOME}/glassfish/lib/endorsed
+cp ${PAYARA_USER_HOME}/${WAR}/WEB-INF/lib/dataio-log-store-service-types-1.0-SNAPSHOT.jar ${PAYARA_HOME}/glassfish/lib/endorsed
+cp ${PAYARA_USER_HOME}/${WAR}/WEB-INF/lib/dataio-commons-utils-invariant-1.0-SNAPSHOT.jar ${PAYARA_HOME}/glassfish/lib/endorsed
+
 rm ${PAYARA_USER_HOME}/dbc-payara.d/02-ejb-jar.xml
 
 cd ${PAYARA_USER_HOME}/${WAR}
@@ -29,4 +35,3 @@ mv ${WAR}.war ..
 cd ..
 
 rm -rf ${WAR}
-

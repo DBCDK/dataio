5520142318a2f2a765e5c66977da82bcfcf12260
Steen Lillethorup Frederiksen <slf@dbc.dk>
2017-03-29 14:05:32 +0000

    [20983] US #1000: Vis debug info i GUI - Ny SystemEnv proxy som skal bruges til at hente SVN Revisionsnummer
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@111866 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ gui/src/main/java/dk/dbc/dataio/gui/server/SystemEnvProxyServlet.java
@@ -0,0 +1,45 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.gui.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import dk.dbc.dataio.gui.client.proxies.SystemEnvProxy;
+
+import javax.servlet.ServletException;
+import java.util.Map;
+
+public class SystemEnvProxyServlet extends RemoteServiceServlet implements SystemEnvProxy {
+    private static final long serialVersionUID = 5292864551744486800L;
+    private transient SystemEnvProxy systemEnvProxy = null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        systemEnvProxy = new SystemEnvProxyImpl();
+    }
+
+    @Override
+    public Map<String, String> getSystemEnvironment() {
+        return systemEnvProxy.getSystemEnvironment();
+    }
+
+}

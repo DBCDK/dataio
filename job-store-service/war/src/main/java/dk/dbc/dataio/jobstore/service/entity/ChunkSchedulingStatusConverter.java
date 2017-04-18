876f3e44e6f720fc79ccb54082f2b7c1ff0d9f8a
Jan Bauer Nielsen <jbn@dbc.dk>
2017-04-18 08:40:00 +0000

    [21014] Fixed PMD warning
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@112367 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/entity/ChunkSchedulingStatusConverter.java
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/entity/ChunkSchedulingStatusConverter.java
@@ -4,28 +4,20 @@ import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 import static dk.dbc.dataio.jobstore.service.entity.DependencyTrackingEntity.ChunkSchedulingStatus;
-import static dk.dbc.dataio.jobstore.service.entity.DependencyTrackingEntity.ChunkSchedulingStatus.BLOCKED;
-import static dk.dbc.dataio.jobstore.service.entity.DependencyTrackingEntity.ChunkSchedulingStatus.QUEUED_FOR_DELIVERY;
-import static dk.dbc.dataio.jobstore.service.entity.DependencyTrackingEntity.ChunkSchedulingStatus.QUEUED_FOR_PROCESSING;
-import static dk.dbc.dataio.jobstore.service.entity.DependencyTrackingEntity.ChunkSchedulingStatus.READY_FOR_DELIVERY;
-import static dk.dbc.dataio.jobstore.service.entity.DependencyTrackingEntity.ChunkSchedulingStatus.READY_FOR_PROCESSING;
 
 /**
- * JPA Converter Class for enum ChunkProcessStatus
- *
+ * JPA Converter Class for enum ChunkSchedulingStatus
  */
 @Converter(autoApply = true)
 public class ChunkSchedulingStatusConverter implements AttributeConverter<ChunkSchedulingStatus, Integer> {
-
     @Override
     public Integer convertToDatabaseColumn(ChunkSchedulingStatus chunkSchedulingStatus) {
         switch (chunkSchedulingStatus) {
-            case READY_FOR_PROCESSING:     return 1;
-            case QUEUED_FOR_PROCESSING:    return 2;
-            case BLOCKED:              return 3;
-            case READY_FOR_DELIVERY:     return 4;
-            case QUEUED_FOR_DELIVERY: return 5;
-            default:
+            case READY_FOR_PROCESSING:  return 1;
+            case QUEUED_FOR_PROCESSING: return 2;
+            case BLOCKED:               return 3;
+            case READY_FOR_DELIVERY:    return 4;
+            case QUEUED_FOR_DELIVERY:   return 5;
         }
         return null;
     }
@@ -33,15 +25,12 @@ public class ChunkSchedulingStatusConverter implements AttributeConverter<ChunkS
     @Override
     public ChunkSchedulingStatus convertToEntityAttribute(Integer chunkProcessStatus) {
         switch ( chunkProcessStatus ) {
-            case 1: return READY_FOR_PROCESSING;
-            case 2: return QUEUED_FOR_PROCESSING;
-            case 3: return BLOCKED;
-            case 4: return READY_FOR_DELIVERY;
-            case 5: return QUEUED_FOR_DELIVERY;
-            default:
+            case 1: return ChunkSchedulingStatus.READY_FOR_PROCESSING;
+            case 2: return ChunkSchedulingStatus.QUEUED_FOR_PROCESSING;
+            case 3: return ChunkSchedulingStatus.BLOCKED;
+            case 4: return ChunkSchedulingStatus.READY_FOR_DELIVERY;
+            case 5: return ChunkSchedulingStatus.QUEUED_FOR_DELIVERY;
         }
         return null;
     }
-
-
 }

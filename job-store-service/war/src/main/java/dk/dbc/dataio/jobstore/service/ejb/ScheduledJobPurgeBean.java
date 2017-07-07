de770d88a7fa2fa1fd90422d283f35f2d4a67117
Jan Bauer Nielsen <jbn@dbc.dk>
2017-07-07 07:42:00 +0000

    Change purge schedule from 07:00 to 23:00.
    git-svn-id: https://svn.dbc.dk/repos/dataio/trunk@114565 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/ejb/ScheduledJobPurgeBean.java
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/ejb/ScheduledJobPurgeBean.java
@@ -19,7 +19,7 @@ public class ScheduledJobPurgeBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledJobPurgeBean.class);
 
-    @Schedule(hour="7", minute = "*", second = "*", persistent = false)
+    @Schedule(hour="23", minute = "*", second = "*", persistent = false)
     public void run() {
         try {
             jobPurgeBean.purgeJobs();

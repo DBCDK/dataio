82729da967a0e7e3fd84e0e952648608004f2f52
Jens Stein Jørgensen <jsj@dbc.dk>
2017-07-27 10:36:32 +0200

    Add missing class JobCreatorInfo
    Which I forgot about in the last commit.

---
+++ cli/job-creator/src/main/java/dk/dbc/dataio/cli/jobcreator/JobCreatorInfo.java
@@ -0,0 +1,71 @@
+package dk.dbc.dataio.cli.jobcreator;
+
+import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnector;
+import dk.dbc.dataio.commons.types.JobSpecification;
+
+public class JobCreatorInfo {
+    private JobSpecification jobSpecification;
+    // six-digit submitter number in human readable format
+    private long submitterNumber;
+    // database id of submitter entity
+    private long submitterId;
+    private String targetSinkName;
+    private FlowStoreServiceConnector sourceFlowStoreConnector,
+        targetFlowStoreConnector;
+
+    public JobSpecification getJobSpecification() {
+        return jobSpecification;
+    }
+
+    public JobCreatorInfo withJobSpecification(JobSpecification jobSpecification) {
+        this.jobSpecification = jobSpecification;
+        return this;
+    }
+
+    public long getSubmitterNumber() {
+        return submitterNumber;
+    }
+
+    public long getSubmitterId() {
+        return submitterId;
+    }
+
+    public JobCreatorInfo withSubmitterId(long submitterId) {
+        this.submitterId = submitterId;
+        return this;
+    }
+
+    public JobCreatorInfo withSubmitterNumber(long submitterNumber) {
+        this.submitterNumber = submitterNumber;
+        return this;
+    }
+
+    public String getTargetSinkName() {
+        return targetSinkName;
+    }
+
+    public JobCreatorInfo withTargetSinkName(String targetSinkName) {
+        this.targetSinkName = targetSinkName;
+        return this;
+    }
+
+    public FlowStoreServiceConnector getSourceFlowStoreConnector() {
+        return sourceFlowStoreConnector;
+    }
+
+    public JobCreatorInfo withSourceFlowStoreConnector(
+            FlowStoreServiceConnector sourceFlowStoreConnector) {
+        this.sourceFlowStoreConnector = sourceFlowStoreConnector;
+        return this;
+    }
+
+    public FlowStoreServiceConnector getTargetFlowStoreConnector() {
+        return targetFlowStoreConnector;
+    }
+
+    public JobCreatorInfo withTargetFlowStoreConnector(
+            FlowStoreServiceConnector targetFlowStoreConnector) {
+        this.targetFlowStoreConnector = targetFlowStoreConnector;
+        return this;
+    }
+}

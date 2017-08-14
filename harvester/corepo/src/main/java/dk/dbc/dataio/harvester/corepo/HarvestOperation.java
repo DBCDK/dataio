8c0a51b1eedba057e5794195448c66c865e255f5
Jens Stein Jørgensen <jsj@dbc.dk>
2017-08-14 10:28:01 +0200

    Use correct class for logging

--- harvester/corepo/src/main/java/dk/dbc/dataio/harvester/corepo/HarvestOperation.java
+++ harvester/corepo/src/main/java/dk/dbc/dataio/harvester/corepo/HarvestOperation.java
@@ -47,7 +47,7 @@ import java.util.Date;
 import java.util.List;
 
 public class HarvestOperation {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HarvesterBean.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HarvestOperation.class);
     private static final long HARVEST_INTERVAL_DURATION_IN_SECONDS = 600;
     private static final long HARVEST_LAG_IN_SECONDS = 30;
     static int HARVEST_MAX_BATCH_SIZE = 100000;

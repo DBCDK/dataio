8786491a6b63826651a8bfd335173083db7aecad
Jan Bauer Nielsen <jbn@dbc.dk>
2017-09-06 13:48:49 +0200

    Add query-language module
    Uses javacc to generate query language parser.

---
+++ query-language/src/test/java/dk/dbc/dataio/querylanguage/DataIOQLParserTest.java
@@ -0,0 +1,33 @@
+/*
+ * DataIO - Data IO
+ *
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.querylanguage;
+
+import org.junit.Test;
+
+public class DataIOQLParserTest {
+    @Test
+    public void test() throws ParseException {
+        final DataIOQLParser ioqlParser = new DataIOQLParser();
+        ioqlParser.parse("job:id = hest AND job:creationtime > \"2017-09-06\"");
+    }
+}

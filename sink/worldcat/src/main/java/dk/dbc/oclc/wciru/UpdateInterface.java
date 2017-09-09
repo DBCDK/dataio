8f7350a2df94a18653d8fc8c08514d8f0fddc597
Jan Bauer Nielsen <jbn@dbc.dk>
2017-09-09 13:12:50 +0200

    Fix PMD warnings

--- sink/worldcat/src/main/java/dk/dbc/oclc/wciru/UpdateInterface.java
+++ sink/worldcat/src/main/java/dk/dbc/oclc/wciru/UpdateInterface.java
@@ -38,13 +38,13 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 public interface UpdateInterface {
     @WebMethod
     @WebResult(name = "updateResponse", targetNamespace = "http://www.loc.gov/zing/srw/update/", partName = "updateResponse")
-    public UpdateResponseType update(
+    UpdateResponseType update(
         @WebParam(name = "updateRequest", targetNamespace = "http://www.loc.gov/zing/srw/update/", partName = "updateRequest")
         UpdateRequestType updateRequest);
 
     @WebMethod
     @WebResult(name = "explainResponse", targetNamespace = "http://www.loc.gov/zing/srw/update/", partName = "explainResponse")
-    public ExplainResponseType explain(
+    ExplainResponseType explain(
         @WebParam(name = "explainRequest", targetNamespace = "http://www.loc.gov/zing/srw/update/", partName = "explainRequest")
         ExplainRequestType explainRequest);
 }

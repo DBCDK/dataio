86914d5a217da58a1df3444b9cb8485b0fbd0e4e
Jens Stein Jørgensen <jsj@dbc.dk>
2017-09-15 09:22:06 +0200

    Add validate only option to update requests
    Based on a UPDATE_VALIDATE_ONLY_FLAG environment variable.

--- commons/types/src/main/java/dk/dbc/dataio/commons/types/Constants.java
+++ commons/types/src/main/java/dk/dbc/dataio/commons/types/Constants.java
@@ -28,6 +28,7 @@ public class Constants {
     public static final String PROCESSOR_SHARD_ENV_VARIABLE = "PROCESSOR_SHARD";
     public static final String MISSING_FIELD_VALUE = "__MISSING__";
     public static final String CALL_OPEN_AGENCY = "__CALL_OPEN_AGENCY__";
+    public static final String UPDATE_VALIDATE_ONLY_FLAG = "UPDATE_VALIDATE_ONLY_FLAG";
 
     // I'm introducing the invariant that submitter number 1 indicates missing value.
     public static final long MISSING_SUBMITTER_VALUE            = 1;

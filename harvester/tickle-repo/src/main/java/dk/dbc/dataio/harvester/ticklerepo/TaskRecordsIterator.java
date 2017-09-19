0e9fb544c8c93a8a1db66790ef951a45d80c67ac
Jan Bauer Nielsen <jbn@dbc.dk>
2017-09-19 09:58:06 +0200

    Add support for selector based data set harvests
    The harvester accepts tasks with the following types of selectors:
    dataSetName = [NAME]
    dataSet = [ID]

--- harvester/tickle-repo/src/main/java/dk/dbc/dataio/harvester/ticklerepo/TaskRecordsIterator.java
+++ harvester/tickle-repo/src/main/java/dk/dbc/dataio/harvester/ticklerepo/TaskRecordsIterator.java
@@ -25,10 +25,13 @@ package dk.dbc.dataio.harvester.ticklerepo;
 import dk.dbc.dataio.commons.types.AddiMetaData;
 import dk.dbc.dataio.harvester.task.TaskRepo;
 import dk.dbc.dataio.harvester.task.entity.HarvestTask;
+import dk.dbc.dataio.harvester.types.HarvestTaskSelector;
 import dk.dbc.dataio.harvester.types.TickleRepoHarvesterConfig;
 import dk.dbc.ticklerepo.TickleRepo;
 import dk.dbc.ticklerepo.dto.DataSet;
 import dk.dbc.ticklerepo.dto.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.Iterator;
@@ -42,10 +45,13 @@ import java.util.Optional;
  * </p>
  */
 public class TaskRecordsIterator implements RecordsIterator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskRecordsIterator.class);
+
     private final TickleRepo tickleRepo;
     private final TaskRepo taskRepo;
     private final TickleRepoHarvesterConfig config;
     private final HarvestTask task;
+    private final int dataSetId;
     private final Iterator<Record> recordsIterator;
 
     public TaskRecordsIterator(TickleRepo tickleRepo, TaskRepo taskRepo, TickleRepoHarvesterConfig config) {
@@ -53,10 +59,11 @@ public class TaskRecordsIterator implements RecordsIterator {
         this.taskRepo = taskRepo;
         this.config = config;
         this.task = taskRepo.findNextHarvestTask(config.getId()).orElse(null);
+        this.dataSetId = getDataSet().map(DataSet::getId).orElse(0);
         if (task != null) {
-            recordsIterator = new ByList(task.getRecords());
+            this.recordsIterator = createIteratorFromTask(task);
         } else {
-            recordsIterator = Collections.emptyIterator();
+            this.recordsIterator = Collections.emptyIterator();
         }
     }
 
@@ -72,13 +79,57 @@ public class TaskRecordsIterator implements RecordsIterator {
         }
     }
 
-    public class ByList implements Iterator<Record> {
-        private final Iterator<AddiMetaData> list;
-        private final int dataSetId;
+    @Override
+    public void close() {
+        if (recordsIterator instanceof IterateBy) {
+            ((IterateBy) recordsIterator).close();
+        }
+    }
 
-        public ByList(List<AddiMetaData> records) {
+    private Iterator<Record> createIteratorFromTask(HarvestTask task) {
+        if (task.getSelector() != null) {
+            return createIteratorFromSelector(task.getSelector());
+        }
+        if (task.getRecords() != null) {
+            return new IterateByList(task.getRecords());
+        }
+        return Collections.emptyIterator();
+    }
+
+    private Iterator<Record> createIteratorFromSelector(HarvestTaskSelector selector) {
+        if ("datasetname".equalsIgnoreCase(selector.getField())) {
+            if (!config.getContent().getDatasetName().equals(selector.getValue())) {
+                LOGGER.error("Data set name selector mismatch: config:{} != selector:{}",
+                        config.getContent().getDatasetName(), selector.getField());
+                return Collections.emptyIterator();
+            }
+            return new IterateByDataSet();
+        }
+        if ("dataset".equalsIgnoreCase(selector.getField())) {
+            if (!Integer.toString(dataSetId).equals(selector.getValue())) {
+                LOGGER.error("Data set selector mismatch: config:{} != selector:{}",
+                        dataSetId, selector.getValue());
+                return Collections.emptyIterator();
+            }
+            return new IterateByDataSet();
+        }
+        LOGGER.error("Unknown selector field: {}", selector.getField());
+        return Collections.emptyIterator();
+    }
+
+    private Optional<DataSet> getDataSet() {
+        return tickleRepo.lookupDataSet(new DataSet().withName(config.getContent().getDatasetName()));
+    }
+
+    private abstract class IterateBy implements Iterator<Record> {
+        public void close() {}
+    }
+
+    public class IterateByList extends IterateBy {
+        private final Iterator<AddiMetaData> list;
+
+        public IterateByList(List<AddiMetaData> records) {
             list = records.iterator();
-            dataSetId = getDataSet().map(DataSet::getId).orElse(0);
         }
 
         @Override
@@ -98,9 +149,30 @@ public class TaskRecordsIterator implements RecordsIterator {
                     .withLocalId(metaData.bibliographicRecordId()))
                     .orElse(null);
         }
+    }
 
-        private Optional<DataSet> getDataSet() {
-            return tickleRepo.lookupDataSet(new DataSet().withName(config.getContent().getDatasetName()));
+    public class IterateByDataSet extends IterateBy {
+        private final TickleRepo.ResultSet<Record> resultSet;
+        private final Iterator<Record> resultSetIterator;
+
+        public IterateByDataSet() {
+            resultSet = tickleRepo.getRecordsInDataSet(new DataSet().withId(dataSetId));
+            resultSetIterator = resultSet.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return resultSetIterator.hasNext();
+        }
+
+        @Override
+        public Record next() {
+            return resultSetIterator.next();
+        }
+
+        @Override
+        public void close() {
+            resultSet.close();
         }
     }
 }

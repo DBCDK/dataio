eeccb8fbe2b2b55e07753d90591077d3821ff083
Stine Mia Madsen <sma@dbc.dk>
2017-10-09 16:04:28 +0200

    Add JobRerunProxy
    Added JobRerunProxy in order to encapsulate the reun functionality.

---
+++ gui/src/main/java/dk/dbc/dataio/gui/client/proxies/JobRerunProxy.java
@@ -0,0 +1,49 @@
+/*
+ * DataIO - Data IO
+ * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
+ * Denmark. CVR: 15149043
+ *
+ * This file is part of DataIO.
+ *
+ * DataIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.dataio.gui.client.proxies;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnectorException;
+import dk.dbc.dataio.gui.client.model.JobModel;
+import dk.dbc.dataio.gui.server.JobRerunScheme;
+
+@RemoteServiceRelativePath("JobRerunProxy")
+public interface JobRerunProxy extends RemoteService {
+
+    JobRerunScheme parse(JobModel jobModel) throws FlowStoreServiceConnectorException;
+
+    void close();
+
+    class Factory {
+
+        private static JobRerunProxyAsync asyncInstance = null;
+
+        public static JobRerunProxyAsync getAsyncInstance() {
+            if (asyncInstance == null) {
+                asyncInstance = GWT.create(JobRerunProxy.class);
+            }
+            return asyncInstance;
+        }
+    }
+}
--- gui/src/main/java/dk/dbc/dataio/gui/client/proxies/JobRerunProxyAsync.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/proxies/JobRerunProxyAsync.java
@@ -19,35 +19,15 @@
  * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package dk.dbc.dataio.gui.server;
+package dk.dbc.dataio.gui.client.proxies;
 
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import dk.dbc.dataio.gui.client.model.JobModel;
+import dk.dbc.dataio.gui.server.JobRerunScheme;
 
-public class JobRerunScheme implements Serializable {
+public interface JobRerunProxyAsync {
 
-    public enum Action { COPY, RERUN_ALL, RERUN_FAILED }
-    public enum Type { RR, TICKLE, ORIGINAL_FILE }
+    void close(AsyncCallback<Void> async);
 
-    private Set<Action> actions;
-    private Type type;
-
-    public Type getType() {
-        return type;
-    }
-
-    public JobRerunScheme withType(Type type) {
-        this.type = type;
-        return this;
-    }
-
-    public Set<Action> getActions() {
-        return actions;
-    }
-
-    public JobRerunScheme withActions(Set<Action> actions) {
-        this.actions = new HashSet<>(actions);
-        return this;
-    }
+    void parse(JobModel jobModel, AsyncCallback<JobRerunScheme> async);
 }

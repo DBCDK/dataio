2d75320a05c3e859b13795e821a6a0df55cf431c
Jens Stein Jørgensen <jsj@dbc.dk>
2018-09-11 10:09:49 +0200

    Remove select part from harvester config migration
    Otherwise all the rows get replaced by the first one the select
    statement finds.

--- flow-store-service/src/main/resources/db/migration/V23__update_table_harvester_configs_remove_json_key_openagencytarget.sql
+++ flow-store-service/src/main/resources/db/migration/V23__update_table_harvester_configs_remove_json_key_openagencytarget.sql
@@ -1,1 +1,1 @@
-UPDATE harvester_configs SET content = (c.content - 'openAgencyTarget') FROM (SELECT content FROM harvester_configs) AS c;
+UPDATE harvester_configs SET content = (content - 'openAgencyTarget');

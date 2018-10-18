e27df958aacb055fd312656c1aaa1a904b40522d
Henrik Witt-Hansen <hwha@dbc.dk>
2018-10-18 15:54:53 +0200

    Remove two tests that tests behaviour no longer present in the RawRepoConnector
    The tests tested that RawRepoDAO.fetchMergedRecord() was called for a deleted
    record and that RawRepoDAO.fetchRecordCollectionExpanded was called for non-deleted records.
    
    This operation has been delegated to the record-service, and activated by the
    use of the mode=expanded parameter as given in the harvesters.

--- harvester/utils/rawrepo-connector/src/test/java/dk/dbc/dataio/harvester/utils/rawrepo/RawRepoConnectorTest.java
+++ harvester/utils/rawrepo-connector/src/test/java/dk/dbc/dataio/harvester/utils/rawrepo/RawRepoConnectorTest.java
@@ -22,14 +22,6 @@
 package dk.dbc.dataio.harvester.utils.rawrepo;
 
 import dk.dbc.dataio.commons.utils.test.jndi.InMemoryInitialContextFactory;
-import dk.dbc.marcxmerge.MarcXMerger;
-import dk.dbc.marcxmerge.MarcXMergerException;
-import dk.dbc.rawrepo.MockedRecord;
-import dk.dbc.rawrepo.RawRepoDAO;
-import dk.dbc.rawrepo.RawRepoException;
-import dk.dbc.rawrepo.Record;
-import dk.dbc.rawrepo.RecordId;
-import dk.dbc.rawrepo.RelationHintsOpenAgency;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -37,17 +29,12 @@ import org.junit.Test;
 import javax.naming.Context;
 import javax.sql.DataSource;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class RawRepoConnectorTest {
     private static final String DATA_SOURCE_RESOURCE_NAME = "resourceName";
@@ -133,47 +120,6 @@ public class RawRepoConnectorTest {
             fail("No exception thrown");
         } catch (NullPointerException e) {
         }
-    }
-
-    @Test
-    public void getStringRecordMap_recordIsDeleted_returnsRecordMapDerivedFromFetchMergedRecord() throws RawRepoException, MarcXMergerException {
-        final RecordId recordId = new RecordId("id", 42);
-        final MockedRecord mockedRecord = new MockedRecord(recordId, true);
-
-        when(rawRepoDAO.recordExistsMaybeDeleted(recordId.getBibliographicRecordId(), recordId.getAgencyId()))
-                .thenReturn(true);
-        when(rawRepoDAO.recordExists(recordId.getBibliographicRecordId(), recordId.getAgencyId()))
-                .thenReturn(false);
-        when(rawRepoDAO.fetchMergedRecord(eq(recordId.getBibliographicRecordId()), eq(recordId.getAgencyId()), any(MarcXMerger.class), eq(true)))
-                .thenReturn(mockedRecord);
-
-        final RawRepoConnector connector = getRawRepoConnector();
-        final Map<String, Record> recordMap = connector.getStringRecordMap(recordId, rawRepoDAO, true);
-
-        assertThat("recordMap", recordMap, is(notNullValue()));
-        assertThat("recordMap.size", recordMap.size(), is(1));
-        assertThat("recordMap.key",  recordMap.containsKey(recordId.getBibliographicRecordId()), is(true));
-        assertThat("recordMap.value", (MockedRecord) recordMap.get(recordId.getBibliographicRecordId()), is(mockedRecord));
-    }
-
-    @Test
-    public void getStringRecordMap_recordIsNotDeleted_returnsRecordMapFromFetchRecordCollection() throws RawRepoException, MarcXMergerException {
-        final RecordId recordId = new RecordId("id", 42);
-        final MockedRecord mockedRecord = new MockedRecord(recordId, true);
-        final Map<String, Record> expectedRecordMap = new HashMap<>(1);
-        expectedRecordMap.put("anotherid", mockedRecord);
-
-        when(rawRepoDAO.recordExistsMaybeDeleted(recordId.getBibliographicRecordId(), recordId.getAgencyId()))
-                .thenReturn(true);
-        when(rawRepoDAO.recordExists(recordId.getBibliographicRecordId(), recordId.getAgencyId()))
-                .thenReturn(true);
-        when(rawRepoDAO.fetchRecordCollectionExpanded(eq(recordId.getBibliographicRecordId()), eq(recordId.getAgencyId()), any(MarcXMerger.class)))
-                .thenReturn(expectedRecordMap);
-
-        final RawRepoConnector connector = getRawRepoConnector();
-        final Map<String, Record> recordMap = connector.getStringRecordMap(recordId, rawRepoDAO, true);
-
-        assertThat("recordMap", recordMap, is(expectedRecordMap));
     }
 
     private RawRepoConnector getRawRepoConnector() {

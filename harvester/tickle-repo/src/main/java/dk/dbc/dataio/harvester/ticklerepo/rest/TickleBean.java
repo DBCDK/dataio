aff163cedd90f02f52cbaf62190c8d6dd5a0b037
Jan Bauer Nielsen <jbn@dbc.dk>
2019-04-29 12:48:47 +0200

    Add deleteOutdatedRecords method to harvester API

--- harvester/tickle-repo/src/main/java/dk/dbc/dataio/harvester/ticklerepo/rest/TickleBean.java
+++ harvester/tickle-repo/src/main/java/dk/dbc/dataio/harvester/ticklerepo/rest/TickleBean.java
@@ -23,16 +23,20 @@
 package dk.dbc.dataio.harvester.ticklerepo.rest;
 
 import dk.dbc.ticklerepo.TickleRepo;
+import dk.dbc.ticklerepo.dto.Batch;
 import dk.dbc.ticklerepo.dto.DataSet;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.time.Instant;
 
 @Stateless
 @Path("/")
@@ -57,5 +61,35 @@ public class TickleBean {
                 .withName(dataSetName))
                 .orElse(null);
         return Response.ok().entity(tickleRepo.estimateSizeOf(dataSet)).build();
+    }
+
+    /**
+     * Change the status of records in the dataset to DELETED if their time
+     * of last modification is before cut-off time POSTed as milliseconds
+     * since epoch.
+     * @param dataSetId ID of dataset for which to delete outdated records
+     * @param cutOffEpochMillis threshold for outdated records as milliseconds
+     *                          since epoch
+     * @return a HTTP 200 OK response on success,
+     *         a HTTP 204 NO_CONTENT response on unknown dataset ID
+     */
+    @POST
+    @Path("dataset/{id:\\d+}/time-of-last-modification-cut-off")
+    @Consumes({ MediaType.TEXT_PLAIN })
+    public Response deleteOutdatedRecords(@PathParam("id") Integer dataSetId, Long cutOffEpochMillis) {
+        final DataSet dataSet = tickleRepo.lookupDataSet(new DataSet()
+                .withId(dataSetId))
+                .orElse(null);
+        if (dataSet == null) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+        final Instant cutOff = Instant.ofEpochMilli(cutOffEpochMillis);
+        final Batch batch = tickleRepo.createBatch(new Batch()
+                .withDataset(dataSet.getId())
+                .withType(Batch.Type.INCREMENTAL)
+                .withBatchKey(0));
+        tickleRepo.deleteOutdatedRecordsInBatch(batch, cutOff);
+        tickleRepo.closeBatch(batch);
+        return Response.ok().build();
     }
 }
---
+++ harvester/tickle-repo/src/test/java/dk/dbc/dataio/harvester/ticklerepo/rest/TickleBeanIT.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.harvester.ticklerepo.rest;
+
+
+import dk.dbc.commons.persistence.JpaTestEnvironment;
+import dk.dbc.dataio.harvester.ticklerepo.IntegrationTest;
+import dk.dbc.ticklerepo.TickleRepo;
+import dk.dbc.ticklerepo.dto.Batch;
+import dk.dbc.ticklerepo.dto.DataSet;
+import dk.dbc.ticklerepo.dto.Record;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.Query;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class TickleBeanIT extends IntegrationTest {
+    private TickleBean tickleBean;
+
+    @Before
+    public void createTickleBean() {
+        tickleBean = new TickleBean();
+        tickleBean.tickleRepo = new TickleRepo(
+                environment.get("ticklerepo").getEntityManager());
+    }
+
+    @Test
+    public void deleteOutdatedRecords() {
+        final Set<Integer> expectedRecords = new HashSet<>();
+
+        final JpaTestEnvironment env = environment.get("ticklerepo");
+        final TickleRepo tickleRepo = tickleBean.tickleRepo;
+
+        final DataSet dataset = tickleRepo.lookupDataSet(
+                new DataSet().withName("dataset"))
+                .orElse(null);
+
+        /* Force two records from the dataset to have a
+           timeOfLastModification value in the past and
+           remember their IDs. */
+        env.getPersistenceContext().run(() -> {
+            try (TickleRepo.ResultSet<Record> rs = tickleRepo.getRecordsInDataSet(dataset)) {
+                final Query updateTimeOfLastModification = env.getEntityManager()
+                        .createNativeQuery("UPDATE record SET timeOfLastModification = ?1 WHERE id = ?2")
+                        .setParameter(1, Timestamp.from(
+                                Instant.now().minus(2, ChronoUnit.DAYS)));
+                for (Record record : rs) {
+                    if (expectedRecords.size() == 2) {
+                        break;
+                    }
+                    updateTimeOfLastModification.setParameter(2, record.getId());
+                    updateTimeOfLastModification.executeUpdate();
+                    expectedRecords.add(record.getId());
+                }
+            }
+        });
+
+        /* Call deleteOutdatedRecords while ensuring that
+           the two records modified above are included. */
+        env.getPersistenceContext().run(() -> tickleBean.deleteOutdatedRecords(
+                dataset.getId(),
+                Instant.now().minus(1, ChronoUnit.DAYS).toEpochMilli()));
+
+        final Batch batch = tickleRepo.getNextBatch(new Batch()
+                .withDataset(dataset.getId())
+                .withId(3))
+                .orElse(null);
+        assertThat("batch timeOfCompletion",
+                batch.getTimeOfCompletion(), is(notNullValue()));
+
+        /* Verify the expected records. */
+        int numberOfRecordsInBatch = 0;
+        for (Record record : env.getPersistenceContext().run(
+                        () -> tickleRepo.getRecordsInBatch(batch))) {
+            assertThat("expected set of records contains ID " + record.getId(),
+                    expectedRecords.contains(record.getId()), is(true));
+            numberOfRecordsInBatch++;
+        }
+        assertThat("number of outdated records",
+                numberOfRecordsInBatch, is(expectedRecords.size()));
+    }
+}

43e8f9dcab69396f2f1e11fdce987e54a63db28b
Henrik Witt-Hansen <hwha@dbc.dk>
2019-05-06 23:02:45 +0200

    Rewrite zippedxmlpartitioner to partition the zipfile entry by entry
    This allows us to handle large size zipfiles, as well as adhering better to the overall
    architechure of dataio

--- job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/partitioner/ZippedXmlDataPartitioner.java
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/partitioner/ZippedXmlDataPartitioner.java
@@ -1,122 +1,138 @@
 package dk.dbc.dataio.jobstore.service.partitioner;
 
+import dk.dbc.dataio.common.utils.io.ByteCountingInputStream;
+import dk.dbc.dataio.jobstore.types.InvalidEncodingException;
+import dk.dbc.dataio.jobstore.types.PrematureEndOfDataException;
 import dk.dbc.dataio.jobstore.types.UnrecoverableDataException;
 import dk.dbc.invariant.InvariantUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+// Todo: Fix start import
+import java.io.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-public class ZippedXmlDataPartitioner extends DefaultXmlDataPartitioner {
+public class ZippedXmlDataPartitioner implements DataPartitioner {
     private static final Logger LOGGER = LoggerFactory.getLogger(ZippedXmlDataPartitioner.class);
+
+    private final ByteCountingInputStream inputStream;
+    private final String encodingExpected;
+    private ZipInputStream zipStream;
+    private Iterator<DataPartitionerResult> iterator;
+    private Iterator<DataPartitionerResult> currentZipIterator = null;
 
     public static ZippedXmlDataPartitioner newInstance(InputStream inputStream, String encoding)
             throws NullPointerException, IllegalArgumentException, UnrecoverableDataException {
         InvariantUtil.checkNotNullOrThrow(inputStream, "inputStream");
         InvariantUtil.checkNotNullNotEmptyOrThrow(encoding, "encoding");
 
-        // Setup input- and outputstreams for unzipping
-        ZipInputStream zipStream = new ZipInputStream(inputStream);
-        ZipEntry zipEntry;
-        ByteArrayOutputStream uncompressedOutputStream = new ByteArrayOutputStream();
-
-        // Read all entries and combine into one doc.
-        // The sequential read of the zipfile ensures that the individual chunks will always
-        // end up the same place in the combined document
-        try {
-
-            // Combined document
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder outputDocumentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document outputDocument = outputDocumentBuilder.newDocument();
-            Element root = null;
-
-            // Process all entries
-            while((zipEntry = zipStream.getNextEntry()) != null) {
-                LOGGER.info("Reading zipped file " + zipEntry.getName() + " with uncompressed size " + zipEntry.getSize());
-
-                // Read all data from this entry. Due to the compressed format, we can not read the entire
-                // unzipped file in one go, hence the need for the loop.
-                byte[] buffer = new byte[(int) 2048];
-                ByteArrayOutputStream chunk = new ByteArrayOutputStream();
-                int len;
-                while( (len = zipStream.read(buffer)) > 0) {
-                    chunk.write(buffer, 0, len);
-                }
-                zipStream.closeEntry();
-
-                // Load file into DOM document
-                ByteArrayInputStream chunkStream = new ByteArrayInputStream(chunk.toByteArray());
-                DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-                Document originalDocument = documentBuilder.parse(chunkStream);
-
-                // If we have no root element yet, create it from the input root.
-                // We can safely assume that the root element is the same for all documents in the
-                // zipfile, and besides, the root element is dropped by the xml partitioner
-                if( root == null ) {
-                    root = (Element) outputDocument.importNode(originalDocument.getFirstChild(), false);
-                    outputDocument.appendChild(root);
-                }
-
-                // Import all childnodes under the root into the output document
-                NodeList innerXml = originalDocument.getDocumentElement().getChildNodes();
-                for( int nid = 0; nid < innerXml.getLength(); nid++ ) {
-                    Node node = innerXml.item(nid);
-                    Node importedNode = outputDocument.importNode(node, true);
-                    root.appendChild(importedNode);
-                }
-            }
-
-            // Write completed document to the output stream
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource domSource = new DOMSource(outputDocument);
-            StreamResult streamResult = new StreamResult(uncompressedOutputStream);
-            transformer.transform(domSource, streamResult);
-
-            // Convert to input stream, ready for the xml parser
-            ByteArrayInputStream uncompressedInputStream = new ByteArrayInputStream(uncompressedOutputStream.toByteArray());
-
-            // Create a new DefaultXMLDataPartitioner (parent class)
-            return new ZippedXmlDataPartitioner(uncompressedInputStream, encoding);
-        }
-        catch( IOException ioe ) {
-            LOGGER.error("Caught IOException when uncompressing zipped input data: " + ioe.getMessage());
-            throw new UnrecoverableDataException(ioe);
-        }
-        catch( javax.xml.transform.TransformerException te) {
-            LOGGER.error("Caught TransformerException when writing combined xml document: " + te.getMessage());
-            throw new UnrecoverableDataException(te);
-        }
-        catch( javax.xml.parsers.ParserConfigurationException pce ) {
-            LOGGER.error("Caught ParserConfigurationException when parsing one file from the zip stream: " + pce.getMessage());
-            throw new UnrecoverableDataException(pce);
-        }
-        catch(org.xml.sax.SAXException se) {
-            LOGGER.error("SAXException: " + se.getMessage());
-            throw new UnrecoverableDataException(se);
-        }
+        return new ZippedXmlDataPartitioner(inputStream, encoding);
     }
 
     protected ZippedXmlDataPartitioner(InputStream inputStream, String encoding) {
-        super(inputStream, encoding);
+        this.inputStream = new ByteCountingInputStream(inputStream);
+        this.encodingExpected = encoding;
+        this.zipStream = new ZipInputStream(inputStream);
+    }
+
+    // Todo: Add constructors for fetching parts of the inputstream
+
+    @Override
+    public Charset getEncoding() throws InvalidEncodingException {
+        return StandardCharsets.UTF_8;
+    }
+
+    @Override
+    public long getBytesRead() {
+        return inputStream.getBytesRead();
+    }
+
+    @Override
+    public Iterator<DataPartitionerResult> iterator() throws UnrecoverableDataException, PrematureEndOfDataException {
+
+        if (iterator == null) {
+
+            // We need to read ahead since there is no other way of known if we are at the end of the zipfile.
+            getNextEntryWithData();
+
+            // Create iterator
+            iterator = new Iterator<DataPartitionerResult>() {
+
+                /**
+                 * @inheritDoc
+                 */
+                @Override
+                public boolean hasNext() throws UnrecoverableDataException, PrematureEndOfDataException {
+                    return currentZipIterator != null;
+                }
+
+                /**
+                 * @inheritDoc
+                 */
+                @Override
+                public DataPartitionerResult next() throws UnrecoverableDataException {
+
+                    // Get next chunk
+                    DataPartitionerResult result = currentZipIterator.next();
+
+                    // If no more chunks available, try to advance the iterator
+                    if( !currentZipIterator.hasNext() ) {
+                        getNextEntryWithData();
+                    }
+
+                    return result;
+                }
+
+                /**
+                 * @inheritDoc This method does not do anything.
+                 */
+                @Override
+                public void remove() {
+                }
+            };
+
+        }
+        return iterator;
+    }
+
+    private DefaultXmlDataPartitioner getXmlDataPartitioner(ZipInputStream zipStream) throws IOException {
+
+        // Read all data from this entry. Due to the compressed format, we can not read the entire
+        // unzipped file in one go, hence the need for the loop.
+        byte[] buffer = new byte[(int) 2048];
+        ByteArrayOutputStream chunks = new ByteArrayOutputStream();
+        int len;
+        while( (len = zipStream.read(buffer)) > 0) {
+            chunks.write(buffer, 0, len);
+        }
+
+        // Get inputstream with the uncompressed chunk
+        ByteArrayInputStream uncompressedChunks = new ByteArrayInputStream(chunks.toByteArray());
+        return new DefaultXmlDataPartitioner(uncompressedChunks, encodingExpected);
+    }
+
+    private void getNextEntryWithData() {
+
+        try {
+            do {
+                // Fetch next entry, check if we have reached eof
+                ZipEntry entry = zipStream.getNextEntry();
+                if(entry == null) {
+                    currentZipIterator = null;
+                    return;
+                }
+
+                // Partition chunks contained in the new entry, get the iterator
+                DefaultXmlDataPartitioner partitioner = getXmlDataPartitioner(zipStream);
+                currentZipIterator = partitioner.iterator();
+            } while(!currentZipIterator.hasNext());
+        } catch(IOException ioe) {
+            throw new UnrecoverableDataException(ioe);
+        }
     }
 }

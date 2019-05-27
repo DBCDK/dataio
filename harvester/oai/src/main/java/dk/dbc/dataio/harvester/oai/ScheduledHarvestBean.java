cc2c768bde864824115274310480870b39088d06
Jan Bauer Nielsen <jbn@dbc.dk>
2019-05-27 09:57:30 +0200

    Add bean responsible for harvest scheduling

---
+++ harvester/oai/src/main/java/dk/dbc/dataio/harvester/oai/ScheduledHarvestBean.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.harvester.oai;
+
+import dk.dbc.dataio.harvester.AbstractScheduledHarvestBean;
+import dk.dbc.dataio.harvester.types.OaiHarvesterConfig;
+import dk.dbc.util.RunSchedule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.ejb.ScheduleExpression;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * This singleton Enterprise Java Bean (EJB) class schedules harvests
+ * every thirty seconds
+ */
+@Singleton
+@Startup
+public class ScheduledHarvestBean extends AbstractScheduledHarvestBean<HarvesterBean, OaiHarvesterConfig, HarvesterConfigurationBean> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledHarvestBean.class);
+
+    @EJB
+    HarvesterBean harvesterBean;
+
+    @EJB
+    HarvesterConfigurationBean harvesterConfigurationBean;
+
+    @Override
+    public HarvesterBean getHarvesterBeanImpl() {
+        return harvesterBean;
+    }
+
+    @Override
+    public HarvesterConfigurationBean getHarvesterConfigurationBeanImpl() {
+        return harvesterConfigurationBean;
+    }
+
+    @Override
+    public ScheduleExpression getTimerSchedule() {
+        final ScheduleExpression scheduleExpression = new ScheduleExpression();
+        scheduleExpression.second("*/30");
+        scheduleExpression.minute("*");
+        scheduleExpression.hour("*");
+        return scheduleExpression;
+    }
+
+    @Override
+    public Logger getLogger() {
+        return LOGGER;
+    }
+
+    @Override
+    public boolean canRun(OaiHarvesterConfig config) {
+        try {
+            return new RunSchedule(config.getContent().getSchedule())
+                    .withTimezone(ZoneId.of("Europe/Copenhagen"))
+                    .isSatisfiedBy(new Date(), config.getContent().getTimeOfLastHarvest());
+        } catch (RuntimeException e) {
+            LOGGER.error("Unable to check schedule for {}", config.getLogId(), e);
+        }
+        return false;
+    }
+}

541490dadfcfc5d49d75d6ef546b75ea345d83a9
Jan Bauer Nielsen <jbn@dbc.dk>
2019-07-08 12:09:54 +0200

    Switch worldcat-sink to payara5

--- sink/worldcat/src/main/docker/dockerfile/dbc-payara.d/mdb-container.cmd
+++
@@ -1,2 +0,0 @@
-set server.mdb-container.max-pool-size=1 --passwordfile=./passfile.txt
-set server.mdb-container.pool-resize-quantity=1 --passwordfile=./passfile.txt
--- sink/worldcat/src/test/java/dk/dbc/dataio/sink/worldcat/WciruServiceBrokerTest.java
+++ sink/worldcat/src/test/java/dk/dbc/dataio/sink/worldcat/WciruServiceBrokerTest.java
@@ -37,8 +37,8 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 

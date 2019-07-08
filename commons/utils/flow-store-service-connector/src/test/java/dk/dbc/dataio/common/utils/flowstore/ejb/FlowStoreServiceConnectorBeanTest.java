cf858825e347e4fd86bba22d259e9157e01d939c
Jan Bauer Nielsen <jbn@dbc.dk>
2019-07-08 09:55:58 +0200

    Use environment variable instead of JNDI lookup

--- commons/utils/flow-store-service-connector/src/test/java/dk/dbc/dataio/common/utils/flowstore/ejb/FlowStoreServiceConnectorBeanTest.java
+++ commons/utils/flow-store-service-connector/src/test/java/dk/dbc/dataio/common/utils/flowstore/ejb/FlowStoreServiceConnectorBeanTest.java
@@ -22,14 +22,9 @@
 package dk.dbc.dataio.common.utils.flowstore.ejb;
 
 import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnector;
-import dk.dbc.dataio.commons.types.jndi.JndiConstants;
-import dk.dbc.dataio.commons.utils.test.jndi.InMemoryInitialContextFactory;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
-
-import javax.ejb.EJBException;
-import javax.naming.Context;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -38,42 +33,37 @@ import static org.hamcrest.core.IsNull.nullValue;
 import static org.mockito.Mockito.mock;
 
 public class FlowStoreServiceConnectorBeanTest {
-    @BeforeClass
-    public static void setup() {
-        // sets up the InMemoryInitialContextFactory as default factory.
-        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, InMemoryInitialContextFactory.class.getName());
-    }
+    @Rule
+    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
-    @Before
-    public void clearContext() {
-        InMemoryInitialContextFactory.clear();
-    }
-
-    @Test(expected = EJBException.class)
-    public void initializeConnector_urlResourceLookupThrowsNamingException_throws() {
+    @Test(expected = NullPointerException.class)
+    public void initializeConnector_environmentNotSet_throws() {
         final FlowStoreServiceConnectorBean jobStoreServiceConnectorBean = newFlowStoreServiceConnectorBean();
         jobStoreServiceConnectorBean.initializeConnector();
     }
 
     @Test
-    public void getConnector_connectorIsInitialized_connectorIsReturned() {
-        FlowStoreServiceConnector flowStoreServiceConnector = mock(FlowStoreServiceConnector.class);
-        FlowStoreServiceConnectorBean flowStoreServiceConnectorBean = newFlowStoreServiceConnectorBean();
-        flowStoreServiceConnectorBean.flowStoreServiceConnector = flowStoreServiceConnector;
-        assertThat(flowStoreServiceConnectorBean.getConnector(), is(flowStoreServiceConnector));
-    }
-
-    @Test
-    public void initializeConnector_connectorIsInitialized_connectorIsNotNull() {
-        InMemoryInitialContextFactory.bind(JndiConstants.FLOW_STORE_SERVICE_ENDPOINT_RESOURCE, "someURL");
-        FlowStoreServiceConnectorBean flowStoreServiceConnectorBean = newFlowStoreServiceConnectorBean();
+    public void initializeConnector() {
+        environmentVariables.set("FLOWSTORE_URL", "http://test");
+        final FlowStoreServiceConnectorBean flowStoreServiceConnectorBean =
+                newFlowStoreServiceConnectorBean();
         flowStoreServiceConnectorBean.initializeConnector();
+
         assertThat(flowStoreServiceConnectorBean.getConnector(), not(nullValue()));
     }
 
-    /*
-     * Private methods
-     */
+    @Test
+    public void getConnector() {
+        final FlowStoreServiceConnector flowStoreServiceConnector =
+                mock(FlowStoreServiceConnector.class);
+        final FlowStoreServiceConnectorBean flowStoreServiceConnectorBean =
+                newFlowStoreServiceConnectorBean();
+        flowStoreServiceConnectorBean.flowStoreServiceConnector = flowStoreServiceConnector;
+
+        assertThat(flowStoreServiceConnectorBean.getConnector(),
+                is(flowStoreServiceConnector));
+    }
+
     private FlowStoreServiceConnectorBean newFlowStoreServiceConnectorBean() {
         return new FlowStoreServiceConnectorBean();
     }

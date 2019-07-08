7584387e1e0a0fad2a5a002790e6947b5dd78d5a
Jan Bauer Nielsen <jbn@dbc.dk>
2019-07-08 13:47:18 +0200

    Switch batch-exchange-sink to payara5

--- sink/batch-exchange/src/test/java/dk/dbc/dataio/sink/batchexchange/BatchFinalizerBeanIT.java
+++ sink/batch-exchange/src/test/java/dk/dbc/dataio/sink/batchexchange/BatchFinalizerBeanIT.java
@@ -38,8 +38,8 @@ import static dk.dbc.commons.testutil.Assert.isThrowing;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -67,7 +67,7 @@ public class BatchFinalizerBeanIT extends IntegrationTest {
      *   Then: finalizer throws SinkException
      */
     @Test
-    public void finalizeNextCompletedBatch_jobStoreUploadThrows() throws JobStoreServiceConnectorException, SinkException {
+    public void finalizeNextCompletedBatch_jobStoreUploadThrows() throws JobStoreServiceConnectorException {
         executeScriptResource("/completed_batch.sql");
 
         when(jobStoreServiceConnector.addChunkIgnoreDuplicates(any(Chunk.class), anyLong(), anyLong()))

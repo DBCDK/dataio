16da0b704e7e91dffa30cc4f9e8337fffe8e9b3d
Jan Bauer Nielsen <jbn@dbc.dk>
2019-07-10 07:00:17 +0200

    Use environment variable instead of JNDI lookup

--- commons/utils/binary-file-store/src/main/java/dk/dbc/dataio/bfs/ejb/BinaryFileStoreConfigurationBean.java
+++ commons/utils/binary-file-store/src/main/java/dk/dbc/dataio/bfs/ejb/BinaryFileStoreConfigurationBean.java
@@ -21,41 +21,30 @@
 
 package dk.dbc.dataio.bfs.ejb;
 
-import dk.dbc.dataio.commons.utils.service.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.ejb.EJBException;
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
-import javax.naming.NamingException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * This Enterprise Java Bean (EJB) singleton bean is used to gain
- * access to binary file store configuration.
- */
+// TODO: 05-07-19 replace EJB with @ApplicationScoped CDI producer
+
 @Singleton
 @LocalBean
 public class BinaryFileStoreConfigurationBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(BinaryFileStoreConfigurationBean.class);
 
-    @Resource(lookup = "java:app/env/dataio/bfs/jndi/path")
-    String basePathJndiName;
-
-    private String basePath;
+    String basePath;
 
     @PostConstruct
     public void initialize() {
-        LOGGER.info("basePathJndiName={}", basePathJndiName);
-        try {
-            basePath = ServiceUtil.getStringValueFromResource(basePathJndiName);
-        } catch (NamingException e) {
-            LOGGER.error("Exception caught during JNDI lookup of {}", basePathJndiName, e);
-            throw new EJBException(e);
+        basePath = System.getenv("BFS_ROOT");
+        if (basePath == null || basePath.trim().isEmpty()) {
+            throw new EJBException("BFS_ROOT must be set");
         }
         LOGGER.info("basePath={}", basePath);
     }

33a3c8cb24e1ac7f4a8732cbfd287df9dda8df4c
Jan Bauer Nielsen <jbn@dbc.dk>
2019-08-06 14:06:03 +0200

    Use original eclipselink version

--- gatekeeper/src/test/java/dk/dbc/dataio/gatekeeper/JobDispatcherIT.java
+++ gatekeeper/src/test/java/dk/dbc/dataio/gatekeeper/JobDispatcherIT.java
@@ -60,7 +60,7 @@ import java.util.concurrent.TimeUnit;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
--- gatekeeper/src/test/java/dk/dbc/dataio/gatekeeper/operation/CreateJobOperationTest.java
+++ gatekeeper/src/test/java/dk/dbc/dataio/gatekeeper/operation/CreateJobOperationTest.java
@@ -48,7 +48,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;

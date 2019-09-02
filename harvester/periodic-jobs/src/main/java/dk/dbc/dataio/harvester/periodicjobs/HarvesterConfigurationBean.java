79e8bb749c56f5bceaba527a648a51d8ce20c02a
Jan Bauer Nielsen <jbn@dbc.dk>
2019-09-02 12:20:41 +0200

    Add configurations bean

---
+++ harvester/periodic-jobs/src/main/java/dk/dbc/dataio/harvester/periodicjobs/HarvesterConfigurationBean.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.harvester.periodicjobs;
+
+import dk.dbc.dataio.harvester.AbstractHarvesterConfigurationBean;
+import dk.dbc.dataio.harvester.types.PeriodicJobsHarvesterConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Singleton;
+
+@Singleton
+public class HarvesterConfigurationBean extends AbstractHarvesterConfigurationBean<PeriodicJobsHarvesterConfig> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HarvesterConfigurationBean.class);
+
+    @Override
+    public Logger getLogger() {
+        return LOGGER;
+    }
+
+    @Override
+    public Class<PeriodicJobsHarvesterConfig> getConfigClass() {
+        return PeriodicJobsHarvesterConfig.class;
+    }
+}

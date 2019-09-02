b9a1dfe1555c830edf5735fb6c645715bc142a31
Jan Bauer Nielsen <jbn@dbc.dk>
2019-09-02 13:00:45 +0200

    Add harvest scheduler

---
+++ harvester/periodic-jobs/src/test/java/dk/dbc/dataio/harvester/periodicjobs/ScheduledHarvestBeanTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.harvester.periodicjobs;
+
+import dk.dbc.dataio.harvester.types.PeriodicJobsHarvesterConfig;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ScheduledHarvestBeanTest {
+    @Rule
+    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+
+    private final ScheduledHarvestBean scheduledHarvestBean = new ScheduledHarvestBean();
+
+    @Before
+    public void setTimeZone() {
+        environmentVariables.set("TZ", "Europe/Copenhagen");
+    }
+
+    @Test
+    public void canRun_withInvalidSchedule() {
+        final PeriodicJobsHarvesterConfig config = new PeriodicJobsHarvesterConfig(1, 2,
+                new PeriodicJobsHarvesterConfig.Content()
+                        .withSchedule("invalid"));
+
+        assertThat(scheduledHarvestBean.canRun(config), is(false));
+    }
+
+    @Test
+    public void canRun_withValidSchedule() {
+        final PeriodicJobsHarvesterConfig config = new PeriodicJobsHarvesterConfig(1, 2,
+                new PeriodicJobsHarvesterConfig.Content()
+                        .withSchedule("* * * * *")
+                        .withTimeOfLastHarvest(null));
+
+        assertThat(scheduledHarvestBean.canRun(config), is(true));
+    }
+
+    @Test
+    public void canRun_withTimeOfLastHarvestTooCloseToNow() {
+        final PeriodicJobsHarvesterConfig config = new PeriodicJobsHarvesterConfig(1, 2,
+                new PeriodicJobsHarvesterConfig.Content()
+                        .withSchedule("* * * * *")
+                        .withTimeOfLastHarvest(new Date()));
+
+        assertThat(scheduledHarvestBean.canRun(config), is(false));
+    }
+
+    @Test
+    public void canRun_isOverdue() {
+        final PeriodicJobsHarvesterConfig config = new PeriodicJobsHarvesterConfig(1, 2,
+                new PeriodicJobsHarvesterConfig.Content()
+                        .withSchedule("* * * * *")
+                        .withTimeOfLastHarvest(Date.from(
+                                Instant.now().minus(1, ChronoUnit.HOURS))));
+
+        assertThat(scheduledHarvestBean.canRun(config), is(true));
+    }
+}

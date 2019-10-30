ed234d6bb1399d2f80e118ac3afa846817c78170
Jan Bauer Nielsen <jbn@dbc.dk>
2019-10-30 12:00:13 +0100

    Add double record check to service broker
    Auditors: atm, jbr

--- sink/dpf/src/main/java/dk/dbc/dataio/sink/dpf/ServiceBroker.java
+++ sink/dpf/src/main/java/dk/dbc/dataio/sink/dpf/ServiceBroker.java
@@ -9,6 +9,10 @@ import dk.dbc.dataio.sink.dpf.model.DpfRecord;
 import dk.dbc.jsonb.JSONBException;
 import dk.dbc.lobby.LobbyConnector;
 import dk.dbc.lobby.LobbyConnectorException;
+import dk.dbc.oss.ns.catalogingupdate.UpdateRecordResult;
+import dk.dbc.oss.ns.catalogingupdate.UpdateStatusEnum;
+import dk.dbc.updateservice.UpdateServiceDoubleRecordCheckConnector;
+import dk.dbc.updateservice.UpdateServiceDoubleRecordCheckConnectorException;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -16,8 +20,18 @@ import javax.inject.Inject;
 @Stateless
 public class ServiceBroker {
     @Inject LobbyConnector lobbyConnector;
+    @Inject UpdateServiceDoubleRecordCheckConnector doubleRecordCheckConnector;
+
+    private BibliographicRecordFactory bibliographicRecordFactory = new BibliographicRecordFactory();
 
     public void sendToLobby(DpfRecord dpfRecord) throws LobbyConnectorException, JSONBException {
         lobbyConnector.createOrReplaceApplicant(dpfRecord.toLobbyApplicant());
+    }
+
+    public boolean isDoubleRecord(DpfRecord dpfRecord)
+            throws BibliographicRecordFactoryException, UpdateServiceDoubleRecordCheckConnectorException {
+        final UpdateRecordResult updateRecordResult = doubleRecordCheckConnector.doubleRecordCheck(
+                bibliographicRecordFactory.toBibliographicRecord(dpfRecord.getBody()));
+        return updateRecordResult.getUpdateStatus() != UpdateStatusEnum.OK;
     }
 }

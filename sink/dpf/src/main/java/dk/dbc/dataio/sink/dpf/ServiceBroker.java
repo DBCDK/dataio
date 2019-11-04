b920897d3c090dd46b22a72dcdbb8e90815370ee
Jan Bauer Nielsen <jbn@dbc.dk>
2019-11-04 12:39:11 +0100

    Add update service connector to service broker
    Auditors: atm, jbr, hwhadbc

--- sink/dpf/src/main/java/dk/dbc/dataio/sink/dpf/ServiceBroker.java
+++ sink/dpf/src/main/java/dk/dbc/dataio/sink/dpf/ServiceBroker.java
@@ -5,12 +5,15 @@
 
 package dk.dbc.dataio.sink.dpf;
 
+import dk.dbc.dataio.commons.types.DpfSinkConfig;
 import dk.dbc.dataio.sink.dpf.model.DpfRecord;
+import dk.dbc.dataio.sink.openupdate.connector.OpenUpdateServiceConnector;
 import dk.dbc.dataio.sink.dpf.model.RawrepoRecord;
 import dk.dbc.jsonb.JSONBException;
 import dk.dbc.lobby.Applicant;
 import dk.dbc.lobby.LobbyConnector;
 import dk.dbc.lobby.LobbyConnectorException;
+import dk.dbc.oss.ns.catalogingupdate.BibliographicRecord;
 import dk.dbc.marc.binding.MarcRecord;
 import dk.dbc.marc.reader.MarcReaderException;
 import dk.dbc.opennumberroll.OpennumberRollConnector;
@@ -22,15 +25,26 @@ import dk.dbc.rawrepo.RecordServiceConnector;
 import dk.dbc.rawrepo.RecordServiceConnectorException;
 import dk.dbc.updateservice.UpdateServiceDoubleRecordCheckConnector;
 import dk.dbc.updateservice.UpdateServiceDoubleRecordCheckConnectorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import dk.dbc.weekresolver.WeekResolverResult;
 import dk.dbc.weekresolver.WeekresolverConnector;
 import dk.dbc.weekresolver.WeekresolverConnectorException;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 @Stateless
 public class ServiceBroker {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBean.class);
+
+    @Inject LobbyConnector lobbyConnector;
+    @Inject UpdateServiceDoubleRecordCheckConnector doubleRecordCheckConnector;
+    OpenUpdateServiceConnector openUpdateServiceConnector;
+
+    @EJB ConfigBean configBean;
+    DpfSinkConfig config;
     @Inject
     LobbyConnector lobbyConnector;
     @Inject
@@ -53,6 +67,31 @@ public class ServiceBroker {
         final UpdateRecordResult updateRecordResult = doubleRecordCheckConnector.doubleRecordCheck(
                 bibliographicRecordFactory.toBibliographicRecord(dpfRecord.getBody()));
         return updateRecordResult.getUpdateStatus() != UpdateStatusEnum.OK;
+    }
+
+    public UpdateRecordResult sendToUpdate(String groupId, String updateTemplate,
+                                           BibliographicRecord bibliographicRecord, String trackingId) {
+        return getOpenUpdateServiceConnector()
+                .updateRecord(groupId, updateTemplate, bibliographicRecord, trackingId);
+    }
+
+    private boolean isConfigUpdated() {
+        final DpfSinkConfig latestConfig = configBean.getConfig();
+        if (!latestConfig.equals(config)) {
+            config = latestConfig;
+            return true;
+        }
+        return false;
+    }
+
+    private OpenUpdateServiceConnector getOpenUpdateServiceConnector() {
+        if (isConfigUpdated()) {
+            LOGGER.debug("Updating update service connector");
+            openUpdateServiceConnector = new OpenUpdateServiceConnector("url",
+                config.getUpdateServiceUserId(),
+                config.getUpdateServicePassword());
+        }
+        return openUpdateServiceConnector;
     }
 
     public RawrepoRecord getRawrepoRecord(String bibliographicRecordId, int agencyId) throws RecordServiceConnectorException, MarcReaderException {

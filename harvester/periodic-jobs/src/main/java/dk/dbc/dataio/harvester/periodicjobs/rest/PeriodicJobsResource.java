8c226c0e5fb38715cce401b26f2ca56be1e1f620
Jesper Bruun <jbr@dbc.dk>
2019-11-25 12:53:06 +0100

    Add path spec
    Auditors: atm, jbn, hwha

--- harvester/periodic-jobs/src/main/java/dk/dbc/dataio/harvester/periodicjobs/rest/PeriodicJobsResource.java
+++ harvester/periodic-jobs/src/main/java/dk/dbc/dataio/harvester/periodicjobs/rest/PeriodicJobsResource.java
@@ -8,9 +8,11 @@ import java.util.Optional;
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Path("/jobs")
 public class PeriodicJobsResource {
     @EJB
     public HarvesterConfigurationBean harvesterConfigurationBean;

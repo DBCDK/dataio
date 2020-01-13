0b66b9efd024187a506a974803fd28d5627893ea
Jan Bauer Nielsen <jbn@dbc.dk>
2020-01-13 13:15:20 +0100

    Add status endpoint
    Auditors: atm, jbr, hwha

---
+++ sink/periodic-jobs/src/main/java/dk/dbc/dataio/sink/periodicjobs/rest/PeriodicJobsSinkApplication.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.sink.periodicjobs.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This class defines the other classes that make up this JAX-RS application by
+ * having the getClasses method return a specific set of resources.
+ */
+@ApplicationPath("/")
+public class PeriodicJobsSinkApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return new HashSet<>(Collections.singletonList(StatusBean.class));
+    }
+}

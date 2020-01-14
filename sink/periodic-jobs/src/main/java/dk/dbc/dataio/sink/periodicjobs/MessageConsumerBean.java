21bc74871ff9c2d86dded213e0252e567c6ae552
Jan Bauer Nielsen <jbn@dbc.dk>
2020-01-14 12:23:58 +0100

    Add MessageConsumerBean
    Auditors: atm, jbr, hwha

---
+++ sink/periodic-jobs/src/main/java/dk/dbc/dataio/sink/periodicjobs/MessageConsumerBean.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.sink.periodicjobs;
+
+import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ConsumedMessage;
+import dk.dbc.dataio.commons.types.exceptions.InvalidMessageException;
+import dk.dbc.dataio.sink.types.AbstractSinkMessageConsumerBean;
+import dk.dbc.dataio.sink.types.SinkException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.MessageDriven;
+
+@MessageDriven
+public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBean.class);
+
+    @Override
+    public void handleConsumedMessage(ConsumedMessage consumedMessage)
+            throws InvalidMessageException, NullPointerException, SinkException {
+        final Chunk chunk = unmarshallPayload(consumedMessage);
+        LOGGER.info("Received chunk {}/{}", chunk.getJobId(), chunk.getChunkId());
+
+        final Chunk result = handleChunk(chunk);
+        uploadChunk(result);
+    }
+
+    Chunk handleChunk(Chunk chunk) {
+        // TODO: 14/01/2020 implement handleChunk
+        return null;
+    }
+}

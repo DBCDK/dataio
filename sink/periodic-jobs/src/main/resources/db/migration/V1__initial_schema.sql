9a91dc3b28dc8b526ecb65eb972cb09707257a51
Jan Bauer Nielsen <jbn@dbc.dk>
2020-01-14 11:02:00 +0100

    Add database schema and persistence context
    Auditors: atm, jbr, hwha

---
+++ sink/periodic-jobs/src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE datablock (
+    id       SERIAL PRIMARY KEY,
+    jobId    INTEGER NOT NULL,
+    sortkey  TEXT NOT NULL,
+    bytes    BYTEA NOT NULL
+);
+CREATE INDEX datablock_jobid_index ON datablock(jobid);
+CREATE INDEX datablock_sortkey_index ON datablock(sortkey);
+
+CREATE TABLE delivery (
+    jobId   INTEGER PRIMARY KEY,
+    param   JSONB
+);

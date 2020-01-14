4b2a2f9adfe251e470a61de5f9a574b9db0d0576
Jan Bauer Nielsen <jbn@dbc.dk>
2020-01-14 11:02:56 +0100

    Migrate database using flyway
    Auditors: atm, jbr, hwha

---
+++ sink/periodic-jobs/src/main/java/dk/dbc/dataio/sink/periodicjobs/DatabaseMigrator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.sink.periodicjobs;
+
+import org.flywaydb.core.Flyway;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.sql.DataSource;
+
+@Startup
+@Singleton
+@TransactionManagement(TransactionManagementType.BEAN)
+public class DatabaseMigrator {
+    @Resource(lookup = "jdbc/dataio/sink/periodic-jobs")
+    DataSource dataSource;
+
+    public DatabaseMigrator() {}
+
+    public DatabaseMigrator(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @PostConstruct
+    public void migrate() {
+        final Flyway flyway = Flyway.configure()
+                .table("schema_version")
+                .baselineOnMigrate(true)
+                .dataSource(dataSource)
+                .load();
+        flyway.migrate();
+    }
+}

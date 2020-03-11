972dadca971088012ea4c7c312a067967d97ec6d
Jesper Bruun <jbr@dbc.dk>
2020-03-11 10:35:42 +0100

    Add GWT gui part
    Auditors: atm, jbn, hwha

--- gui/src/main/java/dk/dbc/dataio/gui/client/pages/harvester/periodicjobs/modify/Texts.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/pages/harvester/periodicjobs/modify/Texts.java
@@ -6,6 +6,7 @@
 package dk.dbc.dataio.gui.client.pages.harvester.periodicjobs.modify;
 
 import dk.dbc.dataio.gui.client.i18n.MainConstants;
+import dk.dbc.dataio.harvester.types.MailPickup;
 
 
 public interface Texts extends com.google.gwt.i18n.client.Constants {
@@ -55,6 +56,18 @@ public interface Texts extends com.google.gwt.i18n.client.Constants {
     String prompt_Subject();
 
     @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String prompt_FtpAddress();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String prompt_FtpUser();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String prompt_FtpPasword();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String prompt_FtpSubdir();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
     String prompt_Contact();
 
     @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
@@ -72,6 +85,9 @@ public interface Texts extends com.google.gwt.i18n.client.Constants {
 
     @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
     String selection_PickupTypeMail();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String selection_PickupTypeFtp();
 
     // Buttons
     @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
@@ -129,6 +145,18 @@ public interface Texts extends com.google.gwt.i18n.client.Constants {
 
     @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
     String help_Subject();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String help_FtpAddress();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String help_FtpUser();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String help_FtpPassword();
+
+    @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)
+    String help_FtpSubdir();
 
     // Error messages
     @DefaultStringValue(MainConstants.TRANSLATED_TEXT_IS_MISSING)

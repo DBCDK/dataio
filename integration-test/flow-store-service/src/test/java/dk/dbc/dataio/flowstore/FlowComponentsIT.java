af453844cd7321e49ad17445bc7171e53492e851
Jan Bauer Nielsen <jbn@dbc.dk>
2020-03-17 13:08:33 +0100

    Test Pre* hooks for flow components view
    Auditors: atm, jbr, hwha

--- integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/FlowComponentsIT.java
+++ integration-test/flow-store-service/src/test/java/dk/dbc/dataio/flowstore/FlowComponentsIT.java
@@ -21,6 +21,7 @@
 
 package dk.dbc.dataio.flowstore;
 
+import dk.dbc.commons.jdbc.util.JDBCUtil;
 import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnectorException;
 import dk.dbc.dataio.common.utils.flowstore.FlowStoreServiceConnectorUnexpectedStatusCodeException;
 import dk.dbc.dataio.commons.types.FlowComponent;
@@ -29,10 +30,15 @@ import dk.dbc.dataio.commons.types.FlowComponentView;
 import dk.dbc.dataio.commons.types.JavaScript;
 import dk.dbc.dataio.commons.types.rest.FlowStoreServiceConstants;
 import dk.dbc.dataio.commons.utils.test.model.FlowComponentContentBuilder;
+import dk.dbc.dataio.jsonb.JSONBContext;
+import dk.dbc.dataio.jsonb.JSONBException;
 import dk.dbc.httpclient.HttpClient;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -55,6 +61,7 @@ public class FlowComponentsIT extends AbstractFlowStoreServiceContainerTest {
      * Then : a flow component is created and returned
      * And  : assert that the flow component created contains the same information
      *        as the flowComponentContent given as input
+     * And  : the flow component view is updated
      */
     @Test
     public void createFlowComponent_ok() throws FlowStoreServiceConnectorException {
@@ -68,6 +75,13 @@ public class FlowComponentsIT extends AbstractFlowStoreServiceContainerTest {
 
         // And...
         assertThat(flowComponent.getContent(), is(flowComponentContent));
+
+        // And...
+        final FlowComponentView flowComponentView = getFlowComponentView(flowComponent.getId());
+        assertThat("flow component view version", flowComponentView.getVersion(),
+                is(1L));
+        assertThat("flow component view name", flowComponentView.getName(),
+                is(flowComponent.getContent().getName()));
     }
 
     /**
@@ -218,6 +232,7 @@ public class FlowComponentsIT extends AbstractFlowStoreServiceContainerTest {
      * Then : assert the correct fields have been set with the correct values
      * And  : assert that the id of the flow component has not changed
      * And  : assert that the version number has been updated
+     * And  : the flow component view is updated
      */
     @Test
     public void updateFlowComponent_ok() throws FlowStoreServiceConnectorException {
@@ -247,6 +262,13 @@ public class FlowComponentsIT extends AbstractFlowStoreServiceContainerTest {
 
         // And...
         assertThat(updatedFlowComponent.getVersion(), is(flowComponent.getVersion() + 1));
+
+        // And...
+        final FlowComponentView flowComponentView = getFlowComponentView(flowComponent.getId());
+        assertThat("flow component view version", flowComponentView.getVersion(),
+                is(flowComponent.getVersion() + 1));
+        assertThat("flow component view method", flowComponentView.getScriptName(),
+                is(updatedContent.getInvocationJavascriptName()));
     }
 
     /**
@@ -477,6 +499,17 @@ public class FlowComponentsIT extends AbstractFlowStoreServiceContainerTest {
         } catch(FlowStoreServiceConnectorUnexpectedStatusCodeException fssce) {
             // And...
             assertThat(Response.Status.fromStatusCode(fssce.getStatusCode()), is(NOT_FOUND));
+        }
+    }
+
+    private FlowComponentView getFlowComponentView(long flowComponentId) {
+        try (PreparedStatement stmt = JDBCUtil.query(flowStoreDbConnection,
+                "SELECT view FROM flow_components WHERE id=?", flowComponentId)) {
+            final ResultSet resultSet = stmt.getResultSet();
+            resultSet.next();
+            return new JSONBContext().unmarshall(resultSet.getString(1), FlowComponentView.class);
+        } catch (SQLException | JSONBException e) {
+            throw new IllegalStateException(e);
         }
     }
 }

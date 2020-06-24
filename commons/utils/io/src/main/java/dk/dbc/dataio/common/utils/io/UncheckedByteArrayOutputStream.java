d5620f7c49ca7c54f40a0fda24ce45dee94280f9
Jan Bauer Nielsen <jbn@dbc.dk>
2020-06-24 09:12:38 +0200

    Add UncheckedByteArrayOutputStream class
    Auditors: atm, jbr, hwha

---
+++ commons/utils/io/src/main/java/dk/dbc/dataio/common/utils/io/UncheckedByteArrayOutputStream.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.common.utils.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+/**
+ * This class wraps checked IOExceptions from ByteArrayOutputStream
+ * class methods as UncheckedIOException, so that it can be used in
+ * lambda expressions.
+ */
+public class UncheckedByteArrayOutputStream extends ByteArrayOutputStream {
+    @Override
+    public void write(byte[] b) {
+        try {
+            super.write(b);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+}

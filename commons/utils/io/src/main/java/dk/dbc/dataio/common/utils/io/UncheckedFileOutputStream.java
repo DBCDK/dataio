5481168cad50bf2bbaec0642ca6e7b15c7b06c5f
Jan Bauer Nielsen <jbn@dbc.dk>
2020-06-24 09:50:47 +0200

    Set group headers for ftp delivery
    Auditors: atm, jbr, hwha

---
+++ commons/utils/io/src/main/java/dk/dbc/dataio/common/utils/io/UncheckedFileOutputStream.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.common.utils.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+/**
+ * This class wraps checked IOExceptions from FileOutputStream
+ * class methods as UncheckedIOException, so that it can be used in
+ * lambda expressions.
+ */
+public class UncheckedFileOutputStream extends FileOutputStream {
+    public UncheckedFileOutputStream(File file) throws FileNotFoundException {
+        super(file);
+    }
+
+    @Override
+    public void write(byte[] b) {
+        try {
+            super.write(b);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+}

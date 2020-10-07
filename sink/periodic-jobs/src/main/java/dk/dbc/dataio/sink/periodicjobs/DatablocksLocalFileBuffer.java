afd2ff5b6521030a3e025598197a1390bdc6ec98
Jesper Bruun <jbr@dbc.dk>
2020-10-07 15:47:29 +0200

    Add contentHeader and footer for alle types of periodicjob sinks
    Todo: Proper test for http type of delivery
    
    Auditors: atm, jbn, hwha, mib

---
+++ sink/periodic-jobs/src/main/java/dk/dbc/dataio/sink/periodicjobs/DatablocksLocalFileBuffer.java
@@ -0,0 +1,74 @@
+package dk.dbc.dataio.sink.periodicjobs;
+
+import dk.dbc.commons.jpa.ResultSet;
+import dk.dbc.dataio.common.utils.io.UncheckedFileOutputStream;
+import dk.dbc.dataio.commons.macroexpansion.MacroSubstitutor;
+import dk.dbc.dataio.sink.types.SinkException;
+import java.io.File;
+import java.io.IOException;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+public class DatablocksLocalFileBuffer {
+    private EntityManager entityManager;
+    private PeriodicJobsDelivery delivery;
+    private File tmpFile;
+    private MacroSubstitutor macroSubstitutor;
+
+    public DatablocksLocalFileBuffer withEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+        return this;
+    }
+
+    public DatablocksLocalFileBuffer withDelivery(PeriodicJobsDelivery delivery) {
+        this.delivery = delivery;
+        return this;
+    }
+
+    public DatablocksLocalFileBuffer withTmpFile(File tmpFile) {
+        this.tmpFile = tmpFile;
+        return this;
+    }
+
+    public DatablocksLocalFileBuffer withMacroSubstitutor(MacroSubstitutor macroSubstitutor) {
+        this.macroSubstitutor = macroSubstitutor;
+        return this;
+    }
+
+    public void createLocalFile() throws SinkException {
+        String contentHeader = delivery.getConfig().getContent().getPickup().getContentHeader();
+        String contentFooter = delivery.getConfig().getContent().getPickup().getContentFooter();
+
+        if (contentHeader != null) {
+            contentHeader = macroSubstitutor.replace(contentHeader);
+        } else {
+            contentHeader = "";
+        }
+
+        if (contentFooter != null) {
+            contentFooter = macroSubstitutor.replace(contentFooter);
+        } else {
+            contentFooter = "";
+        }
+
+        final GroupHeaderIncludePredicate groupHeaderIncludePredicate = new GroupHeaderIncludePredicate();
+        final Query getDataBlocksQuery = entityManager
+                .createNamedQuery(PeriodicJobsDataBlock.GET_DATA_BLOCKS_QUERY_NAME)
+                .setParameter(1, delivery.getJobId());
+        try (final UncheckedFileOutputStream datablocksOutputStream = new UncheckedFileOutputStream(tmpFile);
+             final ResultSet<PeriodicJobsDataBlock> datablocks = new ResultSet<>(entityManager, getDataBlocksQuery,
+                     new PeriodicJobsDataBlockResultSetMapping())) {
+            datablocksOutputStream.write(contentHeader.getBytes());
+            for (PeriodicJobsDataBlock datablock : datablocks) {
+                if (groupHeaderIncludePredicate.test(datablock)) {
+                    datablocksOutputStream.write(datablock.getGroupHeader());
+                }
+                datablocksOutputStream.write(datablock.getBytes());
+            }
+            datablocksOutputStream.write(contentFooter.getBytes());
+            datablocksOutputStream.flush();
+        } catch (IOException e) {
+            throw new SinkException(e);
+        }
+    }
+}

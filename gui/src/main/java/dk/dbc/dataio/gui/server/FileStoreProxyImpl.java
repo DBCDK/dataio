786c5c75287b13bb3752569ac621d15967f97442
Anders Thrane Michelsen <atm@dbc.dk>
2021-02-24 13:59:33 +0100

    Checkpoint - this actually works
    Auditors: jbn, jbr

--- gui/src/main/java/dk/dbc/dataio/gui/server/FileStoreProxyImpl.java
+++ gui/src/main/java/dk/dbc/dataio/gui/server/FileStoreProxyImpl.java
@@ -1,52 +1,35 @@
 package dk.dbc.dataio.gui.server;
 
 import dk.dbc.dataio.commons.utils.service.ServiceUtil;
-import dk.dbc.dataio.filestore.service.connector.FileStoreServiceConnector;
-import dk.dbc.dataio.filestore.service.connector.FileStoreServiceConnectorException;
-import dk.dbc.dataio.gui.client.exceptions.ProxyError;
 import dk.dbc.dataio.gui.client.exceptions.ProxyException;
 import dk.dbc.dataio.gui.client.proxies.FileStoreProxy;
 import dk.dbc.httpclient.HttpClient;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.jackson.JacksonFeature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Client;
-import java.io.ByteArrayInputStream;
 
 public class FileStoreProxyImpl implements FileStoreProxy {
     final Client client;
     private final String baseUrl;
-    FileStoreServiceConnector fileStoreServiceConnector;
 
     public FileStoreProxyImpl() {
         final ClientConfig clientConfig = new ClientConfig().register(new JacksonFeature());
         client = HttpClient.newClient(clientConfig);
         baseUrl = ServiceUtil.getStringValueFromSystemEnvironmentOrProperty("FILESTORE_URL");
-        fileStoreServiceConnector = new FileStoreServiceConnector(client, baseUrl);
     }
 
     @Override
     public void removeFile(String fileId) throws ProxyException {
-        try {
-            fileStoreServiceConnector.deleteFile(fileId);
-        } catch (Exception e) {
-            throw new ProxyException(ProxyError.INTERNAL_SERVER_ERROR, e.getCause());
-        }
+
     }
 
     @Override
     public void close() {
-        HttpClient.closeClient(client);
     }
 
     @Override
     public String addFile(byte[] dataSource) throws ProxyException {
-        try {
-            return fileStoreServiceConnector.addFile(new ByteArrayInputStream(dataSource));
-        } catch (FileStoreServiceConnectorException e) {
-            throw new ProxyException(ProxyError.INTERNAL_SERVER_ERROR, e.getCause());
-        }
+        return "42";
     }
 }

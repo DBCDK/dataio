3acdf1dcc530142e7e7f6ec42706ffbfd5c25848
Anders Thrane Michelsen <atm@dbc.dk>
2021-02-26 07:48:22 +0100

    Add FileStore proxy injection
    Auditors: jbn, jbr

--- gui/src/main/java/dk/dbc/dataio/gui/client/util/CommonGinjector.java
+++ gui/src/main/java/dk/dbc/dataio/gui/client/util/CommonGinjector.java
@@ -25,6 +25,7 @@ import com.google.gwt.inject.client.Ginjector;
 import dk.dbc.dataio.gui.client.exceptions.texts.ProxyErrorTexts;
 import dk.dbc.dataio.gui.client.pages.navigation.Texts;
 import dk.dbc.dataio.gui.client.proxies.ConfigProxyAsync;
+import dk.dbc.dataio.gui.client.proxies.FileStoreProxyAsync;
 import dk.dbc.dataio.gui.client.proxies.FlowStoreProxyAsync;
 import dk.dbc.dataio.gui.client.proxies.FtpProxyAsync;
 import dk.dbc.dataio.gui.client.proxies.JavaScriptProjectFetcherAsync;
@@ -40,6 +41,7 @@ import dk.dbc.dataio.gui.client.resources.Resources;
 public interface CommonGinjector extends Ginjector {
     Texts getMenuTexts();
     ProxyErrorTexts getProxyErrorTexts();
+    FileStoreProxyAsync getFileStoreProxyAsync();
     FlowStoreProxyAsync getFlowStoreProxyAsync();
     PeriodicJobsHarvesterProxyAsync getPeriodicJobsHarvesterProxy();
     JobStoreProxyAsync getJobStoreProxyAsync();

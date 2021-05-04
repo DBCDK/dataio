ec5d06fb5288b1252481d22329dcb453c8f2b4d4
Jan Bauer Nielsen <jbn@dbc.dk>
2021-05-04 07:59:49 +0200

    Add holdings items sink skeleton
    Auditors: atm, hwha, jbr, mib

---
+++ sink/holdings-items/src/main/java/dk/dbc/dataio/sink/holdingsitems/MessageConsumerBean.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.sink.holdingsitems;
+
+import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ConsumedMessage;
+import dk.dbc.dataio.commons.types.exceptions.InvalidMessageException;
+import dk.dbc.dataio.sink.types.AbstractSinkMessageConsumerBean;
+import dk.dbc.dataio.sink.types.SinkException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.MessageDriven;
+
+@MessageDriven
+public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBean.class);
+
+    @Override
+    public void handleConsumedMessage(ConsumedMessage consumedMessage) throws InvalidMessageException, SinkException {
+        final Chunk chunk = unmarshallPayload(consumedMessage);
+        LOGGER.info("Received chunk {}/{}", chunk.getJobId(), chunk.getChunkId());
+
+        // TODO: 04/05/2021 delivery 
+    }
+}

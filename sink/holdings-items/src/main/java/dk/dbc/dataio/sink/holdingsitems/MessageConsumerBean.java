b8c4b9203720db8cca6812d54a402538a8cbc425
Jan Bauer Nielsen <jbn@dbc.dk>
2021-05-05 13:48:48 +0200

    Implement MessageConsumerBean
    Auditors: atm, hwha, jbr, mib

--- sink/holdings-items/src/main/java/dk/dbc/dataio/sink/holdingsitems/MessageConsumerBean.java
+++ sink/holdings-items/src/main/java/dk/dbc/dataio/sink/holdingsitems/MessageConsumerBean.java
@@ -5,25 +5,131 @@
 
 package dk.dbc.dataio.sink.holdingsitems;
 
+import com.fasterxml.jackson.databind.type.CollectionType;
+import dk.dbc.commons.addi.AddiReader;
+import dk.dbc.commons.addi.AddiRecord;
+import dk.dbc.commons.jsonb.JSONBContext;
+import dk.dbc.commons.jsonb.JSONBException;
 import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ChunkItem;
 import dk.dbc.dataio.commons.types.ConsumedMessage;
+import dk.dbc.dataio.commons.types.Diagnostic;
 import dk.dbc.dataio.commons.types.exceptions.InvalidMessageException;
+import dk.dbc.dataio.commons.utils.lang.StringUtil;
 import dk.dbc.dataio.sink.types.AbstractSinkMessageConsumerBean;
 import dk.dbc.dataio.sink.types.SinkException;
+import dk.dbc.log.DBCTrackedLogContext;
+import dk.dbc.solrdocstore.connector.SolrDocStoreConnector;
+import dk.dbc.solrdocstore.connector.SolrDocStoreConnectorException;
+import dk.dbc.solrdocstore.connector.SolrDocStoreConnectorUnexpectedStatusCodeException;
+import dk.dbc.solrdocstore.connector.model.HoldingsItems;
+import dk.dbc.solrdocstore.connector.model.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @MessageDriven
 public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBean.class);
+
+    private final JSONBContext jsonbContext = new JSONBContext();
+    private final CollectionType holdingsItemsListType = jsonbContext.getTypeFactory()
+            .constructCollectionType(List.class, HoldingsItems.class);
+
+    @Inject SolrDocStoreConnector solrDocStoreConnector;
 
     @Override
     public void handleConsumedMessage(ConsumedMessage consumedMessage) throws InvalidMessageException, SinkException {
         final Chunk chunk = unmarshallPayload(consumedMessage);
         LOGGER.info("Received chunk {}/{}", chunk.getJobId(), chunk.getChunkId());
 
-        // TODO: 04/05/2021 delivery 
+        uploadChunk(handleChunk(chunk));
+    }
+
+    Chunk handleChunk(Chunk chunk) throws SinkException {
+        final Chunk result = new Chunk(chunk.getJobId(), chunk.getChunkId(), Chunk.Type.DELIVERED);
+        try {
+            for (ChunkItem chunkItem : chunk.getItems()) {
+                DBCTrackedLogContext.setTrackingId(chunkItem.getTrackingId());
+                result.insertItem(handleChunkItem(chunkItem));
+            }
+        } finally {
+            DBCTrackedLogContext.remove();
+        }
+        return result;
+    }
+
+    private ChunkItem handleChunkItem(ChunkItem chunkItem) throws SinkException {
+        final ChunkItem result = new ChunkItem()
+                .withId(chunkItem.getId())
+                .withTrackingId(chunkItem.getTrackingId())
+                .withType(ChunkItem.Type.STRING)
+                .withEncoding(StandardCharsets.UTF_8);
+        try {
+            switch (chunkItem.getStatus()) {
+                case FAILURE:
+                    return result
+                            .withStatus(ChunkItem.Status.IGNORE)
+                            .withData("Failed by processor");
+                case IGNORE:
+                    return result
+                            .withStatus(ChunkItem.Status.IGNORE)
+                            .withData("Ignored by processor");
+                default:
+                    return result
+                            .withStatus(ChunkItem.Status.SUCCESS)
+                            .withData(deliverHoldingsItems(chunkItem));
+            }
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            return result
+                    .withStatus(ChunkItem.Status.FAILURE)
+                    .withDiagnostics(new Diagnostic(Diagnostic.Level.FATAL, e.getMessage(), e))
+                    .withData(e.getMessage());
+        } catch (SolrDocStoreConnectorException e) {
+            throw new SinkException(e);
+        }
+    }
+
+    private String deliverHoldingsItems(ChunkItem chunkItem) throws SolrDocStoreConnectorException {
+        final StringBuilder resultStatus = new StringBuilder();
+        final AddiReader addiReader = new AddiReader(new ByteArrayInputStream(chunkItem.getData()));
+        boolean hasError = false;
+        try {
+            while (addiReader.hasNext()) {
+                final AddiRecord addiRecord = addiReader.next();
+
+                final List<HoldingsItems> holdingsItemsList = jsonbContext.unmarshall(
+                        StringUtil.asString(addiRecord.getContentData()), holdingsItemsListType);
+                for (HoldingsItems holdingsItems : holdingsItemsList) {
+                    try {
+                        final Status status = solrDocStoreConnector.setHoldings(holdingsItems);
+                        resultStatus.append(String.format("%s:%d consumer service response - %s\n",
+                                holdingsItems.getBibliographicRecordId(), holdingsItems.getAgencyId(), status.getText()));
+                    } catch (SolrDocStoreConnectorUnexpectedStatusCodeException e) {
+                        String error = "status code " + e.getStatusCode();
+                        if (e.getStatus() != null) {
+                            error = e.getStatus().getText();
+                        }
+                        resultStatus.append(String.format("%s:%d consumer service response - %s\n",
+                                holdingsItems.getBibliographicRecordId(), holdingsItems.getAgencyId(), error));
+
+                        hasError = true;
+                    }
+                }
+            }
+        } catch (IOException | JSONBException e) {
+            throw new IllegalArgumentException("Invalid chunk item", e);
+        }
+
+        if (hasError) {
+            throw new IllegalStateException(resultStatus.toString());
+        }
+        return resultStatus.toString();
     }
 }
---
+++ sink/holdings-items/src/test/java/dk/dbc/dataio/sink/holdingsitems/MessageConsumerBeanTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.dataio.sink.holdingsitems;
+
+import dk.dbc.commons.addi.AddiRecord;
+import dk.dbc.commons.jsonb.JSONBContext;
+import dk.dbc.commons.jsonb.JSONBException;
+import dk.dbc.dataio.commons.types.Chunk;
+import dk.dbc.dataio.commons.types.ChunkItem;
+import dk.dbc.dataio.commons.utils.lang.StringUtil;
+import dk.dbc.dataio.commons.utils.test.model.ChunkBuilder;
+import dk.dbc.dataio.commons.utils.test.model.ChunkItemBuilder;
+import dk.dbc.dataio.sink.types.SinkException;
+import dk.dbc.solrdocstore.connector.SolrDocStoreConnector;
+import dk.dbc.solrdocstore.connector.SolrDocStoreConnectorException;
+import dk.dbc.solrdocstore.connector.SolrDocStoreConnectorUnexpectedStatusCodeException;
+import dk.dbc.solrdocstore.connector.model.HoldingsItems;
+import dk.dbc.solrdocstore.connector.model.IndexKeys;
+import dk.dbc.solrdocstore.connector.model.Status;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class MessageConsumerBeanTest {
+    private final SolrDocStoreConnector solrDocStoreConnector = mock(SolrDocStoreConnector.class);
+    private final MessageConsumerBean messageConsumerBean = newMessageConsumerBean();
+    private final JSONBContext jsonbContext = new JSONBContext();
+
+    @Test
+    void handleChunk() throws SinkException, JSONBException, SolrDocStoreConnectorException {
+        final IndexKeys indexKeys = new IndexKeys();
+        indexKeys.put("k1", Collections.singletonList("v1"));
+        final List<IndexKeys> indexKeysList = new ArrayList<>();
+        indexKeysList.add(indexKeys);
+
+        final HoldingsItems holdingsItemsOK1 = new HoldingsItems();
+        holdingsItemsOK1.setAgencyId(123456);
+        holdingsItemsOK1.setBibliographicRecordId("id1_1");
+        holdingsItemsOK1.setIndexKeys(indexKeysList);
+        final HoldingsItems holdingsItemsFail = new HoldingsItems();
+        holdingsItemsFail.setAgencyId(123456);
+        holdingsItemsFail.setBibliographicRecordId("id1_2");
+        holdingsItemsFail.setIndexKeys(indexKeysList);
+        final HoldingsItems holdingsItemsOK2 = new HoldingsItems();
+        holdingsItemsOK2.setAgencyId(123456);
+        holdingsItemsOK2.setBibliographicRecordId("id1_3");
+        holdingsItemsOK2.setIndexKeys(indexKeysList);
+
+        final Status statusOk = new Status();
+        statusOk.setText("ok");
+        when(solrDocStoreConnector.setHoldings(eq(holdingsItemsOK1))).thenReturn(statusOk);
+        when(solrDocStoreConnector.setHoldings(eq(holdingsItemsOK2))).thenReturn(statusOk);
+        final Status statusFail = new Status();
+        statusFail.setText("error");
+        final SolrDocStoreConnectorUnexpectedStatusCodeException internalServerError =
+                new SolrDocStoreConnectorUnexpectedStatusCodeException("internal server error", 500);
+        internalServerError.setStatus(statusFail);
+        when(solrDocStoreConnector.setHoldings(eq(holdingsItemsFail))).thenThrow(internalServerError);
+
+        final AddiRecord addiRecordInvalid = new AddiRecord(new byte[0],
+                StringUtil.asBytes("not json"));
+        final AddiRecord addiRecordFail = new AddiRecord(new byte[0],
+                StringUtil.asBytes(jsonbContext.marshall(Arrays.asList(holdingsItemsOK1, holdingsItemsFail, holdingsItemsOK2))));
+        final AddiRecord addiRecordOk = new AddiRecord(new byte[0],
+                StringUtil.asBytes(jsonbContext.marshall(Collections.singletonList(holdingsItemsOK1))));
+
+        final List<ChunkItem> chunkItems = Arrays.asList(
+                new ChunkItemBuilder().setId(0L).setStatus(ChunkItem.Status.FAILURE)
+                        .build(),
+                new ChunkItemBuilder().setId(1L).setStatus(ChunkItem.Status.SUCCESS)
+                        .setData(addiRecordInvalid.getBytes())
+                        .build(),
+                new ChunkItemBuilder().setId(2L).setStatus(ChunkItem.Status.IGNORE)
+                        .build(),
+                new ChunkItemBuilder().setId(3L).setStatus(ChunkItem.Status.SUCCESS)
+                        .setData(addiRecordOk.getBytes())
+                        .build(),
+                new ChunkItemBuilder().setId(4L).setStatus(ChunkItem.Status.SUCCESS)
+                        .setData(addiRecordFail.getBytes())
+                        .build());
+
+        final int jobId = 42;
+        final Chunk chunk = new ChunkBuilder(Chunk.Type.PROCESSED)
+                .setJobId(jobId)
+                .setChunkId(0L)
+                .setItems(chunkItems)
+                .build();
+
+        final Chunk result = messageConsumerBean.handleChunk(chunk);
+
+        assertThat("number of chunk items", result.size(), is(5));
+        assertThat("1st chunk item",
+                result.getItems().get(0).getStatus(), is(ChunkItem.Status.IGNORE));
+        assertThat("2nd chunk item",
+                result.getItems().get(1).getStatus(), is(ChunkItem.Status.FAILURE));
+        assertThat("3rd chunk item",
+                result.getItems().get(2).getStatus(), is(ChunkItem.Status.IGNORE));
+        assertThat("4th chunk item",
+                result.getItems().get(3).getStatus(), is(ChunkItem.Status.SUCCESS));
+        assertThat("5th chunk item",
+                result.getItems().get(4).getStatus(), is(ChunkItem.Status.FAILURE));
+
+        assertThat("4th chunk item data", StringUtil.asString(result.getItems().get(3).getData()),
+                is("id1_1:123456 consumer service response - ok\n"));
+        assertThat("5th chunk item data", StringUtil.asString(result.getItems().get(4).getData()),
+                is("id1_1:123456 consumer service response - ok\nid1_2:123456 consumer service response - error\nid1_3:123456 consumer service response - ok\n"));
+    }
+
+    private MessageConsumerBean newMessageConsumerBean() {
+        final MessageConsumerBean messageConsumerBean = new MessageConsumerBean();
+        messageConsumerBean.solrDocStoreConnector = solrDocStoreConnector;
+        return messageConsumerBean;
+    }
+}

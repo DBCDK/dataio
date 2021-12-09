8b25d1dba0569b75c7bad920f27ce6e9626ae59f
Henrik Witt-Hansen <hwha@dbc.dk>
2021-12-09 13:37:44 +0100

    Implement delivery to dmat-service using the dmat connector
    Auditors: atm, jbr, mib, akri

--- sink/dmat/src/main/java/dk/dbc/dataio/sink/dmat/MessageConsumerBean.java
+++ sink/dmat/src/main/java/dk/dbc/dataio/sink/dmat/MessageConsumerBean.java
@@ -1,12 +1,21 @@
 package dk.dbc.dataio.sink.dmat;
 
+import dk.dbc.commons.addi.AddiReader;
+import dk.dbc.commons.addi.AddiRecord;
+import dk.dbc.commons.jsonb.JSONBContext;
+import dk.dbc.commons.jsonb.JSONBException;
 import dk.dbc.dataio.commons.types.Diagnostic;
 import dk.dbc.dataio.commons.types.exceptions.ServiceException;
 import dk.dbc.dataio.commons.utils.jobstore.JobStoreServiceConnectorException;
 import dk.dbc.dataio.commons.utils.jobstore.JobStoreServiceConnectorUnexpectedStatusCodeException;
 import dk.dbc.dataio.commons.utils.jobstore.ejb.JobStoreServiceConnectorBean;
+import dk.dbc.dataio.commons.utils.lang.StringUtil;
 import dk.dbc.dataio.jobstore.types.JobError;
 
+import dk.dbc.dmat.service.connector.DMatServiceConnector;
+import dk.dbc.dmat.service.connector.DMatServiceConnectorException;
+import dk.dbc.dmat.service.dto.RecordData;
+import dk.dbc.dmat.service.persistence.DMatRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,14 +29,25 @@ import dk.dbc.log.DBCTrackedLogContext;
 import dk.dbc.util.Timed;
 import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @MessageDriven
 public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBean.class);
 
+    private final JSONBContext jsonbContext = new JSONBContext();
+
     @EJB
     JobStoreServiceConnectorBean jobStoreServiceConnectorBean;
+
+    @Inject
+    DMatServiceConnector connector;
 
     @Timed
     @Override
@@ -35,9 +55,19 @@ public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
         final Chunk chunk = unmarshallPayload(consumedMessage);
         LOGGER.info("Received chunk {}/{}", chunk.getJobId(), chunk.getChunkId());
 
+        // Give up if we have no dmat connector
+        if (connector == null) {
+            LOGGER.error("Connector of DMat sink is uninitialized!");
+            throw new SinkException("Connector is uninitialized");
+        }
+
+        // Process all chunks
         final Chunk deliveredChunk = handleChunk(chunk);
+
+        // Store delivered chunks
         try {
-            jobStoreServiceConnectorBean.getConnector().addChunkIgnoreDuplicates(deliveredChunk, deliveredChunk.getJobId(), deliveredChunk.getChunkId());
+            jobStoreServiceConnectorBean.getConnector()
+                    .addChunkIgnoreDuplicates(deliveredChunk, deliveredChunk.getJobId(), deliveredChunk.getChunkId());
         } catch (JobStoreServiceConnectorException e) {
             String message = String.format("Error in communication with job-store for chunk %d/%d",
                     deliveredChunk.getJobId(), deliveredChunk.getChunkId());
@@ -86,16 +116,74 @@ public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
                             .withStatus(ChunkItem.Status.IGNORE)
                             .withData("Ignored by processor");
                 default:
-                    return result
-                            .withStatus(ChunkItem.Status.SUCCESS)
-                            .withData("{}");  // Todo: Handle incoming chunk data
+                    try {
+                        return result
+                                .withStatus(ChunkItem.Status.SUCCESS)
+                                .withData(sendToDMat(getDMatDataRecords(chunkItem), id));
+                    } catch (DMatServiceConnectorException e) {
+                        LOGGER.info("DMat connector threw an DMatServiceConnectorException for chunk id {}: {}", id, e.getMessage());
+                        return result
+                                .withStatus(ChunkItem.Status.FAILURE)
+                                .withData(e.getMessage());
+                    } catch (DMatSinkException e) {
+                        LOGGER.info("Processing of chunk id {} failed with reason: {}", id, e.getMessage());
+                        return result
+                                .withStatus(ChunkItem.Status.FAILURE)
+                                .withData(e.getMessage());
+                    } catch (Exception e) {
+                        LOGGER.error("An unexpected exception was thrown for chunk id {}: {}", id, e.getMessage());
+                        return result
+                                .withStatus(ChunkItem.Status.FAILURE)
+                                .withData(e.getMessage());
+                    }
             }
-        } catch (Exception e) { // Todo: If possible, replace with more specific exceptions
+        } catch (Exception e) {
             return result
                     .withStatus(ChunkItem.Status.FAILURE)
                     .withDiagnostics(
                             new Diagnostic(Diagnostic.Level.FATAL, e.getMessage(), e))
                     .withData(e.getMessage());
         }
+    }
+
+    private List<RecordData> getDMatDataRecords(ChunkItem chunkItem)
+            throws IOException, JSONBException {
+        final List<RecordData> dataRecords = new ArrayList<>();
+
+        final AddiReader addiReader = new AddiReader(new ByteArrayInputStream(chunkItem.getData()));
+        int cnt = 1;
+        while (addiReader.hasNext()) {
+            final AddiRecord addiRecord = addiReader.next();
+            final RecordData recordData = jsonbContext.unmarshall(
+                            StringUtil.asString(addiRecord.getContentData()), RecordData.class);
+            dataRecords.add(recordData);
+        }
+
+        return dataRecords;
+    }
+
+    private String sendToDMat(List<RecordData> dataRecords, String id) throws Exception {
+        List<String> upsertedRecords = new ArrayList<>();
+
+        for (RecordData recordData : dataRecords) {
+
+            // Check for obvious errors that would always make the record fail
+            if (recordData.getDatestamp() == null || recordData.getDatestamp().isEmpty()) {
+                throw new DMatSinkException("Null or empty datestamp field. Record will fail");
+            }
+            if (recordData.getRecordReference() == null || recordData.getRecordReference().isEmpty()) {
+                throw new DMatSinkException("Null or empty record reference field. Record will fail");
+            }
+
+            // Post new/updated record to DMat
+            DMatRecord dMatRecord = connector.upsertRecord(recordData);
+
+            // Result. Status chunk/item id, record reference of processsed record and
+            // the records seqno. (id) and current status (after processing)
+            upsertedRecords.add(String.format("%s: %s@%s => seqno %d status %s", id,
+                            recordData.getRecordReference(), recordData.getDatestamp(),
+                            dMatRecord.getId(), dMatRecord.getStatus()));
+        }
+        return upsertedRecords.stream().collect(Collectors.joining("\n"));
     }
 }
--- sink/dmat/src/test/java/dk/dbc/dataio/sink/dmat/MessageConsumerBeanTest.java
+++ sink/dmat/src/test/java/dk/dbc/dataio/sink/dmat/MessageConsumerBeanTest.java
@@ -1,28 +1,62 @@
 package dk.dbc.dataio.sink.dmat;
 
+import dk.dbc.commons.jsonb.JSONBException;
 import dk.dbc.dataio.commons.types.Chunk;
 import dk.dbc.dataio.commons.types.ChunkItem;
 import dk.dbc.dataio.commons.utils.test.model.ChunkBuilder;
+import dk.dbc.dmat.service.connector.DMatServiceConnector;
+import dk.dbc.dmat.service.connector.DMatServiceConnectorException;
+import dk.dbc.dmat.service.dto.RecordData;
+import dk.dbc.dmat.service.dto.Status;
+import dk.dbc.dmat.service.persistence.DMatRecord;
 import org.hamcrest.MatcherAssert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MessageConsumerBeanTest {
-    private final MessageConsumerBean messageConsumerBean = newMessageConsumerBean();
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBeanTest.class);
+    private MessageConsumerBean messageConsumerBean = new MessageConsumerBean();
+
+    @Before
+    public void setupMocks() throws DMatServiceConnectorException, JSONBException {
+        messageConsumerBean.connector = mock(DMatServiceConnector.class);
+        when(messageConsumerBean.connector.upsertRecord(any(RecordData.class))).thenReturn(
+                        new DMatRecord().withId(1).withStatus(Status.NEW));
+    }
+
+    private byte[] readLocalFile(String name) throws IOException {
+        Path path = Paths.get(MessageConsumerBeanTest.class.getResource("/__files/" + name).getPath());
+        return Files.readAllBytes(path);
+    }
 
     @Test
-    public void testHandleChunk() {
+    public void testHandleChunk() throws IOException {
+
+        byte[] validRecordAddi = readLocalFile("valid_recorddata_addi.json");
+
         final List<ChunkItem> chunkItems = Arrays.asList(
                 ChunkItem.failedChunkItem().withId(0L),     // failed by job processor
                 ChunkItem.ignoredChunkItem().withId(1L),    // ignored by job processor
-                ChunkItem.successfulChunkItem().withId(2L)  // invalid processing instructions
-                        .withData("invalid-chunk"), // Todo: We may have to be more specifically invalid
+                ChunkItem.successfulChunkItem().withId(2L)  // invalid input record (not json)
+                        .withData("invalid-chunk"),
                 ChunkItem.successfulChunkItem().withId(3L)  // successfully delivered
-                        .withData("{}") // Todo: Add minimalistic dmat record data json
+                        .withData(validRecordAddi)
         );
 
         final Chunk chunk = new ChunkBuilder(Chunk.Type.PROCESSED)
@@ -38,13 +72,35 @@ public class MessageConsumerBeanTest {
         MatcherAssert.assertThat("2nd chunk item", result.getItems().get(1).getStatus(),
                 is(ChunkItem.Status.IGNORE));
         MatcherAssert.assertThat("3rd chunk item", result.getItems().get(2).getStatus(),
-                is(ChunkItem.Status.SUCCESS));  // Todo: Should be FAILURE when handling has been implemented
+                is(ChunkItem.Status.FAILURE));
         MatcherAssert.assertThat("4th chunk item", result.getItems().get(3).getStatus(),
                 is(ChunkItem.Status.SUCCESS));
     }
 
-    private MessageConsumerBean newMessageConsumerBean() {
-        final MessageConsumerBean messageConsumerBean = new MessageConsumerBean();
-        return messageConsumerBean;
+    @Test
+    public void testHandleChunkWithError() throws IOException {
+
+        byte[] invalidRecordAddiNoDatestamp = readLocalFile("invalid_recorddata_addi_no_datestamp.json");
+        byte[] invalidRecordAddiNoRecordReference = readLocalFile("invalid_recorddata_addi_no_recordreference.json");
+
+        final List<ChunkItem> chunkItems = Arrays.asList(
+                ChunkItem.successfulChunkItem().withId(0L)  // failed du to invalid record (missing datestamp)
+                        .withData(invalidRecordAddiNoDatestamp),
+                ChunkItem.successfulChunkItem().withId(1L)  // failed du to invalid record (missing record reference)
+                        .withData(invalidRecordAddiNoRecordReference)
+        );
+
+        final Chunk chunk = new ChunkBuilder(Chunk.Type.PROCESSED)
+                .setChunkId(0L)
+                .setItems(chunkItems)
+                .build();
+
+        final Chunk result = messageConsumerBean.handleChunk(chunk);
+
+        MatcherAssert.assertThat("number of chunk items", result.size(), is(2));
+        MatcherAssert.assertThat("1st chunk item", result.getItems().get(0).getStatus(),
+                is(ChunkItem.Status.FAILURE));
+        MatcherAssert.assertThat("2nd chunk item", result.getItems().get(1).getStatus(),
+                is(ChunkItem.Status.FAILURE));
     }
 }

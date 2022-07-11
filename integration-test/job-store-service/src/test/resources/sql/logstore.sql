ea9a79fcf5a3740586fe787223736e79defe6a80
jbr <jbr@dbc.dk>
2022-07-11 15:22:28 +0200

    Test clean up of super transient jobs. (And do a REAL cleanup of postgres testing equipment: use DBCPostgreSQLContainer for containertests).

--- integration-test/job-store-service/src/test/resources/sql/logstore.sql
+++ integration-test/job-store-service/src/test/resources/sql/logstore.sql
@@ -1,5 +1,12 @@
 --
--- Job 1
+--
+--
+-- First part here are the jobs used for testing compact feature for old jobs.
+-- 
+--
+--
+--
+-- First job: Old
 --
 INSERT INTO logentry VALUES (5581923, '__DATE_1__:19.991', '__DATE_1__:19.106 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41434-0-0]
 __DATE_1__:19.11 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
@@ -90,3 +97,102 @@ __DATE_2__:20.293 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101785|150005 WA
 __DATE_2__:20.294 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
 __DATE_2__:20.294 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
 ', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-2, flowName=Litteratursiden2Brønd, logStoreTrackingId=41435-0-5}', '41435', 0, 5);
+--
+--
+--
+-- Second part here are the jobs used for testing SUPER_TRANSIENT feature for old jobs.
+-- 
+--
+--
+--
+-- First job: Two days old
+--
+INSERT INTO logentry VALUES (5581935, '__DATE_3__:19.991', '__DATE_3__:19.106 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41436-0-0]
+__DATE_3__:19.11 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_3__:19.125 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101787|150005 WAS INITIATED
+__DATE_3__:19.987 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101787|150005 WAS CONVERTED
+__DATE_3__:19.99 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_3__:19.991 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-1, flowName=Litteratursiden2Brønd, logStoreTrackingId=41436-0-0}', '41436', 0, 0);
+INSERT INTO logentry VALUES (5581936, '__DATE_3__:20.106', '__DATE_3__:20.013 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41436-0-1]
+__DATE_3__:20.014 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.014 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101035|150005 WAS INITIATED
+__DATE_3__:20.105 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101035|150005 WAS CONVERTED
+__DATE_3__:20.106 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.106 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-5, flowName=Litteratursiden2Brønd, logStoreTrackingId=41436-0-1}', '41436', 0, 1);
+INSERT INTO logentry VALUES (5581937, '__DATE_3__:20.163', '__DATE_3__:20.129 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41436-0-2]
+__DATE_3__:20.129 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.13 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101777|150005 WAS INITIATED
+__DATE_3__:20.163 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101777|150005 WAS CONVERTED
+__DATE_3__:20.163 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.163 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-3, flowName=Litteratursiden2Brønd, logStoreTrackingId=41436-0-2}', '41436', 0, 2);
+INSERT INTO logentry VALUES (5581938, '__DATE_3__:20.216', '__DATE_3__:20.181 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41436-0-3]
+__DATE_3__:20.181 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.181 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101768|150005 WAS INITIATED
+__DATE_3__:20.215 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101768|150005 WAS CONVERTED
+__DATE_3__:20.215 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.216 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-4, flowName=Litteratursiden2Brønd, logStoreTrackingId=41436-0-3}', '41436', 0, 3);
+INSERT INTO logentry VALUES (5581939, '__DATE_3__:20.257', '__DATE_3__:20.231 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41436-0-4]
+__DATE_3__:20.231 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.232 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101788|150005 WAS INITIATED
+__DATE_3__:20.257 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101788|150005 WAS CONVERTED
+__DATE_3__:20.257 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.257 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-0, flowName=Litteratursiden2Brønd, logStoreTrackingId=41436-0-4}', '41436', 0, 4);
+INSERT INTO logentry VALUES (5581940, '__DATE_3__:20.294', '__DATE_3__:20.271 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41436-0-5]
+__DATE_3__:20.271 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.271 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101785|150005 WAS INITIATED
+__DATE_3__:20.293 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101785|150005 WAS CONVERTED
+__DATE_3__:20.294 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_3__:20.294 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-2, flowName=Litteratursiden2Brønd, logStoreTrackingId=41436-0-5}', '41436', 0, 5);
+
+
+--
+-- Job 2
+--
+INSERT INTO logentry VALUES (5581941, '__DATE_4__:19.991', '__DATE_4__:19.106 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41437-0-0]
+__DATE_4__:19.11 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_4__:19.125 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101787|150005 WAS INITIATED
+__DATE_4__:19.987 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101787|150005 WAS CONVERTED
+__DATE_4__:19.99 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_4__:19.991 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-1, flowName=Litteratursiden2Brønd, logStoreTrackingId=41437-0-0}', '41437', 0, 0);
+INSERT INTO logentry VALUES (5581942, '__DATE_4__:20.106', '__DATE_4__:20.013 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41437-0-1]
+__DATE_4__:20.014 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.014 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101035|150005 WAS INITIATED
+__DATE_4__:20.105 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101035|150005 WAS CONVERTED
+__DATE_4__:20.106 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.106 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-5, flowName=Litteratursiden2Brønd, logStoreTrackingId=41437-0-1}', '41437', 0, 1);
+INSERT INTO logentry VALUES (5581943, '__DATE_4__:20.163', '__DATE_4__:20.129 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41437-0-2]
+__DATE_4__:20.129 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.13 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101777|150005 WAS INITIATED
+__DATE_4__:20.163 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101777|150005 WAS CONVERTED
+__DATE_4__:20.163 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.163 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-3, flowName=Litteratursiden2Brønd, logStoreTrackingId=41437-0-2}', '41437', 0, 2);
+INSERT INTO logentry VALUES (5581944, '__DATE_4__:20.216', '__DATE_4__:20.181 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41437-0-3]
+__DATE_4__:20.181 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.181 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101768|150005 WAS INITIATED
+__DATE_4__:20.215 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101768|150005 WAS CONVERTED
+__DATE_4__:20.215 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.216 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-4, flowName=Litteratursiden2Brønd, logStoreTrackingId=41437-0-3}', '41437', 0, 3);
+INSERT INTO logentry VALUES (5581945, '__DATE_4__:20.257', '__DATE_4__:20.231 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41437-0-4]
+__DATE_4__:20.231 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.232 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101788|150005 WAS INITIATED
+__DATE_4__:20.257 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101788|150005 WAS CONVERTED
+__DATE_4__:20.257 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.257 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-0, flowName=Litteratursiden2Brønd, logStoreTrackingId=41437-0-4}', '41437', 0, 4);
+INSERT INTO logentry VALUES (5581946, '__DATE_4__:20.294', '__DATE_4__:20.271 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor invokeScript(): starting javascript [Litteratursiden2Brønd] with invocation method: [createDatawellXmlFromLitteratursiden] and logging ID [41437-0-5]
+__DATE_4__:20.271 INFO JavaScript.Logger JS: Entering: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.271 INFO JavaScript.Logger JS: CONVERSION OF RECORD(S) BASED ON 101785|150005 WAS INITIATED
+__DATE_4__:20.293 INFO JavaScript.Logger JS: RECORD(S) BASED ON 101785|150005 WAS CONVERTED
+__DATE_4__:20.294 INFO JavaScript.Logger JS: Leaving: createDatawellXmlFromLitteratursiden
+__DATE_4__:20.294 INFO dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor Done
+', 'orb-thread-pool-1 (pool #1): worker-3', 'dk.dbc.dataio.jobprocessor.util.ChunkItemProcessor', 'INFO', '?', '?', '?', '-1', NULL, '{flowVersion=8, logStoreTrackingIdCommit=true, DBCTrackingId=traceid:10.233.37.192-41433-0-2, flowName=Litteratursiden2Brønd, logStoreTrackingId=41437-0-5}', '41437', 0, 5);

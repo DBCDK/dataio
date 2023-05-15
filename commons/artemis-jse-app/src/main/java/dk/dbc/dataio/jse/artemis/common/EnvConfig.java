fcec9451c27429f3066ab00c36e93dc715025eea
Jens Grønborg Andersen <jega@dbc.dk>
2023-05-15 13:54:35 +0200

    Processor for staging with configurable artemis and qpid client

--- commons/artemis-jse-app/src/main/java/dk/dbc/dataio/jse/artemis/common/EnvConfig.java
+++ commons/artemis-jse-app/src/main/java/dk/dbc/dataio/jse/artemis/common/EnvConfig.java
@@ -1,7 +1,9 @@
 package dk.dbc.dataio.jse.artemis.common;
 
+import java.time.DateTimeException;
 import java.time.Duration;
 import java.util.Optional;
+import java.util.function.Function;
 
 public interface EnvConfig {
     default Optional<String> asOptionalString() {
@@ -9,15 +11,15 @@ public interface EnvConfig {
     }
 
     default Optional<Integer> asOptionalInteger() {
-        try {
-            return asOptionalString().map(Integer::parseInt);
-        } catch (NumberFormatException nfe) {
-            throw new NumberFormatException("Unable to parse key + " + name() + ", with value: " + asOptionalString().orElse("<empty>"));
-        }
+        return asOptionalString().map(s -> parseNumber(s, Integer::valueOf));
     }
 
     default Optional<Duration> asOptionalDuration() {
-        return asOptionalString().map(Duration::parse);
+        try {
+            return asOptionalString().map(Duration::parse);
+        } catch (DateTimeException dte) {
+            throw new DateTimeException("Unable to parse key + " + name() + ", with value: " + asOptionalString().orElse("<empty>"));
+        }
     }
 
     default Integer asInteger() {
@@ -54,6 +56,14 @@ public interface EnvConfig {
 
     default String getName() {
         return name();
+    }
+
+    private <T extends Number> T parseNumber(String s, Function<String, T> parser) {
+        try {
+            return parser.apply(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("Unable to parse key + " + name() + ", with value: " + asOptionalString().orElse("<empty>"));
+        }
     }
 
     String getDefaultValue();

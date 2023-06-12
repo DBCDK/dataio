6161994a53b96b361832ee348cd103b5d990db03
Jens Grønborg Andersen <jega@dbc.dk>
2023-06-12 12:19:47 +0200

    Migrating marcconv to se

--- sink/marcconv/src/main/java/dk/dbc/dataio/sink/marcconv/MessageConsumerBean.java
+++ sink/marcconv/src/main/java/dk/dbc/dataio/sink/marcconv/MessageConsumerBean.java
@@ -1,5 +1,7 @@
 package dk.dbc.dataio.sink.marcconv;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import dk.dbc.commons.addi.AddiReader;
 import dk.dbc.commons.addi.AddiRecord;
 import dk.dbc.dataio.commons.conversion.Conversion;
@@ -11,61 +13,41 @@ import dk.dbc.dataio.commons.types.ChunkItem;
 import dk.dbc.dataio.commons.types.ConsumedMessage;
 import dk.dbc.dataio.commons.types.Diagnostic;
 import dk.dbc.dataio.commons.types.exceptions.InvalidMessageException;
-import dk.dbc.dataio.commons.utils.cache.Cache;
-import dk.dbc.dataio.commons.utils.cache.CacheManager;
 import dk.dbc.dataio.commons.utils.lang.StringUtil;
-import dk.dbc.dataio.sink.types.AbstractSinkMessageConsumerBean;
-import dk.dbc.dataio.sink.types.SinkException;
+import dk.dbc.dataio.jse.artemis.common.jms.MessageConsumerAdapter;
+import dk.dbc.dataio.jse.artemis.common.service.ServiceHub;
 import dk.dbc.jsonb.JSONBContext;
 import dk.dbc.jsonb.JSONBException;
 import dk.dbc.log.DBCTrackedLogContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.EJB;
-import javax.ejb.MessageDriven;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
-@MessageDriven(name = "marcConvListener", activationConfig = {
-        // Please see the following url for a explanation of the available settings.
-        // The message selector variable is defined in the dataio-secrets project
-        // https://activemq.apache.org/activation-spec-properties
-        @ActivationConfigProperty(propertyName = "destination", propertyValue = "${ENV=QUEUE}"),
-        @ActivationConfigProperty(propertyName = "useJndi", propertyValue = "false"),
-        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-        @ActivationConfigProperty(propertyName = "resourceAdapter", propertyValue = "artemis"),
-        @ActivationConfigProperty(propertyName = "initialRedeliveryDelay", propertyValue = "5000"),
-        @ActivationConfigProperty(propertyName = "redeliveryBackOffMultiplier", propertyValue = "4"),
-        @ActivationConfigProperty(propertyName = "maximumRedeliveries", propertyValue = "3"),
-        @ActivationConfigProperty(propertyName = "redeliveryUseExponentialBackOff", propertyValue = "true"),
-        @ActivationConfigProperty(propertyName = "MaxSession", propertyValue = "4")
-})
-public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
+public class MessageConsumerBean extends MessageConsumerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumerBean.class);
-
-    @PersistenceContext(unitName = "marcconv_PU")
-    EntityManager entityManager;
-
-    @EJB
-    ConversionFinalizerBean conversionFinalizerBean;
-
     private final JSONBContext jsonbContext = new JSONBContext();
     private final ConversionFactory conversionFactory = new ConversionFactory();
-    private final Cache<Integer, Conversion> conversionCache = CacheManager.createLRUCache(10);
+    private final Cache<Integer, Conversion> conversionCache = CacheBuilder.newBuilder().expireAfterAccess(Duration.ofMinutes(30)).maximumSize(10).build();
+    //    @PersistenceContext(unitName = "marcconv_PU")
+    EntityManager entityManager;
+    ConversionFinalizerBean conversionFinalizerBean;
+
+    public MessageConsumerBean(ServiceHub serviceHub) {
+        super(serviceHub);
+    }
 
     @Override
-    public void handleConsumedMessage(ConsumedMessage consumedMessage)
-            throws InvalidMessageException, NullPointerException, SinkException {
-        final Chunk chunk = unmarshallPayload(consumedMessage);
+    public void handleConsumedMessage(ConsumedMessage consumedMessage) throws InvalidMessageException {
+        Chunk chunk = unmarshallPayload(consumedMessage);
         LOGGER.info("Received chunk {}/{}", chunk.getJobId(), chunk.getChunkId());
 
-        final Chunk result;
+        Chunk result;
         if (chunk.isTerminationChunk()) {
             try {
                 // Give the before-last message enough time to commit
@@ -77,29 +59,39 @@ public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
                 // before all data was available.)
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
-                throw new SinkException(e);
+                throw new RuntimeException(e);
             }
             result = conversionFinalizerBean.handleTerminationChunk(chunk);
         } else {
             result = handleChunk(chunk);
         }
-        uploadChunk(result);
+        sendResultToJobStore(result);
+    }
+
+    @Override
+    public String getQueue() {
+        return null;
+    }
+
+    @Override
+    public String getAddress() {
+        return null;
     }
 
     Chunk handleChunk(Chunk chunk) {
-        final Integer jobId = Math.toIntExact(chunk.getJobId());
-        final Integer chunkId = Math.toIntExact(chunk.getChunkId());
-        final Chunk result = new Chunk(jobId, chunkId, Chunk.Type.DELIVERED);
-        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        Integer jobId = Math.toIntExact(chunk.getJobId());
+        long chunkId = chunk.getChunkId();
+        Chunk result = new Chunk(jobId, chunkId, Chunk.Type.DELIVERED);
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         try {
             for (ChunkItem chunkItem : chunk.getItems()) {
                 DBCTrackedLogContext.setTrackingId(chunkItem.getTrackingId());
                 result.insertItem(handleChunkItem(jobId, chunkItem, buffer));
             }
-            storeConversion(jobId, chunkId, buffer.toByteArray());
-            final Conversion cachedConversion = conversionCache.get(jobId);
+            storeConversion(jobId, Math.toIntExact(chunkId), buffer.toByteArray());
+            Conversion cachedConversion = conversionCache.getIfPresent(jobId);
             if (cachedConversion != null) {
-                final ConversionParam param = cachedConversion.getParam();
+                ConversionParam param = cachedConversion.getParam();
                 if (param != null) {
                     storeConversionParam(jobId, param);
                 }
@@ -111,32 +103,19 @@ public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
     }
 
     private ChunkItem handleChunkItem(Integer jobId, ChunkItem chunkItem, ByteArrayOutputStream buffer) {
-        final ChunkItem result = new ChunkItem()
-                .withId(chunkItem.getId())
-                .withTrackingId(chunkItem.getTrackingId())
-                .withType(ChunkItem.Type.STRING)
-                .withEncoding(StandardCharsets.UTF_8);
+        ChunkItem result = new ChunkItem().withId(chunkItem.getId()).withTrackingId(chunkItem.getTrackingId()).withType(ChunkItem.Type.STRING).withEncoding(StandardCharsets.UTF_8);
         try {
             switch (chunkItem.getStatus()) {
                 case FAILURE:
-                    return result
-                            .withStatus(ChunkItem.Status.IGNORE)
-                            .withData("Failed by processor");
+                    return result.withStatus(ChunkItem.Status.IGNORE).withData("Failed by processor");
                 case IGNORE:
-                    return result
-                            .withStatus(ChunkItem.Status.IGNORE)
-                            .withData("Ignored by processor");
+                    return result.withStatus(ChunkItem.Status.IGNORE).withData("Ignored by processor");
                 default:
                     appendToBuffer(buffer, convertChunkItem(jobId, chunkItem));
-                    return result
-                            .withStatus(ChunkItem.Status.SUCCESS)
-                            .withData("Converted");
+                    return result.withStatus(ChunkItem.Status.SUCCESS).withData("Converted");
             }
         } catch (RuntimeException e) {
-            return result
-                    .withStatus(ChunkItem.Status.FAILURE)
-                    .withDiagnostics(new Diagnostic(Diagnostic.Level.FATAL, e.getMessage(), e))
-                    .withData(e.getMessage());
+            return result.withStatus(ChunkItem.Status.FAILURE).withDiagnostics(new Diagnostic(Diagnostic.Level.FATAL, e.getMessage(), e)).withData(e.getMessage());
         }
     }
 
@@ -151,14 +130,13 @@ public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
     }
 
     private byte[] convertChunkItem(Integer jobId, ChunkItem chunkItem) {
-        final AddiReader addiReader = new AddiReader(new ByteArrayInputStream(chunkItem.getData()));
+        AddiReader addiReader = new AddiReader(new ByteArrayInputStream(chunkItem.getData()));
         try {
-            final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
             while (addiReader.hasNext()) {
-                final AddiRecord addiRecord = addiReader.next();
-                final ConversionParam conversionParam = jsonbContext.unmarshall(
-                        StringUtil.asString(addiRecord.getMetaData()), ConversionParam.class);
-                final Conversion conversion = getConversion(jobId, conversionParam);
+                AddiRecord addiRecord = addiReader.next();
+                ConversionParam conversionParam = jsonbContext.unmarshall(StringUtil.asString(addiRecord.getMetaData()), ConversionParam.class);
+                Conversion conversion = getConversion(jobId, conversionParam);
                 appendToBuffer(buffer, conversion.apply(addiRecord.getContentData()));
             }
             return buffer.toByteArray();
@@ -168,17 +146,12 @@ public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
     }
 
     private Conversion getConversion(Integer jobId, ConversionParam conversionParam) {
-        if (conversionCache.containsKey(jobId)) {
-            return conversionCache.get(jobId);
-        }
-        final Conversion conversion = conversionFactory.newConversion(conversionParam);
-        conversionCache.put(jobId, conversion);
-        return conversion;
+        return conversionCache.asMap().computeIfAbsent(jobId, id -> conversionFactory.newConversion(conversionParam));
     }
 
     private void storeConversion(Integer jobId, Integer chunkId, byte[] conversionBytes) {
         if (conversionBytes.length != 0) {
-            final ConversionBlock.Key key = new ConversionBlock.Key(jobId, chunkId);
+            ConversionBlock.Key key = new ConversionBlock.Key(jobId, chunkId);
             ConversionBlock conversionBlock = entityManager.find(ConversionBlock.class, key);
             if (conversionBlock == null) {
                 conversionBlock = new ConversionBlock();
@@ -195,8 +168,7 @@ public class MessageConsumerBean extends AbstractSinkMessageConsumerBean {
     }
 
     private void storeConversionParam(Integer jobId, ConversionParam param) {
-        StoredConversionParam storedConversionParam =
-                entityManager.find(StoredConversionParam.class, jobId);
+        StoredConversionParam storedConversionParam = entityManager.find(StoredConversionParam.class, jobId);
         if (storedConversionParam == null) {
             storedConversionParam = new StoredConversionParam(jobId);
             storedConversionParam.setParam(param);
---
+++ sink/marcconv/src/main/java/dk/dbc/dataio/sink/marcconv/SinkConfig.java
@@ -0,0 +1,23 @@
+package dk.dbc.dataio.sink.marcconv;
+
+import dk.dbc.dataio.jse.artemis.common.EnvConfig;
+
+public enum SinkConfig implements EnvConfig {
+    QUEUE("sink::marcconv"),
+    MESSAGE_FILTER(null);
+
+    private final String defaultValue;
+
+    SinkConfig() {
+        this(null);
+    }
+
+    SinkConfig(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+}

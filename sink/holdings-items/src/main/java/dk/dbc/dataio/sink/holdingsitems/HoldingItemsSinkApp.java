e4481e3c1881ce4d39a5821b9913705a7966aaa5
Jens Grønborg Andersen <jega@dbc.dk>
2023-06-21 16:14:55 +0200

    Migrating sinks to Java SE

--- sink/holdings-items/src/main/java/dk/dbc/dataio/sink/holdingsitems/HoldingItemsSinkApp.java
+++ sink/holdings-items/src/main/java/dk/dbc/dataio/sink/holdingsitems/HoldingItemsSinkApp.java
@@ -9,7 +9,7 @@ import java.util.function.Supplier;
 
 public class HoldingItemsSinkApp extends MessageConsumerApp {
     private static final ServiceHub SERVICE_HUB = ServiceHub.defaultHub();
-    private static final SolrDocStoreConnector SOLR_DOC_STORE = new SolrDocStoreConnector(ClientBuilder.newClient(), SinkConfig.SOLR_DOC_STORE_SERVICE_URL.asPGJDBCUrl());
+    private static final SolrDocStoreConnector SOLR_DOC_STORE = new SolrDocStoreConnector(ClientBuilder.newClient(), SinkConfig.SOLR_DOC_STORE_SERVICE_URL.asString());
     private static final HoldingsItemsUnmarshaller HOLDINGS_ITEMS_UNMARSHALLER = new HoldingsItemsUnmarshaller(SOLR_DOC_STORE);
     private static final Supplier<MessageConsumer> MESSAGE_CONSUMER = () -> new MessageConsumer(SERVICE_HUB, SOLR_DOC_STORE, HOLDINGS_ITEMS_UNMARSHALLER);
 

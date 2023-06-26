f50192c0a26e56368a6909ab0f1303b56b11b933
Jens Grønborg Andersen <jega@dbc.dk>
2023-06-26 17:21:45 +0200

    Migrating sinks to Java SE

--- sink/tickle-repo/src/main/java/dk/dbc/dataio/sink/ticklerepo/TickleRepoSinkApp.java
+++ sink/tickle-repo/src/main/java/dk/dbc/dataio/sink/ticklerepo/TickleRepoSinkApp.java
@@ -2,16 +2,11 @@ package dk.dbc.dataio.sink.ticklerepo;
 
 import dk.dbc.dataio.jse.artemis.common.app.MessageConsumerApp;
 import dk.dbc.dataio.jse.artemis.common.service.ServiceHub;
-import org.eclipse.persistence.internal.jpa.deployment.SEPersistenceUnitInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -21,32 +16,21 @@ public class TickleRepoSinkApp extends MessageConsumerApp {
     private static final Supplier<MessageConsumer> MESSAGE_CONSUMER = () -> new MessageConsumer(SERVICE_HUB, makeEntityManager());
 
     private static EntityManager makeEntityManager() {
-        LOGGER.info("Creating entiymanager");
-        SEPersistenceUnitInfo puInfo = new SEPersistenceUnitInfo();
         try {
-            puInfo.setPersistenceUnitRootUrl(new URL("http://dataio/jse"));
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            LOGGER.info("Creating entity manager");
+            Map<String, Object> config = Map.of(
+                    "javax.persistence.transactionType", "RESOURCE_LOCAL",
+                    "provider", "org.eclipse.persistence.jpa.PersistenceProvider",
+                    "javax.persistence.schema-generation.database.action", "none",
+                    "javax.persistence.jdbc.driver", "org.postgresql.Driver",
+                    "javax.persistence.jdbc.url", SinkConfig.TICKLE_REPO_DB_URL.asPGJDBCUrl());
+            EntityManager entityManager = Persistence.createEntityManagerFactory("tickleRepoPU", config).createEntityManager();
+            LOGGER.info("Created entity manager");
+            return entityManager;
+        } catch (RuntimeException re) {
+            LOGGER.error("Failed to create entity manager", re);
+            throw re;
         }
-        puInfo.setPersistenceUnitName("tickleRepoPU1");
-        puInfo.setManagedClassNames(List.of(
-                "dk.dbc.jsonb.JsonConverter",
-                "dk.dbc.ticklerepo.dto.DataSet",
-                "dk.dbc.ticklerepo.dto.BatchTypeConverter",
-                "dk.dbc.ticklerepo.dto.Batch",
-                "dk.dbc.ticklerepo.dto.RecordStatusConverter",
-                "dk.dbc.ticklerepo.dto.Record"));
-        Map<String, Object> config = Map.of(
-                "javax.persistence.transactionType", "RESOURCE_LOCAL",
-                "provider", "org.eclipse.persistence.jpa.PersistenceProvider",
-                "javax.persistence.schema-generation.database.action", "none",
-                "javax.persistence.jdbc.driver", "org.postgresql.Driver",
-                "javax.persistence.jdbc.url", SinkConfig.TICKLE_REPO_DB_URL.asPGJDBCUrl(),
-                "eclipselink.se-puinfo", puInfo);
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("tickleRepoPU1", config);
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        LOGGER.info("Created entiymanager");
-        return entityManager;
     }
 
     public static void main(String[] args) {

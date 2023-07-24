00cf0af85f487b04a8da498454f3eb8bea59bcc0
Jens Grønborg Andersen <jega@dbc.dk>
2023-07-24 15:26:32 +0200

    !!Job abortion

--- job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/entity/DependencyTrackingEntity.java
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/entity/DependencyTrackingEntity.java
@@ -61,7 +61,7 @@ import java.util.Set;
                 query = "SELECT sinkid, status, count(*) FROM dependencytracking GROUP BY sinkid, status ORDER BY sinkid, status",
                 resultSetMapping = DependencyTrackingEntity.SINKID_STATUS_COUNT_RESULT),
         @NamedNativeQuery(name = DependencyTrackingEntity.SINKID_STATUS_COUNT_QUERY,
-                query = "SELECT sinkid, status, count(*) FROM dependencytracking WHERE sinkid=? GROUP BY status ORDER BY sinkid, status",
+                query = "SELECT sinkid, status, count(*) FROM dependencytracking WHERE sinkid=? GROUP BY sinkid, status ORDER BY sinkid, status",
                 resultSetMapping = DependencyTrackingEntity.SINKID_STATUS_COUNT_RESULT),
         @NamedNativeQuery(name = DependencyTrackingEntity.JOB_COUNT_CHUNK_COUNT_QUERY,
                 query = "SELECT count(DISTINCT jobid) AS numberOfJobs, count(jobid) AS NumberOfChunks FROM dependencytracking WHERE sinkid = ?"),

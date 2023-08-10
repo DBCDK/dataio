c8f7426602761bc4fb270a8b3652fa04a2a05dd3
Jens Grønborg Andersen <jega@dbc.dk>
2023-08-10 10:49:09 +0200

    !!Added chunk timeouts and metrics.

--- commons/artemis-jse-db-app/src/main/java/dk/dbc/dataio/jse/artemis/common/db/JPAHelper.java
+++ commons/artemis-jse-db-app/src/main/java/dk/dbc/dataio/jse/artemis/common/db/JPAHelper.java
@@ -3,6 +3,8 @@ package dk.dbc.dataio.jse.artemis.common.db;
 import dk.dbc.dataio.jse.artemis.common.EnvConfig;
 import org.flywaydb.core.Flyway;
 import org.postgresql.ds.PGSimpleDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -10,18 +12,25 @@ import javax.sql.DataSource;
 import java.util.Map;
 
 public class JPAHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JPAHelper.class);
+
     public static EntityManager makeEntityManager(String persistenceUnit, EnvConfig config) {
         return makeEntityManager(persistenceUnit, config.asPGJDBCUrl());
     }
 
     public static EntityManager makeEntityManager(String persistenceUnit, String jdbcUrl) {
-        Map<String, String> config = Map.of(
-                "javax.persistence.transactionType", "RESOURCE_LOCAL",
-                "provider", "org.eclipse.persistence.jpa.PersistenceProvider",
-                "javax.persistence.schema-generation.database.action", "none",
-                "javax.persistence.jdbc.driver", "org.postgresql.Driver",
-                "javax.persistence.jdbc.url", jdbcUrl);
-        return makeEntityManager(persistenceUnit, config);
+        try {
+            Map<String, String> config = Map.of(
+                    "javax.persistence.transactionType", "RESOURCE_LOCAL",
+                    "provider", "org.eclipse.persistence.jpa.PersistenceProvider",
+                    "javax.persistence.schema-generation.database.action", "none",
+                    "javax.persistence.jdbc.driver", "org.postgresql.Driver",
+                    "javax.persistence.jdbc.url", jdbcUrl);
+            return makeEntityManager(persistenceUnit, config);
+        } catch (RuntimeException e) {
+            LOGGER.info("Unable to create an entity manager for {}", persistenceUnit, e);
+            throw e;
+        }
     }
 
     public static EntityManager makeEntityManager(String persistenceUnit, Map<String, String> config) {

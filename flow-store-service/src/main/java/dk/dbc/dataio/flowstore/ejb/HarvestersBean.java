6d010ad26aad3f4abc5c5a429a69f99dc965f3a2
jega <jega@dbc.dk>
2024-01-09 12:25:34 +0100

    Added cluster config and did some code cleanup

--- flow-store-service/src/main/java/dk/dbc/dataio/flowstore/ejb/HarvestersBean.java
+++ flow-store-service/src/main/java/dk/dbc/dataio/flowstore/ejb/HarvestersBean.java
@@ -110,7 +110,6 @@ public class HarvestersBean {
             return Response.status(Response.Status.NOT_FOUND).entity(NO_CONTENT).build();
         }
 
-        entityManager.detach(harvesterConfig);
         if (type != null && !type.trim().isEmpty()) {
             // Update type if given as header parameter
             harvesterConfig.withType(type);
@@ -119,8 +118,6 @@ public class HarvestersBean {
 
         harvesterConfig.setContent(configContent);
         harvesterConfig.setVersion(version);
-        harvesterConfig = entityManager.merge(harvesterConfig);
-        entityManager.flush();
 
         return Response.ok().entity(jsonbContext.marshall(harvesterConfig))
                 .tag(harvesterConfig.getVersion().toString())

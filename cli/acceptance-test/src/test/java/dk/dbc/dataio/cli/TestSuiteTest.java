f178ff11d4a2024e11d93f1df92e8c5934e467dc
jega <jega@dbc.dk>
2024-02-12 10:30:23 +0100

    Some cli tools needs to be for java 11

--- cli/acceptance-test/src/test/java/dk/dbc/dataio/cli/TestSuiteTest.java
+++ cli/acceptance-test/src/test/java/dk/dbc/dataio/cli/TestSuiteTest.java
@@ -1,13 +1,14 @@
 package dk.dbc.dataio.cli;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,22 +27,27 @@ public class TestSuiteTest {
 
     @Test
     public void findTestSuite_suiteIsIncomplete_returnsEmpty() throws IOException {
-        final File tests = dir.newFolder("test");
-        FileUtils.write(new File(tests, "suite.acctest"), "id=suite");
+        Path tests = createTestDir();
+        Files.writeString(tests.resolve("suite.acctest"), "id=suite");
         assertThat(TestSuite.findTestSuite(dir.getRoot().toPath(), "suite"), is(Optional.empty()));
+    }
+
+    private Path createTestDir() throws IOException {
+        Path tests = dir.getRoot().toPath().resolve("tests");
+        return Files.createDirectories(tests);
     }
 
     @Test
     public void findTestSuite() throws IOException {
-        final File tests = dir.newFolder("test");
-        final File dataFile = new File(tests, "suite.txt");
-        FileUtils.write(dataFile, "data");
-        FileUtils.write(new File(tests, "suite.acctest"), "id=suite");
+        Path tests = createTestDir();
+        Path dataFile = tests.resolve("suite.txt");
+        Files.writeString(dataFile, "data");
+        Files.writeString(tests.resolve("suite.acctest"), "id=suite");
 
-        final TestSuite testsuite = TestSuite.findTestSuite(dir.getRoot().toPath(), "suite").orElse(null);
+        TestSuite testsuite = TestSuite.findTestSuite(dir.getRoot().toPath(), "suite").orElse(null);
         assertThat("testsuite", testsuite, is(notNullValue()));
         assertThat("testsuite name", testsuite.getName(), is("suite"));
-        assertThat("testsuite data file", testsuite.getDataFile(), is(dataFile.toPath()));
+        assertThat("testsuite data file", testsuite.getDataFile(), is(dataFile));
         assertThat("testsuite properties", testsuite.getProperties().getProperty("id"), is("suite"));
     }
 
@@ -52,24 +58,26 @@ public class TestSuiteTest {
 
     @Test
     public void findAllTestSuites() throws IOException {
-        final File suites = dir.newFolder("test", "suites");
-        final File data = dir.newFolder("test", "data");
-        final File suite1DataFile = new File(data, "suite1.txt");
-        final File suite2DataFile = new File(data, "suite2.dat");
-        FileUtils.write(suite1DataFile, "data");
-        FileUtils.write(suite2DataFile, "data");
-        FileUtils.write(new File(suites, "suite1.acctest"), "id=suite1");
-        FileUtils.write(new File(suites, "suite2.acctest"), "id=suite2");
-        FileUtils.write(new File(suites, "suite3.acctest"), "id=suite3");
+        Path suites = dir.getRoot().toPath().resolve("test").resolve("suites");
+        Files.createDirectories(suites);
+        Path data = dir.getRoot().toPath().resolve("test").resolve("data");
+        Files.createDirectories(data);
+        Path suite1DataFile = data.resolve("suite1.txt");
+        Path suite2DataFile = data.resolve("suite2.dat");
+        Files.writeString(suite1DataFile, "data");
+        Files.writeString(suite2DataFile, "data");
+        Files.writeString(suites.resolve("suite1.acctest"), "id=suite1");
+        Files.writeString(suites.resolve("suite2.acctest"), "id=suite2");
+        Files.writeString(suites.resolve("suite3.acctest"), "id=suite3");
 
         final List<TestSuite> testsuites = TestSuite.findAllTestSuites(dir.getRoot().toPath());
         assertThat("number of testsuites", testsuites.size(), is(2));
-        Collections.sort(testsuites, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+        testsuites.sort(Comparator.comparing(TestSuite::getName));
         assertThat("1st testsuite name", testsuites.get(0).getName(), is("suite1"));
-        assertThat("1st testsuite data file", testsuites.get(0).getDataFile(), is(suite1DataFile.toPath()));
+        assertThat("1st testsuite data file", testsuites.get(0).getDataFile(), is(suite1DataFile));
         assertThat("1st testsuite properties", testsuites.get(0).getProperties().getProperty("id"), is("suite1"));
         assertThat("2nd testsuite name", testsuites.get(1).getName(), is("suite2"));
-        assertThat("2nd testsuite data file", testsuites.get(1).getDataFile(), is(suite2DataFile.toPath()));
+        assertThat("2nd testsuite data file", testsuites.get(1).getDataFile(), is(suite2DataFile));
         assertThat("2nd testsuite properties", testsuites.get(1).getProperties().getProperty("id"), is("suite2"));
     }
 }

a248ef129b8c1da1b18999c20fa0f797fbc1afbe
Jens Andersen <jega@dbc.dk>
2024-04-04 11:19:44 +0200

    !!Datacluster ting

--- job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/rs/ResponseNodeDecorator.java
+++ job-store-service/war/src/main/java/dk/dbc/dataio/jobstore/service/rs/ResponseNodeDecorator.java
@@ -18,5 +18,6 @@ public class ResponseNodeDecorator implements ContainerResponseFilter {
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
         LOGGER.info("Adding header");
         containerResponseContext.getHeaders().add("Cluster-Node", InetAddress.getLocalHost().getHostName());
+        containerResponseContext.getHeaders().add("Connection", "close");
     }
 }

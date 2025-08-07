8f41e5c9438858fafa1bcb4ff66f669e150f6026
Jens Grønborg Andersen <104764460+JensGroenborgAndersen@users.noreply.github.com>
2025-08-07 10:44:12 +0200

    We should not keep people from regretting setting their initials on a job. (#243)

--- job-store-service/types/src/test/java/dk/dbc/dataio/jobstore/types/WorkflowNoteTest.java
+++ job-store-service/types/src/test/java/dk/dbc/dataio/jobstore/types/WorkflowNoteTest.java
@@ -2,23 +2,11 @@ package dk.dbc.dataio.jobstore.types;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class WorkflowNoteTest {
 
     final static boolean PROCESSED = false;
     final static String ASSIGNEE = "initials";
     final static String DESCRIPTION = "description";
-
-    @Test
-    public void constructor_assigneeArgIsNull_throws() {
-        assertThrows(NullPointerException.class, () -> new WorkflowNote(PROCESSED, null, DESCRIPTION));
-    }
-
-    @Test
-    public void constructor_assigneeArgIsEmpty_throws() {
-        assertThrows(IllegalArgumentException.class, () -> new WorkflowNote(PROCESSED, "", DESCRIPTION));
-    }
 
     @Test
     public void constructor_descriptionArgIsNull_WorkflowNoteCreated() {

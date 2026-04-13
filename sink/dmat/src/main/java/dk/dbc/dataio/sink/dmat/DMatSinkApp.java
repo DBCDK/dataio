5a3c34e47fa6566a05f580581dc59df26ca5350c
Jan Bauer Nielsen <jbn@dbc.dk>
2026-04-13 08:15:55 +0200

    Fix HttpClient runtime error
    When instantiating connectors in sink/dmat and sink/holdings-items
    modules.

--- sink/dmat/src/main/java/dk/dbc/dataio/sink/dmat/DMatSinkApp.java
+++ sink/dmat/src/main/java/dk/dbc/dataio/sink/dmat/DMatSinkApp.java
@@ -1,9 +1,16 @@
 package dk.dbc.dataio.sink.dmat;
 
+import dk.dbc.commons.useragent.UserAgent;
 import dk.dbc.dataio.jse.artemis.common.app.MessageConsumerApp;
 import dk.dbc.dataio.jse.artemis.common.service.ServiceHub;
 import dk.dbc.dmat.service.connector.DMatServiceConnector;
-import dk.dbc.dmat.service.connector.DMatServiceConnectorFactory;
+import dk.dbc.dmat.service.connector.JacksonConfig;
+import dk.dbc.httpclient.FailSafeHttpClient;
+import dk.dbc.httpclient.HttpClient;
+import jakarta.ws.rs.client.Client;
+import net.jodah.failsafe.RetryPolicy;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.jackson.JacksonFeature;
 
 import java.util.function.Supplier;
 
@@ -13,10 +20,15 @@ import java.util.function.Supplier;
  */
 public class DMatSinkApp extends MessageConsumerApp {
     private static final ServiceHub serviceHub = ServiceHub.defaultHub();
-    private static final DMatServiceConnector DMAT_SERVICE_CONNECTOR = DMatServiceConnectorFactory.createWithoutRetryPolicy(SinkConfig.DMAT_SERVICE_URL.asString());
+    private static final DMatServiceConnector DMAT_SERVICE_CONNECTOR = createDmatServiceConnector(SinkConfig.DMAT_SERVICE_URL.asString());
     private static final Supplier<DMatMessageConsumer> messageConsumer = () -> new DMatMessageConsumer(serviceHub, DMAT_SERVICE_CONNECTOR);
 
     public static void main(String[] args) {
         new DMatSinkApp().go(serviceHub, messageConsumer);
+    }
+
+    private static DMatServiceConnector createDmatServiceConnector(String dmatServiceBaseUrl) {
+        Client client = HttpClient.newClient((new ClientConfig()).register(new JacksonConfig()).register(new JacksonFeature()));
+        return new DMatServiceConnector(FailSafeHttpClient.create(client, UserAgent.forInternalRequests(), new RetryPolicy()), dmatServiceBaseUrl);
     }
 }
--- sink/holdings-items/src/main/java/dk/dbc/dataio/sink/holdingsitems/HoldingItemsSinkApp.java
+++ sink/holdings-items/src/main/java/dk/dbc/dataio/sink/holdingsitems/HoldingItemsSinkApp.java
@@ -1,20 +1,43 @@
 package dk.dbc.dataio.sink.holdingsitems;
 
+import dk.dbc.commons.useragent.UserAgent;
 import dk.dbc.dataio.jse.artemis.common.app.MessageConsumerApp;
 import dk.dbc.dataio.jse.artemis.common.service.ServiceHub;
+import dk.dbc.httpclient.FailSafeHttpClient;
 import dk.dbc.solrdocstore.connector.SolrDocStoreConnector;
+import jakarta.ws.rs.ProcessingException;
+import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.Response;
+import net.jodah.failsafe.RetryPolicy;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 public class HoldingItemsSinkApp extends MessageConsumerApp {
+
+    private static final RetryPolicy<Response> RETRY_POLICY = new RetryPolicy<Response>()
+            .handle(ProcessingException.class)
+            .handleResultIf(response ->
+                    response.getStatus() == 404
+                            || response.getStatus() == 500
+                            || response.getStatus() == 502)
+            .withDelay(Duration.ofSeconds(5))
+            .withMaxRetries(3);
+
     private static final ServiceHub SERVICE_HUB = ServiceHub.defaultHub();
-    private static final SolrDocStoreConnector SOLR_DOC_STORE = new SolrDocStoreConnector(ClientBuilder.newClient().register(new JacksonFeature()), SinkConfig.SOLR_DOC_STORE_SERVICE_URL.asString());
+    private static final SolrDocStoreConnector SOLR_DOC_STORE = createSolrDocStoreConnector(SinkConfig.SOLR_DOC_STORE_SERVICE_URL.asString());
     private static final HoldingsItemsUnmarshaller HOLDINGS_ITEMS_UNMARSHALLER = new HoldingsItemsUnmarshaller(SOLR_DOC_STORE);
     private static final Supplier<MessageConsumer> MESSAGE_CONSUMER = () -> new MessageConsumer(SERVICE_HUB, SOLR_DOC_STORE, HOLDINGS_ITEMS_UNMARSHALLER);
 
     public static void main(String[] args) {
         new HoldingItemsSinkApp().go(SERVICE_HUB, MESSAGE_CONSUMER);
+    }
+
+    private static SolrDocStoreConnector createSolrDocStoreConnector(String solrDocStoreBaseUrl) {
+        final Client client = ClientBuilder.newClient().register(new JacksonFeature());
+        final FailSafeHttpClient failSafeHttpClient = FailSafeHttpClient.create(client, UserAgent.forInternalRequests(), RETRY_POLICY);
+        return new SolrDocStoreConnector(failSafeHttpClient, solrDocStoreBaseUrl);
     }
 }
